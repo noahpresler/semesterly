cff9e9db6d809e59327b3abf59382e2625d289c6
Rohan Das <das.rohan1993@gmail.com>
2015-12-24 14:31:07 -0500

    Modified gulp to watch and concat JSX files

--- gulpfile.js
+++ gulpfile.js
@@ -2,13 +2,22 @@ const gulp = require('gulp');
 const babel = require('gulp-babel');
 const react = require('gulp-react');
 const watch = require('gulp-watch');
+const concat = require('gulp-concat');
+const batch = require('gulp-batch');
 
-gulp.task('default', function() {
-	return gulp.src('static/js/timetable/*.jsx')
-	.pipe(watch('static/js/timetable/*.jsx'))
-	.pipe(babel({
-		presets: ['es2015', 'react']
-	}))
-	.pipe(gulp.dest('static/js/gulp'));
+gulp.task('transform', function() {
+  return gulp.src('static/js/timetable/*.jsx')
+  .pipe(babel({
+        presets: ['es2015', 'react']
+    }))
+  .pipe(concat('application.js'))
+  .pipe(gulp.dest('static/js/gulp'));
 });
+gulp.task('watcher', function () {
+    watch('static/js/timetable/*.jsx', batch(function(events, done) {
+        gulp.start('transform', done);
+    }));
+});
+
+gulp.task('default', ['watcher', 'transform'], function(){});
 
