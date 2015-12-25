b0f744a0b7f4a097f7c090b3704a7b7c8828dea6
Rohan Das <das.rohan1993@gmail.com>
2015-12-25 02:13:55 -0500

    Fixed SlotManager logic

--- static/js/gulp/application.js
+++ static/js/gulp/application.js
@@ -1,44 +1,46 @@
 "use strict";
 
 //             [blue,    bright red,  purple,    teal,       green,    yellow,      pink,      grey]
-var colour_list = ["#449DCA", "#fb6b5b", "#8A7BDD", "#26ADA1", "#8ec165", "#f0ad4e", "#FF6699", "#6E6E6E"];
+var colour_list = ["#3A539B", "#D24D57", "#66C3A3", "#26ADA1", "#8ec165", "#f0ad4e", "#FF6699", "#6E6E6E"];
 // flat UI colours:
 // colour_list = ["#3498db", "#e74c3c", "#8e44ad", "#1abc9c", "#2ecc71", "#f39c12"]
 // how big a slot of half an hour would be, in pixels
 var HALF_HOUR_HEIGHT = 30;
 
-var test_timetables = [{
-    code: 'CSC108H1',
+var test_timetable = [{
+    code: 'MAT223H1',
     lecture_section: 'L0101',
+    title: 'Linear Algebra Methodology',
     slots: [{
-        code: 'CSC108H1',
-        lecture_section: 'L0101',
         day: 'Monday',
         start_time: '14:00',
         end_time: '16:00'
     }, {
-        code: 'CSC108H1',
-        lecture_section: 'L0101',
         day: 'Wednesday',
         start_time: '10:00',
         end_time: '12:15'
     }]
-
 }, {
     code: 'CSC148H1',
     lecture_section: 'L5001',
+    title: 'Introduction to Computer Programming',
     slots: [{
-        code: 'CSC148H1',
-        lecture_section: 'L5001',
         day: 'Tuesday',
         start_time: '13:00',
         end_time: '15:20'
     }, {
-        code: 'CSC148H1',
-        lecture_section: 'L5001',
         day: 'Friday',
         start_time: '9:45',
         end_time: '10:45'
+    }]
+}, {
+    code: 'LIN203H1',
+    lecture_section: 'L2001',
+    title: 'English Words',
+    slots: [{
+        day: 'Thursday',
+        start_time: '12:00',
+        end_time: '15:00'
     }]
 }];
 
@@ -68,6 +70,11 @@ var Slot = React.createClass({
                     "div",
                     { className: "fc-title" },
                     this.props.course.code
+                ),
+                React.createElement(
+                    "div",
+                    { className: "fc-title" },
+                    this.props.course.title
                 )
             )
         );
@@ -82,7 +89,11 @@ var Slot = React.createClass({
         var top = (start_hour - 8) * 62 + start_minute;
         var bottom = (end_hour - 8) * 62 + end_minute;
         var height = bottom - top - 2;
-        return { top: top, height: height };
+        return {
+            top: top,
+            height: height,
+            backgroundColor: this.props.course.colour,
+            border: "1px solid " + this.props.course.colour };
     }
 
 });
@@ -98,10 +109,14 @@ var SlotManager = React.createClass({
             'Thursday': [],
             'Friday': []
         };
-        for (var timetable in test_timetables) {
-            var tt = test_timetables[timetable];
-            for (var slot_id in tt.slots) {
-                var slot = tt.slots[slot_id];
+        for (var course in test_timetable) {
+            var crs = test_timetable[course];
+            for (var slot_id in crs.slots) {
+                var slot = crs.slots[slot_id];
+                slot["colour"] = colour_list[course];
+                slot["code"] = crs.code;
+                slot["title"] = crs.title;
+                slot["lecture_section"] = crs.lecture_section;
                 slots_by_day[slot.day].push(slot);
             }
         }
@@ -110,9 +125,9 @@ var SlotManager = React.createClass({
 
     render: function render() {
         var days = ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday"];
-        var x = this.state.slots_by_day;
+        var slots_by_day = this.state.slots_by_day;
         var all_slots = days.map(function (day) {
-            var day_slots = x[day].map(function (slot) {
+            var day_slots = slots_by_day[day].map(function (slot) {
                 return React.createElement(Slot, { course: slot });
             });
             return React.createElement(
@@ -145,7 +160,7 @@ var SlotManager = React.createClass({
         var days = { 1: 'mon', 2: 'tue', 3: 'wed', 4: 'thu', 5: 'fri' };
         var d = new Date();
         var selector = ".fc-" + days[d.getDay()];
-        $(selector).addClass("fc-today");
+        // $(selector).addClass("fc-today");
     }
 
 });
