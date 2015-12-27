f1e0b3de41caf8cc540a9478e5fa3eed60a7c73b
Rohan Das <das.rohan1993@gmail.com>
2015-12-27 00:27:25 -0500

    Added local_settings.py file

---
+++ semesterly/local_settings.py
@@ -0,0 +1,10 @@
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'YOUR_DB_INFO_HERE',
+        'USER': 'YOUR_DB_INFO_HERE',
+        'PASSWORD': '',
+        'HOST': 'localhost',
+        'PORT': '5432',                     
+    }
+}
