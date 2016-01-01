2a65f95a9d5d2b768ba679196c203c3da0ab0b5e
felixzhuologist <felix.czhu@gmail.com>
2016-01-01 16:22:36 +0000

    Fix input formatting

--- static/js/new_timetable/stores/update_timetables.js
+++ static/js/new_timetable/stores/update_timetables.js
@@ -3,8 +3,8 @@ var actions = require('../actions/update_timetables.js');
 
 var obj = {
   school: "jhu",
-  semester: "F",
-  course_to_section: {},
+  semester: "S",
+  courses_to_sections: {},
   preferences: {
     'no_classes_before': false,
     'no_classes_after': false,
@@ -17,29 +17,29 @@ var obj = {
 
 module.exports = Reflux.createStore({
   listenables: [actions],
-  course_to_section: {},
+  courses_to_sections: {},
 
   updateTimetables: function(new_course_id) {
-    var c_to_s = $.extend(true, {}, this.course_to_section); // deep copy of this.course_to_section
+    var c_to_s = $.extend(true, {}, this.courses_to_sections); // deep copy of this.courses_to_sections
     if (c_to_s[new_course_id] == null) { // adding course
-      c_to_s[new_course_id] = [];
+      c_to_s[new_course_id] = {'L': '', 'T': '', 'P': ''};
     }
     else { // removing course
       delete c_to_s[new_course_id];
       if (Object.keys(c_to_s).length == 0) { // removed last course
-          this.course_to_section = {};
+          this.courses_to_sections = {};
           this.trigger(this.getInitialState());
           return;  
       }
     }
 
-    obj.course_to_section = c_to_s; // to make the POST request
+    obj.courses_to_sections = c_to_s; // to make the POST request
     $.post('/timetable/', JSON.stringify(obj), function(response) {
         if (response.length > 0) {
-            this.course_to_section = c_to_s;
+            this.courses_to_sections = c_to_s;
             this.trigger({
                 timetables: response,
-                course_to_section: this.course_to_section,
+                courses_to_sections: this.courses_to_sections,
                 current_index: 0
             });
         }
@@ -47,6 +47,6 @@ module.exports = Reflux.createStore({
   },
 
   getInitialState: function() {
-    return {timetables: [], course_to_section: {}, current_index: -1};
+    return {timetables: [], courses_to_sections: {}, current_index: -1};
   }
 });
