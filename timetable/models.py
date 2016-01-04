41b307b37f22a2c466392b2315aa7864eed4dc29
noah@presler.me <noah@presler.me>
2016-01-04 15:56:05 -0500

    Generalize smilarity engine.
    Closes #9

--- timetable/models.py
+++ timetable/models.py
@@ -8,10 +8,12 @@ class Course(models.Model):
 	code = models.CharField(max_length=20)
 	name = models.CharField(max_length=250)
 	description = models.TextField(max_length=1500)
+	unstopped_description = models.TextField(max_length=1500)
 	campus = models.IntegerField()
 	breadths = models.CharField(max_length=5, default='')
 	prerequisites = models.TextField(max_length=1000, default='')
 	exclusions = models.TextField(max_length=1000, default='')
+	related_courses = models.ManyToManyField("self", blank=True)
 	
 	def __unicode__(self):
 		return self.code + ": " + self.name
@@ -85,6 +87,11 @@ class HopkinsCourse(models.Model):
 
 	def get_exclusions(self):
 		return self.exclusions
+
+	def get_dept_matches(self):
+		code_pattern = re.compile(r"(.*\..*)\.(.*)")
+		department = re.search(code_pattern,self.get_code()).group(1)
+		return HopkinsCourse.objects.filter(code__contains=department)
 
 class HopkinsCourseEvaluation(models.Model):
 	score = models.FloatField(default=5.0)
