ff45df7acb5198de1178043648f9f290433b901e
noah@presler.me <noah@presler.me>
2016-01-04 09:32:11 -0500

    remove stop words

--- SimilarityEngine.py
+++ SimilarityEngine.py
@@ -3,18 +3,22 @@ import django
 import re
 from fuzzywuzzy import fuzz
 from fuzzywuzzy import process
+import nltk
+from nltk.corpus import stopwords
 
 django.setup()
 
 class SimilarityFinder:
 
-    def __init__(self):
-    	self.courses = HopkinsCourse.objects.all()
-    	self.code_pattern = re.compile(r"(.*\..*)\.(.*)")
+	def __init__(self):
+		self.courses = HopkinsCourse.objects.all()
+		self.code_pattern = re.compile(r"(.*\..*)\.(.*)")
+		self.cachedStopWords = stopwords.words("english")
+		self.unstop_descriptions()
 
-    def parse_classes(self):
-    	for course in self.courses:
-    		if course.description not in ["Infomration not required for this course type","Not Available","None provided"]:
+	def parse_classes(self):
+		for course in self.courses:
+			if course.description not in ["Infomration not required for this course type","Not Available","None provided"]:
 				department = re.search(self.code_pattern,course.get_code()).group(1)
 				department_matches = HopkinsCourse.objects.filter(code__contains=department).values_list('description', flat=True)
 				matches = process.extract(course.description,department_matches, limit=4)
@@ -31,6 +35,14 @@ class SimilarityFinder:
 					except HopkinsCourse.DoesNotExist:
 						print "ADDING " + course_match.name + " as related to " + course.name
 						course.related_courses.add(course_match)
+
+	def unstop_descriptions(self):
+		courses = HopkinsCourse.objects.filter(unstopped_description="")
+		for course in courses:
+			print "Unstopping: " + course.name
+			course.unstopped_description = ' '.join([word for word in course.description.split() if word not in (self.cachedStopWords)])
+			course.save()
+			print course.unstopped_description
 
 cf = SimilarityFinder()
 cf.parse_classes()
