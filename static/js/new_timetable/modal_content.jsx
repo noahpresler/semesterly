5b97b4243a7dc5d2540f348caec6cf393a16bce1
noah@presler.me <noah@presler.me>
2016-01-06 21:26:37 -0500

    modal content filled

--- static/js/new_timetable/modal_content.jsx
+++ static/js/new_timetable/modal_content.jsx
@@ -10,7 +10,7 @@ module.exports = React.createClass({
 		var description = this.state.loading ? null : this.getDescription()
 		var evaluations = this.state.loading ? null : this.getEvaluations()
 		var recomendations = this.state.loading ? null : this.getRecomendations()
-		var textbooks = this.state.loading ? null : this.getTextbooks()
+		var textbooks =this.state.loading ? null : this.getTextbooks()
 		var sections = this.state.loading ? null : this.getSections()
 		return (
 			<div id="modal-content">
@@ -42,35 +42,94 @@ module.exports = React.createClass({
 	},
 
 	getEvaluations: function() {
+		var eval_items = this.state.course_info.eval_info.map(function(e) {
+			return (
+				<div className="eval-item">
+					<div className="year">{e.year}</div>
+					<div className="rating-wrapper">
+						<div className="star-ratings-sprite"><span style={{width: 100*e.score/5 + "%"}} className="rating"></span></div>
+						<div className="numeric-rating">{"(" + e.score + ")"}</div>
+					</div>
+				</div>
+				)
+		}.bind(this));
+		evals = (
+			<div className="eval-wrapper">
+				{eval_items}
+			</div>)
+		var click_notice = this.state.course_info.eval_info.length == 0 ? null : (
+			<div id="eval-intro">Click an evaluation item above to read the comments</div>)
 		var evaluations = 
 			(<div className="modal-entry" id="course-evaluations">
 				<h6>Course Evaluations:</h6>
+				{evals}
+				{click_notice}
 			</div>)
 		return evaluations
 	},
 
 	getRecomendations: function() {
+		var related = this.state.course_info.related_courses.slice(0,3).map(function(rc) {
+            return (
+            	<div id="recomendation">
+            		<div id="rec-wrapper">
+	            		<div id="name">{rc.name}</div>
+	            		<div id="code">{rc.code}</div>
+	            	</div>
+            	</div>)
+        }.bind(this));
 		var recomendations = 
-			(<div className="modal-entry" id="course-recomendations">
+			(<div className="modal-entry">
 				<h6>Courses You Might Like:</h6>
+				<div id="course-recomendations">
+					{related}
+				</div>
 			</div>)
 		return recomendations
 	},
 
 	getTextbooks: function() {
+		var textbook_elements = this.state.course_info.textbook_info[0].textbooks.map(function(tb) {
+            return (
+            	<div className="textbook">
+            		<img height="125" src={tb.image_url}/>
+            		<h6>{tb.title}</h6>
+            		<div>{tb.author}</div>
+            		<div>ISBN:{tb.isbn}</div>
+            		<a href={tb.detail_url} target="_blank">
+            			<img src="https://images-na.ssl-images-amazon.com/images/G/01/associates/remote-buy-box/buy5._V192207739_.gif" width="120" height="28" border="0"/>
+            		</a>
+            	</div>)
+        }.bind(this));
 		var textbooks = 
 			(<div className="modal-entry" id="course-textbooks">
 				<h6>Textbooks:</h6>
+				<div id="textbooks">
+	            	{textbook_elements}
+	            </div>
 			</div>)
 		return textbooks
 	},
 
 	getSections: function() {
-		var sections = 
-			(<div className="modal-entry" id="course-sections">
-				<h6>Course Sections:</h6>
-			</div>)
-		return sections
+		// var F = this.state.course_info.sections_S.map(function(f){
+		// 	return (<div>
+		// 		{f}
+		// 	</div>)
+		// }.bind(this));
+		// var S = this.state.course_info.sections_F.map(function(s){
+		// 	return (<div>
+		// 		{s}
+		// 	</div>)
+		// }.bind(this));
+		// var sections = 
+		// 	(<div className="modal-entry" id="course-sections">
+		// 		<h6>Course Sections:</h6>
+		// 		{F}
+		// 		{S}
+		// 	</div>)
+		// return sections
+		return null
 	},
 
 
