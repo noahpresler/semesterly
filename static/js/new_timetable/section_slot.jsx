74ab19729a14eb3cc95dbf47140ed289cae393a5
noah@presler.me <noah@presler.me>
2016-01-09 00:33:23 -0500

    formatting content for sections on modal (filled but not styled)

--- static/js/new_timetable/section_slot.jsx
+++ static/js/new_timetable/section_slot.jsx
@@ -17,9 +17,23 @@ var colour_to_highlight = {
 // how big a slot of half an hour would be, in pixels
 var HALF_HOUR_HEIGHT = 30;
 
+var day_to_letter = {
+    'M':  'M', 
+    'T':  'T', 
+    'W':  'W',
+    'R': 'Th',
+    'F':  'F',
+    'S': 'Sa',
+    'U': 'S'
+}
+
 module.exports = React.createClass({
     render: function() {
-        return <div>{this.props.section}{String(this.getRelatedCourseOfferings())}</div>
+        var cos = this.getRelatedCourseOfferings()
+        var dayAndTimes = this.getDaysAndTimes(cos);
+        var sect = <div id="section-num">{cos[0].meeting_section}</div>
+        var prof = <div id="profs">{cos[0].instructors}</div>
+        return <div id="section-wrapper">{sect}{prof}{dayAndTimes}</div>
     },
 
     getRelatedCourseOfferings: function() {
@@ -27,9 +41,18 @@ module.exports = React.createClass({
         for (var i = 0; i < this.props.all_sections.length; i++) {
             var o = this.props.all_sections[i];
             if (o.meeting_section == this.props.section) {
-                co_objects.push(o.day);
+                co_objects.push(o);
             }
         }
         return co_objects;
+    },
+
+    getDaysAndTimes: function(cos) {
+        var dayAndTimes = cos.map(function(o) {
+            return (<div id="day-time">{day_to_letter[o.day] + " " + o.time_start + "-" + o.time_end}</div>);
+        }.bind(this));
+        return ( <div id="dt-container">
+                {dayAndTimes}
+            </div> )
     }
 });
