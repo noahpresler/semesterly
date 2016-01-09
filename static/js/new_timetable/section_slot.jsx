73ca6579a3d71832d8ae5e239b0e3aaac808848d
noah@presler.me <noah@presler.me>
2016-01-08 23:07:02 -0500

    passing section data and displaying in modal (not styled)

---
+++ static/js/new_timetable/section_slot.jsx
@@ -0,0 +1,35 @@
+var TimetableActions = require('./actions/update_timetables.js');
+var TimetableStore = require('./stores/update_timetables.js');
+
+// maps base colour of slot to colour on highlight
+var colour_to_highlight = {
+    "#FD7473" : "#E26A6A",
+    "#44BBFF" : "#28A4EA",
+    "#4CD4B0" : "#3DBB9A",
+    "#8870FF" : "#7059E6",
+    "#F9AE74" : "#F7954A",
+    "#D4DBC8" : "#B5BFA3",
+    "#E7F76D" : "#C4D44D",
+    "#F182B4" : "#DE699D",
+    "#7499A2" : "#668B94",
+} // consider #CF000F, #e8fac3
+
+// how big a slot of half an hour would be, in pixels
+var HALF_HOUR_HEIGHT = 30;
+
+module.exports = React.createClass({
+    render: function() {
+        return <div>{this.props.section}{String(this.getRelatedCourseOfferings())}</div>
+    },
+
+    getRelatedCourseOfferings: function() {
+        co_objects = []
+        for (var i = 0; i < this.props.all_sections.length; i++) {
+            var o = this.props.all_sections[i];
+            if (o.meeting_section == this.props.section) {
+                co_objects.push(o.day);
+            }
+        }
+        return co_objects;
+    }
+});
