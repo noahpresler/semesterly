abcc2c31944bde42f811848cf3b0bb857ffc696d
Rohan Das <das.rohan1993@gmail.com>
2016-01-12 14:04:46 -0500

    Added SimpleModal files

---
+++ static/js/new_timetable/school_list.jsx
@@ -0,0 +1,28 @@
+TimetableActions = require('./actions/update_timetables');
+
+module.exports = React.createClass({
+
+	render: function() {
+		return 	(
+			<div className="school-list">
+				<div className="school-picker school-uoft">
+					<img src="/static/img/school_logos/uoft_logo.png" 
+						className="school-logo"
+			             onClick={this.setSchool("uoft")}/>
+				</div>
+				<div className="school-picker school-jhu">
+					<img src="/static/img/school_logos/jhu_logo.png" 
+						className="school-logo"
+			             onClick={this.setSchool("jhu")}/>
+				</div>
+			</div>);
+	},
+
+	setSchool: function(new_school) {
+		return (function() {
+			TimetableActions.setSchool(new_school);
+		}.bind(this));
+	},
+
+});
+
