c2920ac75927568a2840815a02b1ec4e28ea09d5
felixzhuologist <felix.czhu@gmail.com>
2016-01-13 21:42:20 +0100

    Add get_dept for uoft course model

--- timetable/models.py
+++ timetable/models.py
@@ -34,6 +34,9 @@ class Course(models.Model):
 	def get_prereqs(self):
 		return self.prerequisites
 
+	def get_dept(self):
+		return self.code[:3]
+
 	def get_all_textbook_info(self):
 		# Implement
 		textbook_info = []
