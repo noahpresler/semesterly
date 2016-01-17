806d6018af5eac5fa81487803b04bc3c61ff1ecb
Rohan Das <das.rohan1993@gmail.com>
2016-01-17 12:35:42 -0500

    Implementing get_dept_matches for UofT Course object

--- timetable/models.py
+++ timetable/models.py
@@ -37,6 +37,10 @@ class Course(models.Model):
 	def get_dept(self):
 		return self.code[:3]
 
+	def get_dept_matches(self):
+		department = self.get_dept()
+		return Course.objects.filter(code__contains=department)
+
 	def get_all_textbook_info(self):
 		# Implement
 		textbook_info = []
