e20cba896ca837cf30558fd9abf4c4a86009b8ae
noah@presler.me <noah@presler.me>
2016-01-17 22:07:47 -0500

    addons on view all modal

--- static/js/new_timetable/textbook_list.jsx
+++ static/js/new_timetable/textbook_list.jsx
@@ -5,6 +5,50 @@ var SimpleModal = require('./simple_modal');
 module.exports = React.createClass({
   mixins: [Reflux.connect(TimetableStore)],
 
+  getAddons: function() {
+  	var addons = [
+  		{
+  			link: "http://amzn.to/1OzFaOQ",
+  			img: "http://ecx.images-amazon.com/images/I/71508stXp7L._SX522_.jpg",
+  			title: "Mead Spiral Notebook",
+  			price: "$8.98",
+  			prime_eligible: true
+  		},
+  		{
+  			link: "http://amzn.to/1ZuQRLT",
+  			img: "http://ecx.images-amazon.com/images/I/61V6woEdngL._SY679_.jpg",
+  			title: "BIC Highlighters",
+  			price: "$4.04",
+  			prime_eligible: true
+  		},
+  		{
+  			link: "http://amzn.to/1ZuR3dY",
+  			img: "http://ecx.images-amazon.com/images/I/81qjewvKndL._SX522_.jpg",
+  			title: "25 Pocket Folders",
+  			price: "$6.98",
+  			prime_eligible: true
+  		}
+  	]
+  	var addonsHTML = addons.map(function(item) {
+  		var img = <img height="125" src={item.img}/>
+  		var title = <h6 className="line-clamp title">{item.title}</h6>
+  		var price = <h6 className="price">{item.price}</h6>
+  		var prime_logo = item.prime_eligible ? <img className="prime" height="15px" src="/static/img/prime.png"/> : null
+  		return (
+  			<div className="addon">
+  				<a href={item.link}> 
+	  				{img}
+	  				{title}
+	  				<div id="price-prime-container">
+		  				{price}
+		  				{prime_logo}
+		  			</div>
+	  			</a>
+  			</div>)
+  	}.bind(this));
+  	return (<div className="addon-wrapper">{addonsHTML}</div>)
+  },
+
   render: function() {
   	var html = this.props.courses.map(function(c) {
   		if ( c.textbooks.length > 0 ) {
@@ -42,7 +86,14 @@ module.exports = React.createClass({
   			return null
   		}
   	}.bind(this));
-    return (<div className="textbook-list-wrapper">{html}</div>)
+    return (
+    	<div className="textbook-list-wrapper">
+    		{html}
+    		<div className="textbook-list-entry">
+  				<h6>Popular Addons</h6>
+    			{this.getAddons()}
+    		</div>
+    	</div>)
   },
 
 });
