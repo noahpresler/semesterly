0175c6ffbdeca5e4c780a2f0b3b00827d36f9bd4
Rohan Das <das.rohan1993@gmail.com>
2016-02-03 02:33:18 -0500

    Making textbooks clickable inside Course Modal, Fixing 'View All Textbooks' button name (now 'View By Course')

--- static/js/new_timetable/side_bar.jsx
+++ static/js/new_timetable/side_bar.jsx
@@ -135,7 +135,7 @@ var TextbookRoster = React.createClass({
     }
     var see_all = null;
     if (tb_elements != null && tb_elements.length > 0) {
-      see_all = (<div className="view-tbs" onClick={this.toggle}>View All Textbooks</div>)
+      see_all = (<div className="view-tbs" onClick={this.toggle}>View By Course</div>)
     }
     var courses = this.state.timetables.length > 0 ? this.state.timetables[this.state.current_index].courses : null
     return (
