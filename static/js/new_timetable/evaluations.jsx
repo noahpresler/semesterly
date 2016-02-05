0137607797052ab5443caa93779c75092f4b9ed9
Rohan Das <das.rohan1993@gmail.com>
2016-02-04 19:54:59 -0500

    Adding state tracker for eval scroller

--- static/js/new_timetable/evaluations.jsx
+++ static/js/new_timetable/evaluations.jsx
@@ -3,19 +3,25 @@ var SideScroller = require('./side_scroller.jsx');
 
 var Evaluation = React.createClass({
 	render: function() {
-		var classes = "eval-item selected";
-		var details = (
-			<div id="details">{this.props.eval_data.summary.replace(/\u00a0/g, " ")}</div>
-		);
-		var prof = (
-			<div id="prof"><b>Professor: {this.props.eval_data.professor}</b></div>
-		);
+		var classes = this.props.mini ? "eval-item eval-mini" : "eval-item selected";
+		var details = prof = null;
+
+		if (!this.props.mini) { // only show extra information if this eval isn't mini
+			// (i.e. full evaluation, not nav item for full evaluations)
+			var details = (
+				<div id="details">{this.props.eval_data.summary.replace(/\u00a0/g, " ")}</div>
+			);
+			var prof = (
+				<div id="prof"><b>Professor: {this.props.eval_data.professor}</b></div>
+			);
+		}	
+ 		
 		var year = this.props.eval_data.year.indexOf(":") > -1 ? 
 		(this.props.eval_data.year.replace(":", " ")) :
 		(this.props.eval_data.year);
 		return (
 		<div className={classes}>
-			<div id="eval-wrapper">
+			<div className="eval-wrapper">
 				<div className="year"><b>{year}</b></div>
 				{prof}
 				<div className="rating-wrapper">
@@ -30,16 +36,21 @@ var Evaluation = React.createClass({
 	},
 });
 
+
+
 module.exports = React.createClass({
 	
 
 	render: function() {
 
-		var i = 0;
-		var evals = this.props.eval_info.map(function(e) {
-			i++;
-			return (<Evaluation eval_data={e} key={e.id} selected={true} />);
+		var navs = this.props.eval_info.map(function(e) {
+			return (<Evaluation eval_data={e} key={e.id} mini={true} />);
 		}.bind(this));
+
+		var evals = this.props.eval_info.map(function(e) {
+			return (<Evaluation eval_data={e} key={e.id} />);
+		}.bind(this));
+
 		var click_notice = this.props.eval_info.length == 0 ? (<div id="empty-intro">No course evaluations for this course yet.</div>) 
 		: (<div id="click-intro">Click an evaluation item above to read the comments.</div>);
 		
@@ -48,10 +59,11 @@ module.exports = React.createClass({
 		var custom_class = "";
 		if (evals.length > 0) {
 			evaluation_scroller = (<SideScroller 
-			content={evals}/>);
+			nav_items={navs}
+			content={evals}
+			id={"evaluations-carousel"}/>);
 			custom_class = "spacious-entry";
 		}
-
 
 		return (
 		<div className={"modal-entry " + custom_class} id="course-evaluations">
--- static/js/new_timetable/side_scroller.jsx
+++ static/js/new_timetable/side_scroller.jsx
@@ -4,18 +4,19 @@ module.exports = React.createClass({
   mixins: [Carousel.ControllerMixin],
 
   getInitialState: function() {
-    return {num_items: 1};
+    return {slides_shown_count: 1};
   },
   updateNumItems: function() {
-    if (!this.props.slidesToShow) {
+
+    if (!this.props.slidesToShow || $(".slider").length == 0 || !this.isMounted()) {
       return;
     }
     var width = $(".slider").width();
     var section_width = $(".section-wrapper").width() + 15;
-    items = Math.max(2, parseInt(width/section_width));
-    this.setState({num_items: items});
+    var count = Math.max(2, parseInt(width/section_width));
+    this.setState({slides_shown_count: count});
 
-    // move slider list left (so that first item is centered)
+    // move slider list left (so that first item is centered).
     // currently only done for sections: (".sec-0").parent().parent()
     // so any other items using a slider element are ignored
     var slider_list_left = "35";
@@ -24,32 +25,61 @@ module.exports = React.createClass({
     }
     $(".sec-0").parent().parent()
               .css("margin-left", slider_list_left + "%");
-    return items;
+    return count;
   },
 
 
   render: function() {
-  	if (this.props.content.length == 1) {
-  		return <div style={{marginBottom: "-30px !important"}}>{this.props.content[0]}</div>;
+  	if (this.props.content.length <= 2) {
+  		return <div style={{marginBottom: "-30px !important"}}>{this.props.content}</div>;
   	}
+    var nav_items = null;
+    if (this.props.nav_items && this.state.carousels.carousel) {
+      var navs = [];
+
+      for (var i = 0; i < this.props.nav_items.length; i++) {
+        var cls = this.state.carousels.carousel.state.currentSlide == i ? " nav-item-active" : "";
+        navs.push(
+          <span key={i} className={"nav-item" + cls} onClick={this.changeSlide(i)}>{this.props.nav_items[i]}</span>
+        );
+      }
+      nav_items = <div className="scroll-nav">{navs}</div>;
+    }
 
     return (
-      <Carousel ref="carousel" data={this.setCarouselData.bind(this, 'carousel')}
-        slidesToShow={this.state.num_items} 
-        dragging={true}
-        cellSpacing={30}>
-        {this.props.content}
-      </Carousel>
+      <div>
+        {nav_items}
+        <Carousel ref="carousel" data={this.setCarouselData.bind(this, 'carousel')}
+          slidesToShow={this.state.slides_shown_count} 
+          dragging={true}
+          cellSpacing={30}
+          id={this.props.id}>
+          {this.props.content}
+        </Carousel>
+      </div>
     )
+  },
+  // changes the currently selected slide to slide i (indexed starting at 0)
+  changeSlide: function(i) {
+    return function() {
+      var new_carousels = jQuery.extend(true, {}, this.state.carousels);
+      new_carousels.carousel.state.currentSlide = i;
+      this.setState({carousels: new_carousels});
+    }.bind(this);
+
   },
 
   componentDidMount: function() {
-    if (this.props.content.length > 1) {
+    var length = this.props.content.length;
+    if (length > 1) {
       this.updateNumItems();
+      if (length > 2 && this.props.id == "sections-carousel") {
+        $(".slider-decorator-1 > .sections-carousel").click() // make third section default to middle of slider
+      }
     }
 
     $(window).resize(function() {
-      if (this.props.content.length <= 1) {return;}
+      if (length <= 1) {return;}
       this.updateNumItems();
     }.bind(this));
   },
