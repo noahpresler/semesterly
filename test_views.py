6a9cf04682fa826ddf23ceda0a7938174cbd488b
felixzhuologist <felix.czhu@gmail.com>
2016-03-26 22:00:31 +0100

    Update conflict testing workflow

--- test_views.py
+++ test_views.py
@@ -2,7 +2,7 @@ import unittest, time
 
 from django.test import TestCase
 from timetable.models import Course, CourseOffering
-# from timetable.views import *
+from timetable.views import *
 
 class Conflicts(TestCase):
   def setUp(self):
@@ -13,20 +13,29 @@ class Conflicts(TestCase):
     c1 = Course.objects.get(code='C1')
     CourseOffering.objects.create(course=c1,
                                   semester='S',
-                                  meeting_section='CO1',
+                                  meeting_section='C1',
                                   day='M',
-                                  time_start='',
-                                  time_end='')
+                                  time_start='13:00',
+                                  time_end='14:00',
+                                  section_type='A')
+    CourseOffering.objects.create(course=c1,
+                                  semester='S',
+                                  meeting_section='C1',
+                                  day='M',
+                                  time_start='15:00',
+                                  time_end='16:00',
+                                  section_type='B')
 
     # create second course offering
     Course.objects.create(code='C2', name='Course Two')
     c2 = Course.objects.get(code='C2')
     CourseOffering.objects.create(course=c1,
                                   semester='S',
-                                  meeting_section='CO1',
+                                  meeting_section='C2',
                                   day='M',
-                                  time_start='',
-                                  time_end='')
+                                  time_start='16:00',
+                                  time_end='18:00',
+                                  section_type='B')
 
 
   def tearDown(self):
@@ -34,6 +43,8 @@ class Conflicts(TestCase):
     print "%s: %.3f" % (self.id(), t)
 
   def test_overlay(self):
+    courses = Course.objects.all()
+    tts = courses_to_timetables(courses, 'S')
     self.assertTrue(True)
 
   def test_squeeze(self):
