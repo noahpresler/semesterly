f2a8891acbd980967ee679093af5a5820ec84cd3
felixzhuologist <felix.czhu@gmail.com>
2016-03-27 01:44:08 +0100

    Link rutgers to front end

--- static/js/new_timetable/stores/update_timetables.js
+++ static/js/new_timetable/stores/update_timetables.js
@@ -25,7 +25,7 @@ TT_STATE = {
   sid: SID,
 }
 
-SCHOOL_LIST = ["jhu", "uoft", "umd"];
+SCHOOL_LIST = ["jhu", "uoft", "umd", "rutgers"];
 SEMESTER_LIST = ["S","F"]
 
 // flag to check if the user just turned conflicts off
