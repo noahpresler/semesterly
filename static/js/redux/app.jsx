d4c986037ae1f4e734ef7a197c8c2fca15fb2b90
Rohan Das <das.rohan1993@gmail.com>
2016-03-31 17:21:02 -0400

    Adding new redux application entry point; revamped gulpfile

---
+++ static/js/redux/app.jsx
@@ -0,0 +1,20 @@
+const counter = (state = 0, action) => {
+	switch (action.type) {
+		case 'INCREMENT':
+			return state + 1;
+		case 'DECREMENT':
+			return state - 1;
+		default:
+			return state;
+	}
+}
+
+const { createStore } = Redux;
+const store = createStore(counter);
+console.log(store.getState());
+
+// ReactDOM.render(
+//  <div>LMFAO</div>,
+//  document.getElementById('page')
+// );
+
