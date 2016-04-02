80c87be101eabd860ef558d6c7c6ab30f1b48003
Rohan Das <das.rohan1993@gmail.com>
2016-04-02 19:05:59 -0400

    Adding skeleton redux files

---
+++ static/js/redux/actions/network.jsx
@@ -0,0 +1,14 @@
+export function requestTimetables(state) {
+  return {
+    type: REQUEST_TIMETABLES,
+    state
+  }
+}
+
+export function receiveTimetables(state, json) {
+  return {
+    type: RECEIVE_TIMETABLES,
+    state,
+    receivedAt: Date.now()
+  }
+}
---
+++ static/js/redux/constants.jsx
@@ -0,0 +1,4 @@
+export const VALID_SCHOOLS = ["uoft", "jhu", "umd", "uo", "rutgers"];
+export const REQUEST_TIMETABLES = 'REQUEST_TIMETABLES';
+export const RECEIVE_TIMETABLES = 'RECEIVE_TIMETABLES';
+export const SET_SEMESTER = "SET_SEMESTER";
