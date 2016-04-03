b5f857e8bdb65f834e6e6699763479710c045787
Rohan Das <das.rohan1993@gmail.com>
2016-04-03 15:16:30 -0400

    Adding button to eventually trigger courses

--- static/js/redux/util.jsx
+++ static/js/redux/util.jsx
@@ -3,4 +3,3 @@ export const randomString = (length = 30, chars="!?()*&^%$#@![]0123456789abcdefg
     for (var i = length; i > 0; --i) result += chars[Math.floor(Math.random() * chars.length)];
     return result;
 }
-//lmao
