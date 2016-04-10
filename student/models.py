35db1384b0560cfab5ef7b81aba4e47c14bf92d9
noah@presler.me <noah@presler.me>
2016-04-10 11:19:29 -0400

    student app for student class holding friends, etc

---
+++ student/models.py
@@ -0,0 +1,7 @@
+from django.db import models
+from django.contrib.auth.models import User
+
+class Student(models.Model):
+    user = models.OneToOneField(User)
+    img_url = models.CharField(max_length=300, default=-1)
+    friends = models.ManyToManyField("self", blank=True)
---
+++ student/utils.py
@@ -0,0 +1,44 @@
+from social import *
+from student.models import *
+import urllib2, json, pprint
+from django.conf import settings
+from django.db import models
+
+
+def create_student(strategy, details, response, user, *args, **kwargs):
+    if Student.objects.filter(user=user).exists():
+    	new_student = Student.objects.get(user=user)
+    else:
+        new_student = Student(user=user)
+        new_student.save()
+    social_user = user.social_auth.filter(
+        provider='facebook',
+    ).first()
+    if social_user:
+        url = u'https://graph.facebook.com/{0}/' \
+              u'?fields=picture&type=large' \
+              u'&access_token={1}'.format(
+                  social_user.uid,
+                  social_user.extra_data['access_token'],
+              )
+        request = urllib2.Request(url)
+        data = json.loads(urllib2.urlopen(request).read())
+        new_student.img_url = data['picture']['data']['url']
+        new_student.save()
+        url = u'https://graph.facebook.com/{0}/' \
+              u'friends?fields=id' \
+              u'&access_token={1}'.format(
+                  social_user.uid,
+                  social_user.extra_data['access_token'],
+              )
+        request = urllib2.Request(url)
+        friends = json.loads(urllib2.urlopen(request).read()).get('data')
+        
+        #TODO: how can I query Student by where has USER which has social user with uid?
+        for friend in friends:
+        	friend_social_user = SocialAuthUser.objects.get(uid=friend['id'])
+        	friend_student = friend_social_user.student
+        	if not new_student.friends.filter(user=friend_student.user):
+        		new_student.friends.add(friend_student)
+
+    return kwargs
