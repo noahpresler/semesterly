8d5971f7c363035c667dd6c1e050b5ece539b5c0
Rohan Das <das.rohan1993@gmail.com>
2016-04-16 20:17:38 -0400

    Tracking which courses are in roster

--- static/js/redux/ui/containers/search_bar_container.jsx
+++ static/js/redux/ui/containers/search_bar_container.jsx
@@ -2,28 +2,38 @@ import { connect } from 'react-redux';
 import { fetchSearchResults } from '../../actions/search_actions.jsx';
 import { fetchTimetables } from '../../actions/timetable_actions.jsx';
 import { SearchBar } from '../search_bar.jsx';
+
 const mapStateToProps = (state) => {
 	return {
-    	searchResults: state.searchResults.items
+    	searchResults: state.searchResults.items,
+    	isCourseInRoster: (course_id) => state.courseSections[course_id] !== undefined,
+    	isSectionLocked: (course_id, section) => {
+    		if (state.courseSections[course_id] === undefined) {
+    			return false;
+    		}
+    		return Object.keys(state.courseSections[course_id]).some( 
+    			(type) => state.courseSections[course_id][type] == section
+			)
+    	}
 	}
 }
 
 const mapDispatchToProps = (dispatch) => {
 	return {
-  	fetchCourses: (query) => dispatch(fetchSearchResults(query)),
-  	addCourse: (course) => dispatch(fetchTimetables(course)),
-  	hoverCourse: (course, section) => {
+	  	fetchCourses: (query) => dispatch(fetchSearchResults(query)),
+	  	addCourse: (course) => dispatch(fetchTimetables(course)),
+	  	hoverCourse: (course, section) => {
 			dispatch({
-  			type: "HOVER_COURSE",
-  			course: Object.assign({}, course, {slots: course.slots[section]})
+				type: "HOVER_COURSE",
+				course: Object.assign({}, course, { slots: course.slots[section] })
 			});
 
-	  },
-	  unhoverCourse: () => {
+		},
+		unhoverCourse: () => {
 			dispatch({
-  			type: "UNHOVER_COURSE",
+				type: "UNHOVER_COURSE",
 			});
-	  }
+		}
 	}
 }
 
