0802dafdc7a8e8c67192ef20f30e07d07f1d4acc
noah@presler.me <noah@presler.me>
2016-04-20 14:24:12 -0400

    add class year, major to students

--- student/models.py
+++ student/models.py
@@ -2,7 +2,23 @@ from django.db import models
 from django.contrib.auth.models import User
 
 class Student(models.Model):
+    FRESHMAN = 'FR'
+    SOPHOMORE = 'SO'
+    JUNIOR = 'JR'
+    SENIOR = 'SR'
+    YEAR_IN_SCHOOL_CHOICES = (
+        (FRESHMAN, 'Freshman'),
+        (SOPHOMORE, 'Sophomore'),
+        (JUNIOR, 'Junior'),
+        (SENIOR, 'Senior'),
+    )
+    class_year = models.CharField(max_length=2,
+                                      choices=YEAR_IN_SCHOOL_CHOICES,
+                                      default=FRESHMAN)
     user = models.OneToOneField(User)
     img_url = models.CharField(max_length=300, default=-1)
     friends = models.ManyToManyField("self", blank=True)
-    fbook_uid = models.CharField(max_length=255, null=True, default='')
+    fbook_uid = models.CharField(max_length=255, default='')
+    major = models.CharField(max_length=255, default='')
+    social_courses = models.BooleanField(default=False)
+    social_offerings = models.BooleanField(default=False)
