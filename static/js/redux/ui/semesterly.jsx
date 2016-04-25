bb6564d6df7afd424b6594f523f38d206021b9ce
noah@presler.me <noah@presler.me>
2016-04-25 10:07:28 -0400

    expand and collapse sidebar

--- static/js/redux/ui/semesterly.jsx
+++ static/js/redux/ui/semesterly.jsx
@@ -3,21 +3,63 @@ import CalendarContainer from './containers/calendar_container.jsx';
 import SearchBarContainer from './containers/search_bar_container.jsx';
 import CourseModalContainer from './containers/course_modal_container.jsx';
 
-const Semesterly = () => (
-	<div id="page-wrapper">
-		<div id="top-bar">
-			<SearchBarContainer />
-			<CourseModalContainer />
-		</div>
-		<div id="all-cols">
-		  	<div id="main-bar">
-				<CalendarContainer />
-			</div>
-			<div id="side-bar">
-				I am the sidebar
-			</div>
-		</div>
-	</div>
-);
+class Semesterly extends React.Component {
+	constructor(props){
+		super(props);
+		this.sidebar_collapsed = 'neutral';
+		this.toggleSideBar = this.toggleSideBar.bind(this);
+	}
+
+	toggleSideBar() {
+		if (this.sidebar_collapsed == 'neutral') {
+			var bodyw = $(window).width();
+			if (bodyw > 999) {
+				this.collapseSideBar();
+				this.sidebar_collapsed = 'open';
+			} else {
+				this.expandSideBar();
+				this.sidebar_collapsed = 'closed';
+			}
+		}
+		if (this.sidebar_collapsed == 'closed') {
+			this.expandSideBar();
+			this.sidebar_collapsed = 'open';
+		} else {
+			this.collapseSideBar();
+			this.sidebar_collapsed = 'closed';
+		}
+	}
+
+	expandSideBar() {
+		$('#main-bar, #side-bar').removeClass('full-cal').addClass('less-cal');
+	}
+
+	collapseSideBar() {
+		$('#main-bar, #side-bar').removeClass('less-cal').addClass('full-cal');
+	}
+
+	render() {
+		return (
+			<div id="page-wrapper">
+				<div id="top-bar">
+					<SearchBarContainer />
+					<CourseModalContainer />
+					<div id="navicon" onClick={this.toggleSideBar}>
+						<span></span>
+						<span></span>
+						<span></span>
+					</div>
+				</div>
+				<div id="all-cols">
+					<div id="main-bar">
+						<CalendarContainer />
+					</div>
+					<div id="side-bar">
+						I am the sidebar
+					</div>
+				</div>
+			</div>);
+	}
+}
 
 export default Semesterly;
