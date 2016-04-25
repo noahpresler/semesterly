27c350d790c647bd22d05c2e18845938c0d97d81
noah@presler.me <noah@presler.me>
2016-04-24 23:42:12 -0400

    adding html for side bar

--- static/js/redux/ui/semesterly.jsx
+++ static/js/redux/ui/semesterly.jsx
@@ -4,11 +4,20 @@ import SearchBarContainer from './containers/search_bar_container.jsx';
 import CourseModalContainer from './containers/course_modal_container.jsx';
 
 const Semesterly = () => (
-  <div>
-    <SearchBarContainer />
-    <CalendarContainer />
-    <CourseModalContainer />
-  </div>
+	<div id="page-wrapper">
+		<div id="top-bar">
+			<SearchBarContainer />
+			<CourseModalContainer />
+		</div>
+		<div id="all-cols">
+		  	<div id="main-bar">
+				<CalendarContainer />
+			</div>
+			<div id="side-bar">
+				I am the sidebar
+			</div>
+		</div>
+	</div>
 );
 
 export default Semesterly;
