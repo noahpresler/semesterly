df2ef94f5568e1b72435d02a563b90892d182740
Rohan Das <das.rohan1993@gmail.com>
2016-04-26 16:31:24 -0400

    Adding Alerts; currently displaying conflict alerts

---
+++ static/js/redux/ui/alerts/conflict_alert.jsx
@@ -0,0 +1,27 @@
+import React from 'react';
+
+class ConflictAlert extends React.Component {
+	constructor(props) {
+		super(props);
+	}
+	componentWillUnmount() {
+		this.props.dismissSelf();
+	}
+	handleClick() {
+		this.props.turnConflictsOn();
+		this.props.dismissSelf();
+	}
+	render() {
+		return (
+		<div className="conflict-alert">
+			Adding that course causes a conflict!
+			<button 
+				onClick={() => this.handleClick()}
+				className="conflict-alert-btn">
+				Turn Conflicts On (soon)
+			</button>
+	 	</div>);
+ 	}
+};
+
+export default ConflictAlert;
