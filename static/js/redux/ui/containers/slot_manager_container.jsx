404b77e1e6e3cbb42d7d9b18a0eab0c405a96ad2
Rohan Das <das.rohan1993@gmail.com>
2016-04-28 16:39:13 -0400

    Refactor SlotManager component to have SlotManagerContainer component for consistency

---
+++ static/js/redux/ui/containers/slot_manager_container.jsx
@@ -0,0 +1,27 @@
+import { connect } from 'react-redux';
+import { fetchCourseInfo } from '../../actions/modal_actions.jsx'
+import { fetchTimetables } from '../../actions/timetable_actions.jsx'
+import SlotManager from '../slot.jsx';
+
+const mapStateToProps = (state) => {
+	return {
+		timetable: state.timetables.items[state.timetables.active] || [],
+	}
+}
+
+const mapDispatchToProps = (dispatch) => {
+	return {
+		fetchCourseInfo: (course) => dispatch(fetchCourseInfo(course)),
+		removeCourse: (course_id) => {
+	  		let course = {id: course_id}
+	  		dispatch(fetchTimetables(course));
+	  	},
+	}
+}
+
+const SlotManagerContainer = connect(
+	mapStateToProps,
+	mapDispatchToProps
+)(SlotManager);
+
+export default SlotManagerContainer;
