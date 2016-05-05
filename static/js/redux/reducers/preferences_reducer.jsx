8d873616925d52d6f4518eb1594e92d55ea65eaa
Rohan Das <das.rohan1993@gmail.com>
2016-05-05 14:42:53 -0400

    Generalizing preference setting

--- static/js/redux/reducers/preferences_reducer.jsx
+++ static/js/redux/reducers/preferences_reducer.jsx
@@ -7,8 +7,8 @@ export const preferences = (state = {
     'try_with_conflicts': false
   }, action) => {
 	switch (action.type) {
-        case 'SET_CONFLICTS':
-            return Object.assign({}, state, {'try_with_conflicts': action.value});
+        case 'SET_PREFERENCE':
+            return Object.assign({}, state, {[action.key]: action.value});
 		default:
 			return state;
 	}
