40c2aec9ed3c91c5dcf020a993cf011779b9b465
Max Yeo <mzwyeo@gmail.com>
2016-05-06 17:12:21 -0400

    forgot to add some stuff

--- static/js/redux/ui/containers/search_side_bar_container.jsx
+++ static/js/redux/ui/containers/search_side_bar_container.jsx
@@ -1,38 +1,17 @@
 import { connect } from 'react-redux';
-import Calendar from '../calendar.jsx';
+import SearchSideBar from '../SearchSideBar.jsx';
 
-/*
-gets the end hour of the current timetable, based on the class that ends latest
-*/
-const getMaxEndHour = (timetable, hasCourses) => {
-    let max_end_hour = 17;
-    if (!hasCourses) {
-      return max_end_hour;
-    }
-
-    let courses = timetable.courses;
-    for (let course_index in courses) {
-      let course = courses[course_index];
-      for (let slot_index in course.slots) {
-        let slot = course.slots[slot_index];
-        let end_hour = parseInt(slot.time_end.split(":")[0]);
-        max_end_hour = Math.max(max_end_hour, end_hour);
-      }
-    }
-    return max_end_hour;
-
-}
 const mapStateToProps = (state) => {
-	let timetables = state.timetables.items;
-	let active = state.timetables.active;
-	let hasTimetables = timetables[active].courses.length > 0 
+	let hovered = state.searchResults.items[state.ui.searchHover];
+	let slots = hovered.slots;
 	return {
-    	endHour: getMaxEndHour(timetables[active], hasTimetables)
+    	hovered: hovered,
+      slots: slots
 	}
 }
 
-const CalendarContainer = connect(
+const SearchSideBarContainer = connect(
 	mapStateToProps
-)(Calendar);
+)(SearchSideBar);
 
-export default CalendarContainer;
+export default SearchSideBarContainer;
