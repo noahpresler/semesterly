28b9cca792a313ee4c69f5907f3131b975950f1f
noah@presler.me <noah@presler.me>
2016-05-06 17:59:46 -0400

    fixing the sidebar issues

--- static/js/redux/ui/search_side_bar.jsx
+++ static/js/redux/ui/search_side_bar.jsx
@@ -2,21 +2,16 @@ import React from 'react';
 
 export class SearchSideBar extends React.Component {
     render() {
-            console.log(this.props);
-//         let sections = Object.keys(this.props.slots).map(sec => 
-//             <SearchResultSection key={course.id + sec} course={hovered} section={sec} 
-//                 locked={this.props.isSectionLocked(course.id, sec)}
-//                 hoverCourse={() => this.props.hoverCourse(hovered, sec)}
-//                 unhoverCourse={this.props.unhoverCourse} 
-//                 onClick={this.addCourse.bind(this, course, sec)}
-//                 sectionHoverOn={this.props.sectionHoverOn}
-//                 sectionHoverOff={this.props.sectionHoverOff}
-//             />
-//         );
-        let content = this.props.isFetching ? <div className="modal-loader"></div> :
-        (<div>
-            <h2>Course: {this.props.data.code}</h2><h3>In Roster? {String(this.props.inRoster)}</h3>
-        </div>);
+        let sections = Object.keys(this.props.slots).map(sec => 
+            <SearchResultSection key={this.props.hovered.id + sec} course={this.props.hovered} section={sec} 
+                // locked={this.props.isSectionLocked(this.props.hovered.id, sec)}
+                // hoverCourse={() => this.props.hoverCourse(this.props.hovered, sec)}
+                unhoverCourse={this.props.unhoverCourse} 
+                // onClick={this.addCourse.bind(this, course, sec)}
+                sectionHoverOn={this.props.sectionHoverOn}
+                sectionHoverOff={this.props.sectionHoverOff}
+            />
+        );
         return (
             <div id="search-bar-side">
                 <div id="search-bar-side-sections">
