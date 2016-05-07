5ee6b84f0878d9fd5ab9fb2f8b5cdf78c20d62ff
noah@presler.me <noah@presler.me>
2016-05-06 22:05:57 -0400

    adding not yet working from side bar, hover functional

--- static/js/redux/ui/containers/search_side_bar_container.jsx
+++ static/js/redux/ui/containers/search_side_bar_container.jsx
@@ -1,17 +1,45 @@
 import { connect } from 'react-redux';
 import { SearchSideBar } from '../search_side_bar.jsx';
+import { addOrRemoveCourse } from '../../actions/timetable_actions.jsx';
 
 const mapStateToProps = (state) => {
 	let hovered = state.searchResults.items[state.ui.searchHover];
 	let slots = hovered.slots;
 	return {
 		hovered: hovered,
-		slots: slots
+		slots: slots,
+		isSectionLocked: (course_id, section) => {
+			if (state.courseSections.objects[course_id] === undefined) {
+				return false;
+			}
+			return Object.keys(state.courseSections.objects[course_id]).some( 
+				(type) => state.courseSections.objects[course_id][type] == section
+			)
+		}
+	}
+}
+
+const mapDispatchToProps = (dispatch) => {
+	return {
+	  	addCourse: addOrRemoveCourse,
+	  	hoverCourse: (course, section) => {
+	  		course.section = section;
+			dispatch({
+				type: "HOVER_COURSE",
+				course: Object.assign({}, course, { slots: course.slots[section] })
+			});
+		},
+		unhoverCourse: () => {
+			dispatch({
+				type: "UNHOVER_COURSE",
+			});
+		}
 	}
 }
 
 const SearchSideBarContainer = connect(
-	mapStateToProps
+	mapStateToProps,
+	mapDispatchToProps
 )(SearchSideBar);
 
 export default SearchSideBarContainer;
