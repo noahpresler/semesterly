9639293c2553432d646685056a9f4772e489e46c
noah@presler.me <noah@presler.me>
2016-05-07 23:12:50 -0400

    add dropdown functionality

--- static/js/redux/ui/social_profile.jsx
+++ static/js/redux/ui/social_profile.jsx
@@ -2,6 +2,14 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 
 export class SocialProfile extends React.Component {
+    constructor(props){
+        super(props);
+        this.state = {showDropdown: false};
+        this.toggleDropdown = this.toggleDropdown.bind(this);
+    }
+    toggleDropdown() {
+    	this.setState({showDropdown: !this.state.showDropdown});
+    }
     render() {
 		let divStyle = {
 			backgroundImage: 'url(' + this.props.userImg + ')',
