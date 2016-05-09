d96f304b1ef6a6f9303fefb1bd8e9a4a5d5d7645
Max Yeo <mzwyeo@gmail.com>
2016-05-09 14:06:55 -0400

    course modal styles are a beginnin', start spreadin' the news

--- static/js/redux/ui/course_modal.jsx
+++ static/js/redux/ui/course_modal.jsx
@@ -1,25 +1,60 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import classNames from 'classnames';
 import fetch from 'isomorphic-fetch';
 import { getSchool } from '../init.jsx';
 import Modal from 'boron/DropModal';
 
 export class CourseModal extends React.Component {
-    componentWillReceiveProps(nextProps) {
+    componentWillReceiveProps(nextProps) { 
         if (nextProps.id != null) {
             this.refs.modal.show();
         }
     }
+    addCourse() {
+        this.props.addCourse(this.props.id);
+        this.refs.modal.hide();
+    }
     render() {
+        console.log(this.props.data);
         let modalStyle = {
             width: '100%',
             heigh: '80%'
         };
+
+        let inRoster = this.props.inRoster;
         let content = this.props.isFetching ? <div className="modal-loader"></div> :
         (<div id="modal-content">
             <div id="modal-header">
-                <h2>Course: {this.props.data.code}</h2>
-                <h3>In Roster? {String(this.props.inRoster)}</h3>
+                <h1>{this.props.data.name}</h1>
+                <h2>{this.props.data.code}, Applied Mathematics and Statistics </h2>
+                <div id="modal-close" onClick={() => this.refs.modal.hide()}>
+                    <i className="fa fa-times"></i>
+                </div>
+                <div id="modal-share">
+                    <i className="fa fa-share-alt"></i>
+                </div>
+                <div id="modal-save">
+                    <i className="fa fa-bookmark"></i>
+                </div>
+                <div id="modal-add" 
+                    className={classNames('search-course-add', {'in-roster': inRoster})}
+                    onClick={this.addCourse.bind(this)}>
+                    <i className={classNames('fa', {'fa-plus' : !inRoster, 'fa-check' : inRoster})}></i>
+                </div>
+            </div>
+            <div id="modal-body" className="cf">
+                <div className="col-1-4">
+                    <h3>5</h3>
+                    <h4>quantitative</h4>
+                    <p>{this.props.data.prerequisites}</p>
+                </div>
+                <div className="col-1-2">
+                    <p>{this.props.data.description}</p>
+                </div>
+                <div className="col-1-4">
+
+                </div>
             </div>
         </div>);
         return (
@@ -29,7 +64,6 @@ export class CourseModal extends React.Component {
                 onHide={this.props.hideModal}
                 >
                 {content}
-                <button onClick={() => this.refs.modal.hide()}>Close</button>
             </Modal>
         );
     }
