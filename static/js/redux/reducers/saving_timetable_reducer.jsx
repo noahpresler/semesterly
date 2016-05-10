f3439db9808dfd7744d98527ced982aaf645dbec
Rohan Das <das.rohan1993@gmail.com>
2016-05-10 14:47:08 -0400

    Moving save button; initial italicizing when unsaved

--- static/js/redux/reducers/saving_timetable_reducer.jsx
+++ static/js/redux/reducers/saving_timetable_reducer.jsx
@@ -1,11 +1,15 @@
-export const savingTimetable = (state = { name: "Untitled Schedule", saving: false }, action) => {
+export const savingTimetable = (state = { activeTimetable: { name: String("May 10 2016 2:42pm") } , saving: false, upToDate: true }, action) => {
 	switch (action.type) {
 		case 'REQUEST_SAVE_TIMETABLE':
 			return Object.assign( {}, state, { saving: true });
 		case 'RECEIVE_TIMETABLE_SAVED':
-			return Object.assign( {}, state, { saving: false });
+			return Object.assign( {}, state, { saving: false, upToDate: true });
+		case 'RECEIVE_TIMETABLES':
+			return Object.assign( {}, state, { upToDate: action.preset === true });
 		case 'CHANGE_TIMETABLE_NAME':
 			return Object.assign( {}, state, { name: action.name });
+		case 'CHANGE_ACTIVE_SAVED_TIMETABLE':
+			return Object.assign( {}, state, { activeTimetable: action.timetable });
 		default:
 			return state;
 	}
