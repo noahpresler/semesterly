c3c5d1bd2c71d6cadee26c725bd6c9397a1513b5
Max Yeo <mzwyeo@gmail.com>
2016-05-10 13:26:02 -0400

    modal styles sections and module headers

--- static/js/redux/ui/containers/course_modal_container.jsx
+++ static/js/redux/ui/containers/course_modal_container.jsx
@@ -4,11 +4,22 @@ import { addOrRemoveCourse } from '../../actions/timetable_actions.jsx';
 import { setCourseId } from '../../actions/modal_actions.jsx'
 
 const mapStateToProps = (state) => {
+	let lectureSections = [];
+	let tutorialSections = [];
+	let practicalSections = [];
+	if (state.courseInfo.data.sections) {
+		lectureSections = state.courseInfo.data.sections['L'];
+		tutorialSections = state.courseInfo.data.sections['T'];
+		practicalSections = state.courseInfo.data.sections['P'];
+	}
 	return {
 		isFetching: state.courseInfo.isFetching,
 		data: state.courseInfo.data,
 		id: state.courseInfo.id,
-		inRoster: state.courseSections.objects[state.courseInfo.id] !== undefined
+		inRoster: state.courseSections.objects[state.courseInfo.id] !== undefined,
+		lectureSections: lectureSections,
+		tutorialSections: tutorialSections,
+		practicalSections: practicalSections
 	}
 }
 
