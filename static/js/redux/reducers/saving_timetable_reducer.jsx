aa4127a343e1b372e5ab55c83193bd7703383ed5
Rohan Das <das.rohan1993@gmail.com>
2016-05-10 23:19:27 -0400

    Updating saving timetable input

--- static/js/redux/reducers/saving_timetable_reducer.jsx
+++ static/js/redux/reducers/saving_timetable_reducer.jsx
@@ -3,16 +3,21 @@ export const savingTimetable = (state = { activeTimetable: { name: String("Untit
 		case 'REQUEST_SAVE_TIMETABLE':
 			let saving = !state.upToDate;
 			return Object.assign( {}, state, { saving });
+
 		case 'RECEIVE_TIMETABLE_SAVED':
 			return Object.assign( {}, state, { saving: false, upToDate: true });
+
 		case 'RECEIVE_TIMETABLES':
 			return Object.assign( {}, state, { upToDate: action.preset === true });
+
 		case 'USER_INFO_RECEIVED':
 			let activeTimetable = action.data.timetables && action.data.timetables[0] ? 
 			action.data.timetables[0] : state.activeTimetable;			
 			return Object.assign({}, state, { activeTimetable });
+
 		case 'CHANGE_ACTIVE_SAVED_TIMETABLE':
 			return Object.assign( {}, state, { activeTimetable: action.timetable });
+
 		case 'CHANGE_ACTIVE_SAVED_TIMETABLE_NAME':
 			return Object.assign( {}, state, { activeTimetable: Object.assign({}, state.activeTimetable, {name: action.name}) });
 		default:
