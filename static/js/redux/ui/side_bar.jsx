ce86bc3f7feb02d3b90a36be5d16bdc9971b9214
Max Yeo <mzwyeo@gmail.com>
2016-05-11 03:28:29 -0400

    optional master slots, restyled side bar

--- static/js/redux/ui/side_bar.jsx
+++ static/js/redux/ui/side_bar.jsx
@@ -37,7 +37,11 @@ class SideBar extends React.Component {
                     onBlur={this.setTimetableName}
                     />
                 { savedTimetables }
-                <div id="sb-rating" className="col-1-2">
+                <div id="sb-credits" className="col-1-3">
+                    <h3>16</h3>
+                    <h4>credits</h4>
+                </div>
+                <div id="sb-rating" className="col-2-3">
                     <h3>Average Course Rating</h3>
                     <div className="sub-rating-wrapper">
                         <div className="star-ratings-sprite">
@@ -45,10 +49,7 @@ class SideBar extends React.Component {
                         </div>
                     </div>
                 </div>
-                <div id="sb-credits" className="col-1-2">
-                    <h3>16</h3>
-                    <h4>credits</h4>
-                </div>
+                <h4 className="sb-header">Current Courses</h4>
                 <div id="sb-master-slots">
                     <div className="master-slot">
                         <div className="slot-bar"></div>
@@ -71,8 +72,26 @@ class SideBar extends React.Component {
                     </div>
                 </div>
                 <h4 className="sb-header">Optional Courses</h4>
-                <div className="side-bar-section">
-                    content
+                <div id="sb-optional-slots">
+                    <div className="master-slot optional">
+                        <div className="slot-bar"></div>
+                        <div className="master-slot-content">
+                            <h3>EN.650.311</h3>
+                            <h3>Discrete Mathematics</h3>
+                            <h3>Baryl Castello</h3>
+                            <h3>4 credits</h3>
+                        </div>
+                        <div className="master-slot-actions">
+                            <i className="fa fa-share-alt"></i>
+                            <i className="fa fa-times"></i>
+                        </div>
+                        <div className="master-slot-friends">
+                            <div className="ms-friend">5+</div>
+                            <div className="ms-friend" style={{backgroundImage: 'url(/static/img/blank.jpg)' }}></div>
+                            <div className="ms-friend" style={{backgroundImage: 'url(/static/img/blank.jpg)' }}></div>
+                            <div className="ms-friend" style={{backgroundImage: 'url(/static/img/blank.jpg)' }}></div>
+                        </div>
+                    </div>
                 </div>
                 <h4 className="sb-header">Textbooks</h4>
                 <div className="side-bar-section">
