8382fab90e39f215d47de3db14fbe79f98be7a38
noah@presler.me <noah@presler.me>
2016-05-11 16:28:07 -0400

    optional courses reducer

--- static/js/redux/reducers/course_sections_reducer.jsx
+++ static/js/redux/reducers/course_sections_reducer.jsx
@@ -1,6 +1,6 @@
 import { saveLocalTimetable } from '../util.jsx';
 // we need "active" (the active timetable's index) to properly save timetables.
-// so, the courseSections variable we care about is instead found in 
+// so, the courseSections variable we care about is instead found in
 // state.objects
 export const courseSections = (state = { objects: {}, active: 0 }, action) => {
 	switch(action.type) {
