4f9b72f18161015b3e544021f90dec9703ffda7d
Max Yeo <mzwyeo@gmail.com>
2016-05-10 23:25:09 -0400

    master slots and friend circles styling

--- static/js/redux/ui/side_bar.jsx
+++ static/js/redux/ui/side_bar.jsx
@@ -12,46 +12,50 @@ class SideBar extends React.Component {
         }) : null;
         return (
             <div id="side-bar">
-                <div className="side-bar-header">
-                  <h4>Your Semester</h4>
-                </div>
                 <input ref="input" className={classnames({"unsaved": !this.props.upToDate})}
                     placeholder={this.props.activeTimetable.name}
                     onChange={this.changeName.bind(this)}
                     />
                 { savedTimetables }
-                <div className="col-1-2">
-                    <h3>Average</h3>
+                <div id="sb-rating" className="col-1-2">
+                    <h3>Average Course Rating</h3>
                     <div className="sub-rating-wrapper">
                         <div className="star-ratings-sprite">
                             <span className="rating"></span>
                         </div>
                     </div>
                 </div>
-                <div className="col-1-2">
-                    <h4>16</h4>
-                    <h3>credits</h3>
+                <div id="sb-credits" className="col-1-2">
+                    <h3>16</h3>
+                    <h4>credits</h4>
                 </div>
-                <div className="side-bar-header">
-                  <h4>Optional Courses</h4>
+                <div id="sb-master-slots">
+                    <div className="master-slot">
+                        <div className="slot-bar"></div>
+                        <div className="master-slot-content">
+                            <h3>EN.650.311</h3>
+                            <h3>Discrete Mathematics</h3>
+                            <h3>Baryl Castello</h3>
+                            <h3>4 credits</h3>
+                        </div>
+                        <div className="master-slot-actions">
+                            <i className="fa fa-share-alt"></i>
+                            <i className="fa fa-times"></i>
+                        </div>
+                        <div className="master-slot-friends">
+                            <div className="ms-friend">5+</div>
+                            <div className="ms-friend" style={{backgroundImage: 'url(/static/img/blank.jpg)' }}></div>
+                            <div className="ms-friend" style={{backgroundImage: 'url(/static/img/blank.jpg)' }}></div>
+                            <div className="ms-friend" style={{backgroundImage: 'url(/static/img/blank.jpg)' }}></div>
+                        </div>
+                    </div>
                 </div>
+                <h4 className="sb-header">Optional Courses</h4>
                 <div className="side-bar-section">
                     content
-                    content
-                    content
-                    content
-                    content
-                    content
                 </div>
-                <div className="side-bar-header">
-                  <h4>Textbooks</h4>
-                </div>
+                <h4 className="sb-header">Textbooks</h4>
                 <div className="side-bar-section">
-                    content
-                    content
-                    content
-                    content
-                    content
                     content
                 </div>
             </div>
