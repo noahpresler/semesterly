5c2a72a71b33ad54792b6634fce1901196fe4faa
felixzhuologist <felix.czhu@gmail.com>
2016-05-12 22:33:33 +0200

    Add dnd infrastructure

--- static/js/redux/reducers/custom_slots_reducer.jsx
+++ static/js/redux/reducers/custom_slots_reducer.jsx
@@ -1,6 +1,6 @@
 import update from 'react/lib/update';
 
-export const customSlots = (state = [], action) => {
+export const customSlots = (state = [{'time_start': '10:00', 'time_end': '12:00', 'day': 'M'}], action) => {
   switch(action.type) {
     case 'ADD_CUSTOM_SLOT':
       return update(state, {
