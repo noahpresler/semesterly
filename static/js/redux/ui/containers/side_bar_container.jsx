da2f9fb7f396453a86457d5fcdab6f5e81dcf4ba
Rohan Das <das.rohan1993@gmail.com>
2016-05-12 15:27:29 -0400

    Adding timetableNameInput component, fixing saving name bug

--- static/js/redux/ui/containers/side_bar_container.jsx
+++ static/js/redux/ui/containers/side_bar_container.jsx
@@ -2,8 +2,6 @@ import { connect } from 'react-redux';
 import SideBar from '../side_bar.jsx';
 import { fetchCourseInfo } from '../../actions/modal_actions.jsx'
 import { loadTimetable } from '../../actions/timetable_actions.jsx';
-import { saveTimetable } from '../../actions/user_actions.jsx';
-import { MAX_TIMETABLE_NAME_LENGTH } from '../../constants.jsx';
 
 const mapStateToProps = (state) => {
 	let courseSections = state.courseSections.objects;
@@ -11,10 +9,7 @@ const mapStateToProps = (state) => {
 	// don't pass fake courses as part of roster
 	let activeTimetable = state.timetables.items[state.timetables.active];
 	return {
-		activeLoadedTimetable: savingTimetable.activeTimetable, // the user's saved timetable object that they are currently editing
 		liveTimetableCourses: activeTimetable.courses.filter(c => !c.fake),
-		saving: savingTimetable.saving,
-		upToDate: savingTimetable.upToDate,
 		savedTimetables: state.userInfo.data.timetables,
 		courseToColourIndex: state.ui.courseToColourIndex,
 		optionalCourses: state.optionalCourses.courses,
@@ -25,16 +20,7 @@ const mapStateToProps = (state) => {
 
 const mapDispatchToProps = (dispatch) => {
 	return {
-		changeTimetableName: (name) => {
-			if (name.length === 0 || name.length > MAX_TIMETABLE_NAME_LENGTH) { return; }
-			dispatch({
-				type: "CHANGE_ACTIVE_SAVED_TIMETABLE_NAME",
-				name,
-			})
-			dispatch(saveTimetable());
-		},
 		fetchCourseInfo: (courseId) => dispatch(fetchCourseInfo(courseId)),
-
 		loadTimetable
 	}
 }
