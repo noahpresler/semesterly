c36184a970f92f56ed27767fd6c1af60c39df1f3
Max Yeo <mzwyeo@gmail.com>
2016-05-13 01:52:41 -0400

    finally passing section info to course page

--- timetable/views.py
+++ timetable/views.py
@@ -695,9 +695,16 @@ def course_page(request, code):
   try:
     course_obj = SchoolCourse.objects.filter(code__iexact=code)[0]
     course_dict = my_model_to_dict(course_obj, SchoolCourseOffering, "F")
+    l = course_dict.get('sections').get('L').values() if course_dict.get('sections').get('L') else None
+    t = course_dict.get('sections').get('T').values() if course_dict.get('sections').get('T') else None
+    p = course_dict.get('sections').get('P').values() if course_dict.get('sections').get('P') else None
     return render_to_response("course_page.html", 
       {'school': school, 
-       'course': course_dict}, 
+       'course': course_dict,
+       'lectures': l,
+       'tutorials': t,
+       'practicals': p,
+       }, 
     context_instance=RequestContext(request))
   except Exception as e:
     return HttpResponse(str(e))
