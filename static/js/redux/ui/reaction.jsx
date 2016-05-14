79587b2b5b1c87adf992ca786d62f0de12febb15
noah@presler.me <noah@presler.me>
2016-05-13 22:51:12 -0400

    reactions onclick check icon

--- static/js/redux/ui/reaction.jsx
+++ static/js/redux/ui/reaction.jsx
@@ -1,18 +1,36 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { REACTION_MAP } from '../constants.jsx';
+import classNames from 'classnames';
 
 class Reaction extends React.Component {
+	constructor(props) {
+		super(props);
+		this.state = {didSelect: false};
+		this.toggleSelected = this.toggleSelected.bind(this);
+	}
+	toggleSelected() {
+		console.log(this.state.didSelect);
+		this.setState({didSelect: !this.state.didSelect});
+	}
 	render() {
 		let size = 20 + this.props.count/this.props.total * 45;
 		let actionSize = 15 + 45*(this.props.count/this.props.total);
 		let emojiStyle = {height: size, width: size};
 		return (
-			<div className="reaction">
+			<div className="reaction" onClick={this.toggleSelected}>
 				<div className="emoji" style={emojiStyle}
 					dangerouslySetInnerHTML={{__html: twemoji.parse(REACTION_MAP[this.props.emoji].unicode)}}/>
-				<div id="emoji-action">
-					<i className="fa fa-plus"/>
+				<div className={classNames({'action-container' : true, 'selected': this.state.didSelect})}>
+					<div className="emoji-action">
+						<i className="fa fa-plus"/>
+					</div>
+					<div className="emoji-count">
+						<span>{this.props.count}</span>
+					</div>
+					<div className="emoji-check">
+						<i className="fa fa-check"/>
+					</div>
 				</div>
 			</div>);
 	}
