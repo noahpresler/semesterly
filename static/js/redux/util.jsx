0c32f1766dec86a8fee747dca1f1344d6a0c53c8
Rohan Das <das.rohan1993@gmail.com>
2016-05-18 15:19:16 -0400

    Caching local timetables for semester

--- static/js/redux/util.jsx
+++ static/js/redux/util.jsx
@@ -3,7 +3,7 @@ export const randomString = (length = 30, chars = "!?()*&^%$#@![]0123456789abcde
     for (var i = length; i > 0; --i) result += chars[Math.floor(Math.random() * chars.length)];
     return result;
 }
-const browserSupportsLocalStorage = () => {
+export const browserSupportsLocalStorage = () => {
 	try {
 		localStorage.setItem("test", "test");
 		localStorage.removeItem("test");
@@ -12,10 +12,21 @@ const browserSupportsLocalStorage = () => {
 		return false;
 	}
 }
-export const saveLocalTimetable = (courseSections, active) => {
+export const saveLocalCourseSections = (courseSections) => {
 	if (!browserSupportsLocalStorage()) { return; }
 	localStorage.setItem("courseSections", JSON.stringify(courseSections));
-	localStorage.setItem("active", active);
+}
+export const saveLocalActiveIndex = (activeIndex) => {
+	if (!browserSupportsLocalStorage()) { return; }
+	localStorage.setItem("active", activeIndex);
+}
+export const saveLocalPreferences = (preferences) => {
+	if (!browserSupportsLocalStorage()) { return; }
+	localStorage.setItem("preferences", JSON.stringify(preferences));
+}
+export const saveLocalSemester = (semester) => {
+	if (!browserSupportsLocalStorage()) { return; }
+	localStorage.setItem("semester", semester);
 }
 export const getLocalTimetable = () => {
 	if (!browserSupportsLocalStorage()) { return {}; }
