3c3aabc04e355afbc33ba3242c9d6055776dd493
Rohan Das <das.rohan1993@gmail.com>
2016-05-20 09:32:41 -0400

    Improving local caching

--- static/js/redux/reducers/semester_reducer.jsx
+++ static/js/redux/reducers/semester_reducer.jsx
@@ -1,12 +1,8 @@
-import { saveLocalSemester } from '../util.jsx';
 import { VALID_SEMESTERS, SET_SEMESTER } from '../constants.jsx';
 
 export const semester = (state = "", action) => {
 	switch (action.type) {
 		case SET_SEMESTER:
-			if (!action.initialPageLoad) {
-				saveLocalSemester(action.semester);
-			}
 			return action.semester;
 		default:
 			return state;
