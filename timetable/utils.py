7fd473c052fc00046890106da8889fc28ba36679
Rohan Das <das.rohan1993@gmail.com>
2016-05-21 21:13:13 -0400

    Adding timetable utils file

---
+++ timetable/utils.py
@@ -0,0 +1,14 @@
+from school_mappers import VALID_SCHOOLS
+
+def validate_subdomain(view_func):
+  def wrapper(request, *args, **kwargs):
+    if request.subdomain not in VALID_SCHOOLS:
+      return render(request, 'index.html')
+    else:
+      return view_func(request, *args, **kwargs)
+  return wrapper
+
+def merge_dicts(d1, d2):
+  d = d1.copy()
+  d.update(d2)
+  return d
