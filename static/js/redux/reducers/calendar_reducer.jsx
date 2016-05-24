0132b3d24b0713bb8fb4c834bf356ae2b3d049e0
Rohan Das <das.rohan1993@gmail.com>
2016-05-24 01:12:04 -0400

    Styling timetable sharing button (initial); stopping new share links for same timetable

--- static/js/redux/reducers/calendar_reducer.jsx
+++ static/js/redux/reducers/calendar_reducer.jsx
@@ -1,9 +1,13 @@
-export const calendar = (state = {shareLink: null, isFetchingShareLink: false}, action) => {
+export const calendar = (state = {shareLink: null, isFetchingShareLink: false, shareLinkValid: false}, action) => {
 	switch (action.type) {
-		case ("REQUEST_SHARE_TIMETABLE_LINK"):
+		case "REQUEST_SHARE_TIMETABLE_LINK":
 			return Object.assign({}, state, { isFetchingShareLink: true });
-		case ("RECEIVE_SHARE_TIMETABLE_LINK"):
-			return Object.assign({}, state, { shareLink: action.shareLink, isFetchingShareLink: false });
+		case "RECEIVE_SHARE_TIMETABLE_LINK":
+			return Object.assign({}, state, { shareLink: action.shareLink, isFetchingShareLink: false, shareLinkValid: true });
+		case "RECEIVE_TIMETABLES":
+		case "CHANGE_ACTIVE_TIMETABLE":
+			return Object.assign({}, state, { shareLinkValid: false });
+
 		default:
 			return state;
 	}
