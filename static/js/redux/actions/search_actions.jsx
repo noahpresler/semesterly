ad0d869cd6b76044f95ada190e53f089310ef557
Rohan Das <das.rohan1993@gmail.com>
2016-05-25 14:00:50 -0400

    Adding 'Remove Optional Course' functionality to Master Slot; improving Exploration Modal state handling

--- static/js/redux/actions/search_actions.jsx
+++ static/js/redux/actions/search_actions.jsx
@@ -90,7 +90,9 @@ export function fetchSearchResults(query) {
 
 export function fetchAdvancedSearchResults(query, filters) {
 	return (dispatch) => {
-		if (query.length <= 1) {
+		// if too small a query AND no filters; don't make request.
+		// we'll allow small query strings if some filters are chosen.
+		if (query.length <= 1 && [].concat(...Object.values(filters)).length === 0) {
 			dispatch({  
 				type: "RECEIVE_ADVANCED_SEARCH_RESULTS",
 				advancedSearchResults: []
