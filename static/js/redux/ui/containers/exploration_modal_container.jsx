88aa8e54255784a865b566a8b35554bb821ed131
Rohan Das <das.rohan1993@gmail.com>
2016-05-25 13:39:01 -0400

    Adding 'Add Course', 'Add Optional Course', and 'Share Course' functionality to exploration modal

--- static/js/redux/ui/containers/exploration_modal_container.jsx
+++ static/js/redux/ui/containers/exploration_modal_container.jsx
@@ -1,7 +1,7 @@
 import { connect } from 'react-redux';
 import { ExplorationModal } from '../exploration_modal.jsx';
 import { fetchAdvancedSearchResults } from '../../actions/search_actions.jsx';
-import { hoverSection, unhoverSection, addOrRemoveCourse } from '../../actions/timetable_actions.jsx';
+import { hoverSection, unhoverSection, addOrRemoveCourse, addOrRemoveOptionalCourse } from '../../actions/timetable_actions.jsx';
 import { getSchoolSpecificInfo } from '../../constants.jsx';
 import { react } from '../../actions/modal_actions.jsx';
 
@@ -48,6 +48,7 @@ const mapDispatchToProps = (dispatch) => {
 		openSignupModal: () => dispatch({ type: "TOGGLE_SIGNUP_MODAL" }),
 	  	fetchAdvancedSearchResults: (query, filters) => dispatch(fetchAdvancedSearchResults(query, filters)),
 	  	setAdvancedSearchResultIndex: (i) => dispatch({ type: "SET_ACTIVE_RESULT", active: i }),
+	  	addOrRemoveOptionalCourse: (course) => dispatch(addOrRemoveOptionalCourse(course)),
 		hoverSection: hoverSection(dispatch),
 		unhoverSection: unhoverSection(dispatch),
 		addOrRemoveCourse,
