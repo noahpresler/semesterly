a5c54975ce1ffaea3bcc46e19cc0c16192d28b90
Rohan Das <das.rohan1993@gmail.com>
2016-05-25 20:06:15 -0400

    Initial time filtering code; Various bug fixes

--- static/js/redux/init.jsx
+++ static/js/redux/init.jsx
@@ -23,6 +23,7 @@ export const getSemester = () => {
 // setup the state. loads the user's timetables if logged in; cached timetable if not.
 // also handles sharing courses and sharing timetables
 function setup(dispatch) {
+
   dispatch({
     type: "SET_SCHOOL",
     school // comes from timetable.html
@@ -32,9 +33,10 @@ function setup(dispatch) {
     type: "SET_SEMESTER",
     semester: currentSemester, // currentSemester comes from timetable.html (rendered by the server). if the user is loading a share course link, we need to set the appropriate semester, so we can't default it to any particular value
   });
-
+  sharedTimetable = JSON.parse(sharedTimetable);
+  sharedCourse = JSON.parse(sharedCourse);
   /* first setup the user's state */
-  let user = currentUser; // currentUser comes from timetable.html
+  let user = JSON.parse(currentUser); // currentUser comes from timetable.html
   dispatch(getUserInfo(user));
   if (!sharedTimetable) { // we load user's timetable (or cached timetable) only if they're _not_ trying to load a shared timetable
     if (user.isLoggedIn && user.timetables.length > 0) { // user is logged in and has saved timetables
@@ -42,14 +44,16 @@ function setup(dispatch) {
       loadTimetable(user.timetables[0]); 
       dispatch({ type: "RECEIVE_TIMETABLE_SAVED" });
     }
-    else { // user isn't logged in (or has no saved timetables); load last browser-cached timetable
-      if (browserSupportsLocalStorage() && localStorage.semester === currentSemester) {
+    else { // user isn't logged in (or has no saved timetables); load last browser-cached timetable, under certain conditions.
+    // we only load the browser-cached timetable if the shared course's semester is the same as the browser-cached timetable's semester OR the user is not trying to load a shared course at all. This results in problematic edge cases, such as showing the course modal of an S course in the F semester, being completely avoided.
+      if (browserSupportsLocalStorage() && (localStorage.semester === currentSemester || !sharedCourse)) {
         loadCachedTimetable();
       }
     }
   }
 
   /* now setup sharing state */
+
   if (sharedTimetable) {
     lockTimetable(dispatch, sharedTimetable, true, user.isLoggedIn);
   }
