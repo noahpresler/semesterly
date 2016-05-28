ca5d0730ec97c30bb46ee73d988199fc92107270
felixzhuologist <felix.czhu@gmail.com>
2016-05-28 13:52:12 -0700

    Send has_conflict along with each timetable

--- scripts/queens/qcumber_scraper/queens_config.py
+++ scripts/queens/qcumber_scraper/queens_config.py
@@ -4,5 +4,5 @@ OUTPUT_DIR = "./data-dump"
 PROFILE = None
 MAX_RETRIES = 10
 RETRY_SLEEP_SECONDS = 10
-LOG_DIR = "./logs"
+LOG_DIR = "./scripts/queens/qcumber_scraper/logs"
 SAVE_TO_DB = False # writes to JSON if False
