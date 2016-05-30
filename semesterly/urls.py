7b28f51b093234831613876e7ab434b985ea8874
Rohan Das <das.rohan1993@gmail.com>
2016-05-30 00:01:18 -0400

    Fixing import bugs

--- semesterly/urls.py
+++ semesterly/urls.py
@@ -31,23 +31,13 @@ urlpatterns = patterns('',
 	url(r'^timetable/*$', 'timetable.views.redirect_to_home'),
 	url(r'^timetable/.+$', 'timetable.views.redirect_to_home'),
 
-	url(r'^exit/*$', 'analytics.views.handle_exit'),
-
-	url(r'^analytics/*$', 'analytics.views.view_analytics_dashboard'),
-	url(r'^analytics/get_num_generated$', 'analytics.views.get_num_generated'),
-
-	url(r'^analytics_data/*$', 'analytics.views.get_analytics_data'),
-	url(r'^timetable_data/*$', 'analytics.views.get_timetable_data'),
-
 	#User,Auth,User Info
 	url(r'^user/logout/$', 'django.contrib.auth.views.logout', {'next_page': '/'}),
 	url(r'^user/save_timetable/$', 'student.views.save_timetable'),
 	url(r'^user/save_settings/$', 'student.views.save_settings'),
 	url(r'^user/get_classmates/$', 'student.views.get_classmates'),
 	url(r'^user/get_saved_timetables/(?P<school>.+)/(?P<sem>[fFsS]{1})', 'student.views.get_student_tts_wrapper'),
-	url(r'^live_user_data/*$', 'analytics.views.get_live_user_data'),
 	url(r'^courses/(?P<school>.+?)/(?P<sem>[fFsS]{1}?)/code/(?P<course_id>.+)/*$', 'timetable.views.get_course_id'),
-	url(r'^reason/*$', 'analytics.views.verify_password'),
 	url(r'^jhu/countdown/*$', 'timetable.views.jhu_timer'),
 	url(r'^courses/(?P<school>.+?)/(?P<sem>[fFsS]{1}?)/id/(?P<id>[0-9]+)/*$', 'timetable.views.get_course'),
 	url(r'^get_timetables/$', 'timetable.views.get_timetables'),
