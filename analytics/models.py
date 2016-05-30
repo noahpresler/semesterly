0961151b03b8b5036278e515a1f9e66612b1ac06
Rohan Das <das.rohan1993@gmail.com>
2016-05-29 23:13:57 -0400

    Loader for Exploration modal; Analytics Timetable model added

--- analytics/models.py
+++ analytics/models.py
@@ -3,8 +3,8 @@ from timetable.models import *
 from student.models import Student
 
 class SharedTimetable(models.Model):
-	""" 
-	A timetable for which a user generated a share link. 
+	"""
+	A timetable for which a user generated a share link.
 	Not necessarily tied to a Student object, since logged out users
 	can also generate links to share a timetable. But if a logged-in user
 	does generate it, this information will be recorded.
@@ -14,6 +14,17 @@ class SharedTimetable(models.Model):
 	semester = models.CharField(max_length=2)
 	school = models.CharField(max_length=50)
 	name = models.CharField(max_length=100, null=True)
+	has_conflict = models.BooleanField(blank=True, default=False)
+	time_created = models.DateTimeField(auto_now_add=True)
+	student = models.ForeignKey(Student, null=True, default=None)
+
+class AnalyticsTimetable(models.Model):
+	"""
+	TODO(Eric): describe this model lol
+	"""
+	courses = models.ManyToManyField(Course)
+	semester = models.CharField(max_length=2)
+	school = models.CharField(max_length=50)
 	has_conflict = models.BooleanField(blank=True, default=False)
 	time_created = models.DateTimeField(auto_now_add=True)
 	student = models.ForeignKey(Student, null=True, default=None)
