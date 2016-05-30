ab4068fbbe3c11f7996bbfa4333c4d2905aa87ae
felixzhuologist <felix.czhu@gmail.com>
2016-05-30 12:08:28 -0700

    break sort state into avail/selected

--- static/js/redux/reducers/preferences_reducer.jsx
+++ static/js/redux/reducers/preferences_reducer.jsx
@@ -1,13 +1,16 @@
 const initPreferences = {
     try_with_conflicts: false,
-    sort_metrics: [
-        {metric: 'sections with friends', selected: false, order: 'most'},
-        {metric: 'number of conflicts', selected: false, order: 'least'},
-        {metric: 'days with class', selected: false, order: 'least'},
-        {metric: 'time on campus', selected: false, order: 'least'},
-        {metric: 'course rating stars', selected: false, order: 'most'}
+    sort_metrics: [],
+    avail_metrics: [
+        {metric: 'sections with friends', order: 'most'},
+        {metric: 'number of conflicts', order: 'least'},
+        {metric: 'days with class', order: 'least'},
+        {metric: 'time on campus', order: 'least'},
+        {metric: 'course rating stars', order: 'most'}
     ]
 }
+
+const deleteElement = (array, index) => [...array.slice(0, index), ...array.slice(index + 1)]
 
 export const preferences = (state=initPreferences, action) => {
 	switch (action.type) {
@@ -18,16 +21,35 @@ export const preferences = (state=initPreferences, action) => {
         case 'SET_ALL_PREFERENCES':
             return action.preferences;
         case 'ADD_METRIC':
-            let addIndex = state.sort_metrics.findIndex(m => m.metric == action.metric)
+            let addIndex = state.avail_metrics.findIndex(m => m.metric == action.metric)
             if (addIndex == -1) 
                 return state
-            let added = Object.assign({}, state.sort_metrics[addIndex], {selected: true})
-            let addedMetrics = [
-            	...state.sort_metrics.slice(0, addIndex), 
-            	...state.sort_metrics.slice(addIndex + 1),
-                added
-            ]
-            return Object.assign({}, state, {sort_metrics: addedMetrics})
+            let added = {
+                sort_metrics: [...state.sort_metrics, state.avail_metrics[addIndex]],
+                avail_metrics: deleteElement(state.avail_metrics, addIndex)
+            }
+            console.log('add', added)
+            return Object.assign({}, state, added)
+        case 'REMOVE_METRIC':
+            let delIndex = state.sort_metrics.findIndex(m => m.metric == action.metric)
+            if (delIndex == -1)
+                return state
+            let removed = {
+                sort_metrics: deleteElement(state.sort_metrics, delIndex),
+                avail_metrics: [...state.avail_metrics, state.sort_metrics[delIndex]]
+            }
+            console.log('del', removed)
+            return Object.assign({}, state, removed)
+        case 'SWITCH_METRIC':
+            let add = state.avail_metrics.findIndex(m => m.metric == action.add)
+            if (add == -1 || delIndex == -1) 
+                return state
+            let del = state.sort_metrics.findIndex(m => m.metric == action.del)
+            let switched = {
+                sort_metrics: [...deleteElement(state.sort_metrics, del), state.avail_metrics[add]],
+                avail_metrics: [...deleteElement(state.avail_metrics, add), state.sort_metrics[del]]
+            }
+            return Object.assign({}, state, switched)
         case 'TOGGLE_METRIC_ORDER':
             let orderIndex = state.sort_metrics.findIndex(m => m.metric == action.metric)
             if (orderIndex == -1)
@@ -35,22 +57,11 @@ export const preferences = (state=initPreferences, action) => {
             let next_order = state.sort_metrics[orderIndex] == 'least' ? 'most' : 'least'
             let reversed = Object.assign({}, state.sort_metrics[orderIndex], {order: next_order})
             let toggledMetrics = [
-            	...state.sort_metrics.slice(0, orderIndex), 
-            	reversed, 
-            	...state.sort_metrics.slice(orderIndex + 1)
+                ...state.sort_metrics.slice(0, orderIndex), 
+                reversed, 
+                ...state.sort_metrics.slice(orderIndex + 1)
             ]
             return Object.assign({}, state, {sort_metrics: toggledMetrics})
-        case 'REMOVE_METRIC':
-            let delIndex = state.sort_metrics.findIndex(m => m.metric == action.metric)
-            if (delIndex == -1)
-                return state
-            let removed = Object.assign({}, state.sort_metrics[delIndex], {selected: false})
-            let removedMetrics = [
-            	removed, 
-            	...state.sort_metrics.slice(0, delIndex), 
-            	...state.sort_metrics.slice(delIndex + 1)
-            ]
-            return Object.assign({}, state, {sort_metrics: removedMetrics})
 		default:
 			return state;
 	}
