cc8038cbea268e15b1649139dc4f7f176ec62603
felixzhuologist <felix.czhu@gmail.com>
2016-05-30 14:12:54 -0700

    Get all preferences except friends working on backend

--- timetable/scoring.py
+++ timetable/scoring.py
@@ -1,41 +1,46 @@
 # tip: timetables with lowest cost get shown first -> metrics should return
 # lower numbers for "better" timetables. timetable: [cid, sid, [offerings]]
 from pprint import pprint
+from timetable.models import *
 
-def get_tt_cost(timetable, sort_params):
-  pprint(timetable)
-  return 0
-  # return tuple([metric(timetable) for metric in map(str_to_func.get, sort_params)])
+def get_tt_cost(tt_stats, sort_params):
+  return tuple([metric(tt_stats) for metric in map(metric_to_stat.get, sort_params)])
 
-def reverse_metric(metric):
-  def _reversed(timetable):
-    return -metric(timetable)
-  return _reversed
-
+# TODO
 def get_num_friends(timetable):
-  pass
+  return 0
 
 def get_num_conflicts(timetable):
   pass
 
-def get_num_days(timetable):
-  pass
+def get_num_days(day_to_usage):
+  got_class = [any(usage)for usage in day_to_usage.values()]
+  return sum(got_class)
 
-def get_average_rating(timetable):
-  pass
+def get_avg_rating(timetable):
+  avgs = [Course.objects.get(id=cid).get_avg_rating() \
+          for cid in set([cid for cid, sid, cos in timetable])]
+  return sum(avgs)/len(avgs) if avgs else 0
 
-def get_day_length(timetable):
-  pass
+def get_avg_day_length(day_to_usage):
+  lengths = [get_day_length(usage) for usage in day_to_usage.values()]
+  return sum(lengths)/float(len(lengths))
 
-str_to_func = {
-  ('sections with friends', 'most'): reverse_metric(get_num_friends),
-  ('sections with friends', 'least'): get_num_friends,
-  ('number of conflicts', 'least'): reverse_metric(get_num_conflicts),
-  ('number of conflicts', 'most'): get_num_conflicts,
-  ('days with class', 'least'): get_num_days,
-  ('days with class', 'most'): reverse_metric(get_num_days),
-  ('course rating stars', 'most'): reverse_metric(get_average_rating),
-  ('course rating stars', 'least'): get_average_rating,
-  ('time on campus', 'most'): reverse_metric(get_day_length),
-  ('time on campus', 'least'): get_day_length,
+def get_day_length(usage):
+  has_class = ''.join(['T' if offering_set else 'F' for offering_set in usage])
+  day_start = has_class.find('T')
+  day_end = len(has_class) - has_class[::-1].find('T')
+  return day_end - day_start
+
+metric_to_stat = {
+  ('sections with friends', 'most'): lambda stats: -stats['num_friends'],
+  ('sections with friends', 'least'): lambda stats: stats['num_friends'],
+  ('number of conflicts', 'least'): lambda stats: -stats['num_conflicts'],
+  ('number of conflicts', 'most'): lambda stats: stats['num_conflicts'],
+  ('days with class', 'least'): lambda stats: stats['days_with_class'],
+  ('days with class', 'most'): lambda stats: -stats['days_with_class'],
+  ('course rating stars', 'most'): lambda stats: -stats['avg_rating'],
+  ('course rating stars', 'least'): lambda stats: stats['avg_rating'],
+  ('time on campus', 'most'): lambda stats: -stats['time_on_campus'],
+  ('time on campus', 'least'): lambda stats: stats['time_on_campus'],
 }
