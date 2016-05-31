fcc298492b6f2808c184a479ee9f5369c36d1357
Eric Calder <ecalder6@jhu.edu>
2016-05-30 22:25:05 -0700

    Added analytics for various counts of timetables.

--- analytics/views.py
+++ analytics/views.py
@@ -15,7 +15,7 @@ to_zone = tz.gettz('America/New_York')
 
 
 def view_analytics_dashboard(request):
-    return render_to_response('analytics_dashboard.html', {}, 
+    return render_to_response('analytics_dashboard.html', {},
         context_instance=RequestContext(request))
 
 def save_analytics_timetable(courses, semester, school, student=None):
@@ -26,6 +26,33 @@ def save_analytics_timetable(courses, semester, school, student=None):
     analytics_timetable.courses.add(*courses)
     analytics_timetable.save()
 
-def number_analytics_timetables(school=None, semester=None, student=None):
-    timetables = AnalyticsTimetable.objects.filter(student=student, school='jhu')
+def number_timetables(timetable = AnalyticsTimetable, school = None, semester = None, student = None, time_start = None, time_end = None):
+    timetables = []
+    if time_start and time_end:
+        timetables += (
+            timetable.objects.filter(
+                time_created__range=(time_start, time_end)
+            )
+        )
+
+    if school:
+        timetables += timetable.objects.filter(school = school)
+
+    if semester:
+        timetables += timetable.objects.filter(semester = semester)
+
+    if student:
+        timetables += timetable.objects.filter(student = student)
+
     return len(timetables)
+
+def number_timetables_per_hour(timetable = AnalyticsTimetable):
+    # TODO: Change start and end time.
+    time_start = datetime.datetime(2016, 5, 30, 0, 0, 0)
+    time_end = datetime.datetime(2016, 5, 31, 12, 0, 0)
+    time_delta = timedelta(hours = 1)
+    num_timetables = []
+    while time_start <= time_end:
+        num_timetables.append(number_timetables(timetable = timetable, time_start = time_start, time_end = time_start + time_delta))
+        time_start += time_delta
+    return num_timetables
