58ed0c761ef7dfbd2fbd4efb7951a86f05660e23
felixzhuologist <felix.czhu@gmail.com>
2016-05-30 17:51:46 -0700

    Add styles, use state conflict as initial value

--- static/js/redux/ui/containers/preference_modal_container.jsx
+++ static/js/redux/ui/containers/preference_modal_container.jsx
@@ -1,16 +1,22 @@
 import { connect } from 'react-redux';
 import { PreferenceModal } from '../preference_modal.jsx';
+import { fetchStateTimetables } from '../../actions/timetable_actions.jsx'
 
 const mapStateToProps = (state) => {
   return {
     isVisible: state.preferenceModal.isVisible,
+    withConflicts: state.preferences.try_with_conflicts
   }
 }
 
 const mapDispatchToProps = (dispatch) => {
   return {
     togglePreferenceModal: () => dispatch({type: "TOGGLE_PREFERENCE_MODAL"}),
-    toggleConflicts: () => dispatch({type: "TOGGLE_CONFLICTS"})
+    toggleConflicts: () => dispatch({type: "TOGGLE_CONFLICTS"}),
+    applyPreferences: () => {
+      dispatch({type: "TOGGLE_PREFERENCE_MODAL"})
+      // fetchStateTimetables()
+    }
   }
 }
 
