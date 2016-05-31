5891aa12b0d720161eab1ce4aa6a918d91029e0c
Rohan Das <das.rohan1993@gmail.com>
2016-05-30 20:09:59 -0400

    New Colours

--- static/js/redux/constants.jsx
+++ static/js/redux/constants.jsx
@@ -101,15 +101,17 @@ export const getSchoolSpecificInfo = (school) => {
 
 export const COLOUR_DATA = [
     // red
-    { background: "#FD7473", highlight: "#E26A6A", border: "#963838", font: "#222" },
+    { background: "#FD7473", highlight: "#e36867", border: "#b15150", font: "#222" },
     // blue
-    { background: "#5AC8FB", highlight: "#28A4EA", border: "#1B6B90", font: "#222" },
+    { background: "#5CCCF2", highlight: "#52b7d9", border: "#408ea9", font: "#222" },
     // turquoise 
-    { background: "#36DEBB", highlight: "#3DBB9A", border: "#1E755E", font: "#222" },
-    // purple
-    { background: "#C585DE", highlight: "#9470CC", border: "#4C2B7D", font: "#222" },
+    { background: "#36DEBB", highlight: "#30c7a8", border: "#259b82", font: "#222" },
     // 'yellow'
-    { background: "#FFD462", highlight: "#F7954A", border: "#AF5E20", font: "#222" },
+    { background: "#FFD462", highlight: "#e5be58", border: "#b29444", font: "#222" },
+    // purple
+    { background: "#C585DE", highlight: "#b177c7", border: "#895d9b", font: "#222" },
+    // green
+    { background: "#53e997", highlight: "#4ad187", border: "#3aa369", font: "#222" },
     { background: "#D4DBC8", highlight: "#B5BFA3", border: "#97A086", font: "#222" },
     { background: "#E7F76D", highlight: "#C9E20A", border: "#AFC11F", font: "#222" },
     { background: "#A3F5F2", highlight: "#7CD2CF", border: "#53ABA8", font: "#222" },
