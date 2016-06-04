e4e293a98b7a94d5e884c6a6423deaed322251ab
noah@presler.me <noah@presler.me>
2016-06-04 13:12:13 -0700

    AUTO SAVING BITCHESSSS

--- static/js/redux/ui/containers/semesterly_container.jsx
+++ static/js/redux/ui/containers/semesterly_container.jsx
@@ -2,7 +2,7 @@ import { connect } from 'react-redux';
 import Semesterly from '../semesterly.jsx';
 import HTML5Backend from 'react-dnd-html5-backend';
 import { DragDropContext } from 'react-dnd';
-import { saveTimetable } from '../../actions/user_actions.jsx';
+import { autoSave, saveTimetable } from '../../actions/user_actions.jsx';
 
 const mapStateToProps = (state) => {
 	return {
@@ -19,7 +19,10 @@ const mapStateToProps = (state) => {
 const mapDispatchToProps = (dispatch) => {
 	return {
 		saveTimetable: () => dispatch(saveTimetable()),
-		setPgActive: (newActive) => dispatch( {type: "CHANGE_ACTIVE_TIMETABLE", newActive} ),
+		setPgActive: (newActive) => {
+			dispatch( {type: "CHANGE_ACTIVE_TIMETABLE", newActive} );
+			autoSave();
+		},
 	}
 }
 
