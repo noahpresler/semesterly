75578b6eb85e33d58d3562f2197b258be50e7336
Rohan Das <das.rohan1993@gmail.com>
2016-06-11 23:54:42 -0400

    Finalized version of automatic deployment script

--- semesterly/views.py
+++ semesterly/views.py
@@ -3,30 +3,42 @@ from django.http import HttpResponse, HttpResponseRedirect, Http404
 from django.core.context_processors import csrf
 from django.views.decorators.csrf import csrf_exempt
 from django.conf import settings
-import hashlib, hmac, json, os, subprocess
-
+from django.core.mail import send_mail
+import hashlib, hmac, json, pprint, os, subprocess
 
 # Compare the HMAC hash signature
-def verify_hmac_hash(data, signature):
-    SECRET_TOKEN = bytes(os.environ.get('SECRET_TOKEN'), 'UTF-8')
-    mac = hmac.new(SECRET_TOKEN, msg=data, digestmod=hashlib.sha1)
-    return hmac.compare_digest('sha1=' + mac.hexdigest(), signature)
+def verify_hmac_hash(request):
+    SECRET_TOKEN = getattr(settings, 'SECRET_TOKEN', False)
+    signature = request.META.get('HTTP_X_HUB_SIGNATURE')
+    computed_signature = 'sha1=' + hmac.new(SECRET_TOKEN, request.body, hashlib.sha1).hexdigest()
+    return signature == computed_signature
  
-
 @csrf_exempt
 def deploy_staging(request):
 
-    if not os.environ.get('SECRET_TOKEN') or not getattr(settings, 'STAGING', False):
+    if not getattr(settings, 'SECRET_TOKEN', False) or not getattr(settings, 'STAGING', False):
         return HttpResponse("Invalid URL", status=404)
 
-    signature = request.META['X-Hub-Signature']
-    data = request.DATA
-    if verify_hmac_hash(data, signature):
-        event_type = request.META['X-Github-Event']
+    if verify_hmac_hash(request):
+        event_type = request.META.get('HTTP_X_GITHUB_EVENT')
         if event_type == "ping":
-            return HttpResponse("ok")
+            return HttpResponse("ok", status=200)
         elif event_type == "push":
-            return HttpResponse("push")
+            body = json.loads(request.body)
+            commit = body['commits'][0]
+            email_info = {
+                'sender_name': commit['committer']['name'],
+                'commit_hash': commit['id'],
+                'time': commit['timestamp'],
+                'compare_link': body['compare'],
+            }
+            send_mail("Staging Server Being Updated", pprint.pformat(email_info, indent=4), 
+                            "semesterly.contact@gmail.com", ["rohan@semester.ly"])
+            return_code = subprocess.call(getattr(settings, 'DEPLOY_COMMAND', ""), shell=True)
+
+            return HttpResponse(status=200)
+        else:
+            return HttpResponse(status=403)
 
     else:
         raise Http404
