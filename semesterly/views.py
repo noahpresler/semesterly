9e3b2d30ee4b6943865a4712bc8dcfbc7acd381a
Rohan Das <das.rohan1993@gmail.com>
2016-06-11 21:05:33 -0400

    Initial automatic deployment for Staging Server

--- semesterly/views.py
+++ semesterly/views.py
@@ -2,16 +2,31 @@ from django.shortcuts import render
 from django.http import HttpResponse, HttpResponseRedirect, Http404
 from django.core.context_processors import csrf
 from django.views.decorators.csrf import csrf_exempt
+from django.conf import settings
+import hashlib, hmac, json, os, subprocess
 
-import subprocess
+
+# Compare the HMAC hash signature
+def verify_hmac_hash(data, signature):
+    SECRET_TOKEN = bytes(os.environ.get('SECRET_TOKEN'), 'UTF-8')
+    mac = hmac.new(SECRET_TOKEN, msg=data, digestmod=hashlib.sha1)
+    return hmac.compare_digest('sha1=' + mac.hexdigest(), signature)
+ 
 
 @csrf_exempt
-def deploy(request):
-	if ('server_deploy_key' not in request.POST) or request.POST['server_deploy_key'] != 'x98as7dhghaskdjhaskj':
-		return HttpResponse("Please go.")
-	
-	r = subprocess.Popen('bash /home/django/timetable_project/deploy.sh', stderr=subprocess.STDOUT, shell=True)
-	out, err = r.communicate()
-	if r.returncode == 0:
-		return HttpResponse("\nUofTReach server message: Deploy successful. Hit return to continue.\n")
-	return HttpResponse('\nUofTReach server message: Deploy Unsuccessful! \nError code: '  + str(r.returncode) + "\nout: " + str(out) + "\nerr: " + str(err) + "\nHit return to continue.\n")
+def deploy_staging(request):
+
+    if not os.environ.get('SECRET_TOKEN') or not getattr(settings, 'STAGING', False):
+        return HttpResponse("Invalid URL", status=404)
+
+    signature = request.META['X-Hub-Signature']
+    data = request.DATA
+    if verify_hmac_hash(data, signature):
+        event_type = request.META['X-Github-Event']
+        if event_type == "ping":
+            return HttpResponse("ok")
+        elif event_type == "push":
+            return HttpResponse("push")
+
+    else:
+        raise Http404
