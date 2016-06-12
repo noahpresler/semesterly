abdf8e4a6e169141437fab8bca7721390dcce09c
Rohan Das <das.rohan1993@gmail.com>
2016-06-12 01:21:27 -0400

    Improve robustness of automatic deployment

--- semesterly/views.py
+++ semesterly/views.py
@@ -6,35 +6,53 @@ from django.conf import settings
 from django.core.mail import send_mail
 import hashlib, hmac, json, pprint, os, subprocess
 
+
+def default_send_email(subject, message):
+    send_mail(subject, message, 
+        getattr(settings, 'DEFAULT_FROM_EMAIL'), 
+        getattr(settings, 'STAGING_NOTIFIED_ADMINS'))
+
 # Compare the HMAC hash signature
 def verify_hmac_hash(request):
     SECRET_TOKEN = getattr(settings, 'SECRET_TOKEN', False)
     signature = request.META.get('HTTP_X_HUB_SIGNATURE')
-    computed_signature = 'sha1=' + hmac.new(SECRET_TOKEN, request.body, hashlib.sha1).hexdigest()
+    computed_signature = 'sha1=' + hmac.new(SECRET_TOKEN, request.body, 
+        hashlib.sha1).hexdigest()
     return signature == computed_signature
  
 @csrf_exempt
 def deploy_staging(request):
-
-    if not getattr(settings, 'SECRET_TOKEN', False) or not getattr(settings, 'STAGING', False):
+    if not getattr(settings, 'SECRET_TOKEN', False) or not getattr(settings, 
+        'STAGING', False):
         return HttpResponse("Invalid URL", status=404)
 
     if verify_hmac_hash(request):
         event_type = request.META.get('HTTP_X_GITHUB_EVENT')
+
         if event_type == "ping":
             return HttpResponse("ok", status=200)
+
         elif event_type == "push":
             body = json.loads(request.body)
             commit = body['commits'][0]
+            ref = body['ref']
             email_info = {
+                'ref': ref,
                 'sender_name': commit['committer']['name'],
                 'commit_hash': commit['id'],
                 'time': commit['timestamp'],
-                'compare_link': body['compare'],
+                'compare_link': body['compare']
             }
-            send_mail("Staging Server Being Updated", pprint.pformat(email_info, indent=4), 
-                            "semesterly.contact@gmail.com", ["rohan@semester.ly"])
-            return_code = subprocess.call(getattr(settings, 'DEPLOY_COMMAND', ""), shell=True)
+
+            if ref.split("/")[-1] != "staging":
+                default_send_email("Non-Staging Branch Updated", 
+                    pprint.pformat(email_info, indent=4))
+                return HttpResponse("200")
+
+            default_send_email("Staging Server Being Updated", 
+                pprint.pformat(email_info, indent=4))
+            return_code = subprocess.call(getattr(settings, 'DEPLOY_COMMAND', ""), 
+                shell=True)
 
             return HttpResponse(status=200)
         else:
