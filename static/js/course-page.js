a7010e39018677f9cb3b43fa662d36e0bbca9647
Max Yeo <mzwyeo@gmail.com>
2016-06-28 23:34:35 -0700

    fixed enrolment numbers and colora

--- static/js/course-page.js
+++ static/js/course-page.js
@@ -4,21 +4,23 @@ document.addEventListener("DOMContentLoaded", function(event) {
 		var s = sections[i];
 		var waitlist = s.getAttribute('data-waitlist');
 		var size = s.getAttribute('data-size');
-		var enrolled = s.getAttribute('data-enrolment');
-		var left = size - enrolled;
+		var open = s.getAttribute('data-enrolment');
 		var h = s.getElementsByClassName('enrollment')[0];
 		if (size < 0) {
 			s.className += '';
 			var txt = 'No enrollment info';
-		} else if (waitlist > 0) {
+		} else if (waitlist >= 0) {
 			s.className += ' red';
 			var txt = '<span>' + waitlist + ' waitlist</span> / ' + size + ' seats';
-		} else if (left < size / 10) {
+		} else if (open == 0) {
+			s.className += ' red';
+			var txt = '<span>' + open + ' open</span> / ' + size + ' seats';
+		} else if (open < size / 10) {
 			s.className += ' yellow';
-			var txt = '<span>' + left + ' open</span> / ' + size + ' seats';
+			var txt = '<span>' + open + ' open</span> / ' + size + ' seats';
 		} else {
 			s.className += ' green';
-			var txt = '<span>' + left + ' open</span> / ' + size + ' seats';
+			var txt = '<span>' + open + ' open</span> / ' + size + ' seats';
 		}
 		h.innerHTML = txt;
 	}
