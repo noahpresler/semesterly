97a2b8971b91afde33fd81f3649b4fc2cee76c81
Rohan Das <das.rohan1993@gmail.com>
2016-08-15 23:36:09 -0400

    peer modal #hacked by #roro and #nono

--- static/js/redux/ui/containers/peer_modal_container.jsx
+++ static/js/redux/ui/containers/peer_modal_container.jsx
@@ -1,17 +1,31 @@
 import { connect } from 'react-redux';
+import { fetchFriends, saveSettings } from '../../actions/user_actions.jsx';
 import { PeerModal } from '../peer_modal.jsx';
 
 const mapStateToProps = (state) => {
+	let activeTimetable = state.timetables.items[state.timetables.active];
 	return {
+		liveTimetableCourses: activeTimetable.courses.filter(c => !c.fake), // don't want to consider courses that are shown on timetable only because of a 'HOVER_COURSE' action (i.e. fake courses)
+		courseToColourIndex: state.ui.courseToColourIndex,
+		peers: state.friends.peers,
+		userInfo: state.userInfo.data
 	}
 }
 
 const mapDispatchToProps = (dispatch) => {
 	return {
+		fetchFriends: () => dispatch(fetchFriends()),
+		saveSettings: (callback) => dispatch(saveSettings(callback)),
+		changeUserInfo: (info) => dispatch({
+			type: "CHANGE_USER_INFO",
+			data: info,
+		})
 	}
 }
 
 const PeerModalContainer = connect(
+	mapStateToProps,
+	mapDispatchToProps
 )(PeerModal);
 
 export default PeerModalContainer;
--- static/js/redux/ui/peer_modal.jsx
+++ static/js/redux/ui/peer_modal.jsx
@@ -3,95 +3,89 @@ import ReactDOM from 'react-dom';
 import classNames from 'classnames';
 import fetch from 'isomorphic-fetch';
 import Modal from 'boron/DropModal';
+import { COLOUR_DATA } from '../constants.jsx';
 
 
 export class PeerModal extends React.Component {
     constructor(props) {
         super(props);
         this.hide = this.hide.bind(this);
+        this.optInAll = this.optInAll.bind(this);
     }
     hide() {
         this.refs.modal.hide();
     }
     componentDidMount() {
         this.refs.modal.show();
+        if (this.props.userInfo.social_all) {
+            this.props.fetchFriends();
+        }
+    }
+    optInAll() {
+        let newUserSettings = {
+            social_courses: true,
+            social_offerings: true,
+            social_all: true
+        }
+        let userSettings = Object.assign({}, this.props.userInfo, newUserSettings);
+        this.props.changeUserInfo(userSettings);
+        this.props.saveSettings(() => {this.props.fetchFriends(); console.log("callback")});
     }
     render() {
         let modalStyle = {
             width: '100%'
         }
+        let sideSlots = this.props.liveTimetableCourses.map(c => {
+            let professors = [ ...new Set(c.slots.map(s => s.instructors)) ];
+            let colourIndex = this.props.courseToColourIndex[c.id] || 0;
+            return (
+                <div className="pm-side-bar-slot" style={ { backgroundColor: COLOUR_DATA[colourIndex].background }} key={c.id}>
+                    <div className="slot-bar" style={ { backgroundColor: COLOUR_DATA[colourIndex].border }} ></div>
+                    <div className="master-slot-content">
+                        <h3>{ c.code }</h3>
+                        <h3>{ c.name }</h3>
+                        <h3>{professors.length == 0 ? "No Professor Listed" : professors}</h3>
+                    </div>
+                </div>)});
         let sideBar =
             <div id="pm-side-bar">
-                <div className="circle-pic" style={{backgroundImage: 'url(/static/img/blank.jpg)'}}></div>
+                <div className="circle-pic" style={{backgroundImage: 'url(http://graph.facebook.com/' + JSON.parse(currentUser).fbook_uid + '/picture?type=normal)'}}></div>
                 <p>Your Courses</p>
-                <div className="pm-side-bar-slot">
-                    <div className="slot-bar" style={{backgroundColor: 'rgb(177, 81, 80)'}}></div>
-                    <div className="master-slot-content">
-                        <h3>{ 'EN.500.650' + ' ' + '(01)' }</h3>
-                        <h3>{ 'Calculus II (Biology and Social Sciences' }</h3>
-                        <h3>{ 'Professor Name' }</h3>
-                    </div>
-                </div>
-                <div className="pm-side-bar-slot">
-                    <div className="slot-bar" style={{backgroundColor: 'rgb(177, 81, 80)'}}></div>
-                    <div className="master-slot-content">
-                        <h3>{ 'EN.500.650' + ' ' + '(01)' }</h3>
-                        <h3>{ 'Calculus II (Biology and Social Sciences' }</h3>
-                        <h3>{ 'Professor Name' }</h3>
-                    </div>
-                </div>
-                <div className="pm-side-bar-slot">
-                    <div className="slot-bar" style={{backgroundColor: 'rgb(177, 81, 80)'}}></div>
-                    <div className="master-slot-content">
-                        <h3>{ 'EN.500.650' + ' ' + '(01)' }</h3>
-                        <h3>{ 'Calculus II (Biology and Social Sciences' }</h3>
-                        <h3>{ 'Professor Name' }</h3>
-                    </div>
-                </div>
-                <div className="pm-side-bar-slot">
-                    <div className="slot-bar" style={{backgroundColor: 'rgb(177, 81, 80)'}}></div>
-                    <div className="master-slot-content">
-                        <h3>{ 'EN.500.650' + ' ' + '(01)' }</h3>
-                        <h3>{ 'Calculus II (Biology and Social Sciences' }</h3>
-                        <h3>{ 'Professor Name' }</h3>
-                    </div>
-                </div>
+                {sideSlots}
             </div>
         let upsell =
             <div className="peer-card upsell">
                 <div className="peer-card-wrapper upsell cf">
                     <h4>Study Buddies, Delivered</h4>
                     <p className="description">See who your classmates are this semester! Click yes below to find Semester.ly users in your courses, message them, or add them on Facebook! <i>By accepting this permission other Semester.ly students outside of your friends will be able to view your name and public Facebook profile.</i></p>
-                    <button className="lure-accept">Yes, I'm In</button>
+                    <button className="lure-accept" onClick={this.optInAll}>Yes, I'm In</button>
                 </div>
             </div>
-        let peerCard =
-            <div className="peer-card">
-                <div className="peer-card-wrapper">
+        let height = this.props.peers[0] && this.props.peers[0].shared_courses ? this.props.peers[0].shared_courses.length * 30 + 85 : 0;
+        let peerCards = this.props.peers.map(p => {
+            let isFriend = p.is_friend ? <p className="friend-status"><i className="fa fa-check"/> Friends</p> : null;
+            let sharedCourses = p.shared_courses.map(sc => {
+                    let colourIndex = this.props.courseToColourIndex[sc.course.id] || 0;
+                    let inSection = sc.in_section ? <i className="fa fa-check"/> : null;
+                    return (<div className="shared-course" key={String(sc.course.id)+p.profile_url}>
+                            <div className="course-color-circle" style={{ backgroundColor: COLOUR_DATA[colourIndex].background }}>{inSection}</div>
+                            <p className="course-title">{sc.course.code + ' - ' +sc.course.name}</p>
+                    </div>)});
+            return (<div className="peer-card" key={p.profile_url}>
+                <div className="peer-card-wrapper" style={{height}} >
                     <div className="card-hat">
-                        <div className="peer-pic" style={{backgroundImage: 'url(/static/img/blank.jpg)'}}></div>
+                        <div className="peer-pic" style={{backgroundImage: 'url(' + p.large_img + ')'}}></div>
                         <div className="user-info">
-                            <h3>Maxwell Ze Wei Yeo</h3>
-                            <button className="view-profile-btn"><i className="fa fa-facebook-square"></i>View Profile</button>
-                            <p className="friend-status"><i className="fa fa-check" />Friends</p>
+                            <h3>{p.name}</h3>
+                            <a href={p.profile_url} target="_blank"><button className="view-profile-btn"><i className="fa fa-facebook-square"></i>View Profile</button></a>
+                            {isFriend}
                         </div>
                     </div>
                     <div className="shared-courses">
-                        <div className="shared-course">
-                            <div className="course-color-circle"><i className="fa fa-check"/></div>
-                            <p className="course-title">Calculus II (Biology and Social Sciences)</p>
-                        </div>
-                        <div className="shared-course">
-                            <div className="course-color-circle" style={{backgroundColor: 'rgb(92, 204, 242)'}}/>
-                            <p className="course-title">Physics: Electricity & Magnetism</p>
-                        </div>
-                        <div className="shared-course">
-                             <div className="course-color-circle" style={{backgroundColor: 'rgb(83, 233, 151)'}}/>
-                            <p className="course-title">Introduction to Business</p>
-                        </div>
+                      {sharedCourses}
                     </div>
                 </div>
-            </div>
+            </div>)});
         let ghostCard =
             <div className="ghost peer-card">
                     <div className="peer-card-wrapper">
@@ -118,6 +112,7 @@ export class PeerModal extends React.Component {
                         </div>
                     </div>
                 </div>
+        let ghostCards = !this.props.userInfo.social_all ? <div>{ghostCard}{ghostCard}{ghostCard}{ghostCard}</div> : null;
         return (
             <Modal ref="modal"
                 className='peer-modal'
@@ -128,24 +123,22 @@ export class PeerModal extends React.Component {
                     <div id="split-modal-wrapper">
                         {sideBar}
                         <div id="main-modal-wrapper">
-                            <h4>Your Classmates</h4>
-                            <div className="key">
-                                <div className="key-entry">
-                                  <div className="course-color-circle" style={{backgroundColor: '#ddd'}}><i className="fa fa-check"/></div>
-                                  <p>peer is in your class & section</p>
-                                </div>
-                                <div className="key-entry">
-                                  <div className="course-color-circle" style={{backgroundColor: '#ddd'}}/>
-                                    <p>peer is in your class only</p>
+                            <div id="pm-header">
+                                <h4>Your Classmates</h4>
+                                <div className="key">
+                                    <div className="key-entry">
+                                      <div className="course-color-circle" style={{backgroundColor: '#ddd'}}><i className="fa fa-check"/></div>
+                                      <p>peer is in your class & section</p>
+                                    </div>
+                                    <div className="key-entry">
+                                      <div className="course-color-circle" style={{backgroundColor: '#ddd'}}/>
+                                        <p>peer is in your class only</p>
+                                    </div>
                                 </div>
                             </div>
-                            {upsell}
-                            {peerCard}
-                            {ghostCard}
-                            {ghostCard}
-                            {ghostCard}
-                            {ghostCard}
-                            {ghostCard}
+                            {!this.props.userInfo.social_all ? upsell : null}
+                            {peerCards}
+                            {ghostCards}
                         </div>
                     </div>
                 </div>
