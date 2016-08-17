0996bf81202a7e121bf8c064ef8a36da207fbe65
Eddie Wang <eddie@eddiewang.me>
2016-08-16 21:01:22 -0400

    added print css queries, top_bar redux container, and linked it to generate dynamic calendars on print

--- static/js/redux/ui/top_bar.jsx
+++ static/js/redux/ui/top_bar.jsx
@@ -18,6 +18,7 @@ class TopBar extends React.Component {
 		super(props);
 		this.sidebar_collapsed = 'neutral';
 		this.toggleSideBar = this.toggleSideBar.bind(this);
+		this.renderUserForPrint = this.renderUserForPrint.bind(this);
 	}
 	toggleSideBar() {
 		if (this.sidebar_collapsed == 'neutral') {
@@ -38,11 +39,21 @@ class TopBar extends React.Component {
 			this.sidebar_collapsed = 'closed';
 		}
 	}
+	renderUserForPrint() {
+		const { userInfo } = this.props;
+		return (
+		<div id="print-content" className="print">
+			<span id="print-name" className="print">{userInfo.userFirstName + ' ' + userInfo.userLastName}</span>
+			<span id="print-major" className="print">{userInfo.major}</span>
+		</div>
+		);
+	}
 	render() {
 		return (
-		<div id="top-bar" className="no-print">
+		<div id="top-bar">
 			<img id="semesterly-logo" src="/static/img/logo2.0-32x32.png"/>
 			<div id="semesterly-name">Semester.ly</div>
+			{this.props.userInfo.isLoggedIn && this.props.userInfo.userFirstName ? this.renderUserForPrint() : null}
 			<SearchBarContainer />
 			<CourseModalContainer />
 			<SocialProfileContainer />
