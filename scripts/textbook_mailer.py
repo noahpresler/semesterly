bd0680645e76bfb3e12512de2762560ac370777d
Eric Calder <ecalder6@jhu.edu>
2016-08-28 15:55:56 -0400

    Textbook mailer: make JSON objects for textbooks.

--- scripts/textbook_mailer.py
+++ scripts/textbook_mailer.py
@@ -4,14 +4,42 @@ django.setup()
 import smtplib
 from email.mime.text import MIMEText
 from django.template.loader import render_to_string
+from student.models import *
+from timetable.models import *
+from django.db.models import Q
+from django.forms.models import model_to_dict
 
-#HANDLE IF THEY HAVE NO TT
-student = Student.objects.get(user__first_name="Noah")
-textbooks = map(lambda s: s.textbooks.all(),student.personaltimetable_set.order_by('last_updated').last().sections.filter(~Q(textbooks=None)).all())
-msg_html = render_to_string('email_textbooks.html', {
-	'user':json.dumps(student.user),
-	'textbooks': json.dumps(textbooks)
-})
+#TODO: Take school as command line.
+school = "jhu"
+
+#TODO: Only students that have personal time tables with courses with textbooks.
+students = PersonalTimetable.objects.filter(school=school).values_list("student", flat=True).distinct()
+for student_id in students:
+    student = Student.objects.get(id=student_id)
+    tt = student.personaltimetable_set.order_by('last_updated').last()
+    textbooks = map(lambda c:
+                    {
+                        "textbooks": map(lambda t: model_to_dict(Textbook.objects.get(isbn=t)), tt.sections.filter(~Q(textbooks=None), course=c).values_list("textbooks", flat=True).distinct()),
+                        "course_name": c.name,
+                        "course_code": c.code
+                    }, tt.courses.all())
+    # Makes the HTML
+
+    # Go through dict. If all empty lists, go to next student.
+
+    print(textbooks)
+    msg_html = render_to_string('email_textbooks.html', {
+        'user':student,
+        'textbooks': json.dumps(textbooks)
+    })
+
+
+
+# student = Student.objects.get(user__first_name="Eric")
+
+# # Gets the textbook for the given student
+# textbooks = map(lambda s: s.textbooks.all(),student.personaltimetable_set.order_by('last_updated').last().sections.filter(~Q(textbooks=None)).all())
+
 
 # Define to/from
 sender = 'textbooks@semester.ly'
@@ -24,9 +52,11 @@ msg['From'] = sender
 msg['To'] = recipient
 
 # Create server object with SSL option
-server = smtplib.SMTP_SSL('smtp.zoho.com', 465)
+# server = smtplib.SMTP_SSL('smtp.zoho.com', 465)
 
-# Perform operations via server
-server.login('textbooks@semester.ly', '7roHan11')
-server.sendmail(sender, [recipient], msg.as_string())
-server.quit()
+# # Perform operations via server
+# server.login('textbooks@semester.ly', '7roHan11')
+
+# # TODO: Ping their email address to make sure it's fine
+# server.sendmail(sender, [recipient], msg.as_string())
+# server.quit()
