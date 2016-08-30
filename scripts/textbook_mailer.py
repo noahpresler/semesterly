bd58dbb06bf4bc0694022dbc6d9be17e2a3718ce
Eric Calder <ecalder6@jhu.edu>
2016-08-29 22:31:37 -0400

    Textbook mailer: added unsubscribe support.

--- scripts/textbook_mailer.py
+++ scripts/textbook_mailer.py
@@ -6,25 +6,39 @@ import smtplib
 from email.mime.text import MIMEText
 from django.template.loader import render_to_string
 from student.models import *
+from student.views import create_unsubscribe_link
 from timetable.models import *
 from django.db.models import Q
 from django.forms.models import model_to_dict
 
-#TODO: Take school as command line.
 if len(sys.argv) < 2:
     print("Please specify a school.")
     exit(0)
 school = sys.argv[1]
 
 students = PersonalTimetable.objects.filter(school=school).values_list("student", flat=True).distinct()
+
+# Create server object with SSL option
+server = smtplib.SMTP_SSL('smtp.zoho.com', 465)
+server.login('textbooks@semester.ly', '7roHan11')
+
+# Define to
+sender = 'textbooks@semester.ly'
+
 for student_id in students:
     student = Student.objects.get(id=student_id)
+
+    if not student.emails_enabled:
+        continue
+
+    unsub_link = "https://semester.ly" + create_unsubscribe_link(student)
+    print(unsub_link)
     tt = student.personaltimetable_set.order_by('last_updated').last()
     textbook_json = map(lambda c:
                     {
                         "textbooks": map(lambda t: model_to_dict(Textbook.objects.get(isbn=t)), tt.sections.filter(~Q(textbooks=None), course=c).values_list("textbooks", flat=True).distinct()),
                         "course_name": c.name,
-                        "course_code": c.code
+                        "course_code": c.code,
                     }, tt.courses.all())
 
     # Go through textbooks. If all empty lists (no textbooks), go to next student.
@@ -37,33 +51,19 @@ for student_id in students:
         continue
 
     msg_html = render_to_string('email_textbooks.html', {
-        'user':student,
+        'user': student,
+        'unsub_link': unsub_link,
         'textbooks_json': textbook_json
     })
-
-    # Define to/from
-    sender = 'textbooks@semester.ly'
-    recipient = student.user.email
 
     # Create message
     msg = MIMEText(msg_html,'html')
 
-    #TODO: Remove testing code
-    f = open(r"/Users/ericcalder/Desktop/test_email.html", "w")
-    f.write(str(msg))
-    #END testing code
-
     msg['Subject'] = "Your Textbooks from Semester.ly"
     msg['From'] = sender
-    msg['To'] = recipient
+    msg['To'] = student.user.email
 
-
-# Create server object with SSL option
-# server = smtplib.SMTP_SSL('smtp.zoho.com', 465)
-
-# # Perform operations via server
-# server.login('textbooks@semester.ly', '7roHan11')
-
-# # TODO: Ping their email address to make sure it's fine
-# server.sendmail(sender, [recipient], msg.as_string())
-# server.quit()
+    # Perform operations via server
+    # TODO: Ping their email address to make sure it's fine
+    server.sendmail(sender, [recipient], msg.as_string())
+server.quit()
