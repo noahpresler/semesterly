93753d812d6430dc73448e75348459fe72d578af
Michael N. Miller <mmill179@jhu.edu>
2016-09-06 19:19:44 -0400

    for eric

---
+++ scripts/vandy/vandy_textbooks.py
@@ -0,0 +1,23 @@
+from scripts.base_bn_textbook_parser import *
+
+
+textbook_parser = TextbookParser(
+	"65163", 
+	"http://vanderbilt.bncollege.com",
+	"vandy"
+)
+
+textbook_parser.parse()
+
+# class VandyTextbookParser:
+
+# 	def __init__(self, store_id, store_link, school):
+
+# 	def parse():
+
+# def main():
+# 	vtp = VandyTextbookParser()
+# 	vtp.parse()
+
+# if __name__ = "__main__":
+# 	main()
