d6dcef50aefa6b491516b4bca2b5bd10c3ab2839
Max Yeo <mzwyeo@gmail.com>
2016-10-04 21:54:02 -0400

    starting chrome notifications

--- static/js/misc/chromenotifications.js
+++ static/js/misc/chromenotifications.js
@@ -1,15 +1,15 @@
-'use strict';
+// 'use strict';
 
-if ('serviceWorker' in navigator) {
-    console.log('Service Worker is supported');
-    navigator.serviceWorker.register('/sw.js').then(function(reg) {
-        console.log(':^)', reg);
-        reg.pushManager.subscribe({
-            userVisibleOnly: true
-        }).then(function(sub) {
-            console.log('endpoint:', sub.endpoint);
-        });
-    }).catch(function(error) {
-        console.log(':^(', error);
-    });
-}
+// if ('serviceWorker' in navigator) {
+//     console.log('Service Worker is supported');
+//     navigator.serviceWorker.register('/sw.js').then(function(reg) {
+//         console.log(':^)', reg);
+//         reg.pushManager.subscribe({
+//             userVisibleOnly: true
+//         }).then(function(sub) {
+//             console.log('endpoint:', sub.endpoint);
+//         });
+//     }).catch(function(error) {
+//         console.log(':^(', error);
+//     });
+// }
