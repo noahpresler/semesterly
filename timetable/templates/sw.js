420049aebfacc432c14153859c7d711320b168b4
Max Yeo <mzwyeo@gmail.com>
2016-10-04 21:54:02 -0400

    starting chrome notifications

--- timetable/templates/sw.js
+++ timetable/templates/sw.js
@@ -33,4 +33,16 @@ self.addEventListener('activate', function(event) {
 self.addEventListener('push', function(event) {
   console.log('Push message received', event);
   // TODO
+  var title = 'Notification';  
+  var body = 'There is newly updated content available on the site. Click to see more.';  
+  var icon = 'https://raw.githubusercontent.com/deanhume/typography/gh-pages/icons/typography.png';  
+  var tag = 'simple-push-demo-notification-tag';
+  
+  event.waitUntil(  
+    self.registration.showNotification(title, {  
+       body: body,  
+       icon: icon,  
+       tag: tag  
+     })  
+   );  
 });
