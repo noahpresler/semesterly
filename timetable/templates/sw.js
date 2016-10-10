d4a4c887f018afaddb34e96ffaa5277d26de9f8a
Max Yeo <mzwyeo@gmail.com>
2016-10-10 19:43:12 -0400

    redux state working for notifications, deletes token from server

--- timetable/templates/sw.js
+++ timetable/templates/sw.js
@@ -41,7 +41,7 @@ self.addEventListener('push', function(event) {
   var body = 'There is newly updated content available on the site. Click to see more.';  
   var icon = 'https://raw.githubusercontent.com/deanhume/typography/gh-pages/icons/typography.png';  
   var tag = 'simple-push-demo-notification-tag';
-  
+
   event.waitUntil(  
     self.registration.showNotification(title, {  
        body: body,  
