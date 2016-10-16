4eb509a1ecf1fbf0f66ad481d85f1bf84247069c
Noah Presler <noah@presler,me>
2016-10-16 19:59:01 -0400

    Revert "Revert "Merge pull request #232 from rohandas/vandy""
    This reverts commit 83efb37b3d28bdd3ae3624ec5d2d59758e1efbcd.

--- scripts/vandy/vandy_textbooks.py
+++ scripts/vandy/vandy_textbooks.py
@@ -1,12 +1,11 @@
 from scripts.base_bn_textbook_parser import *
 
-
 if __name__ == "__main__":
 	textbook_parser = TextbookParser(
-	    "18053",
-	    "johns-hopkins.bncollege.com",
-	    "jhu",
-	    "."
+	    "65163",
+	    "vanderbilt.bncollege.com",
+	    "vandy",
+	    "-"
 	)
 
 	textbook_parser.parse()
