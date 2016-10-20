321d03797264e541683ff071602b2fd75aceeeaa
Michael N. Miller <mmill179@jhu.edu>
2016-10-19 20:28:17 -0400

    stuck on advanced search POST request

--- scripts/gw/gw_courses.py
+++ scripts/gw/gw_courses.py
@@ -87,20 +87,19 @@ class GWParser:
 
 	def direct_to_search_page(self):
 		query = {}
-		# query['name'] = 'bmenu.P_MainMnu'
-		# self.get_html(self.url + '/twbkwbis.P_GenMenu', query)
-		# query['name'] = 'bmenu.P_StuMainMnu'
-		# self.get_html(self.url + '/twbkwbis.P_GenMenu', query)
-		# query['name'] = 'bmenu.P_RegMnu'
-		# self.get_html(self.url + '/twbkwbis.P_GenMenu', query)
-		# query.clear()
+		query['name'] = 'bmenu.P_MainMnu'
+		self.get_html(self.url + '/twbkwbis.P_GenMenu', query)
+		query['name'] = 'bmenu.P_StuMainMnu'
+		self.get_html(self.url + '/twbkwbis.P_GenMenu', query)
+		query['name'] = 'bmenu.P_RegMnu'
+		self.get_html(self.url + '/twbkwbis.P_GenMenu', query)
+		query.clear()
 		query['term_in'] = ''
 		return self.get_html(self.url + '/bwskfcls.P_CrseSearch', query)
 
-
 	def parse(self):
 		self.login()
-		# self.direct_to_search_page()
+		self.direct_to_search_page()
 
 		# NOTE: hardcoded semesters to parse
 		semesters = {'F':'201603', 'S':'201701'}
@@ -110,51 +109,83 @@ class GWParser:
 				'p_calling_proc' : 'P_CrseSearch',
 				'p_term' : semesters[semester]
 			}
+
+			soup = BeautifulSoup(self.post_http(self.url + '/bwckgens.p_proc_term_date', search_query).text, 'html.parser')
+
+			# create search param list
+			search_params = {inp['name'] : inp['value'] if inp.get('value') else '' for inp in soup.find('form', {'action': '/PRODCartridge/bwskfcls.P_GetCrse'}).find_all('input')}
+
 			# get list of departments
-			depts = (dept['value'] for dept in BeautifulSoup(self.post_http(self.url + '/bwckgens.p_proc_term_date', search_query).text, 'html.parser').find('select', {'id' : 'subj_id'}).find_all('option'))
-	
+			depts = (dept['value'] for dept in soup.find('select', {'id' : 'subj_id'}).find_all('option'))
+
 			for dept in depts:
 
-				print self.post_http(self.url + '/bwskfcls.P_GetCrse', GWParser.search_params(term_in=semesters[semester], sel_subj=dept)).text
+				self.headers['Referer'] = self.url + '/bwckgens.p_proc_term_date'
+
+				search_params['sel_subj'] = dept
+				search_params['SUB_BTN'] = 'Course Search'
+				search_params['begin_hh'] = '0'
+				search_params['begin_mi'] = '0'
+				search_params['end_hh'] = '0'
+				search_params['end_mi'] = '0'
+				search_params['sel_ptrm']  = '%'
+
+				search = {
+					'rsts':'dummy',
+					'crn':'dummy',
+					'term_in':'201701',
+					'sel_subj':'dummy',
+					'sel_day':'dummy',
+					'sel_schd':'dummy',
+					'sel_insm':'dummy',
+					'sel_camp':'dummy',
+					'sel_levl':'dummy',
+					'sel_sess':'dummy',
+					'sel_instr':'dummy',
+					'sel_ptrm':'dummy',
+					'sel_attr':'dummy',
+					'sel_subj':'ACCY',
+					'sel_crse':'',
+					'sel_title':'',
+					'sel_from_cred':'',
+					'sel_to_cred':'',
+					'sel_ptrm':'%25',
+					'begin_hh':'0',
+					'begin_mi':'0',
+					'end_hh':'0',
+					'end_mi':'0',
+					'begin_ap':'x',
+					'end_ap':'y',
+					'path':'1',
+					'SUB_BTN':'Advanced Search'
+				}
+
+				self.headers = {
+					'Host':'banweb.gwu.edu',
+					'Connection':'keep-alive',
+					'Content-Length':'350',
+					'Cache-Control':'max-age=0',
+					'Origin':'https://banweb.gwu.edu',
+					'Upgrade-Insecure-Requests':'1',
+					'Content-Type':'application/x-www-form-urlencoded',
+					'Accept':'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8',
+					'Referer':'https://banweb.gwu.edu/PRODCartridge/bwckgens.p_proc_term_date',
+					'Accept-Encoding':'gzip, deflate, br',
+					'Accept-Language':'en-US,en;q=0.8'
+				}
+
+				self.post_http(self.url + '/bwskfcls.P_GetCrse', search).text
+				search['SUB_BTN'] = 'Section Search'
+				print self.post_http(self.url + '/bwskfcls.P_GetCrse_Advanced', search).text
+
+				# for param in search_params:
+				# 	print param + ':' + search_params[param]
+				# print self.post_http(self.url + '/bwskfcls.P_GetCrse', search_params).text
 
 				break
-				
-			break
-		# print BeautifulSoup(self.get_html(self.url + '/bwskfcls.P_CrseSearch'), 'html.parser').prettify()
 
-	@staticmethod
-	def search_params(**kwargs):
-		params = {
-			'rsts':'dummy',
-			'crn':'dummy',
-			'term_in':'201603',
-			'sel_subj':'dummy',
-			'sel_day':'dummy',
-			'sel_schd':'dummy',
-			'sel_insm':'dummy',
-			'sel_camp':'dummy',
-			'sel_levl':'dummy',
-			'sel_sess':'dummy',
-			'sel_instr':'dummy',
-			'sel_ptrm':'dummy',
-			'sel_attr':'dummy',
-			'sel_subj':'dummy',
-			'sel_crse':'',
-			'sel_title':'',
-			'sel_from_cred':'',
-			'sel_to_cred':'',
-			'sel_ptrm':'%25',
-			'begin_hh':'0',
-			'begin_mi':'0',
-			'end_hh':'0',
-			'end_mi':'0',
-			'begin_ap':'x',
-			'end_ap':'y',
-			'path':'1',
-			'SUB_BTN':'Course+Search'
-		}
-		params.update(kwargs)
-		return params
+			break
+
 
 def main():
 	gp = GWParser()
