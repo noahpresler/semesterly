02fe5af42dd889a1efefc2520dfede31a2c38554
Michael N. Miller <mmill179@jhu.edu>
2016-10-22 12:57:15 -0400

    search is good, needed to use list

--- scripts/gw/gw_courses.py
+++ scripts/gw/gw_courses.py
@@ -122,7 +122,7 @@ class GWParser:
 
 				self.headers['Referer'] = self.url + '/bwckgens.p_proc_term_date'
 
-				search_params['sel_subj'] = dept
+				search_params['sel_subj'] = ['dummy', dept]
 				search_params['SUB_BTN'] = 'Course Search'
 				search_params['begin_hh'] = '0'
 				search_params['begin_mi'] = '0'
@@ -130,103 +130,13 @@ class GWParser:
 				search_params['end_mi'] = '0'
 				search_params['sel_ptrm']  = '%'
 
-				search = {
-					'rsts':'dummy',
-					'crn':'dummy',
-					'term_in':'201701',
-					'sel_subj':'dummy',
-					'sel_day':'dummy',
-					'sel_schd':'dummy',
-					'sel_insm':'dummy',
-					'sel_camp':'dummy',
-					'sel_levl':'dummy',
-					'sel_sess':'dummy',
-					'sel_instr':'dummy',
-					'sel_ptrm':'dummy',
-					'sel_attr':'dummy',
-					'sel_subj':'ACCY',
-					'sel_crse':'',
-					'sel_title':'',
-					'sel_from_cred':'',
-					'sel_to_cred':'',
-					'sel_ptrm':'%25',
-					'begin_hh':'0',
-					'begin_mi':'0',
-					'end_hh':'0',
-					'end_mi':'0',
-					'begin_ap':'x',
-					'end_ap':'y',
-					'path':'1',
-					'SUB_BTN':'Advanced Search'
-				}
-
-				# self.headers = {
-				# 	'Host':'banweb.gwu.edu',
-				# 	'Connection':'keep-alive',
-				# 	'Content-Length':'350',
-				# 	'Cache-Control':'max-age=0',
-				# 	'Origin':'https://banweb.gwu.edu',
-				# 	'Upgrade-Insecure-Requests':'1',
-				# 	'Content-Type':'application/x-www-form-urlencoded',
-				# 	'Accept':'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8',
-				# 	'Referer':'https://banweb.gwu.edu/PRODCartridge/bwckgens.p_proc_term_date',
-				# 	'Accept-Encoding':'gzip, deflate, br',
-				# 	'Accept-Language':'en-US,en;q=0.8'
-				# }
-
-				# self.post_http(self.url + '/bwskfcls.P_GetCrse', search).text
-				# search['SUB_BTN'] = 'Section Search'
-				# print self.post_http(self.url + '/bwskfcls.P_GetCrse_Advanced', search).text
-				self.headers = {}
-
-				self.headers = {
-					'Origin': 'https://banweb.gwu.edu',
-					'Accept-Encoding': 'gzip, deflate, br',
-					'Accept-Language': 'en-US,en;q=0.8',
-					'Upgrade-Insecure-Requests': '1',
-					'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/53.0.2785.143 Safari/537.36',
-					'Content-Type': 'application/x-www-form-urlencoded',
-					'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8',
-					'Cache-Control': 'max-age=0',
-					'Referer': 'https://banweb.gwu.edu/PRODCartridge/bwckgens.p_proc_term_date',
-					'Connection': 'keep-alive',
-				}
-
-				data = {
-					'rsts': 'dummy',
-					'crn': 'dummy',
-					'term_in': '201603',
-					'sel_subj': 'dummy,ANTH',
-					'sel_day': 'dummy',
-					'sel_schd': 'dummy',
-					'sel_insm': 'dummy',
-					'sel_camp': 'dummy',
-					'sel_levl': 'dummy',
-					'sel_sess': 'dummy',
-					'sel_instr': 'dummy',
-					'sel_ptrm': 'dummy,%',
-					'sel_attr': 'dummy',
-					'sel_crse': '',
-					'sel_title': '',
-					'sel_from_cred': '',
-					'sel_to_cred': '',
-					'begin_hh': '0',
-					'begin_mi': '0',
-					'end_hh': '0',
-					'end_mi': '0',
-					'begin_ap': 'x',
-					'end_ap': 'y',
-					'path': '1',
-					'SUB_BTN': 'Course Search'
-				}
-
 				# html = requests.post('https://banweb.gwu.edu/PRODCartridge/bwskfcls.P_GetCrse', headers=headers, cookies=self.cookies, data=data).text
 				# print BeautifulSoup(html, 'html.parser').prettify()
 				# exit(0)
 
 				for param in search_params:
-					print param + ':' + search_params[param]
-				html = self.post_http('https://banweb.gwu.edu/PRODCartridge/bwskfcls.P_GetCrse', data).text
+					print param + ':' + str(search_params[param])
+				html = self.post_http(self.url + '/bwskfcls.P_GetCrse', search_params).text
 				print html
 				# print BeautifulSoup(html, 'html.parser').prettify()
 
