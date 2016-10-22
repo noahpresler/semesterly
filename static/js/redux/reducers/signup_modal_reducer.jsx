4873781d2e859ff8f9622b53400c0465d821f4de
Noah Presler <noah@presler,me>
2016-10-22 17:48:45 -0400

    notification logged in or out

--- static/js/redux/reducers/signup_modal_reducer.jsx
+++ static/js/redux/reducers/signup_modal_reducer.jsx
@@ -1,7 +1,9 @@
 export const signupModal = (state = { isVisible: false }, action) => {
 	switch (action.type) {
 		case 'TOGGLE_SIGNUP_MODAL':
-			return {isVisible: !state.isVisible}
+			return {isVisible: !state.isVisible};
+		case 'TRIGGER_SIGNUP_MODAL':
+			return {isVisible: true};
 		default:
 			return state;
 	}
