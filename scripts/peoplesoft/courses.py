a0074297cc395b21e992cb89c3a2b76b1d241f3b
Michael N. Miller <mmill179@jhu.edu>
2016-10-22 01:34:17 -0400

    getting there, integrating pennstate into peoplesoft general parser

--- scripts/peoplesoft/courses.py
+++ scripts/peoplesoft/courses.py
@@ -1,8 +1,3 @@
-# @what	PennState Course Parser
-# @org	Semeseter.ly
-# @author	Michael N. Miller
-# @date	10/21/16
-
 import django, os, datetime, requests, cookielib, re, sys
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "semesterly.settings")
 django.setup()
@@ -11,51 +6,79 @@ from fake_useragent import UserAgent
 from itertools import izip
 from bs4 import BeautifulSoup
 
-from scripts.textbooks.amazon import make_textbook
-from scripts.peoplesoft.courses import PeopleSoftParser
+class PeopleSoftParser:
 
-class PennStateParser(PeopleSoftParser):
+	DAY_MAP = {
+		'Mo' : 'M',
+		'Tu' : 'T',
+		'We' : 'W',
+		'Th' : 'R',
+		'Fr' : 'F',
+		'Sa' : 'S',
+		'Su' : 'U'
+	}
 
-	def __init__(self):
-		school = 'pennstate'
-		url = 'https://public.lionpath.psu.edu/psc/CSPRD/EMPLOYEE/HRMS/c/COMMUNITY_ACCESS.CLASS_SEARCH.GBL'
-		PeopleSoftParser.__init__(self, school, url)
+	def __init__(self, school, url):
+		self.session = requests.Session()
+		self.headers = {'User-Agent' : UserAgent().chrome}
+		self.cookies = cookielib.CookieJar()
+		self.base_url = url
+		self.school = school
+		self.course = {}
 
-	def parse(self):
+	def get_html(self, url, payload=''):
+		html = None
+		while html is None:
+			try:
+				r = self.session.get(
+					url,
+					params = payload,
+					cookies = self.cookies,
+					headers = self.headers,
+					verify = True
+				)
 
-		# NOTE: hardcoded semesters Fall, Interim, Spring 2016-2017
-		terms = {'F':'2168', 'S':'2171'}
+				if r.status_code == 200:
+					html = r.text
 
-		search_page_params = {	
-			'PORTALPARAM_PTCNAV':'HC_CLASS_SEARCH_GBL',
-			'EOPP.SCNode':'HRMS',
-			'EOPP.SCPortal':'EMPLOYEE',
-			'EOPP.SCName':'CO_EMPLOYEE_SELF_SERVICE',
-			'EOPP.SCLabel':'Self Service',
-			'EOPP.SCPTfname':'CO_EMPLOYEE_SELF_SERVICE',
-			'FolderPath':'PORTAL_ROOT_OBJECT.CO_EMPLOYEE_SELF_SERVICE.HC_CLASS_SEARCH_GBL',
-			'IsFolder':'false',
-			'PortalActualURL':'https://public.lionpath.psu.edu/psc/CSPRD/EMPLOYEE/HRMS/c/COMMUNITY_ACCESS.CLASS_SEARCH.GBL',
-			'PortalContentURL':'https://public.lionpath.psu.edu/psc/CSPRD/EMPLOYEE/HRMS/c/COMMUNITY_ACCESS.CLASS_SEARCH.GBL',
-			'PortalContentProvider':'HRMS',
-			'PortalCRefLabel':'Class Search',
-			'PortalRegistryName':'EMPLOYEE',
-			'PortalServletURI':'https://public.lionpath.psu.edu/psp/CSPRD/',
-			'PortalURI':'https://public.lionpath.psu.edu/psc/CSPRD/',
-			'PortalHostNode':'HRMS',
-			'NoCrumbs':'yes',
-			'PortalKeyStruct':'yes'
-		}
+				# print GET, r.url
 
-		# PeopleSoftParser.parse(self, terms, url_params=search_page_params)
+			except (requests.exceptions.Timeout,
+				requests.exceptions.ConnectionError):
+				sys.stderr.write("Unexpected error: " + str(sys.exc_info()[0]))
+				continue
 
-		# exit(0)
+		return html.encode('utf-8')
 
-		soup = BeautifulSoup(self.get_html(self.base_url, search_page_params))
-		# print soup.prettify()
+	def post_http(self, url, form, payload=''):
+
+		try:
+			post = self.session.post(
+				url,
+				data = form,
+				params = payload,
+				cookies = self.cookies,
+				headers = self.headers,
+				verify = True,
+			)
+
+			# print POST, r.url
+
+			return post
+
+		except (requests.exceptions.Timeout,
+			requests.exceptions.ConnectionError):
+			sys.stderr.write("Unexpected error: " + str(sys.exc_info()[0]))
+
+		return None
+
+	def parse(self, terms, **kwargs):
+
+		soup = BeautifulSoup(self.get_html(self.base_url, kwargs['url_params'] if kwargs.get('url_params') else {}))
+		print soup.prettify()
 
 		# create search payload with hidden form data
-		search_query = {a['name']: a['value'] for a  in soup.find('div', id=re.compile(r'win\ddivPSHIDDENFIELDS')).find_all('input')}
+		search_query = {a['name']: a['value'] for a  in soup.find('div', {'id' : 'win0divPSHIDDENFIELDS'}).find_all('input')}
 		search_query['ICAction'] = 'CLASS_SRCH_WRK2_SSR_PB_CLASS_SRCH'
 		search_query['SSR_CLSRCH_WRK_SSR_OPEN_ONLY$chk$4'] = 'N'
 		for day in ['MON', 'TUES', 'WED', 'THURS', 'FRI', 'SAT', 'SUN']:
@@ -63,11 +86,12 @@ class PennStateParser(PeopleSoftParser):
 			search_query['SSR_CLSRCH_WRK_' + day + '$chk$5'] = 'Y'
 		search_query['SSR_CLSRCH_WRK_INCLUDE_CLASS_DAYS$5'] = 'J'
 
-		# print search_query
-
 		# extract search query info
-		departments = soup.find('select', id=re.compile(r'SSR_CLSRCH_WRK_SUBJECT_SRCH\$\d')).find_all('option')[1:]
+		departments = soup.find('select', {'id' : 'SSR_CLSRCH_WRK_SUBJECT_SRCH$2'}).find_all('option')[1:]
 		# NOTE: first element of dropdown lists in search area is empty
+
+		# NOTE: hardcoded semesters Fall, Interim, Spring 2016-2017
+		terms = {'F':'2168', 'I':'2172', 'S':'2174'}
 
 		for term in terms:
 
@@ -88,7 +112,7 @@ class PennStateParser(PeopleSoftParser):
 				search_query['SSR_CLSRCH_WRK_SUBJECT_SRCH$2'] = department['value']
 
 				# Get course listing page for department
-				soup = BeautifulSoup(self.post_http(self.base_url, search_query).text, 'html.parser')
+				soup = BeautifulSoup(self.post_http(PennStateParser.BASE_URL, search_query).text, 'html.parser')
 
 				# check for valid search/page
 				if soup.find('td', {'id' : 'PTBADPAGE_' }) or soup.find('div', {'id' : 'win1divDERIVED_CLSMSG_ERROR_TEXT'}):
@@ -106,7 +130,7 @@ class PennStateParser(PeopleSoftParser):
 					descr_payload['ICAction'] = 'MTG_CLASS_NBR$' + str(i)
 
 					# Get course description page
-					soup = BeautifulSoup(self.get_html(self.base_url, descr_payload))
+					soup = BeautifulSoup(self.get_html(PennStateParser.BASE_URL, descr_payload))
 
 					# scrape info from page
 					title 		= soup.find('span', {'id' : 'DERIVED_CLSRCH_DESCR200'}).text
@@ -157,7 +181,7 @@ class PennStateParser(PeopleSoftParser):
 
 						if rsched:
 							days = map(lambda d: PeopleSoftParser.DAY_MAP[d], re.findall('[A-Z][^A-Z]*', rsched.group(1)))
-							time = (PeopleSoftParser.time_12to24(rsched.group(2)), PeopleSoftParser.time_12to24(rsched.group(3)))
+							time = (PennStateParser.time_12to24(rsched.group(2)), PennStateParser.time_12to24(rsched.group(3)))
 						else: # handle TBA classes
 							days = None
 							time = (None, None)
@@ -169,38 +193,72 @@ class PennStateParser(PeopleSoftParser):
 
 						self.create_offerings(section)
 
-			PennStateParser.wrap_up()
+			PeopleSoftParser.wrap_up()
 
-	def parse_textbooks(self, soup):
-		isbns = zip(soup.find_all('span', id=re.compile(r'DERIVED_SSR_TXB_SSR_TXBDTL_ISBN\$\d*')), soup.find_all('span', id=re.compile(r'DERIVED_SSR_TXB_SSR_TXB_STATDESCR\$\d*')))
-		for i in range(len(isbns)):
-			isbns[i] = (filter(lambda x: x.isdigit(), isbns[i][0].text), isbns[i][1].text[0].upper() == 'R')
-		return isbns
-		# return map(lambda i: (filter(lambda x: x.isdigit(), isbns[i][0].text), isbns[i][1].text[0].upper() == 'R'), range(len(isbns)))
+	@staticmethod
+	def time_12to24(time12):
 
-	# NOTE: chapman specific
-	def extract_prereqs(self, text):
+		# Regex extract
+		match = re.match("(\d*):(\d*)(.)", time12)
 
-		extractions = {
-			'prereqs' : r'Prerequisite(?:s?)[:,]\s(.*?)\.',
-			'coreqs'  : r'Corequisite(?:s?)[:,]\s(.*?)\.'
-		}
+		# Transform to 24 hours
+		hours = int(match.group(1))
+		if re.search(r'[pP]', match.group(3)):
+			hours = (hours%12)+12
 
-		for ex in extractions:
-			rex = re.compile(extractions[ex])
-			extracted = rex.search(text)
-			if extracted:
-				self.course[ex] = extracted.group(1)
-			else:
-				self.course[ex] = ''
-			text = rex.sub('', text).strip()
+		# Return as 24hr-time string
+		return str(hours) + ":" + match.group(2)
 
-		return text
+	@staticmethod
+	def wrap_up():
+			update_object, created = Updates.objects.update_or_create(
+					school=self.school,
+					update_field="Course",
+					defaults={'last_updated': datetime.datetime.now()}
+			)
+			update_object.save()
 
+	def create_course(self):
+		course, CourseCreated = Course.objects.update_or_create(
+			code = self.course['code'],
+			school = self.school,
+			defaults={
+				'name': self.course.get('name'),
+				'description': self.course.get('descr'),
+				'department': self.course.get('department'),
+				'num_credits': self.course.get('credits'),
+				'prerequisites': self.course.get('prereqs'),
+				'corequisites': self.course.get('coreqs'),
+				'notes': self.course.get('notes'),
+				'info' : self.course.get('info')
+				# 'areas': self.course.get('areas'),
+			}
+		)
+		return course
 
-def main():
-	p = PennStateParser()
-	p.parse()
+	def create_section(self, course_model):
+		# TODO - deal with cancelled course?
+		section, section_was_created = Section.objects.update_or_create(
+			course = course_model,
+			semester = self.course['semester'],
+			meeting_section = self.course['section'],
+			defaults = {
+				'instructors': self.course.get('instrs'),
+				'size': self.course.get('size'),
+				'enrolment': self.course.get('enrolment')
+			}
+		)
+		return section
 
-if __name__ == "__main__":
-	main()
+	def create_offerings(self, section_model):
+		if self.course.get('days'):
+			for day in self.course.get('days'):
+				offering_model, offering_was_created = Offering.objects.update_or_create(
+					section = section_model,
+					day = day,
+					time_start = self.course.get('time_start'),
+					time_end = self.course.get('time_end'),
+					defaults = {
+						'location': self.course.get('location')
+					}
+				)
