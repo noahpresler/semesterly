e62dad9f0bba51e031f696cf76bc786060b66c23
Michael N. Miller <mmill179@jhu.edu>
2016-10-23 00:16:55 -0400

    cleanup

--- scripts/gw/gw_textbooks.py
+++ scripts/gw/gw_textbooks.py
@@ -20,10 +20,6 @@ class GWTextbooks:
 	def direct(self):
 
 		self.get_jsessionid()
-		# self.headers['Referer'] = 'http://www.bkstr.com/webapp/wcs/stores/servlet/LocateCourseMaterialsView?storeId=10370'
-		# self.headers['X-Prototype-Version']='1.5.0'
-		# self.headers['X-Requested-With']='XMLHttpRequest'
-		# self.headers['Host']='www.bkstr.com'
 
 		query = {
 			'storeId':self.store_id,
@@ -43,21 +39,21 @@ class GWTextbooks:
 
 				print term
 
-				depts = json.loads(re.search(r'\'(.*)\'', self.get_html(self.url + '/LocateCourseMaterialsServlet', query).text).group(1))['data'][0]
+				depts = json.loads(re.search(r'\'(.*)\'', self.get_http(self.url + '/LocateCourseMaterialsServlet', query).text).group(1))['data'][0]
 				for dept in depts:
 					query['departmentName'] = dept
 					query['requestType'] = 'COURSES'
 
 					print dept
 
-					courses = json.loads(re.search(r'\'(.*)\'', self.get_html(self.url + '/LocateCourseMaterialsServlet', query).text).group(1))['data'][0]
+					courses = json.loads(re.search(r'\'(.*)\'', self.get_http(self.url + '/LocateCourseMaterialsServlet', query).text).group(1))['data'][0]
 					for course in courses:
 						query['courseName'] = course
 						query['requestType'] = 'SECTIONS'
 
 						print course
 
-						sections = json.loads(re.search(r'\'(.*)\'', self.get_html(self.url + '/LocateCourseMaterialsServlet', query).text).group(1))['data'][0]
+						sections = json.loads(re.search(r'\'(.*)\'', self.get_http(self.url + '/LocateCourseMaterialsServlet', query).text).group(1))['data'][0]
 						for section in sections:
 
 							print section
@@ -76,23 +72,22 @@ class GWTextbooks:
 								'purpose':'browse'
 							}
 
-							print self.get_html(self.url + '/CourseMaterialsResultsView', query2).text
+							print self.get_http(self.url + '/CourseMaterialsResultsView', query2).text
 
 	def get_jsessionid(self):
 
-		JSESSIONID = re.search(r'JSESSIONID=(.*?);', self.get_html('http://www.bkstr.com/').text).group(1)
-		print JSESSIONID
-		# self.cookies['JSESSIONID'] = JSESSIONID
+		JSESSIONID = re.search(r'JSESSIONID=(.*?);', self.get_http('http://www.bkstr.com/').text).group(1)
+
 		cookie = cookielib.Cookie(
 			version=0, 
-			name='JSESSIONID', 
+			name='JSESSIONID',
 			value=JSESSIONID,
 			port=None, 
 			port_specified=False,
 			domain="www.bkstr.com", 
 			domain_specified=True, 
 			domain_initial_dot=False,
-			path="/", 
+			path="/",
 			path_specified=True,
 			secure=False,
 			expires=None,
@@ -101,13 +96,10 @@ class GWTextbooks:
 			comment_url=None,
 			rest=None
 		)
-		# cookie = cookielib.Cookie(None, 'JSESSIONID', JSESSIONID, '80', '80', 'www.bkstr.com', None, None, '/', None, False, False, None, None, None, None)
+
 		self.cookies.set_cookie(cookie)
 
-		print self.get_html('http://www.bkstr.com/webapp/wcs/stores/servlet/LocateCourseMaterialsServlet?requestType=SECTIONS&storeId=10370&demoKey=d&programId=270&termId=100041961&divisionName=%20&departmentName=ACA&courseName=6209&_=').text
-
-	# REQUESTS
-	def get_html(self, url, payload=''):
+	def get_http(self, url, payload=''):
 		response = None
 		while response is None:
 			try:
@@ -116,13 +108,10 @@ class GWTextbooks:
 					params = payload,
 					cookies = self.cookies,
 					headers = self.headers,
-					# verify = True
+					verify = True
 				)
 
 				print 'GET', response.url
-
-				# if r.status_code == 200:
-					# response = r.text
 
 			except (requests.exceptions.Timeout,
 				requests.exceptions.ConnectionError):
@@ -132,8 +121,8 @@ class GWTextbooks:
 		return response
 
 	def post_http(self, url, form, payload=''):
-		p = None
-		while p is None:
+		post = None
+		while post is None:
 			try:
 				post = self.session.post(
 					url,
@@ -144,17 +133,14 @@ class GWTextbooks:
 					verify = False,
 				)
 
-				if post.status_code == 200:
-					p = post
-
-				# print 'POST', post.url
+				print 'POST', post.url
 
 			except (requests.exceptions.Timeout,
 				requests.exceptions.ConnectionError):
 				sys.stderr.write("Unexpected error: " + str(sys.exc_info()[0]))
 				continue
 
-		return p
+		return post
 
 def main():
 	tp = GWTextbooks()
