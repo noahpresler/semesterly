6b59739d0194b1af2281dd297fc628cce7e1822b
Max Yeo <mzwyeo@gmail.com>
2016-10-22 23:01:51 -0400

    notifs sending

--- timetable/templates/sw.js
+++ timetable/templates/sw.js
@@ -1,27 +1,6 @@
-/*
-*
-*  Push Notifications codelab
-*  Copyright 2015 Google Inc. All rights reserved.
-*
-*  Licensed under the Apache License, Version 2.0 (the "License");
-*  you may not use this file except in compliance with the License.
-*  You may obtain a copy of the License at
-*
-*      https://www.apache.org/licenses/LICENSE-2.0
-*
-*  Unless required by applicable law or agreed to in writing, software
-*  distributed under the License is distributed on an "AS IS" BASIS,
-*  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-*  See the License for the specific language governing permissions and
-*  limitations under the License
-*
-*/
-
-// Version 0.1
 
 'use strict';
 
-// TODO
 console.log('Started', self);
 self.addEventListener('install', function(event) {
   self.skipWaiting();
@@ -49,4 +28,23 @@ self.addEventListener('push', function(event) {
        tag: tag  
      })  
    );  
+});
+self.addEventListener('notificationclick', function(event) {  
+  console.log('On notification click: ', event.notification.tag);  
+  event.notification.close();
+  event.waitUntil(
+    clients.matchAll({  
+      type: "window"  
+    })
+    .then(function(clientList) {  
+      for (var i = 0; i < clientList.length; i++) {  
+        var client = clientList[i];  
+        if (client.url == '/' && 'focus' in client)  
+          return client.focus();  
+      }  
+      if (clients.openWindow) {
+        return clients.openWindow('https://semester.ly');  
+      }
+    })
+  );
 });
