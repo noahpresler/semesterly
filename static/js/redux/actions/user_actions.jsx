d8f9d016912d0781267197996ff2d35693c8a9c0
Max Yeo <mzwyeo@gmail.com>
2016-10-23 01:29:00 -0400

    custom notifs

--- static/js/redux/actions/user_actions.jsx
+++ static/js/redux/actions/user_actions.jsx
@@ -369,7 +369,8 @@ export function setARegistrationToken() {
 	        reg.pushManager.subscribe({
 	            userVisibleOnly: true
 	        }).then(function(sub) {
-	            sendRegistrationToken(sub.endpoint.substring(40));
+	        	console.log(sub);
+	            sendRegistrationToken(sub.toJSON());
 	        });
 	    }).catch(function(error) {
 	        console.log(':^(', error);
@@ -407,12 +408,9 @@ export function sendRegistrationToken(token) {
     .then(response => response.json()) // TODO(rohan): error-check the response
     .then(json => {
     	if (!json.error) {
-    		console.log("token registered: " + token);
 	      	store.dispatch({
 	        	type: "TOKEN_REGISTERED"
         	});
-    	} else {
-    		console.log("token not registered: " + token);
     	}
 	});
 }
@@ -424,7 +422,7 @@ export function unregisterAToken() {
 	            userVisibleOnly: true
 	        }).then(function(sub) {
 	        	// TODO: unregister token on client side
-	            sendRegistrationTokenForDeletion(sub.endpoint.substring(40))
+	            sendRegistrationTokenForDeletion(sub.toJSON())
 	        });
 	    }).catch(function(error) {
 	        console.log(':^(', error);
