6f46c45607680d92089628759906f4a68710cd94
Max Yeo <mzwyeo@gmail.com>
2016-10-24 19:13:05 -0400

    removed console logs and added pywebpush to requirements.txt

--- static/js/redux/actions/user_actions.jsx
+++ static/js/redux/actions/user_actions.jsx
@@ -249,11 +249,11 @@ export function setARegistrationToken() {
 	        reg.pushManager.subscribe({
 	            userVisibleOnly: true
 	        }).then(function(sub) {
-	        	console.log(sub);
+	        	// console.log(sub);
 	            sendRegistrationToken(sub.toJSON());
 	        });
 	    }).catch(function(error) {
-	        console.log(':^(', error);
+	        // console.log(':^(', error);
 	    });
 	}
 }
@@ -272,7 +272,7 @@ export function isRegistered() {
 				}
 			})
 	    }).catch(function(error) {
-	        console.log(':^(', error);
+	        // console.log(':^(', error);
 	    });
 	}
 }
@@ -305,7 +305,7 @@ export function unregisterAToken() {
 	            sendRegistrationTokenForDeletion(sub.toJSON())
 	        });
 	    }).catch(function(error) {
-	        console.log(':^(', error);
+	        // console.log(':^(', error);
 	    });
 	}
 }
@@ -321,12 +321,12 @@ export function sendRegistrationTokenForDeletion(token) {
     .then(response => response.json()) // TODO(rohan): error-check the response
     .then(json => {
         if (!json.error) {
-            console.log("token deleted: " + token);
+            // console.log("token deleted: " + token);
             store.dispatch({
                 type: "UNREGISTER_TOKEN"
             });
         } else {
-            console.log("token not deleted: " + token);
+            // console.log("token not deleted: " + token);
         }
     });
 }
