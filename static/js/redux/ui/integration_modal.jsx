5a97dd5adfe426a6ce042c748e6f7e207d447d03
Max Yeo <mzwyeo@gmail.com>
2016-10-27 22:42:22 -0400

    starting pilot modal styling

--- static/js/redux/ui/integration_modal.jsx
+++ static/js/redux/ui/integration_modal.jsx
@@ -49,6 +49,9 @@ export class IntegrationModal extends React.Component {
         let integrationLogo = {
             backgroundImage: 'url(/static/img/integrations/pilotLogo.png)'
         };
+        let integrationLogo = {
+            backgroundImage: 'url(/static/img/integrations/pilot-logo.png)'
+        }
         return (
             <Modal ref="modal"
                 className="integration-modal narrow-modal"
