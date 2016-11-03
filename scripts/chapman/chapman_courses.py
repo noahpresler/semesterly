992319b584ea42e2149eec62a9262e760765442f
Michael N. Miller <mmill179@jhu.edu>
2016-11-03 01:12:23 -0400

    fix for large class lookup

--- scripts/chapman/chapman_courses.py
+++ scripts/chapman/chapman_courses.py
@@ -18,7 +18,8 @@ class ChapmanParser(PeopleSoftParser):
 
 	def parse(self):
 		# NOTE: hardcoded semesters Fall, Interim, Spring 2016-2017
-		terms = {'F':'2168', 'I':'2172', 'S':'2174'}
+		terms = {'S':'2174'}
+		# terms = {'F':'2168', 'I':'2172', 'S':'2174'}
 		PeopleSoftParser.parse(self, terms)
 
 def main():
