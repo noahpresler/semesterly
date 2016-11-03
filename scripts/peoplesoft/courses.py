74ef54c5246a71f007669843192ff78a9e41cb74
Michael N. Miller <mmill179@jhu.edu>
2016-11-03 15:33:56 -0400

    peoplesoft parser update: reorganize request order to accomodate dynamic content per term

--- scripts/peoplesoft/courses.py
+++ scripts/peoplesoft/courses.py
@@ -89,21 +89,6 @@ class PeopleSoftParser:
 		search_query['ICAction'] = 'DERIVED_CLSRCH_SSR_EXPAND_COLLAPS$149$$1'
 		soup = BeautifulSoup(self.post_http(self.base_url, search_query).text, 'html.parser')
 
-		# search_query = {a['name']: a['value'] for a  in soup.find('div', id=re.compile(r'win\ddivPSHIDDENFIELDS')).find_all('input')}
-		search_query['ICAction'] = 'CLASS_SRCH_WRK2_SSR_PB_CLASS_SRCH'
-		search_query['SSR_CLSRCH_WRK_SSR_OPEN_ONLY$chk$4'] = 'N'
-		for day in ['MON', 'TUES', 'WED', 'THURS', 'FRI', 'SAT', 'SUN']:
-			search_query['SSR_CLSRCH_WRK_' + day + '$5'] = 'Y'
-			search_query['SSR_CLSRCH_WRK_' + day + '$chk$5'] = 'Y'
-		search_query['SSR_CLSRCH_WRK_INCLUDE_CLASS_DAYS$5'] = 'J'
-		search_query[soup.find('select', id=re.compile(r'SSR_CLSRCH_WRK_INSTRUCTION_MODE\$\d'))['id']] = 'P'
-
-		# extract search query info
-		options = soup.find('select', id=re.compile(r'SSR_CLSRCH_WRK_SUBJECT_SRCH\$\d'))
-		search_id = options['id']
-		departments = options.find_all('option')[1:]
-		# NOTE: first element of dropdown lists in search area is empty
-
 		# TODO - necessary clutter
 		self.course_cleanup()
 
@@ -113,8 +98,29 @@ class PeopleSoftParser:
 
 			self.course['semester'] = term
 
+			# virtually refined search (to get around min param requirement)
+			search_query['SSR_CLSRCH_WRK_SSR_OPEN_ONLY$chk$4'] = 'N'
+			for day in ['MON', 'TUES', 'WED', 'THURS', 'FRI', 'SAT', 'SUN']:
+				search_query['SSR_CLSRCH_WRK_' + day + '$5'] = 'Y'
+				search_query['SSR_CLSRCH_WRK_' + day + '$chk$5'] = 'Y'
+			search_query['SSR_CLSRCH_WRK_INCLUDE_CLASS_DAYS$5'] = 'J'
+			search_query[soup.find('select', id=re.compile(r'SSR_CLSRCH_WRK_INSTRUCTION_MODE\$\d'))['id']] = 'P'
+
 			# update search payload with term as parameter
 			search_query['CLASS_SRCH_WRK2_STRM$35$'] = terms[term]
+			search_query['ICAJAX'] = '1'
+			search_query['ICNAVTYPEDROPDOWN'] = '0'
+			search_query['ICAction'] = 'CLASS_SRCH_WRK2_STRM$35$'
+			soup = BeautifulSoup(self.post_http(self.base_url, search_query).text, 'lxml')
+
+			# TODO - this might not be necessary
+			del search_query['ICAJAX']
+			del search_query['ICNAVTYPEDROPDOWN']
+
+			# extract department query list
+			options = soup.find('select', id=re.compile(r'SSR_CLSRCH_WRK_SUBJECT_SRCH\$\d'))
+			search_id = options['id']
+			departments = options.find_all('option')[1:] # NOTE: first element of dropdown lists in search area is empty
 
 			for department in departments:
 
@@ -127,6 +133,9 @@ class PeopleSoftParser:
 
 				# Update search payload with department code
 				search_query[search_id] = department['value']
+
+				# update search action to get course list
+				search_query['ICAction'] = 'CLASS_SRCH_WRK2_SSR_PB_CLASS_SRCH'
 
 				# Get course listing page for department
 				soup = BeautifulSoup(self.post_http(self.base_url, search_query).text, 'html.parser')
