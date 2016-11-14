13036c489936af3470bcd1c379d86b32c394070b
Michael N Miller <mnm364@gmail.com>
2016-11-13 20:07:48 -0500

    added etc/hosts modification and protected commands from overwriting existsing files etc.

--- init_setup.sh
+++ init_setup.sh
@@ -15,14 +15,11 @@ echo "IMPORTANT: make sure you are in the home directory of the Semesterly proje
 echo "NOTE: view configurable variables in script file setup.sh"
 read -p "Press any key to continue (^C to quit)..." -n1 -s
 
-if [ -e ${SEMESTERLY_HOME}/semesterly/local_settings.py ]; then
-	echo -e "\nexiting setup: ${SEMESTERLY_HOME}/semesterly/local_settings.py already exists"
-	return 1
-fi
-
 echo -e "\nSTATUS: starting basic development environment setup :-)"
 
 ######################################
+
+sudo apt update && sudo apt upgrade
 
 sudo apt install -y \
 	python-dev \
@@ -30,19 +27,23 @@ sudo apt install -y \
 	python3-dev \
 	python3-pip \
 	libpq-dev \
-	postgresql-devel \
 	libxml2-dev \
 	libxslt1-dev \
 	postgresql-client-common \
-	postgresql-contrib
+	postgresql-contrib \
+	libssl-dev \
+	libffi-dev \
+	build-essential \
+	npm
+# postgresql-devel \ # NOTE: not sure if this is needed...?
 
 sudo -H pip install --upgrade pip
-sudo -H pip install virtualenv virtualenvwrapper
 
 # Add the following to the bottom of your ~/.bashrc
 if cat ~/.bashrc | grep -q 'virtualenvwrapper'; then
-	: # do nothing
+	echo "skipping: python virtual environment setup, already exists"
 else
+	sudo -H pip install virtualenv virtualenvwrapper
 	echo ". /usr/local/bin/virtualenvwrapper.sh" >> ${HOME}/.bashrc
 fi
 
@@ -51,21 +52,25 @@ workon "${VIRTUALENV_NAME}"
 pip install -r ${SEMESTERLY_HOME}/requirements.txt
 
 # Setup django database
-read -r -d '' PSQL_COMMANDS << EOF
+read -r -d '' PSQL_COMMANDS <<- EOM
 CREATE DATABASE ${DATABASE_NAME};
 CREATE USER ${DATABASE_USER_NAME} WITH PASSWORD '${DATABASE_USER_PASS}';
 ALTER ROLE ${DATABASE_USER_NAME} SET client_encoding TO 'utf8';
 ALTER ROLE ${DATABASE_USER_NAME} SET default_transaction_isolation TO 'read committed';
 ALTER ROLE ${DATABASE_USER_NAME} SET timezone TO 'UTC';
 GRANT ALL PRIVILEGES ON DATABASE ${DATABASE_NAME} TO ${DATABASE_USER_NAME};
-EOF
+EOM
 
 while read -r cmd; do
 	sudo -u postgres -H -- psql -c "${cmd}"
 done <<< "${PSQL_COMMANDS}"
 unset PSQL_COMMANDS
 
-cat > ${SEMESTERLY_HOME}/semesterly/local_settings.py << EOF
+if [ -e ${SEMESTERLY_HOME}/semesterly/local_settings.py ]; then
+	: # do nothing
+	# echo -e "\nexiting setup: ${SEMESTERLY_HOME}/semesterly/local_settings.py already exists"
+else
+	cat > ${SEMESTERLY_HOME}/semesterly/local_settings.py <<- EOM
 # Database
 # https://docs.djangoproject.com/en/1.6/ref/settings/#databases
 # SECURITY WARNING: don't run with debug turned on in production!
@@ -81,11 +86,35 @@ DATABASES = {
         'PORT': '5432',
     }
 }
-EOF
+EOM
+
+fi
+
 
 # Do first migrations
 python manage.py makemigrations
 python manage.py migrate
+
+# Establish local DNS lookup for local server
+DNSURLS="
+	sem.ly
+	jhu.sem.ly
+	uoft.sem.ly
+	queens.sem.ly
+	vandy.sem.ly
+	umd.sem.ly
+	chapman.sem.ly
+	pennstate.sem.ly
+	gw.sem.ly
+	umich.sem.ly"
+for url in ${DNSURLS//\\n/ }
+do
+	if cat /etc/hosts | grep -q "${url}"; then
+		: # do nothing
+	else
+		echo "127.0.0.1    ${url}" | sudo tee -a /etc/hosts
+	fi
+done
 
 echo "STATUS: finished basic development environment setup :-)"
 
@@ -93,15 +122,15 @@ echo "STATUS: finished basic development environment setup :-)"
 
 # Setup amazon textbooks for parsing
 if [ -e ${HOME}/.amazon-product-api ]; then
-	echo -e "\nexiting extended setup: ~/.amazon-product-api already exists."
-	return 0
-fi
-cat > ${HOME}/.amazon-product-api << EOF
+	echo -e "\nskipping: ~/.amazon-product-api already exists."
+else
+cat > ${HOME}/.amazon-product-api <<- EOM
 [Credentials]
 access_key = AKIAJGUOXN3COOYBPTHQ
 secret_key = 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
 associate_tag = semesterly-20
-EOF
+EOM
+fi
 
 ######################################
 
