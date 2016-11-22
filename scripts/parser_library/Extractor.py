3ba4ff10eab5defd3f4b24fe2167bc3efaef31d7
mnm364 <mnm364@gmail.com>
2016-11-22 14:37:30 -0500

    tabs to spaces

--- scripts/parser_library/Extractor.py
+++ scripts/parser_library/Extractor.py
@@ -1,49 +1,49 @@
 def time_12to24(time12):
-	''' Attempts to convert 12hr time to 24hr time
-	
-	Args:
-		time12 (str): 12 hour time format
+    ''' Attempts to convert 12hr time to 24hr time
 
-	Returns:
-		str: 24 hr time in format hrhr:minmin
-	'''
-	# Regex extract
-	match = re.match("(\d*):(\d*)(.)", time12)
+    Args:
+        time12 (str): 12 hour time format
 
-	# Transform to 24 hours
-	hours = int(match.group(1))
-	if re.search(r'[pP]', match.group(3)):
-		hours = (hours%12)+12
+    Returns:
+        str: 24 hr time in format hrhr:minmin
+    '''
+    # Regex extract
+    match = re.match("(\d*):(\d*)(.)", time12)
 
-	# Return as 24hr-time string
-	return str(hours) + ":" + match.group(2)
+    # Transform to 24 hours
+    hours = int(match.group(1))
+    if re.search(r'[pP]', match.group(3)):
+        hours = (hours%12)+12
+
+    # Return as 24hr-time string
+    return str(hours) + ":" + match.group(2)
 
 def extract_info(course, text):
-	''' Attempts to extract info from text and put it into course object.
+    ''' Attempts to extract info from text and put it into course object.
 
-	Args:
-		course (dict): course object to place extracted information into
-		text (str): text to attempt to extract information from
+    Args:
+        course (dict): course object to place extracted information into
+        text (str): text to attempt to extract information from
 
-	Returns:
-		* modifies course object
-		str: the text trimmed of extracted information
-	'''
-	text = text.encode('utf-8', 'ignore')
+    Returns:
+        * modifies course object
+        str: the text trimmed of extracted information
+    '''
+    text = text.encode('utf-8', 'ignore')
 
-	extractions = {
-		'prereqs' : r'[Pp]r(?:-?)e[rR]eq(?:uisite)?(?:s?)[:,\s]\s*(.*?)(?:\.|$)\s*',
-		'coreqs'  : r'[Cc]o(?:-?)[rR]eq(?:uisite)?(?:s?)[:,\s]\s*(.*?)(?:\.|$)\s*',
-		'geneds' : r'(GE .*)'
-	}
+    extractions = {
+        'prereqs' : r'[Pp]r(?:-?)e[rR]eq(?:uisite)?(?:s?)[:,\s]\s*(.*?)(?:\.|$)\s*',
+        'coreqs'  : r'[Cc]o(?:-?)[rR]eq(?:uisite)?(?:s?)[:,\s]\s*(.*?)(?:\.|$)\s*',
+        'geneds' : r'(GE .*)'
+    }
 
-	for ex in extractions:
-		rex = re.compile(extractions[ex])
-		extracted = rex.search(text)
-		if extracted:
-			if len(course[ex]) > 0:
-				course[ex] += ', '
-			course[ex] += extracted.group(1) # okay b/c of course_cleanup
-		text = rex.sub('', text).strip()
+    for ex in extractions:
+        rex = re.compile(extractions[ex])
+        extracted = rex.search(text)
+        if extracted:
+            if len(course[ex]) > 0:
+                course[ex] += ', '
+            course[ex] += extracted.group(1) # okay b/c of course_cleanup
+        text = rex.sub('', text).strip()
 
-	return text
+    return text
