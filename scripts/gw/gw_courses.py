ffa5f53bc42ef3a7e260bd3d47cf76859b1c5b2d
Michael N Miller <mnm364@gmail.com>
2016-11-27 14:42:33 -0500

    extract additional info from catalog entry page, rudimentary scraper/extractor design implemented

--- scripts/gw/gw_courses.py
+++ scripts/gw/gw_courses.py
@@ -4,7 +4,8 @@
 # @date 11/26/16
 
 import re, sys
-from bs4 import BeautifulSoup
+from bs4 import BeautifulSoup, NavigableString, Tag
+from django.utils.encoding import smart_str, smart_unicode
 
 # parser library
 from scripts.textbooks.amazon import make_textbook
@@ -81,7 +82,7 @@ class GWParser:
 			# get list of departments
 			depts = {dept.text.strip(): dept['value'] for dept in soup.find('select', {'id' : 'subj_id'}).find_all('option')}
 
-			for dept_name, dept_code in depts.iteritems():
+			for dept_name, dept_code in {'Public Health':'PUBH'}.iteritems(): #depts.iteritems():
 				print '>> Parsing courses in department', dept_name
 
 				search_params['sel_subj'] = ['dummy', dept_code]
@@ -91,6 +92,8 @@ class GWParser:
 					rows = rows.find_all('tr')[2:]
 				else:
 					continue
+
+				print 'here'
 
 				# collect offered courses in department
 				for row in rows:
@@ -132,7 +135,9 @@ class GWParser:
 
 						catalog = self.requester.get(self.url + '/PRODCartridge/bwckctlg.p_display_courses', params=catalog_query)
 						if catalog:
-							self.course['descr'] = self.extract_description(catalog)
+							self.course.update(self.parse_catalogentry_page(catalog))
+							# self.course['descr'] = self.extract_description(catalog)
+							# self.scrape_attributes(catalog)
 
 						section_query = {
 							'term_in':term_code,
@@ -143,11 +148,13 @@ class GWParser:
 
 						section = self.requester.get(self.url + '/PRODCartridge/bwckschd.p_disp_listcrse', params=section_query)
 
+						# areas = self.scrape_attributes(section)
+
+						course = self.course.create_course()
+
 						# parse scheduling info
 						meeting_times = GWParser.extract_meeting_times(section)
 
-						# self.print_course()
-						course = self.course.create_course()
 
 						# collect instr info
 						self.course['instrs'] = ', '.join((mt.find_all('td')[6].text for mt in meeting_times))
@@ -168,14 +175,96 @@ class GWParser:
 							self.course.create_offerings(section)
 		self.course.wrap_up()
 
-	def extract_description(self, soup):
+	def parse_catalogentry_page(self, soup):
+		''' Attempts to scrape information from the (at best) ill-formatted catalog entry page.
+			Includes: description, levels, areas, types, attributes
+
+		Args:
+			soup (bs4): table on catalog entry page
+		'''
+		fields = {}
 		try:
-			soup = soup.find('body').find('table', {'class':'datadisplaytable'}).find_all('tr', recursive=False)[1].find('td')
-			descr = re.match(r'<td .*?>\n([^<]+)<[^$]*</td>', soup.prettify())
-			return ' '.join(descr.group(1).strip().splitlines()) if descr else ''
+			meat = soup.find('body').find('table', {'class':'datadisplaytable'})
+
+			fields.update(self.extract_description(meat))
+
+			fields.update(self.extract_otherinfo(meat))
+
 		except AttributeError:
-			sys.stderr.write('Unexpected error: at extract description for course \n' + str(self.course))
+			sys.stderr.write('Unexpected error: at catalog entry parse for course \n' + str(self.course))
 			return ''
+
+	def extract_description(self, soup):
+		return {'descr': self.scrape_description(soup)}
+
+	def scrape_description(self, soup):
+		try:
+			soup = soup.find_all('tr', recursive=False)[1].find('td')
+			descr = re.match(r'<td .*?>\n([^<]+)<[^$]*</td>', soup.prettify())
+			if descr:
+				return ' '.join(descr.group(1).strip().splitlines())
+			else:
+				return '' # no description
+		except AttributeError:
+			sys.stderr.write('Unexpected error: at catalog entry description extraction for course \n' + str(self.course))
+			return ''
+
+	def extract_otherinfo(self, soup):
+		fields = self.scrape_otherinfo(soup)
+		extracted = {}
+		extraction = {
+			'Schedule Types': ('section_type', lambda s: smart_str(s)[0].upper()),
+			'Levels' : ('info', lambda s: 'Levels: ' + smart_str(s).strip()), # NOTE: this needs to be changed in db
+			'Course Attributes': ('areas', lambda x: smart_str(x).strip())
+		}
+
+		for name, data in fields.items():
+			if extraction.get(name):
+				extracted[extraction[name][0]] = extraction[name][1](data)
+
+		return extracted
+
+	def scrape_otherinfo(self, soup):
+		''' Attempts to scrape additional information from ill-formated course catalog entry page.
+			Includes: levels, areas, types, attributes
+
+		Args:
+			soup (bs4): table on catalog entry page
+
+		Examples:
+			...
+			 <span class="fieldlabeltext">
+			  Levels:
+			 </span>
+			 Non-Degree, Undergraduate, Graduate, Consortium, Medicine, Alumni Auditor
+			 <br/>
+			 <span class="fieldlabeltext">
+			  Schedule Types:
+			 </span>
+			 <a href="/PRODCartridge/bwckctlg.p_disp_listcrse?term_in=201701&amp;subj_in=PUBH&amp;crse_in=1101&amp;schd_in=L">
+			  Lecture
+			 </a>
+			...
+			 <span class="fieldlabeltext">
+			  Course Attributes:
+			 </span>
+			 <br/>
+			 ESIA-Global PH Conc (2010), ESIA-Global PH Conc (Pre 2010), Sust Minor: Green Leaf Track C, Sustainability Courses
+			 <br/>
+			 <br/>
+			</td>
+		'''
+		meat = soup.find('td', {'class':'ntdefault'})
+		fields = {}
+		# link field in <span> tag to text proceeding it
+		for t in meat.find_all('span', {'class':'fieldlabeltext'}):
+			data = t.next_sibling
+			while data and isinstance(data, Tag) and data.name == 'br':
+				data = data.next_sibling
+			if not isinstance(data,NavigableString):
+				data = data.text
+			fields[t.text[:-2]] = data
+		return fields
 
 	@staticmethod
 	def extract_meeting_times(soup):
