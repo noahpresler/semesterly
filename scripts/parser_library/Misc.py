e498bd60abf93c8865481760a1a19fb195524810
Michael N Miller <mnm364@gmail.com>
2016-11-27 12:31:43 -0500

    added general safe cast method

--- scripts/parser_library/Misc.py
+++ scripts/parser_library/Misc.py
@@ -3,14 +3,8 @@
 # @author   Michael N. Miller
 # @date     11/26/16
 
-def tofloat(subject, default=None):
+def safe_cast(val, to_type, default=None):
 	try:
-		return float(subject)
-	except ValueError:
-		return default
-
-def toint(subject, default=None):
-	try:
-		return int(subject)
-	except ValueError:
+		return to_type(val)
+	except (ValueError, TypeError):
 		return default
