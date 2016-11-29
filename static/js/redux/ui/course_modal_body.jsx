64ac1956f0a5587d81d6852b09e1ade268b6e8ae
Noah Presler <noah@presler.me>
2016-11-29 15:20:59 -0500

    add similar courses back to side bar

--- static/js/redux/ui/course_modal_body.jsx
+++ static/js/redux/ui/course_modal_body.jsx
@@ -243,7 +243,6 @@ export class CourseModalBody extends React.Component {
                             <EvaluationList evalInfo={evalInfo} />
                         </div>
                         {textbooksDisplay}
-                        {similarCourses}
                         
                     </div>
                     <div id="modal-section-lists"
@@ -251,6 +250,7 @@ export class CourseModalBody extends React.Component {
                         {lectureSections}
                         {tutorialSections}
                         {practicalSections}
+                        {similarCourses}
                     </div>
                 </div>
             </div>
