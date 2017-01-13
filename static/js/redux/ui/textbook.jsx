9b3b4a58d3cfc8f7255788509600a34a305ccc33
mnm364 <mnm364@gmail.com>
2017-01-13 12:27:51 -0500

    cleaned up methods

--- static/js/redux/ui/textbook.jsx
+++ static/js/redux/ui/textbook.jsx
@@ -1,29 +1,29 @@
 import React from 'react';
 
 const Textbook = ({tb}) => {
-    const title = !tb.title || tb.title.length === 0 || tb.title === "Cannot be found" ? tb.isbn : tb.title;
+    const exists = (subject) => subject && subject.length > 0 && subject != "Cannot be found";
+
+    const title = exists(tb.title) ? tb.title : tb.isbn;
 
     let image = null;
-    if (!tb.image_url || tb.image_url.length <= 0 || tb.image_url === "Cannot be found") {
-        image = <img src="/static/img/emptystates/single_textbook.png" style={{opacity: 0.5}}/>;
-    } else {
+    if (exists(tb.image_url)) {
         image = <img src={tb.image_url} />;
+    } else {
+        image = <img src="/static/img/emptystates/single_textbook.png" style={{opacity: 0.5}}/>;
     }
+
     return (
         <a href={tb.detail_url} target="_blank" className="textbook-ctn">
             <div className="textbook">
                 {image}
                 <div className="required">Required</div>
                 <h4>{title}</h4>
-                <div className="amazon-buy"><i className="fa fa-amazon" aria-hidden="true"></i> Buy or Rent</div>
+                {exists(tb.detail_url) && 
+                    <div className="amazon-buy"><i className="fa fa-amazon" aria-hidden="true"></i> Buy or Rent</div>
+                }
             </div>
         </a>
     );
 }
 
 export default Textbook;
-
-// {tb.detail_url.length > 0 ?
-//                 <div className="amazon-buy"><i className="fa fa-amazon" aria-hidden="true"></i> Buy or Rent</div>
-//                 : <div></div>
-//             }
