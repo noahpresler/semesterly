a7cb715ca71d40d6a48bd15fab8e6815f2e50675
Michael N Miller <mnm364@gmail.com>
2017-01-13 09:56:51 -0500

    setup formal json schema definitions for config.json

---
+++ scripts/parser_library/Validator.py
@@ -0,0 +1,32 @@
+# @what     Parsing library - Validator
+# @org      Semeseter.ly
+# @author   Michael N. Miller
+# @date     1/12/17
+
+# https://tools.ietf.org/html/draft-fge-json-schema-validation-00#section-5.4.3
+# http://json-schema.org/
+# https://spacetelescope.github.io/understanding-json-schema/reference/combining.html
+
+import json, jsonschema, argparse
+
+class Validator:
+	def __init__(dir):
+		self.dir = dir
+
+	def validate():
+		pass
+
+	def directory():
+		pass
+
+def get_args():
+	pass
+
+def check_args(args):
+	pass
+
+def main():
+	pass
+
+if __name__ == '__main__':
+	main()
