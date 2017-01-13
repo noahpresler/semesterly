84e82ac110f0598a6eac1bb190dd36a183365dc3
Noah Presler <noah@presler.me>
2017-01-13 12:00:05 -0500

    Fixing image.url etc

--- static/js/redux/ui/textbook.jsx
+++ static/js/redux/ui/textbook.jsx
@@ -4,12 +4,11 @@ const Textbook = ({tb}) => {
     const title = !tb.title || tb.title.length === 0 || tb.title === "Cannot be found" ? tb.isbn : tb.title;
 
     let image = null;
-    if (!tb.image.url || tb.image_url.length) {
-        image = <img src={tb.image_url} />;
-    } else {
+    if (!tb.image_url || tb.image_url.length <= 0 || tb.image_url === "Cannot be found") {
         image = <img src="/static/img/emptystates/single_textbook.png" style={{opacity: 0.5}}/>;
+    } else {
+        image = <img src={tb.image_url} />;
     }
-    console.log("HERE",tb)
     return (
         <a href={tb.detail_url} target="_blank" className="textbook-ctn">
             <div className="textbook">
