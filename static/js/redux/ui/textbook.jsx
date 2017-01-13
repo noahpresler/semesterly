4c041ae126644f77fbef483ca71ea06fd7297bcc
mnm364 <mnm364@gmail.com>
2017-01-13 11:38:49 -0500

    fixing styles inline

--- static/js/redux/ui/textbook.jsx
+++ static/js/redux/ui/textbook.jsx
@@ -7,18 +7,18 @@ const Textbook = ({tb}) => {
     if (!tb.image.url || tb.image_url.length) {
         image = <img src={tb.image_url} />;
     } else {
-        image = <img src="/static/img/emptystates/single_textbook.png" opacity={0.5} />;
+        image = <img src="/static/img/emptystates/single_textbook.png" style={{opacity: 0.5}}/>;
     }
-
+    console.log("HERE",tb)
     return (
-    <a href={tb.detail_url} target="_blank" className="textbook-ctn">
-        <div className="textbook">
-            {image}
-            <div className="required">Required</div>
-            <h4>{title}</h4>
-            <div className="amazon-buy"><i className="fa fa-amazon" aria-hidden="true"></i> Buy or Rent</div>
-        </div>
-    </a>
+        <a href={tb.detail_url} target="_blank" className="textbook-ctn">
+            <div className="textbook">
+                {image}
+                <div className="required">Required</div>
+                <h4>{title}</h4>
+                <div className="amazon-buy"><i className="fa fa-amazon" aria-hidden="true"></i> Buy or Rent</div>
+            </div>
+        </a>
     );
 }
 
