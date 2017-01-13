f76349761f69a196f4d48889129648ae1e21daf5
Michael N Miller <mnm364@gmail.com>
2017-01-13 15:41:05 -0500

    cleaner code, added isbn tag

--- static/js/redux/ui/textbook.jsx
+++ static/js/redux/ui/textbook.jsx
@@ -1,16 +1,21 @@
 import React from 'react';
 
 const Textbook = ({tb}) => {
-    const exists = (subject) => subject && subject.length > 0 && subject != "Cannot be found";
-
-    const title = exists(tb.title) ? tb.title : tb.isbn;
+    const exists = (x) => x && x.length > 0 && x != "Cannot be found";
 
     let image = null;
     if (exists(tb.image_url)) {
         image = <img src={tb.image_url} />;
     } else {
-        image = <img src="/static/img/emptystates/single_textbook.png" style={{opacity: 0.5}}/>;
+        const empty_image_uri = "/static/img/emptystates/single_textbook.png"; 
+        image = <img src={empty_image_uri} style={{opacity: 0.5}}/>;
     }
+
+    const linked = (children) => (
+        <a href={tb.detail_url} target="_blank">
+            {children}
+        </a>
+    );
 
     const textbook = (
         <div className="textbook">
@@ -18,25 +23,23 @@ const Textbook = ({tb}) => {
                 {image}
             </div>
             <div className="required">Required</div>
-            <h4>{title}</h4>
-            {exists(tb.detail_url) && 
-                <div className="amazon-buy"><i className="fa fa-amazon" aria-hidden="true"></i> Buy or Rent</div>
+            <h4>
+                {exists(tb.title) ? tb.title : "isbn: " + tb.isbn}
+            </h4>
+            { exists(tb.detail_url) && 
+                <div className="amazon-buy">
+                    <i className="fa fa-amazon" aria-hidden="true"></i>
+                    Buy or Rent
+                </div>
             }
         </div>
     );
 
-    let ret = null;
-    if (exists(tb.detail_url)) {
-        ret = (
-            <a href={tb.detail_url} target="_blank" className="textbook-ctn">
-                {textbook}      
-            </a>
-        );
-    } else {
-        ret = textbook;
-    }
-
-    return ret;
+    return (
+        <div className="textbook-ctn">
+            {exists(tb.detail_url) ? linked(textbook): textbook}
+        </div>
+    );
 }
 
 export default Textbook;
