c932dbe772108b28428fe1483b94a481b830b4f3
Michael N Miller <mnm364@gmail.com>
2017-01-16 10:50:51 -0500

    directory changes, tester checks for duplicate entries

--- scripts/parser_library/tester.py
+++ scripts/parser_library/tester.py
@@ -1,26 +1,32 @@
+#!/usr/bin/env python2
+
 import os, json, jsonschema
 
-absolute_path_to_base_directory = '/home/mike/Documents/semesterly/scripts/parser_library/'
+def dict_raise_on_duplicates(ordered_pairs):
+    """Reject duplicate keys."""
+    d = {}
+    for k, v in ordered_pairs:
+        if k in d:
+           raise KeyError("duplicate key: %r" % (k,))
+        else:
+           d[k] = v
+    return d
+
+absolute_path_to_base_directory = '/home/mike/Documents/semesterly/scripts/parser_library/schemas/'
 base_filename = 'tester.json'
 
 with open('test01.json') as g:
-	data = json.loads(g.read())
-with open(os.path.join(absolute_path_to_base_directory, base_filename)) as file_object:
-    schema = json.load(file_object)
+	data = json.loads(g.read(), object_pairs_hook=dict_raise_on_duplicates)
+with open(os.path.join(absolute_path_to_base_directory, base_filename)) as f:
+	schema = json.load(f)
 resolver = jsonschema.RefResolver('file://' + absolute_path_to_base_directory + '/', schema)
-jsonschema.Draft4Validator(schema, resolver=resolver).validate(data)
 
-# with open('tester.json') as f:
-# 	schema = f.read()
-# 	with open('test01.json') as g:
-# 		data = g.read()
-# schema_path = 'file:///{0}/'.format(
-#       os.path.dirname(get_file_path(schema)).replace("\\", "/"))
-# resolver = RefResolver(schema_path, schema)
-
-# try:
-# 	jsonschema.validate(json.loads(data), json.loads(schema), resolver=resolver)
-# except jsonschema.ValidationError as e:
-# 	print e.message
-# except jsonschema.SchemaError as e:
-# 	print e
+try:
+	jsonschema.Draft4Validator(schema, resolver=resolver).validate(data)
+except jsonschema.ValidationError as e:
+	print e.message
+	print e.path
+	print e.instance
+except jsonschema.exceptions.SchemaError as e:
+	print 'SCHEMA FAILED'
+	print e
