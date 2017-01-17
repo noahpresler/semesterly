2cb266e44090febe1cc149b32ed195b52caaa06d
Michael N Miller <mnm364@gmail.com>
2017-01-17 18:24:26 -0500

    'dopple' evaluations display last name of prof - #360

--- static/js/redux/ui/evaluation.jsx
+++ static/js/redux/ui/evaluation.jsx
@@ -17,14 +17,39 @@ class Evaluation extends React.Component {
 				<div id="prof"><b>Professor: {evalData.professor}</b></div>
 			);
 		}	
- 		
+
+		// extract last name (if present)
+		const shortProfName = (name) => {
+			if (!name)
+				return '';
+
+			const wrapParen = (s) => ' (' + s + ')';
+
+			const words = name.split(/\s+/).filter(n => n != "");
+
+			if (words.length == 0) {
+				return '';
+			} else if (words.length >= 2) {
+				return wrapParen(words[1])
+			} else {
+				return wrapParen(words[0]);
+			}
+		};
+
 		let year = evalData.year.indexOf(":") > -1 ? 
 		evalData.year.replace(":", " ") :
 		evalData.year;
 		return (
 		<div className={classnames("eval-item", {"mini": this.props.mini, "selected": !this.props.mini})}>
 			<div className="eval-wrapper">
-				<div className="year"><b>{year}</b></div>
+				<div className="year">
+					<b>
+						{year}
+						{evalData.dopple &&
+							shortProfName(evalData.professor)
+						}
+					</b>
+				</div>
 				{prof}
 				<div className="rating-wrapper">
 					<div className="star-ratings-sprite eval-stars">
