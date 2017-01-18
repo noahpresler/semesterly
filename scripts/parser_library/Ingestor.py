b696770385d8f6ca9d3c45404d04a61ed84b42bb
Michael N Miller <mnm364@gmail.com>
2017-01-18 16:04:26 -0500

    peoplesoft emits formatted json, not validated yet

--- scripts/parser_library/Ingestor.py
+++ scripts/parser_library/Ingestor.py
@@ -4,153 +4,187 @@
 # @date     1/13/17
 
 import json
+from pygments import highlight, lexers, formatters, filters
 
 class Ingestor:
 
-    # TODO - abstract dictionary methods into ABC
-    def __init__(self, school):
-        self.map = {}
-        self.school = school
+	# TODO - abstract dictionary methods into ABC
+	def __init__(self, school):
+		self.map = {}
+		self.school = school
 
-    def __setitem__(self, key, value):
-        self.map[key] = value
-        return self
+	def __setitem__(self, key, value):
+		self.map[key] = value
+		return self
 
-    def __getitem__(self, key):
-        return self.map[key]
+	def __getitem__(self, key):
+		return self.map[key]
 
-    def __delitem__(self, key):
-        del self.map[key]
+	def __delitem__(self, key):
+		del self.map[key]
 
-    def __contains__(self, key):
-        return key in self.map
+	def __contains__(self, key):
+		return key in self.map
 
-    def __iter__(self):
-        for key in self.map:
-            yield key
+	def __iter__(self):
+		for key in self.map:
+			yield key
 
-    def __len__(self):
-        return len(self.map)
+	def __len__(self):
+		return len(self.map)
 
-    def __str__(self):
-        l = ''
-        for label, value in self.map.items():
-            l += smart_str(label) + ':' + smart_str(value) + '\n'
-        return l
+	def __str__(self):
+		l = ''
+		for label, value in self.map.items():
+			l += smart_str(label) + ':' + smart_str(value) + '\n'
+		return l
 
-    def update(self, other=None, **kwargs):
-        if other is not None:
-            for k, v in other.items(): 
-                self[k] = v
-        for k, v in kwargs.items():
-            self[k] = v
+	def update(self, other=None, **kwargs):
+		if other is not None:
+			for k, v in other.items(): 
+				self[k] = v
+		for k, v in kwargs.items():
+			self[k] = v
 
-    def clear(self):
-        self.map.clear()
-        self.school = ''
+	def clear(self):
+		self.map.clear()
+		self.school = ''
 
-    def create_course(self):
-        ''' Create course json from info in model map.
+	def create_course(self):
+		''' Create course json from info in model map.
 
-        Returns:
-            json object model for a course
-        '''
-        course = {
-            'kind': 'course',
-            'school_code': self.school,
-            'course_code': self.map['code'],
-            'name': self.map['name'],
-            'department': self.map['dept'],
-            'credits': self.map['credits'],
-            'prerequisites': self.map.get('prereqs'),
-            'corequisites': self.map.get('coreqs'),
-            'exclusions': self.map.get('exclusions'),
-            'description': [self.map.get('descr', ''), self.map.get('notes', ''), self.map.get('info', '')],
-            'areas': self.map.get('areas'),
-            'level': self.map.get('level'),
-            'cores': self.map.get('cores'),
-            'geneds': self.map.get('geneds'),
-            'sections': self.map.get('sections')
-        }
-        course = Ingestor.cleandict(course)
-        print json.dumps(course, sort_keys=True, indent=4, separators=(',', ': '))
-        return course
+		Returns:
+			json object model for a course
+		'''
 
-    def create_section(self, course):
-        ''' Create section json object from info in model map. 
+		course = {
+			'kind': 'course',
+			'school': {
+				'code': self.school
+			},
+			'code': self.map['code'],
+			'name': self.map['name'],
+			'department': {
+				'name': self.map.get('dept_name'),
+				'code': self.map.get('dept_code')
+			},
+			'credits': self.map['credits'],
+			'prerequisites': make_list(self.map.get('prereqs')),
+			'corequisites': make_list(self.map.get('coreqs')),
+			'exclusions': make_list(self.map.get('exclusions')),
+			'description': make_list(self.map.get('descr', '')),
+			'areas': make_list(self.map.get('areas')),
+			'level': self.map.get('level'),
+			'cores': make_list(self.map.get('cores')),
+			'geneds': make_list(self.map.get('geneds')),
+			'sections': make_list(self.map.get('sections')),
+			'homepage': self.map.get('homepage'),
+			'fees': self.map.get('fees')
+		}
+		course = Ingestor.cleandict(course)
+		print Ingestor.color_json(json.dumps(course, sort_keys=True, indent=4, separators=(',', ': ')))
+		return course
 
-        Args:
-            course: course info mapping
+	def create_section(self, course):
+		''' Create section json object from info in model map. 
 
-        Returns:
-            json object model for a section
-        '''
-        section = {
-            'kind': 'section',
-            'course_code': course['course_code'],
-            'name': self.map['section'],
-            'term': self.map['term'],
-            'year': self.map.get('year', ''), # NOTE: should be required
-            'instructors': self.map.get('instrs'),
-            'capacity': self.map.get('size'),
-            'enrollment': self.map.get('enrolment'), #NOTE: change to enrollment
-            'waitlist': self.map.get('waitlist', -1),
-            'waitlist_size': self.map.get('waitlist_size', -1),
-            'remaining_seats': self.map.get('remaining_seats'),
-            'type': self.map.get('section_type'),
-            'fees': self.map.get('fees'),
-            'final_exam': self.map.get('final_exam'),
-            'offerings': self.map.get('offerings')
-        }
+		Args:
+			course: course info mapping
 
-        section = Ingestor.cleandict(section)
-        print json.dumps(section, sort_keys=True, indent=4, separators=(',', ': '))
-        return section
+		Returns:
+			json object model for a section
+		'''
+		section = {
+			'kind': 'section',
+			'course': {
+				'code': course['code']
+			},
+			'code': self.map['section'],
+			'term': self.map['term'],
+			'year': self.map.get('year'), # NOTE: should be required
+			'instructors': make_list(self.map.get('instrs')),
+			'capacity': self.map.get('size'),
+			'enrollment': self.map.get('enrolment'), #NOTE: change to enrollment
+			'waitlist': self.map.get('waitlist'),
+			'waitlist_size': self.map.get('waitlist_size'),
+			'remaining_seats': self.map.get('remaining_seats'),
+			'type': self.map.get('section_type'),
+			'fees': self.map.get('fees'),
+			'final_exam': self.map.get('final_exam'),
+			'offerings': self.map.get('offerings')
+		}
 
-    def create_offerings(self, section_model):
-        self.create_combined_offering(section_model)
+		section = Ingestor.cleandict(section)
+		print Ingestor.color_json(json.dumps(section, sort_keys=True, indent=4, separators=(',', ': ')))
+		return section
 
-    def create_combined_offering(self, section):
-        ''' Create offering in database from info in model map.
+	def create_offerings(self, section_model):
+		self.create_meeting(section_model)
 
-        Args:
-            section: section info mapping
+	def create_meeting(self, section):
+		''' Create offering in database from info in model map.
 
-        Returns:
-            json object model for a section
+		Args:
+			section: section info mapping
 
-        '''
+		Returns:
+			json object model for a section
 
-        offering = {
-            'course_code': section['course_code'],
-            'section': section['name'],
-            'days': self.map['days'],
-            'time_start': self.map['time_start'],
-            'time_end': self.map['time_end'],
-            'location': self.map.get('location')
-        }
+		'''
 
-        offering = Ingestor.cleandict(offering)
-        print json.dumps(offering, sort_keys=True, indent=4, separators=(',', ': '))
-        return offering
+		offering = {
+			'kind': 'meeting',
+			'course': section['course'],
+			'section': {
+				'code': section['code']
+			},
+			'days': make_list(self.map.get('days')),
+			'dates': make_list(self.map.get('dates')),
+			'time': {
+				'start': self.map['time_start'],
+				'time': self.map['time_end']
+			},
+			'location': {
+				'where': self.map.get('location'),
+				'building': 'TODO - location is optionally string'
+			}
+		}
 
-    # TODO - close json list
-    def wrap_up(self):
-        ''' Update time updated for school at end of parse. '''
-        update_object, created = Updates.objects.update_or_create(
-                school=self.school,
-                update_field="Course",
-                defaults={'last_updated': datetime.datetime.now()}
-        )
-        update_object.save()
+		offering = Ingestor.cleandict(offering)
+		print Ingestor.color_json(json.dumps(offering, sort_keys=True, indent=4, separators=(',', ': ')))
+		return offering
 
-    @staticmethod
-    def cleandict(d):
-        if not isinstance(d, dict):
-            return d
-        return dict((k, Ingestor.cleandict(v)) for k,v in d.iteritems() if v is not None)
+	# TODO - close json list
+	def wrap_up(self):
+		self.map.clear()
 
-    @staticmethod
-    def DEBUG():
-        # TODO
-        pass
+	@staticmethod
+	def cleandict(d):
+		if not isinstance(d, dict):
+			return d
+		return dict((k, Ingestor.cleandict(v)) for k,v in d.iteritems() if v is not None)
+
+	@staticmethod
+	def DEBUG():
+		# TODO
+		pass
+
+	# color json output of error message
+	@staticmethod
+	def color_json(j):
+		l = lexers.JsonLexer()
+		l.add_filter('whitespace')
+		colorful_json = highlight(unicode(j, 'UTF-8'), l, formatters.TerminalFormatter())
+		return colorful_json
+
+def make_list(l, base_type=basestring):
+	if isinstance(l, base_type):
+		l = [l]
+	return clean_empty(l)
+
+def clean_empty(a):
+	if not a: return None
+	a = filter(None, a)
+	if len(a) == 0:
+		return None
+	return a
