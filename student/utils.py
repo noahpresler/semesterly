7bc253155be8c546485aef17b5d358fdda55a8fa
Noah Presler <noah@presler.me>
2017-01-19 15:10:16 -0500

    Revert "Add import"
    This reverts commit eed62e3a31ea97f9115784c74f7964467da6bcf0.

--- student/utils.py
+++ student/utils.py
@@ -12,7 +12,7 @@ from hashids import Hashids
 from django.core.signing import TimestampSigner, BadSignature, SignatureExpired
 from googleapiclient.discovery import build
 from oauth2client.client import GoogleCredentials
-from django.http import HttpResponse, HttpResponseRedirect
+
 
 DAY_LIST = ['M','T','W','R','F','S','U'];
 hashids = Hashids(salt="x98as7dhg&h*askdj^has!kj?xz<!9")
@@ -21,7 +21,7 @@ def redirect_if_no_refresh_token(backend, response, social, *args, **kwargs):
     if backend.name == 'google-oauth2' and social and \
        response.get('refresh_token') is None and \
        social.extra_data.get('refresh_token') is None:
-        return HttpResponseRedirect('/login/google-oauth2?approval_prompt=force')
+        return redirect('/login/google-oauth2?approval_prompt=force')
 
 def get_google_credentials(student):
   social_user = student.user.social_auth.filter(
