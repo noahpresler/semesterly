5458fc8f45f4f90bab6f9e7bb84881d0eacc36af
Noah Presler <noah@presler.me>
2017-01-19 15:06:09 -0500

    request offline access

--- student/utils.py
+++ student/utils.py
@@ -17,6 +17,12 @@ from oauth2client.client import GoogleCredentials
 DAY_LIST = ['M','T','W','R','F','S','U'];
 hashids = Hashids(salt="x98as7dhg&h*askdj^has!kj?xz<!9")
 
+def redirect_if_no_refresh_token(backend, response, social, *args, **kwargs):
+    if backend.name == 'google-oauth2' and social and \
+       response.get('refresh_token') is None and \
+       social.extra_data.get('refresh_token') is None:
+        return redirect('/login/google-oauth2?approval_prompt=force')
+
 def get_google_credentials(student):
   social_user = student.user.social_auth.filter(
       provider='google-oauth2',
