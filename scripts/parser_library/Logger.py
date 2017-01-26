6eba6d38ba92598e9ca1c00d9f99dd434f790e5a
Michael N Miller <mnm364@gmail.com>
2017-01-26 10:36:04 -0500

    minor modifications of various error messages

--- scripts/parser_library/Logger.py
+++ scripts/parser_library/Logger.py
@@ -8,13 +8,13 @@ class Logger:
  
 	def log(self, type, error, note='', **kwargs):
 		message = {
-			'INVALID_JSON': lambda e: ValidationError.invalid_json_error(e),
-			'SCHEMA_JSON': lambda e: ValidationError.schema_json_error(e),
-			'SCHEMA_DEFINITION': lambda e: ValidationError.schema_definition_error(e),
-			'SUBJECT_JSON': lambda e: ValidationError.subject_json_error(e),
-			'SUBJECT_DEFINITION': lambda e: ValidationError.subject_definition_error(e),
-			'EXTENDED': lambda e: ValidationError.extended_definition_error(e),
-			'UNKNOWN': lambda e: ValidationError.unknown_error(e)
+			# 'INVALID_JSON': lambda e: ValidationError.invalid_json_error(e),
+			# 'SCHEMA_JSON': lambda e: ValidationError.schema_json_error(e),
+			# 'SCHEMA_DEFINITION': lambda e: ValidationError.schema_definition_error(e),
+			# 'SUBJECT_JSON': lambda e: ValidationError.subject_json_error(e),
+			# 'SUBJECT_DEFINITION': lambda e: ValidationError.subject_definition_error(e),
+			# 'EXTENDED': lambda e: ValidationError.extended_definition_error(e),
+			# 'UNKNOWN': lambda e: ValidationError.unknown_error(e)
 		}.get(type)
 
 		if not message:
