012581076ae1bdedfc61bc50f25b5a27b7632600
Michael N Miller <mnm364@gmail.com>
2017-01-26 16:18:49 -0500

    strategized digestor

--- scripts/parser_library/Logger.py
+++ scripts/parser_library/Logger.py
@@ -2,27 +2,27 @@
 # - handles for file
 # - handles for verbosity
 
+from scripts.parser_library.InternalUtils import *
+
 class Logger:
-	def __init__(self, filename):
-		self.file = open(filename, 'w+')
+	def __init__(self, filename=None, quiet=False):
+		self.quiet = quiet
+		if filename:
+			self.file = open(filename, 'w+')
  
-	def log(self, type, error, note='', **kwargs):
-		message = {
-			# 'INVALID_JSON': lambda e: ValidationError.invalid_json_error(e),
-			# 'SCHEMA_JSON': lambda e: ValidationError.schema_json_error(e),
-			# 'SCHEMA_DEFINITION': lambda e: ValidationError.schema_definition_error(e),
-			# 'SUBJECT_JSON': lambda e: ValidationError.subject_json_error(e),
-			# 'SUBJECT_DEFINITION': lambda e: ValidationError.subject_definition_error(e),
-			# 'EXTENDED': lambda e: ValidationError.extended_definition_error(e),
-			# 'UNKNOWN': lambda e: ValidationError.unknown_error(e)
-		}.get(type)
-
-		if not message:
-			print 'TODO - "' + type + '" ERROR NOT DEALT WITH'
-			print error
+	def log_json(self, j):
+		if not j or len(j) == 0:
 			return
+		if not self.quiet:
+			print pretty_json(j)
 
-		message = message(error)
-		self.file.write(str(message))
+	def log_error(self, message, note='', priority=3):
 		print message
+
+	def log(self, error, note='', priority=3):
 		print error
+
+		# message = message(error)
+		# self.file.write(str(message))
+		# print message
+		# print error
