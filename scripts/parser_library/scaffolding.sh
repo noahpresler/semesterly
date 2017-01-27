05a0734fd009907f47d0dad4594b0c1afca579e8
mnm364 <mnm364@gmail.com>
2017-01-27 13:50:19 -0500

    scaffolding script for new school

--- scripts/parser_library/scaffolding.sh
+++ scripts/parser_library/scaffolding.sh
@@ -2,9 +2,32 @@
 
 # Script to create school directory scaffolding
 
-if [[ -z "${SEMESTERLY_HOME}" ]]; then
-  echo "SEMESTERLY_HOME env variable is not set"
-  echo -e "go to semesterly home dir, and run \n\texport SEMESTERLY_HOME=\$(pwd)"
-  echo "Consider adding to ~/.bashrc"
-  exit
+if [ "$1" == "" ]; then
+	echo "usage: ./scaffolding <school_code>"
+		exit
 fi
+
+school="${1}"
+
+if [[ -z "${SEMESTERLY_HOME}" ]]; then
+	>&2 echo -e "error: SEMESTERLY_HOME env variable is not set.\
+		\nGo to semesterly home dir, and run \n\texport SEMESTERLY_HOME=\$(pwd) \
+		\nConsider adding absolute path to ~/.bashrc"
+	exit
+fi
+
+if [ -d "${SEMESTERLY_HOME}/scripts/${school}" ]; then
+	>&2 echo "error: directory already exists for ${SEMESTERLY_HOME}/scripts/${school}"
+	exit
+fi
+
+dir="${SEMESTERLY_HOME}/scripts/${school}"
+mkdir "${dir}"
+touch "${dir}/__init__.py"
+touch "${dir}/config.json"
+mkdir "${dir}/data"
+mkdir "${dir}/logs"
+
+echo "{\"school\": {\"code\": ${school}, \"name\":\"TODO\"}" > ${dir}/config.json
+
+echo -e "Finished creating rudimentary scaffolding for ${school}. Edit ${dir}/config.json"
