4ef991cf0a4b5be66648fa560d22da50a3da8bf4
mnm364 <mnm364@gmail.com>
2017-01-31 14:36:10 -0500

    adding handles to peoplesoft and chapman parser to assist in full system check

--- timetable/management/commands/args_parse.py
+++ timetable/management/commands/args_parse.py
@@ -2,8 +2,8 @@ import os, argparse
 
 def parser_argparser(parser, subparser_class=None):
 	parser.add_argument('--term-and-year', nargs=2, type=str,
-		help='parse for term and year as two args') 
-	parser.add_argument('--department',
+		help='parse for term and year - two args') 
+	parser.add_argument('--department', default='all',
 		help='parse specific department by code')
 	parser.add_argument('-o', '--output', default='stdout', 
 		help='(default: stdout)')
