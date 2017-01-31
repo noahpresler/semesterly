002b9e99a96e71083ad671dd82717eeb735c328c
Michael N Miller <mnm364@gmail.com>
2017-01-31 16:17:32 -0500

    still adding deep handles

--- scripts/chapman/chapman_courses.py
+++ scripts/chapman/chapman_courses.py
@@ -11,10 +11,10 @@ from scripts.peoplesoft.courses import PeopleSoftParser
 
 class ChapmanParser(PeopleSoftParser):
 
-	def __init__(self):
+	def __init__(self, **kwargs):
 		school = 'chapman'
 		url = 'https://cs90prod.chapman.edu/psc/CS90PROD_1/EMPLOYEE/SA/c/COMMUNITY_ACCESS.CLASS_SEARCH.GBL'
-		PeopleSoftParser.__init__(self, school, url)
+		PeopleSoftParser.__init__(self, school, url, kwargs)
 
 	def parse(self):
 		# NOTE: hardcoded semesters Fall, Interim, Spring 2016-2017
@@ -28,7 +28,14 @@ class ChapmanParser(PeopleSoftParser):
 			}
 		}
 
-		if 
+		if 'term_and_year' in self.options:
+			term = self.options['term_and_year'][0]
+			year = self.options['term_and_year'][1]
+			if year not in terms:
+				raise CourseParserError('year %(year)s not defined')
+			if term not in terms[year]:
+				raise CourseParserError('term not defined for year %(year)s')
+			terms = {year: {term: terms[year][term]}}
 
 		PeopleSoftParser.parse(self, terms)
 
