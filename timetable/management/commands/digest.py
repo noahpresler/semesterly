35707737f2481b570fd18484aec70e44a4c1e317
Michael N Miller <mnm364@gmail.com>
2017-02-03 18:33:48 -0500

    digestor cmd line args error on vacous no-diff no-load options

--- timetable/management/commands/digest.py
+++ timetable/management/commands/digest.py
@@ -1,7 +1,11 @@
 from django.core.management.base import BaseCommand, CommandParser, CommandError
+from timetable.management.commands.args_parse import schoollist_argparser, digestor_argparser, validator_argparser, validate_switch_argparser
 
 class Command(BaseCommand):
 	def add_arguments(self, parser):
 		digestor_argparser(parser)
+		validate_switch_argparser(parser)
+		validator_argparser(parser)
 
 	def handle(self, *args, **options):
+		pass
