f5d45e7a34fb4d29695381dd0ce175269b0f8f4f
Eric Calder <ecalder6@jhu.edu>
2017-02-11 16:19:05 -0500

    Backup script: misc. change to tar.

--- backup.sh
+++ backup.sh
@@ -3,6 +3,7 @@ echo "Dumping data into json."
 python manage.py dumpdata --natural-foreign --exclude auth.permission --exclude contenttypes --indent 2 > semesterly_backup/backup.json
 cd semesterly_backup
 echo "Compressing to tar.gz."
+rm backup.tar.gz
 tar -czvf backup.tar.gz backup.json
 echo "Sending backup to staging."
 scp backup.tar.gz staging@semesterly.info:/home/staging/prod_backup
