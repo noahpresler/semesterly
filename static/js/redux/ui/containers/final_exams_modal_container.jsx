c2c976ca89e64bc6d4b3f9250e6d29216b5088f0
Kristin Yim <k.yim33@gmail.com>
2017-02-15 17:32:23 -0500

    Finished connecting front and backend, now displays final exam schedule as text. Just make pretty now

--- static/js/redux/ui/containers/final_exams_modal_container.jsx
+++ static/js/redux/ui/containers/final_exams_modal_container.jsx
@@ -1,18 +1,21 @@
 import { connect } from 'react-redux';
 import { FinalExamsModal } from '../final_exams_modal.jsx';
-import { createiCalfromTimetable, addTTtoGCal } from '../../actions/calendar_actions.jsx';
+import { fetchFinalExamSchedule } from '../../actions/user_actions.jsx';
 
 const mapStateToProps = (state) => {
 	return {
 		isVisible: state.finalExamsModal.isVisible,
-		isDownloading: state.finalExamsModal.isDownloading
+		finalExamSchedule: state.finalExamsModal.finalExams,
+		hasRecievedSchedule: Boolean(state.finalExamsModal.finalExams),
+		loading: state.finalExamsModal.isLoading,
+		finalExamSchedule: state.finalExamsModal.finalExams
 	}
 }
 
 const mapDispatchToProps = (dispatch) => {
 	return {
 		toggleFinalExamsModal: () => {dispatch({type: "TOGGLE_FINAL_EXAMS_MODAL"})},
-		createiCalfromTimetable: () => dispatch(createiCalfromTimetable()),
+		fetchFinalExamSchedule: () => {dispatch(fetchFinalExamSchedule())}
 	}
 }
 
