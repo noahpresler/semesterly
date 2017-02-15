14a68b204d28f0f27044540a80bbe467e2be66bc
Kristin Yim <k.yim33@gmail.com>
2017-02-15 17:32:23 -0500

    Finished connecting front and backend, now displays final exam schedule as text. Just make pretty now

--- static/js/redux/reducers/final_exams_modal_reducer.jsx
+++ static/js/redux/reducers/final_exams_modal_reducer.jsx
@@ -1,13 +1,16 @@
-export const finalExamsModal = (state = { isVisible: false, isDownloading: false}, action) => {
+export const finalExamsModal = (state = { isVisible: false, isDownloading: false, isLoading: false, finalExams: null }, action) => {
 	switch (action.type) {
 		case 'TOGGLE_FINAL_EXAMS_MODAL':
 			return {isVisible: !state.isVisible};
 		case 'TRIGGER_FINAL_EXAMS_MODAL':
 			return {isVisible: true, isDownloading: false};
+		case 'FETCH_FINAL_EXAMS':
+			return {isLoading: true}
+		case 'RECIEVE_FINAL_EXAMS':
+			return {isLoading: false, finalExams: action.json}
 		case 'DOWNLOAD_CALENDAR':
 			return Object.assign({}, state, {isDownloading: true});
 		default:
 			return state;
 	}
 }
-
