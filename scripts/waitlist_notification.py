ed43ccf06c7e4c9a46f1efb74c4e5199e7433757
Eric Calder <ecalder6@jhu.edu>
2017-02-19 14:31:06 -0500

    WaitlistNotification: started template. Email working.

--- scripts/waitlist_notification.py
+++ scripts/waitlist_notification.py
@@ -3,7 +3,7 @@ os.environ.setdefault("DJANGO_SETTINGS_MODULE", "semesterly.settings")
 django.setup()
 from student.models import *
 from timetable.models import *
-
+from mailer import Mailer
 
 if len(sys.argv) < 3:
     print("Please specify a school (e.g. jhu) and a semester (F or S).")
@@ -11,22 +11,32 @@ if len(sys.argv) < 3:
 school = sys.argv[1]
 semester = sys.argv[2]
 
+client = Mailer()
+d = {}
+
 sections = Section.objects.all()
 for section in sections:
-    if not section.was_full and (section.size == section.enrolment):
+    if not section.was_full and (section.size <= section.enrolment):
         section.was_full = True
         section.save()
-        print section
-        timetables = PersonalTimetable.objects.filter(sections__id__contains=section.id, semester=semester)
+        try:
+            timetables = PersonalTimetable.objects.filter(sections__id__contains=section.id, semester=semester)
+        except:
+            print("Problem with getting section with id = " + str(section.id))
         for student, timetable in zip(map(lambda t: t.student, timetables), timetables):
             if timetable != student.personaltimetable_set.filter(semester=semester).order_by('last_updated').last():
                 # Only applies for the student's last modified schedule.
                 continue
-            print student.user.username
-            title = section.course.name + " " + section.meeting_section
-            print title
             # NOTIFIY STUDENT
+            d[student] = section
 
     elif section.was_full and section.enrolment < section.size:
         section.was_full = False
         section.save()
+
+for student, section in d.items():
+    print ("Sending email to: " + str(student.user.username))
+    client.send_mail(student, "Course Waitlist Notification from Semester.ly", "email_waitlist.html", {'section': section})
+
+
+client.cleanup()
