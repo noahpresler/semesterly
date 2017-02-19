14887b14ebe1a4630fab820b5c7b2312eead6a02
Kristin Yim <k.yim33@gmail.com>
2017-02-19 00:08:51 -0500

    fix logged out state handling

--- static/js/redux/reducers/peer_modal_reducer.jsx
+++ static/js/redux/reducers/peer_modal_reducer.jsx
@@ -1,9 +1,11 @@
-export const peerModal = (state = { isVisible: false, isLoaded: false }, action) => {
+export const peerModal = (state = { isVisible: false, isLoading: false }, action) => {
 	switch (action.type) {
 		case 'TOGGLE_PEER_MODAL':
 			return {isVisible: !state.isVisible};
+		case 'PEER_MODAL_LOADING':
+			return {isLoading: true};
 		case 'PEER_MODAL_LOADED':
-			return {isLoaded: true};
+			return {isLoading: false};
 		default:
 			return state;
 	}
