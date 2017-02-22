7ee8c3a5fc67fa11ea9ccb25bd447d2441faba57
felixzhuologist <felix.czhu@gmail.com>
2017-02-22 17:29:21 -0500

    Change semester state to index into constant

--- static/js/redux/reducers/semester_reducer.jsx
+++ static/js/redux/reducers/semester_reducer.jsx
@@ -1,6 +1,6 @@
 import { VALID_SEMESTERS, SET_SEMESTER } from '../constants.jsx';
 
-export const semester = (state = "", action) => {
+export const semester = (state = 0, action) => {
 	switch (action.type) {
 		case SET_SEMESTER:
 			return action.semester;
