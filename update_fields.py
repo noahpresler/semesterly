a7a4f3f0ac27cc6092d79b9a2f12c7301c52c3dd
felixzhuologist <felix.czhu@gmail.com>
2017-02-23 13:30:32 -0500

    Update script to work over analytics tables

--- update_fields.py
+++ update_fields.py
@@ -3,32 +3,36 @@ import os
 import django
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "semesterly.settings")
 django.setup()
+from operator import attrgetter
 from pprint import pprint
 
 from timetable.models import Section
+from analytics.models import *
 from timetable.school_mappers import VALID_SCHOOLS
+
 
 # schools whose second semester is called the winter semester (instead of spring)
 winter_schools = {'uoft', 'queens', 'umich', 'umich2'}
 valid_semesters = 'FSY'
 
-def update_section_fields():
-  """ Add values to Section.sem_name and Section.year based on Section.semester """
+def update_sem_fields(table, get_school):
+  """ Add values to table.sem_name and table.year based on table.semester """
   num_updated = 0
   bad_semesters = Counter()
-  for section in Section.objects.all():
-    semester_code = section.semester
+  for row in table.objects.all():
+    semester_code = row.semester
     if semester_code not in valid_semesters:
       bad_semesters[semester_code] += 1
     else:
-      section.sem_name = code_to_name(semester_code, section.course.school)
-      section.year = '2017' if semester_code == 'S' else '2016'
-      section.save()
+      row.sem_name = code_to_name(semester_code, get_school(row))
+      row.year = '2017' if semester_code == 'S' else '2016'
+      row.save()
       num_updated += 1
 
-  print "Updated {0} sections' terms".format(num_updated)
-  print "Found the following invalid semester codes:"
+  print "Updated {0}/{1} rows from table {2}".format(num_updated, len(table.objects.all()), str(table))
+  print "Ignored the following invalid semester codes:"
   pprint(bad_semesters)
+  print
 
 def code_to_name(semester_code, school):
   """ Take a valid semester code for a school and return its full name """
@@ -40,4 +44,6 @@ def code_to_name(semester_code, school):
     return 'Winter' if school in winter_schools else 'Spring'
     
 if __name__ == '__main__':
-  update_section_fields()
+  update_sem_fields(Section, get_school=attrgetter('course.school'))
+  for table in [SharedTimetable, AnalyticsTimetable, AnalyticsCourseSearch]:
+    update_sem_fields(table, get_school=attrgetter('school'))
