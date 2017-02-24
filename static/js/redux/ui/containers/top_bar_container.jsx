1f292f7c9e13210a2542adb83025cda8598cdcd7
felixzhuologist <felix.czhu@gmail.com>
2017-02-23 20:07:01 -0500

    stop hardcoding semester in top bar

--- static/js/redux/ui/containers/top_bar_container.jsx
+++ static/js/redux/ui/containers/top_bar_container.jsx
@@ -1,9 +1,13 @@
 import { connect } from 'react-redux';
 import TopBar from '../top_bar.jsx';
+import { getSchoolSpecificInfo } from '../../constants.jsx';
+
 
 const mapStateToProps = (state) => {
+  let allSemesters = getSchoolSpecificInfo(state.school.school).semesters;
   return {
     userInfo: state.userInfo.data,
+    currentSemester: allSemesters[state.semester]
   }
 }
 
