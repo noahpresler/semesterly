bd14a67f81d08c6f35dc0219bf0f205884bfc21a
Kristin Yim <k.yim33@gmail.com>
2017-02-26 10:51:45 -0500

    passing course details into final exams modal

--- static/js/redux/ui/containers/final_exams_modal_container.jsx
+++ static/js/redux/ui/containers/final_exams_modal_container.jsx
@@ -2,13 +2,24 @@ import { connect } from 'react-redux';
 import { FinalExamsModal } from '../final_exams_modal.jsx';
 import { fetchFinalExamSchedule } from '../../actions/user_actions.jsx';
 
+const remapCourseDetails = (courses) => {
+	let remap = {}
+	for (let course in courses) {
+		remap[courses[course].id] = {
+			"courseName" : courses[course].name,
+			"courseCode" : courses[course].code
+		}
+	}
+	return remap
+}
+
 const mapStateToProps = (state) => {
 	return {
 		isVisible: state.finalExamsModal.isVisible,
 		finalExamSchedule: state.finalExamsModal.finalExams,
 		hasRecievedSchedule: Boolean(state.finalExamsModal.finalExams),
 		loading: state.finalExamsModal.isLoading,
-		courseDetails: state.timetables.items[0].courses
+		courseDetails: remapCourseDetails(state.timetables.items[0].courses)
 	}
 }
 
