684ea05ebb601f56a7e9ed009445b6165cab6a17
wyao7 <wyao7@jhu.edu>
2017-02-27 00:01:38 -0500

    Added backend functions to find the class withe most friends in. Added Alert that would use previously mentioned info to ask the user to connect with facebook to find out who.
    To trigger:
    1) logged in with facebook
    2) have more than 1 class on current time table(saved)
    3) have 'find friends in class' turned off
    4) have more than 2 friends in a class
    5) have not seen the alert in the past 3 days

--- static/js/redux/ui/alerts/friends_in_class_alert.jsx
+++ static/js/redux/ui/alerts/friends_in_class_alert.jsx
@@ -4,7 +4,7 @@ import MasterSlot from '../master_slot.jsx';
 import { COLOUR_DATA, getSchoolSpecificInfo } from '../../constants.jsx';
 import { setDeclinedNotifications, getDeclinedNotifications } from '../../util.jsx';
 
-var rc = {"code":"AS.061.150","num_credits":3,"name":"Introduction to Film Production: Rediscovering Early Cinema","textbooks":{"(01)":[]},"department":"AS Film and Media Studies","slots":[{"time_start":"14:00","waitlist":-1,"meeting_section":"(01)","section":15698,"instructors":"J. Mann","section_type":"L","enrolment":9,"time_end":"16:20","waitlist_size":-1,"course":3459,"semester":"S","location":"Gilman 35","textbooks":[],"id":20576,"day":"T","size":12,"colourId":0,"code":"AS.061.150","name":"Introduction to Film Production: Rediscovering Early Cinema","custom":false,"num_conflicts":1,"shift_index":0,"depth_level":0}],"enrolled_sections":["(01)"],"id":3459}
+//var rc = {"code":"AS.061.150","num_credits":3,"name":"Introduction to Film Production: Rediscovering Early Cinema","textbooks":{"(01)":[]},"department":"AS Film and Media Studies","slots":[{"time_start":"14:00","waitlist":-1,"meeting_section":"(01)","section":15698,"instructors":"J. Mann","section_type":"L","enrolment":9,"time_end":"16:20","waitlist_size":-1,"course":3459,"semester":"S","location":"Gilman 35","textbooks":[],"id":20576,"day":"T","size":12,"colourId":0,"code":"AS.061.150","name":"Introduction to Film Production: Rediscovering Early Cinema","custom":false,"num_conflicts":1,"shift_index":0,"depth_level":0}],"enrolled_sections":["(01)"],"id":3459}
 
 class FriendsInClassAlert extends React.Component {
 	constructor(props) {
@@ -20,27 +20,25 @@ class FriendsInClassAlert extends React.Component {
 
 	render() {
 		let maxColourIndex = COLOUR_DATA.length - 1;
-		console.log(this.props.active_tt);
-
 		let professors = []
-        if (rc.slots.length == 0 && rc.oldSlots && rc.oldSlots.length > 0) {
-            professors = [ ...new Set(rc.oldSlots.map(s => s.instructors)) ];
+        if (this.props.mostFriendsClass.slots.length == 0 && this.props.mostFriendsClass.oldSlots && this.props.mostFriendsClass.oldSlots.length > 0) {
+            professors = [ ...new Set(this.props.mostFriendsClass.oldSlots.map(s => s.instructors)) ];
         } else {
-            professors = [ ...new Set(rc.slots.map(s => s.instructors)) ];
+            professors = [ ...new Set(this.props.mostFriendsClass.slots.map(s => s.instructors)) ];
         }
 
 		return (
 		<div className="enable-notification-alert friends-in-class-alert">
 			<h2>{ this.props.msg }</h2>
 			<MasterSlot 
-                key={rc.id} course={rc} 
+                key={this.props.mostFriendsKey} course={this.props.mostFriendsClass} 
                 professors={professors}
-                colourIndex={0}
+                colourIndex={Math.min(this.props.mostFriendsKey, maxColourIndex)}
                 onTimetable={true}
                 hideCloseButton={true}
                 inModal={true}
-                fakeFriends={5}
-                fetchCourseInfo={() => this.fetchCourseInfo(rc.id)}
+                fakeFriends={this.props.mostFriendsCount}
+                fetchCourseInfo={() => this.fetchCourseInfo(this.props.mostFriendsClass.id)}
                 />
 			<small className="alert-extra">
 				Plus 89 more in other classes. Enable the friend feature to find out who!
@@ -50,19 +48,6 @@ class FriendsInClassAlert extends React.Component {
 				className="conflict-alert-btn change-semester-btn">
 				Find Friends in Classes
 			</button>
-			{/*<div className="friends-in-class-fb">
-                <button className="btn abnb-btn fb-btn" onClick={() => {
-                        let link = document.createElement('a');
-                        link.href = 'google.com'
-                        document.body.appendChild(link);
-                        link.click()
-                    }}>
-                    <span className="img-icon">
-                       <i className="fa fa-facebook" />
-                    </span>
-                    <span className="facebook-text">Continue with Facebook</span>
-                </button>
-            </div>*/}
 	 	</div>);
  	}
 };
