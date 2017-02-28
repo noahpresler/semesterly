412d855fa803fe891607467be8c314e87f3b34f7
Michael N Miller <mnm364@gmail.com>
2017-02-27 23:06:11 -0500

    digestor added to master logger

--- timetable/management/commands/digest.py
+++ timetable/management/commands/digest.py
@@ -1,8 +1,14 @@
+import datetime
+from timeit import default_timer as timer
+
 from django.core.management.base import BaseCommand, CommandParser, CommandError
 from timetable.management.commands.args_parse import *
 from scripts.parser_library.Validator import Validator
 from scripts.parser_library.Digestor import Digestor
 from scripts.parser_library.internal_exceptions import JsonException, DigestionError
+
+# FIXME -- horrible design should make upper base class
+from timetable.management.commands.parse import Command as PCommand
 
 class Command(BaseCommand):
 	def add_arguments(self, parser):
@@ -10,15 +16,20 @@ class Command(BaseCommand):
 		validate_switch_argparser(parser)
 		validator_argparser(parser)
 		progressbar_argparser(parser)
+		masterlog_argparser(parser)
 
 	def handle(self, *args, **options):
-		message = "Starting digestion for {}.\n".format(options['school'])
+		timestamp = datetime.datetime.now().strftime("%Y/%m/%d-%H:%M:%S")
+		school = options['school']
+		message = "Starting digestion for {}.\n".format(school)
 		self.stdout.write(self.style.SUCCESS(message))
 		directory = 'scripts/' + options['school']
 
 		# default data
 		if not options.get('data'):
 			options['data'] = '{}/data/courses.json'.format(directory)
+		if not options.get('master_log'):
+			options['log_stats'] = 'scripts/logs/master.log'
 
 		# Perform pre-digestion validation
 		if options['validate']:
@@ -37,20 +48,29 @@ class Command(BaseCommand):
 			except JsonException as e:
 				self.stderr.write(self.style.ERROR('FAILED: validation.'))
 				self.stderr.write(str(e) + '\n')
-				return # Stop here. Do not allow digestion if error present.
+				raise CommandError('Digestion validation failed.') # Stop here. Do not allow digestion if error present.
 
 		if not options.get('output_diff'):
 			options['output_diff'] = directory + '/logs/digest_diff.log.json'
 
+		stats = []
+
 		try:
-			Digestor(options['school'],
+			d = Digestor(options['school'],
 				data=options['data'],
 				output=options['output_diff'],
 				diff=options['diff'],
 				load=options['load']
-			).digest()
+			)
+
+			start_time = timer()
+			d.digest()
+			end_time = timer()
+			stats.append('({}) [Elapsed Time: {:.2f}s] {}'.format(school, end_time - start_time, d.get_stats()))
+
 		except DigestionError as e:
 			self.stderr.write(self.style.ERROR('FAILED: digestion'))
 			self.stderr.write(str(e))
 
 		self.stdout.write(self.style.SUCCESS("Digestion Finished!"))
+		PCommand.log_stats(options['log_stats'], stats=stats, options=options, timestamp=timestamp)
