83e9a42006ab341dee5ed787b048a26217c9199c
Eric Calder <ecalder6@jhu.edu>
2017-03-01 13:55:59 -0500

    manual merge of revamp_bn_parser into revamp

--- scripts/textbooks/amazon.py
+++ scripts/textbooks/amazon.py
@@ -1,23 +1,41 @@
+import django, os
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "semesterly.settings")
+django.setup()
+from timetable.models import *
+from django.utils.encoding import smart_str, smart_unicode
+from timetable.models import Textbook, TextbookLink
 from amazonproduct import API
 api = API(locale='us')
 
-def amazon_textbook_fields(isbn):
-	response = None
-	try:
-		if len(isbn) == 9:
-			response = api.item_lookup(isbn, IdType='ISBN', SearchIndex='Book', ResponseGroup='Large')
-		elif len(isbn) == 13:
-			response = api.item_lookup(isbn, IdType='EAN', SearchIndex='All', ResponseGroup='Large')
+# NOTE: (mostly) copied from base bn parser, need to do full integration
+def get_amazon_fields(isbn):
+    try:
+        result = api.item_lookup(isbn.strip(), IdType='ISBN', SearchIndex='Books', ResponseGroup='Large')
+        info = {
+            "DetailPageURL" : self.get_detail_page(result),
+            "ImageURL" : self.get_image_url(result),
+            "Author" : self.get_author(result),
+            "Title" : self.get_title(result)
+        }
+    except:
+        import traceback
+        traceback.print_exc()
+        info = {
+            "DetailPageURL" : "Cannot be found",
+            "ImageURL" : "Cannot be found",
+            "Author" : "Cannot be found",
+            "Title" : "Cannot be found"
+        }
+    return info
 
-	except:
-		print 'Textbook NOT FOUND for', isbn
+def get_detail_page(result):
+    return smart_str(result.Items.Item.DetailPageURL)
 
-	if response is None:
-		return {}
+def get_image_url(result):
+    return smart_str(result.Items.Item.MediumImage.URL)
 
-	return {
-		'detail_url': str(response.Items.Item.DetailPageURL),
-		'image_url' : str(response.Items.Item.MediumImage.URL),
-		'author':     str(response.Items.Item.ItemAttributes.Author),
-		'title':      str(response.Items.Item.ItemAttributes.Title)
-	}
+def get_author(result):
+    return smart_str(result.Items.Item.ItemAttributes.Author)
+
+def get_title(result):
+    return smart_str(result.Items.Item.ItemAttributes.Title)
