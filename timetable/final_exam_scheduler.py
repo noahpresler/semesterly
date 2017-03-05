69e6ffd8c90c33b3ae07e4e3553c89967bac5c13
Noah Presler <noah@presler.me>
2017-03-05 09:53:47 -0500

    add lsit of codes rule type

--- timetable/final_exam_scheduler.py
+++ timetable/final_exam_scheduler.py
@@ -33,25 +33,27 @@ class FinalExamScheduler:
 		return self.schedule
 
 class Rule:
-	def __init__(self, list_of_days, start_time, result, end_time="24:00", start_only=False):
+	def __init__(self, list_of_days, start_time, result, end_time="24:00", start_only=False, list_of_codes=None):
 		self.list_of_days = list_of_days
 		self.start_time = start_time
 		self.end_time = end_time
 		self.result = result
 		self.start_only = start_only
+		self.list_of_codes = list_of_codes
 
 	def check_times(self,slot):
-		# print int(slot['time_start'].split(':')[0])
-		# print int(self.start_time.split(':')[0])
 		return (not self.start_only and (int(slot['time_start'].split(':')[0]) >= int(self.start_time.split(':')[0]) and int(slot['time_end'].split(':')[0]) <= int(self.end_time.split(':')[0]))) | (self.start_only and (int(slot['time_start'].split(':')[0]) == int(self.start_time.split(':')[0])))
  
 	def apply(self, course):
-		# self.printOut()
-		filtered_slots = filter(lambda slot: slot['section_type'] == 'L' ,course['slots'])
-		for slot in filtered_slots:
-			if slot['day'] in self.list_of_days and self.check_times(slot):
+		if self.list_of_codes:
+			if course['code'] in self.list_of_codes:
 				return self.result
-		return None
+		else:
+			filtered_slots = filter(lambda slot: slot['section_type'] == 'L' ,course['slots'])
+			for slot in filtered_slots:
+				if slot['day'] in self.list_of_days and self.check_times(slot):
+					return self.result
+			return None
 
 	def printOut(self):
 		print 'Days:', self.list_of_days, 'Start time:', self.start_time, 'End time:', self.end_time, "Result:", self.result
