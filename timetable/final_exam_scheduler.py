e8bea4ba6ba49b7d1ed4cf457a752b44b27ac1f0
Noah Presler <noah@presler.me>
2017-03-05 10:23:08 -0500

    regex matching an list of codes, jhu rules updated

--- timetable/final_exam_scheduler.py
+++ timetable/final_exam_scheduler.py
@@ -1,3 +1,4 @@
+import re
 
 class FinalExamScheduler:
 
@@ -33,19 +34,23 @@ class FinalExamScheduler:
 		return self.schedule
 
 class Rule:
-	def __init__(self, list_of_days, start_time, result, end_time="24:00", start_only=False, list_of_codes=None):
+	def __init__(self, list_of_days=None, start_time=None, result=None, end_time="24:00", start_only=False, list_of_codes=None, code_regex=None):
 		self.list_of_days = list_of_days
 		self.start_time = start_time
 		self.end_time = end_time
 		self.result = result
 		self.start_only = start_only
 		self.list_of_codes = list_of_codes
+		self.code_regex = re.compile(code_regex) if code_regex else None
 
 	def check_times(self,slot):
 		return (not self.start_only and (int(slot['time_start'].split(':')[0]) >= int(self.start_time.split(':')[0]) and int(slot['time_end'].split(':')[0]) <= int(self.end_time.split(':')[0]))) | (self.start_only and (int(slot['time_start'].split(':')[0]) == int(self.start_time.split(':')[0])))
  
 	def apply(self, course):
-		if self.list_of_codes:
+		if self.code_regex:
+			if self.code_regex.match(course['code']):
+				return self.result
+		elif self.list_of_codes:
 			if course['code'] in self.list_of_codes:
 				return self.result
 		else:
@@ -53,7 +58,7 @@ class Rule:
 			for slot in filtered_slots:
 				if slot['day'] in self.list_of_days and self.check_times(slot):
 					return self.result
-			return None
+		return None
 
 	def printOut(self):
 		print 'Days:', self.list_of_days, 'Start time:', self.start_time, 'End time:', self.end_time, "Result:", self.result
