b938880c75e15105fc78aa451b1a3e6b035d50d8
Hugh Han <hughhan1@gmail.com>
2017-03-13 19:17:27 -0400

    #520: EvaluationList Now Renders in Sorted Order
    The Evaluation List div in course descriptions now shows all of the evaluations in sorted
    order, starting with the most recent evaluations first. For example, http://jhu.sem.ly:8000/course/EN.600.463/S.

--- static/js/redux/ui/evaluation_list.jsx
+++ static/js/redux/ui/evaluation_list.jsx
@@ -5,17 +5,21 @@ import SideScroller from './side_scroller.jsx';
 class EvaluationList extends React.Component {
 	render() {
 		let { evalInfo } = this.props;
-		let navs = evalInfo.map(e => 
-			(<Evaluation evalData={e} key={e.id} mini={true} />));
-
-		let evals = evalInfo.map(e => 
-			(<Evaluation evalData={e} key={e.id} />));
-		// console.log(navs, evals);
 		
+		let navs = evalInfo
+			.sort( (e1, e2) => this.evalCompare(e1, e2) )
+			.map(e => (<Evaluation evalData={e} key={e.id} mini={true} />));
+
+		let evals = evalInfo
+			.sort( (e1, e2) => this.evalCompare(e1, e2) )
+			.map(e => (<Evaluation evalData={e} key={e.id} />));
+
+		// console.log(navs, evals);
 		
 		let evaluationScroller = <p className="empty-intro">
 			No course evaluations for this course yet.
 		</p>;
+
 		let customClass = "";
 		if (evals.length > 0) {
 			evaluationScroller = <SideScroller 
@@ -24,12 +28,34 @@ class EvaluationList extends React.Component {
 				id={"evaluations-carousel"}/>;
 			customClass = "spacious-entry";
 		}
+
 		// console.log(evaluationScroller)
 
 		return (
 		<div className={"modal-entry " + customClass} id="course-evaluations">
 			{evaluationScroller}
 		</div>);
+	}
+
+	evalCompare(e1, e2) {
+		// Note that Evaluation.year is a string composing of both a semester
+		// and a numerical year (e.g "Spring:2015", "Fall:2013"). We first
+		// obtain the numerical year, and compare those values.
+		let yearComparison = 
+			e1.year.substr(e1.year.length - 4)
+		    .localeCompare(e2.year.substr(e2.year.length - 4));
+
+		// If one year is greater than another, then we return the year
+		// comparison in descending lexigraphical order (i.e. "2015" < "2014").
+		// Otherwise, we compare the semesters, and return that comparison in
+		// ascending lexigraphical order (i.e. "Fall" < "Spring").
+		if (yearComparison != 0) {
+			return -yearComparison;
+		} else {
+			let e1_sem = e1.year.substr(0, e1.year.length - 4);
+			let e2_sem = e2.year.substr(0, e2.year.length - 4);
+			return e1_sem.localeCompare(e2_sem);
+		}
 	}
 }
 
