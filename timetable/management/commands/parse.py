d2ff82ca430f99b4353dd3e4b1f05227c7facbac
Michael N Miller <mnm364@gmail.com>
2017-03-16 12:31:16 -0400

    changed name from Ingestor.py to ingestor.py

--- timetable/management/commands/parse.py
+++ timetable/management/commands/parse.py
@@ -1,3 +1,3 @@
-from timetable.management.commands.scrape import *
+from timetable.management.commands.ingest import *
 # NOTE: parse should really be scrape but it doesn't really matter :-)
 # https://www.quora.com/What-is-the-difference-between-crawling-parsing-and-scraping
