734c616531fb52918be37bcfcc9f427b805e5327
mnm364 <mnm364@gmail.com>
2017-03-27 21:51:17 -0400

    fixed digestor to lookup section using semester object, added textbook support in courses.json (not functional yet), fixed some typos in textbooks json schema, general comments and some Python3 __future__ imports

--- scripts/textbooks/amazon.py
+++ scripts/textbooks/amazon.py
@@ -1,4 +1,10 @@
-from __future__ import print_function # NOTE: slowly move toward Python3
+# @what     Amazon API function definition
+# @org      Semeseter.ly
+# @author   Michael N. Miller
+# @date     2/22/17
+
+from __future__ import print_function, division, absolute_import # NOTE: slowly move toward Python3
+
 from amazonproduct import API
 import sys
 api = API(locale='us')
@@ -10,8 +16,9 @@ def amazon_textbook_fields(isbn):
             response = api.item_lookup(isbn, IdType='ISBN', SearchIndex='Book', ResponseGroup='Large')
         elif len(isbn) == 13:
             response = api.item_lookup(isbn, IdType='EAN', SearchIndex='All', ResponseGroup='Large')
-    except:
-        print("Textbook NOT FOUND for " + isbn, sys.stderr)
+    except Exception as e:
+        pass
+        # FIXME -- something is wrong with the Amazon API (Eric)
 
     if response is None:
         return {}
