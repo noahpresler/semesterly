1119e8b455fed6685fc6b320b8353ab32c52e507
mnm364 <mnm364@gmail.com>
2017-03-30 01:14:32 -0400

    fixed syntax error in vandy texgtbooks

--- scripts/vandy/vandy_textbooks.py
+++ scripts/vandy/vandy_textbooks.py
@@ -1,9 +1,10 @@
 from scripts.textbooks.bn_textbook_parser import BNParser
 
 class VandyTextbookParser(BNParser):
-    def __init__(self, term="Fall", year=2017,**kwargs):
-        super(JHUTextbookParser, self).__init__(
+    def __init__(self,**kwargs):
+        super(VandyTextbookParser, self).__init__(
 			"65163",
 			"vanderbilt.bncollege.com",
 			"vandy",
+			"-",
             **kwargs)
