c28a4db6366288cc4303aba401f48d3769510a9b
Max Yeo <mzwyeo@gmail.com>
2017-03-31 00:15:04 -0400

    added null and loading states to classmates in course

--- static/js/redux/ui/course_modal_body.jsx
+++ static/js/redux/ui/course_modal_body.jsx
@@ -199,38 +199,36 @@ export class CourseModalBody extends React.Component {
                     <p>In the PILOT program, students are organized into study teams consisting of 6-10 members who meet weekly to work problems together.</p>
                 </li>
             </div> : null;
-        let friendDisplay = null;
-        let hasTakenDisplay = null;
+        let friendCircles = <div className="loading"><span className="img-icon"><div className="loader"/></span><p>loading...</p></div>;
+        let hasTakenCircles = <div className="loading"><span className="img-icon"><div className="loader"/></span><p>loading...</p></div>;
         if (!this.props.isFetchingClassmates && this.props.classmates.classmates !== undefined) {
-            let friendCircles = this.props.classmates && this.props.classmates.classmates.length > 0 ? this.props.classmates.classmates.map( c =>
+            friendCircles = this.props.classmates && this.props.classmates.classmates.length > 0 ? this.props.classmates.classmates.map( c =>
                     <div className="friend" key={c.img_url}>
                         <div className="ms-friend" style={{backgroundImage: 'url(' + c.img_url + ')'}}/>
                         <p title={ c.first_name + " " + c.last_name }>{ c.first_name + " " + c.last_name }</p>
-                    </div>) : null;
-            friendDisplay = this.props.classmates && this.props.classmates.classmates.length > 0 ?
-                <div className="modal-module friends">
-                    <h3 className="modal-module-header">Friends In This Course</h3>
-                    <div id="friends-wrapper">
-                        <div id="friends-inner">
-                            { friendCircles }
-                        </div>
-                    </div>
-                </div> : null;
-            let hasTakenCircles = this.props.classmates && this.props.classmates.past_classmates.length > 0 ? this.props.classmates.past_classmates.map( c =>
+                    </div>) : <p className="null">No Classmates Found</p>;
+            hasTakenCircles = this.props.classmates && this.props.classmates.past_classmates.length > 0 ? this.props.classmates.past_classmates.map( c =>
                     <div className="friend" key={c.img_url}>
                         <div className="ms-friend" style={{backgroundImage: 'url(' + c.img_url + ')'}}/>
                         <p title={ c.first_name + " " + c.last_name }>{ c.first_name + " " + c.last_name }</p>
-                    </div>) : null;
-            hasTakenDisplay = this.props.classmates && this.props.classmates.past_classmates.length > 0 ?
-                <div className="modal-module friends">
-                    <h3 className="modal-module-header">Friends Who Have Taken This Course</h3>
-                    <div id="friends-wrapper">
-                        <div id="friends-inner">
-                            { hasTakenCircles }
-                        </div>
-                    </div>
-                </div> : null;
+                    </div>) : <p className="null">No Classmates Found</p>;
         }
+        let friendDisplay = <div className="modal-module friends">
+                <h3 className="modal-module-header">Friends In This Course</h3>
+                <div id="friends-wrapper">
+                    <div id="friends-inner">
+                        { friendCircles }
+                    </div>
+                </div>
+            </div>;
+        let hasTakenDisplay = <div className="modal-module friends">
+                <h3 className="modal-module-header">Friends Who Have Taken This Course</h3>
+                <div id="friends-wrapper">
+                    <div id="friends-inner">
+                        { hasTakenCircles }
+                    </div>
+                </div>
+            </div>;
         let textbooksDisplay = !textbooks || textbooks.length === 0 ? null :
         <div className="modal-module">
             <h3 className="modal-module-header">Textbooks</h3>
