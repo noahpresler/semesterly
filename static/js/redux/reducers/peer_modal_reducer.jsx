171b648bf90729c1ecc4afdef4e43489df5f3fce
Noah Presler <noah@presler.me>
2017-04-02 12:13:00 -0400

    Move all actions from reducer strings to constant actionType

--- static/js/redux/reducers/peer_modal_reducer.jsx
+++ static/js/redux/reducers/peer_modal_reducer.jsx
@@ -1,10 +1,12 @@
+import * as ActionTypes from '../constants/actionTypes.jsx'
+
 export const peerModal = (state = { isVisible: false, isLoading: false }, action) => {
 	switch (action.type) {
-		case 'TOGGLE_PEER_MODAL':
+		case ActionTypes.TOGGLE_PEER_MODAL:
 			return {isVisible: !state.isVisible};
-		case 'PEER_MODAL_LOADING':
+		case ActionTypes.PEER_MODAL_LOADING:
 			return {isLoading: true};
-		case 'PEER_MODAL_LOADED':
+		case ActionTypes.PEER_MODAL_LOADED:
 			return {isLoading: false};
 		default:
 			return state;
