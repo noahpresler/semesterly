2f4094e607e2a0f1bbcd25f588e5c66b5c778632
Noah Presler <noah@presler.me>
2017-04-02 12:13:00 -0400

    Move all actions from reducer strings to constant actionType

--- static/js/redux/reducers/semester_reducer.jsx
+++ static/js/redux/reducers/semester_reducer.jsx
@@ -1,8 +1,8 @@
-import { SET_SEMESTER } from '../constants/constants.jsx';
+import * as ActionTypes from '../constants/actionTypes.jsx'
 
 export const semesterIndex = (state = 0, action) => {
 	switch (action.type) {
-		case SET_SEMESTER:
+		case ActionTypes.SET_SEMESTER:
 			return action.semester;
 		default:
 			return state;
