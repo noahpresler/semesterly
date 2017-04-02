b6aa045b0c399c9b4b967dcb47e57e3f4e0160d2
Noah Presler <noah@presler.me>
2017-04-02 12:13:00 -0400

    Move all actions from reducer strings to constant actionType

--- static/js/redux/reducers/integration_modal_reducer.jsx
+++ static/js/redux/reducers/integration_modal_reducer.jsx
@@ -1,8 +1,10 @@
+import * as ActionTypes from '../constants/actionTypes.jsx'
+
 export const integrationModal = (state = { isVisible: false, id: null, enabled: false, integration_id: null }, action) => {
 	switch (action.type) {
-		case 'TOGGLE_INTEGRATION_MODAL':
+		case ActionTypes.TOGGLE_INTEGRATION_MODAL:
 			return Object.assign({}, state, { isVisible: !state.isVisible, id: action.id });
-		case 'OPEN_INTEGRATION_MODAL':
+		case ActionTypes.OPEN_INTEGRATION_MODAL:
 			return Object.assign({}, state, { enabled: action.enabled, isVisible: true, id: action.id, integration_id: action.integration_id });
 		default:
 			return state;
