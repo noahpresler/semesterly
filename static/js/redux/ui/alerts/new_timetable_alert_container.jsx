1734beadad691daaedbcd038529683a988a8c996
Noah Presler <noah@presler.me>
2017-04-02 23:26:03 -0400

    all dispatches use constants rather than strings

--- static/js/redux/ui/alerts/new_timetable_alert_container.jsx
+++ static/js/redux/ui/alerts/new_timetable_alert_container.jsx
@@ -1,6 +1,8 @@
 import { connect } from 'react-redux';
 import { createNewTimetable } from '../../actions/timetable_actions.jsx'
 import NewTimetableAlert from './new_timetable_alert.jsx';
+import * as ActionTypes from '../../constants/actionTypes.jsx'
+
 
 const mapStateToProps = (state) => {
 	let msg = "You haven't saved this timetable! Still want to start a new one?";
@@ -10,7 +12,7 @@ const mapStateToProps = (state) => {
 }
 const mapDispatchToProps = (dispatch) => {
 	return {
-    	dismissSelf: () => dispatch({type: "DISMISS_ALERT_NEW_TIMETABLE"}),
+    	dismissSelf: () => dispatch({type: ActionTypes.DISMISS_ALERT_NEW_TIMETABLE}),
     	createNewTimetable,
 	}
 }
--- static/js/redux/ui/alerts/timetable_exists_alert_container.jsx
+++ static/js/redux/ui/alerts/timetable_exists_alert_container.jsx
@@ -1,12 +1,14 @@
 import { connect } from 'react-redux';
 import TimetableExistsAlert from './timetable_exists_alert.jsx';
+import * as ActionTypes from '../../constants/actionTypes.jsx'
+
 
 const mapStateToProps = (state) => {
 	return {}
 }
 const mapDispatchToProps = (dispatch) => {
 	return {
-    	dismissSelf: () => dispatch({type: "DISMISS_TIMETABLE_EXISTS"}),
+    	dismissSelf: () => dispatch({type: ActionTypes.DISMISS_TIMETABLE_EXISTS}),
 	}
 }
 
