3160374b82011f34bf50e2c45c7bbf9cade86bdb
Noah Presler <noah@presler.me>
2017-04-03 22:56:53 -0400

    refactor constants into seperate files

--- static/js/redux/actions/timetable_actions.jsx
+++ static/js/redux/actions/timetable_actions.jsx
@@ -1,8 +1,8 @@
 import fetch from 'isomorphic-fetch';
-import { getTimetablesEndpoint } from '../constants/constants.jsx';
+import { getTimetablesEndpoint } from '../constants/endpoints.jsx';
 import { randomString, browserSupportsLocalStorage } from '../util.jsx';
 import { store } from '../init.jsx';
-import { getSchoolSpecificInfo } from '../constants/constants.jsx'
+import { getSchoolSpecificInfo } from '../constants/schools.jsx'
 import { lockActiveSections, fetchClassmates, autoSave } from './user_actions.jsx';
 import { saveLocalSemester, saveLocalPreferences, saveLocalCourseSections, saveLocalActiveIndex } from '../util.jsx';
 import * as ActionTypes from '../constants/actionTypes.jsx'
