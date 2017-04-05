139ba9fca0f775351ffddb7f7a76a8788e17d768
felixzhuologist <felix.czhu@gmail.com>
2017-04-05 18:47:13 -0400

    Separate out timetable views

--- courses/tests.py
+++ courses/tests.py
@@ -5,10 +5,10 @@ class UrlsTest(UrlTestCase):
     """ Test courses/urls.py """
 
     def test_urls_call_correct_views(self):
-        self.assertUrlResolvesToView('/courses/uoft/code/cs1234', 'timetable.views.get_course_id')
-        self.assertUrlResolvesToView('/courses/uoft/Fall/2020/id/38510', 'timetable.views.get_course')
-        self.assertUrlResolvesToView('/course_classmates/jhu/Spring/2018/id/9932', 'timetable.views.get_classmates_in_course')
-        self.assertUrlResolvesToView('/c/somecode0350!', 'timetable.views.course_page')
+        self.assertUrlResolvesToView('/courses/uoft/code/cs1234', 'courses.views.get_course_id')
+        self.assertUrlResolvesToView('/courses/uoft/Fall/2020/id/38510', 'courses.views.get_course')
+        self.assertUrlResolvesToView('/course_classmates/jhu/Spring/2018/id/9932', 'courses.views.get_classmates_in_course')
+        self.assertUrlResolvesToView('/c/somecode0350!', 'courses.views.course_page')
         self.assertUrlResolvesToView('/course/music101/Summer/2021', 'timetable.views.view_timetable')
-        self.assertUrlResolvesToView('/courses', 'timetable.views.all_courses')
-        self.assertUrlResolvesToView('/school_info/semuni', 'timetable.views.school_info')
+        self.assertUrlResolvesToView('/courses', 'courses.views.all_courses')
+        self.assertUrlResolvesToView('/school_info/semuni', 'courses.views.school_info')
