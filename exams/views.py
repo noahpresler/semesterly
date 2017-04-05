9ecb791a3e13666093f5e916f76171c13793f2da
felixzhuologist <felix.czhu@gmail.com>
2017-04-05 18:47:13 -0400

    Separate out timetable views

--- exams/views.py
+++ exams/views.py
@@ -1,3 +1,22 @@
-from django.shortcuts import render
+import json
 
-# Create your views here.
+from django.http import Http404, HttpResponse
+from django.views.decorators.csrf import csrf_exempt
+
+from timetable.jhu_final_exam_scheduler import JHUFinalExamScheduler
+from timetable.utils import validate_subdomain
+from timetable.views import view_timetable
+
+
+@validate_subdomain
+def view_final_exams(request):
+    try:
+        return view_timetable(request, final_exams=True)
+    except Exception:
+        raise Http404
+
+
+@csrf_exempt
+def final_exam_scheduler(request):
+    final_exam_schedule = JHUFinalExamScheduler().make_schedule(json.loads(request.body))
+    return HttpResponse(json.dumps(final_exam_schedule), content_type="application/json")
