89fe90b6fd83cf93edbbff9e782fa0d7a0583737
felixzhuologist <felix.czhu@gmail.com>
2017-04-05 18:47:25 -0400

    Reformat for PEP8

--- exams/urls.py
+++ exams/urls.py
@@ -1,12 +1,12 @@
 from django.conf.urls import patterns, url
 from django.contrib import admin
 
-import timetable.views
+import exams.views
 
 
 admin.autodiscover()
 
 urlpatterns = patterns('',
-    url(r'^get_final_exams/*$', timetable.views.final_exam_scheduler),
-    url(r'^final_exams/*$', timetable.views.view_final_exams),
-)
+                       url(r'^get_final_exams/*$', exams.views.final_exam_scheduler),
+                       url(r'^final_exams/*$', exams.views.view_final_exams),
+                       )
--- timetable/urls.py
+++ timetable/urls.py
@@ -4,31 +4,29 @@ from django.contrib import admin
 import student.views
 import timetable.views
 
-
 admin.autodiscover()
 
-
 urlpatterns = patterns('',
-    # marketing urls
-    url(r'^signup/*', timetable.views.launch_user_acq_modal),
-    url(r'^textbooks/*$', timetable.views.view_textbooks),
-    url(r'^export_calendar/*$', timetable.views.export_calendar),
-    url(r'^notifyme/*$', timetable.views.enable_notifs),
-    url(r'^find_friends/$', timetable.views.find_friends),
-    url(r'react/*', student.views.react_to_course),
-    url(r'^jhu/countdown/*$', timetable.views.jhu_timer),
-    url(r'^callback/google_calendar/*$', timetable.views.google_calendar_callback),
-    url(r'^user/log_final_exam/*$', timetable.views.log_final_exam_view),
+                       # marketing urls
+                       url(r'^signup/*', timetable.views.launch_user_acq_modal),
+                       url(r'^textbooks/*$', timetable.views.view_textbooks),
+                       url(r'^export_calendar/*$', timetable.views.export_calendar),
+                       url(r'^notifyme/*$', timetable.views.enable_notifs),
+                       url(r'^find_friends/$', timetable.views.find_friends),
+                       url(r'react/*', student.views.react_to_course),
+                       url(r'^jhu/countdown/*$', timetable.views.jhu_timer),
+                       url(r'^callback/google_calendar/*$', timetable.views.google_calendar_callback),
+                       url(r'^user/log_final_exam/*$', timetable.views.log_final_exam_view),
 
-    # redirects
-    url(r'^timetable/*$', timetable.views.redirect_to_home),
-    url(r'^timetable/.+$', timetable.views.redirect_to_home),
-    url(r'^complete/facebook/.*$', timetable.views.view_timetable),
+                       # redirects
+                       url(r'^timetable/*$', timetable.views.redirect_to_home),
+                       url(r'^timetable/.+$', timetable.views.redirect_to_home),
+                       url(r'^complete/facebook/.*$', timetable.views.view_timetable),
 
-    # timetables
-    url(r'^get_timetables/$', timetable.views.get_timetables),
+                       # timetables
+                       url(r'^get_timetables/$', timetable.views.get_timetables),
 
-    # timetable sharing
-    url(r'share/link/*$', timetable.views.create_share_link),
-    url(r'share/(?P<ref>.+)/*$', timetable.views.share_timetable),
-)
+                       # timetable sharing
+                       url(r'share/link/*$', timetable.views.create_share_link),
+                       url(r'share/(?P<ref>.+)/*$', timetable.views.share_timetable),
+                       )
