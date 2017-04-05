f96edc1b8be541c00004e6f794409989bc6c12c3
felixzhuologist <felix.czhu@gmail.com>
2017-04-05 10:01:17 -0400

    Use new UrlTestCase in UrlTests

--- timetable/tests.py
+++ timetable/tests.py
@@ -1,9 +1,9 @@
 import json
 
-from django.test import TestCase, SimpleTestCase
-from django.core.urlresolvers import resolve
+from django.test import TestCase
 
-from timetable.test_utils import get_default_tt_request
+from test.utils import get_default_tt_request
+from test.test_cases import UrlTestCase
 
 
 class RegressionTests(TestCase):
@@ -42,45 +42,45 @@ class RegressionTests(TestCase):
         pass
 
 
-class UrlTestCase(TestCase): # TODO: set to SimpleTestCase after fixing school_mappers
+class UrlsTest(UrlTestCase):
     """ Test timetable/urls.py """
 
     def test_urls_call_correct_views(self):
         # marketing urls
-        self.assertEqual('timetable.views.launch_user_acq_modal', resolve('/signup/').view_name)
-        self.assertEqual('timetable.views.view_textbooks', resolve('/textbooks/').view_name)
-        self.assertEqual('timetable.views.export_calendar', resolve('/export_calendar/').view_name)
-        self.assertEqual('timetable.views.enable_notifs', resolve('/notifyme/').view_name),
-        self.assertEqual('timetable.views.find_friends', resolve('/find_friends/').view_name),
-        self.assertEqual('student.views.react_to_course', resolve('/react/').view_name),
+        self.assertUrlResolvesToView('/signup/', 'timetable.views.launch_user_acq_modal')
+        self.assertUrlResolvesToView('/textbooks/', 'timetable.views.view_textbooks')
+        self.assertUrlResolvesToView('/export_calendar/', 'timetable.views.export_calendar')
+        self.assertUrlResolvesToView('/notifyme/', 'timetable.views.enable_notifs'),
+        self.assertUrlResolvesToView('/find_friends/', 'timetable.views.find_friends'),
+        self.assertUrlResolvesToView('/react/', 'student.views.react_to_course'),
 
         # redirects
-        self.assertEqual('timetable.views.redirect_to_home', resolve('/timetable/random_stuff').view_name)
-        self.assertEqual('timetable.views.redirect_to_home', resolve('/timetable/').view_name)
+        self.assertUrlResolvesToView('/timetable/random_stuff', 'timetable.views.redirect_to_home')
+        self.assertUrlResolvesToView('/timetable/', 'timetable.views.redirect_to_home')
 
         # course pages
-        self.assertEqual('timetable.views.get_course_id', resolve('/courses/uoft/code/cs1234').view_name)
-        self.assertEqual('timetable.views.get_course', resolve('/courses/uoft/Fall/2020/id/38510').view_name)
-        self.assertEqual('timetable.views.get_classmates_in_course', resolve('/course_classmates/jhu/Spring/2018/id/9932').view_name)
-        self.assertEqual('timetable.views.course_page', resolve('/c/somecode0350!').view_name)
-        self.assertEqual('timetable.views.view_timetable', resolve('/course/music101/Summer/2021').view_name)
-        self.assertEqual('timetable.views.all_courses', resolve('/courses').view_name)
-        self.assertEqual('timetable.views.school_info', resolve('/school_info/semuni').view_name)
+        self.assertUrlResolvesToView('/courses/uoft/code/cs1234', 'timetable.views.get_course_id')
+        self.assertUrlResolvesToView('/courses/uoft/Fall/2020/id/38510', 'timetable.views.get_course')
+        self.assertUrlResolvesToView('/course_classmates/jhu/Spring/2018/id/9932', 'timetable.views.get_classmates_in_course')
+        self.assertUrlResolvesToView('/c/somecode0350!', 'timetable.views.course_page')
+        self.assertUrlResolvesToView('/course/music101/Summer/2021', 'timetable.views.view_timetable')
+        self.assertUrlResolvesToView('/courses', 'timetable.views.all_courses')
+        self.assertUrlResolvesToView('/school_info/semuni', 'timetable.views.school_info')
 
         # timetables
-        self.assertEqual('timetable.views.get_timetables', resolve('/get_timetables/').view_name)
+        self.assertUrlResolvesToView('/get_timetables/', 'timetable.views.get_timetables')
 
         # search
-        self.assertEqual('timetable.views.course_search', resolve('/search/jhu/Intermission/2019/opencv/').view_name)
-        self.assertEqual('timetable.views.advanced_course_search', resolve('/advanced_search/').view_name)
+        self.assertUrlResolvesToView('/search/jhu/Intermission/2019/opencv/', 'timetable.views.course_search')
+        self.assertUrlResolvesToView('/advanced_search/', 'timetable.views.advanced_course_search')
 
         # timetable sharing
-        self.assertEqual('timetable.views.create_share_link', resolve('/share/link').view_name)
-        self.assertEqual('timetable.views.share_timetable', resolve('/share/dIcMED').view_name)
+        self.assertUrlResolvesToView('/share/link', 'timetable.views.create_share_link')
+        self.assertUrlResolvesToView('/share/dIcMED', 'timetable.views.share_timetable')
 
         # integration
-        self.assertEqual('timetable.views.get_integration', resolve('/integration/get/3DCe3/course/csc148/').view_name)
-        self.assertEqual('timetable.views.delete_integration', resolve('/integration/del/39Ced/course/SD3910/').view_name)
-        self.assertEqual('timetable.views.add_integration', resolve('/integration/add/139051/course/eng101/').view_name)
+        self.assertUrlResolvesToView('/integration/get/3DCe3/course/csc148/', 'timetable.views.get_integration')
+        self.assertUrlResolvesToView('/integration/del/39Ced/course/SD3910/', 'timetable.views.delete_integration')
+        self.assertUrlResolvesToView('/integration/add/139051/course/eng101/', 'timetable.views.add_integration')
 
 
