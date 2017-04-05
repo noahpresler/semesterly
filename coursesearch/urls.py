82e6ef65b9b00969c31ef5ca629320a439e0c69d
felixzhuologist <felix.czhu@gmail.com>
2017-04-05 18:47:25 -0400

    Reformat for PEP8

--- coursesearch/urls.py
+++ coursesearch/urls.py
@@ -1,12 +1,13 @@
 from django.conf.urls import patterns, url
 from django.contrib import admin
 
-import timetable.views
+import coursesearch.views
 
 
 admin.autodiscover()
 
 urlpatterns = patterns('',
-    url(r'^search/(?P<school>.+?)/(?P<sem_name>.+?)/(?P<year>[0-9]{4})/(?P<query>.+?)/', timetable.views.course_search),
-    url(r'^advanced_search/', timetable.views.advanced_course_search),
-)
+                       url(r'^search/(?P<school>.+?)/(?P<sem_name>.+?)/(?P<year>[0-9]{4})/(?P<query>.+?)/',
+                           coursesearch.views.course_search),
+                       url(r'^advanced_search/', coursesearch.views.advanced_course_search),
+                       )
