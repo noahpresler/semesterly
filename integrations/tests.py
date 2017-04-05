3ace88885f90f46853126166b26d1d4dbc6ba01e
felixzhuologist <felix.czhu@gmail.com>
2017-04-05 18:23:51 -0400

    separate out timetable urls

--- integrations/tests.py
+++ integrations/tests.py
@@ -1,3 +1,10 @@
-from django.test import TestCase
+from test_utils.test_cases import UrlTestCase
 
-# Create your tests here.
+
+class UrlsTest(UrlTestCase):
+    """ Test integrations/urls.py """
+
+    def test_urls_call_correct_views(self):
+        self.assertUrlResolvesToView('/integration/get/3DCe3/course/csc148/', 'timetable.views.get_integration')
+        self.assertUrlResolvesToView('/integration/del/39Ced/course/SD3910/', 'timetable.views.delete_integration')
+        self.assertUrlResolvesToView('/integration/add/139051/course/eng101/', 'timetable.views.add_integration')
--- timetable/urls.py
+++ timetable/urls.py
@@ -1,6 +1,4 @@
-from django.conf.urls import patterns, include, url
-from django.http import HttpResponse
-from django.conf import settings
+from django.conf.urls import patterns, url
 from django.contrib import admin
 
 import student.views
@@ -22,37 +20,15 @@ urlpatterns = patterns('',
     url(r'^callback/google_calendar/*$', timetable.views.google_calendar_callback),
     url(r'^user/log_final_exam/*$', timetable.views.log_final_exam_view),
 
-    # timetable
-        # redirects
+    # redirects
     url(r'^timetable/*$', timetable.views.redirect_to_home),
     url(r'^timetable/.+$', timetable.views.redirect_to_home),
     url(r'^complete/facebook/.*$', timetable.views.view_timetable),
-        # course pages
-    url(r'^courses/(?P<school>.+?)/code/(?P<course_id>.+)/*$', timetable.views.get_course_id),
-    url(r'^courses/(?P<school>.+?)/(?P<sem_name>.+)/(?P<year>[0-9]{4})/id/(?P<id>[0-9]+)/*$',
-        timetable.views.get_course),
-    url(r'^course_classmates/(?P<school>.+?)/(?P<sem_name>.+)/(?P<year>[0-9]{4})/id/(?P<id>[0-9]+)/*$',
-        timetable.views.get_classmates_in_course),
-    url(r'c/(?P<code>.+?)$', timetable.views.course_page),
-    url(r'course/(?P<code>.+?)/(?P<sem_name>.+?)/(?P<year>.+?)/*$', timetable.views.view_timetable),
-    url(r'^school_info/(?P<school>.+?)', timetable.views.school_info),
-    url(r'courses/*', timetable.views.all_courses),
-        # timetables
+
+    # timetables
     url(r'^get_timetables/$', timetable.views.get_timetables),
-        # search
-    url(r'^search/(?P<school>.+?)/(?P<sem_name>.+?)/(?P<year>[0-9]{4})/(?P<query>.+?)/', timetable.views.course_search),
-    url(r'^advanced_search/', timetable.views.advanced_course_search),
-        # timetable sharing
+
+    # timetable sharing
     url(r'share/link/*$', timetable.views.create_share_link),
     url(r'share/(?P<ref>.+)/*$', timetable.views.share_timetable),
-        # Integration
-    url(r'^integration/get/(?P<integration_id>.+?)/course/(?P<course_id>.+?)/',
-        timetable.views.get_integration),
-    url(r'^integration/del/(?P<integration_id>.+?)/course/(?P<course_id>.+?)/',
-        timetable.views.delete_integration),
-    url(r'^integration/add/(?P<integration_id>.+?)/course/(?P<course_id>.+?)/',
-        timetable.views.add_integration),
-        # final exam scheduler
-    url(r'^get_final_exams/*$', timetable.views.final_exam_scheduler),
-    url(r'^final_exams/*$', timetable.views.view_final_exams),
 )
