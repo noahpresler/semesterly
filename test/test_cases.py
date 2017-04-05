016b67d3d704bc03b4c7710cb5f143373d974063
felixzhuologist <felix.czhu@gmail.com>
2017-04-04 22:37:22 -0400

    Add SimpleTestCase for testing urls

---
+++ test/test_cases.py
@@ -0,0 +1,8 @@
+from django.test import TestCase, SimpleTestCase
+from django.core.urlresolvers import resolve
+
+
+class UrlTestCase(SimpleTestCase):
+
+    def assertUrlResolvesToView(self, url, view_name):
+        self.assertEqual(resolve(url).view_name, view_name)
