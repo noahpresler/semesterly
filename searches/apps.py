79bfc99ff330d0df8e16eefa621741d96ce04e75
felixzhuologist <felix.czhu@gmail.com>
2017-04-06 13:25:37 -0400

    rename coursesearch app to searches

--- searches/apps.py
+++ searches/apps.py
@@ -3,5 +3,5 @@ from __future__ import unicode_literals
 from django.apps import AppConfig
 
 
-class CoursesearchConfig(AppConfig):
-    name = 'coursesearch'
+class SearchesConfig(AppConfig):
+    name = 'searches'
