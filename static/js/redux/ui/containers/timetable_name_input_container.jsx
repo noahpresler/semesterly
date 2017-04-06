87ae798549c849fb8a9d4d4ad1e283b52dde679c
Noah Presler <noah@presler.me>
2017-04-06 16:20:21 -0400

    code clean up

--- static/js/redux/ui/containers/timetable_name_input_container.jsx
+++ static/js/redux/ui/containers/timetable_name_input_container.jsx
@@ -1,37 +1,39 @@
-import { connect } from 'react-redux';
-import TimetableNameInput from '../timetable_name_input.jsx';
-import { MAX_TIMETABLE_NAME_LENGTH } from '../../constants/constants.jsx';
-import { saveTimetable } from '../../actions/user_actions.jsx';
-import * as ActionTypes from '../../constants/actionTypes.jsx'
+import {connect} from "react-redux";
+import TimetableNameInput from "../timetable_name_input.jsx";
+import {MAX_TIMETABLE_NAME_LENGTH} from "../../constants/constants.jsx";
+import {saveTimetable} from "../../actions/user_actions.jsx";
+import * as ActionTypes from "../../constants/actionTypes.jsx";
 
 
 const mapStateToProps = (state) => {
-	let savingTimetable = state.savingTimetable;
-	return {
-		activeLoadedTimetableName: savingTimetable.activeTimetable.name, // the name of the user's "being-edited" saved timetable
-		saving: savingTimetable.saving,
-		upToDate: savingTimetable.upToDate,
-		isLoggedIn: state.userInfo.data.isLoggedIn
-	}
+    let savingTimetable = state.savingTimetable;
+    return {
+        activeLoadedTimetableName: savingTimetable.activeTimetable.name, // the name of the user's "being-edited" saved timetable
+        saving: savingTimetable.saving,
+        upToDate: savingTimetable.upToDate,
+        isLoggedIn: state.userInfo.data.isLoggedIn
+    }
 }
 
 const mapDispatchToProps = (dispatch) => {
-	return {
-		changeTimetableName: (name) => {
-			if (name.length === 0 || name.length > MAX_TIMETABLE_NAME_LENGTH) { return; }
-			dispatch({
-				type: ActionTypes.CHANGE_ACTIVE_SAVED_TIMETABLE_NAME,
-				name,
-			})
-			dispatch(saveTimetable());
-		},
-		openSignupModal: () => dispatch({type: ActionTypes.TOGGLE_SIGNUP_MODAL})
-	}
+    return {
+        changeTimetableName: (name) => {
+            if (name.length === 0 || name.length > MAX_TIMETABLE_NAME_LENGTH) {
+                return;
+            }
+            dispatch({
+                type: ActionTypes.CHANGE_ACTIVE_SAVED_TIMETABLE_NAME,
+                name,
+            })
+            dispatch(saveTimetable());
+        },
+        openSignupModal: () => dispatch({type: ActionTypes.TOGGLE_SIGNUP_MODAL})
+    }
 }
 
 const TimetableNameInputContainer = connect(
-	mapStateToProps,
-	mapDispatchToProps
+    mapStateToProps,
+    mapDispatchToProps
 )(TimetableNameInput);
 
 export default TimetableNameInputContainer;
--- static/js/redux/ui/integration_slot.jsx
+++ static/js/redux/ui/integration_slot.jsx
@@ -1,59 +1,59 @@
-import React from 'react'
+import React from "react";
 
 // tip: metrics are friends, conflicts, days off, time on/off campus, rating
 const SortRow = (props) => (
-  <div className="sort-row metric-row">
-    <div className="sort-text">
-      <input
-        type="text"
-      />    
+    <div className="sort-row metric-row">
+        <div className="sort-text">
+            <input
+                type="text"
+            />
+        </div>
+        <div className="sort-order-dropdown">
+            <input
+                type="text"
+            />
+        </div>
+        <div className="sort-metric-dropdown">
+            <input
+                type="text"
+            />
+        </div>
+        <div className="sort-close"
+             onClick={() => props.removeMetric(props.chosenMetric.metric)}>
+            <i className="fa fa-times"></i>
+        </div>
     </div>
-    <div className="sort-order-dropdown">
-      <input
-        type="text"
-      />       
-    </div>
-    <div className="sort-metric-dropdown">
-      <input
-        type="text"
-      />
-    </div>
-    <div className="sort-close"
-         onClick={() => props.removeMetric(props.chosenMetric.metric)}>
-      <i className="fa fa-times"></i>
-    </div>
-  </div>
 )
 
-const FooterRow = ({ addNextMetric }) => (
-  <div className="sort-row footer-row"
-        onClick={addNextMetric} >
-    <span style={{ margin: 'auto' }}> New Session </span>
-  </div>
+const FooterRow = ({addNextMetric}) => (
+    <div className="sort-row footer-row"
+         onClick={addNextMetric}>
+        <span style={{margin: 'auto'}}> New Session </span>
+    </div>
 )
 
 export class IntegrationSlot extends React.Component {
-  render() {
-    let { metrics } = this.props;
-    let selectedMetrics = metrics.filter( m => m.selected )
-    let availMetrics = metrics.filter( m => !m.selected )
-    let rows = selectedMetrics.map( (m, i) => (
-      <SortRow {...this.props}
-               actionText="then by"
-               chosenMetric={m}
-               availMetrics={availMetrics} 
-               key={i}
-               /> 
-    ))
-    let footer = availMetrics.length > 0 ? 
-      <FooterRow addNextMetric={() => this.props.addMetric(availMetrics[0].metric)} /> 
-      : null
+    render() {
+        let {metrics} = this.props;
+        let selectedMetrics = metrics.filter(m => m.selected)
+        let availMetrics = metrics.filter(m => !m.selected)
+        let rows = selectedMetrics.map((m, i) => (
+            <SortRow {...this.props}
+                     actionText="then by"
+                     chosenMetric={m}
+                     availMetrics={availMetrics}
+                     key={i}
+            />
+        ))
+        let footer = availMetrics.length > 0 ?
+            <FooterRow addNextMetric={() => this.props.addMetric(availMetrics[0].metric)}/>
+            : null
 
-    return (
-      <div className="sort-menu">
-        {rows}
-        {footer}
-      </div>
-    )
-  }
+        return (
+            <div className="sort-menu">
+                {rows}
+                {footer}
+            </div>
+        )
+    }
 }
