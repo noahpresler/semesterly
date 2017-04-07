f333e76caeac3ffc5fd9355294813d994e93e9fe
felixzhuologist <felix.czhu@gmail.com>
2017-04-06 21:43:07 -0400

    Add new search API endpoints

--- searches/urls.py
+++ searches/urls.py
@@ -10,4 +10,7 @@ urlpatterns = patterns('',
                        url(r'^search/(?P<school>.+?)/(?P<sem_name>.+?)/(?P<year>[0-9]{4})/(?P<query>.+?)/',
                            searches.views.course_search),
                        url(r'^advanced_search/', searches.views.advanced_course_search),
+                       url(r'^api/search/(?P<sem_name>.+?)/(?P<year>[0-9]{4})/(?P<query>.+?)/',
+                           searches.views.CourseSearchList.as_view()),
+                       url(r'^api/advanced_search/', searches.views.AdvancedCourseSearchList.as_view())
                        )
