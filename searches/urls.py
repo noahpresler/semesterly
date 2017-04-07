50ef3e7c6eec7156070d38b6e2048d18287f9b61
felixzhuologist <felix.czhu@gmail.com>
2017-04-06 22:20:44 -0400

    Change advanced search method to GET

--- searches/urls.py
+++ searches/urls.py
@@ -6,11 +6,14 @@ import searches.views
 
 admin.autodiscover()
 
+search_params = '(?P<sem_name>.+?)/(?P<year>[0-9]{4})/(?P<query>.+?)/'
+
 urlpatterns = patterns('',
-                       url(r'^search/(?P<school>.+?)/(?P<sem_name>.+?)/(?P<year>[0-9]{4})/(?P<query>.+?)/',
+                       url(r'^search/(?P<school>.+?)/' + search_params,
                            searches.views.course_search),
                        url(r'^advanced_search/', searches.views.advanced_course_search),
-                       url(r'^api/search/(?P<sem_name>.+?)/(?P<year>[0-9]{4})/(?P<query>.+?)/',
+                       url(r'^api/search/' + search_params,
                            searches.views.CourseSearchList.as_view()),
-                       url(r'^api/advanced_search/', searches.views.AdvancedCourseSearchList.as_view())
+                       url(r'^api/advanced_search/' + search_params,
+                           searches.views.AdvancedCourseSearchList.as_view())
                        )
