d62ae98355e329551f24c1f6afd1090b1478acbb
Noah Presler <noah@presler.me>
2017-04-07 11:44:11 -0400

    Refactor actions/dispatch on social profile

--- static/js/redux/ui/containers/final_exams_modal_container.jsx
+++ static/js/redux/ui/containers/final_exams_modal_container.jsx
@@ -2,7 +2,7 @@ import {connect} from "react-redux";
 import {FinalExamsModal} from "../final_exams_modal.jsx";
 import {fetchFinalExamSchedule} from "../../actions/user_actions.jsx";
 import {logFinalExamView} from "../../actions/calendar_actions.jsx";
-import {hideFinalExamsModal, triggerAcquisitonModal} from "../../actions/modal_actions.jsx";
+import {hideFinalExamsModal, triggerAcquisitionModal} from "../../actions/modal_actions.jsx";
 
 const remapCourseDetails = (courses) => {
     let remap = {}
@@ -38,7 +38,7 @@ const FinalExamsModalContainer = connect(
     {
         hideFinalExamsModal,
         fetchFinalExamSchedule,
-        launchUserAcquisitionModal: triggerAcquisitonModal,
+        launchUserAcquisitionModal: triggerAcquisitionModal,
         logFinalExamView
     }
 )(FinalExamsModal);
