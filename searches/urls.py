3dd814525711d823a4fb1a360eb231d4835e0084
felixzhuologist <felix.czhu@gmail.com>
2017-04-07 10:05:28 -0400

    clean up imports, formatting

--- searches/urls.py
+++ searches/urls.py
@@ -12,6 +12,7 @@ urlpatterns = patterns('',
                        url(r'^search/(?P<school>.+?)/' + search_params,
                            searches.views.course_search),
                        url(r'^advanced_search/', searches.views.advanced_course_search),
+
                        url(r'^api/search/' + search_params,
                            searches.views.CourseSearchList.as_view()),
                        url(r'^api/advanced_search/' + search_params,
