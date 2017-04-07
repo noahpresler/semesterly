a9306b1db6e25050e713e49d4a1d95e70c21142b
felixzhuologist <felix.czhu@gmail.com>
2017-04-06 21:41:38 -0400

    Move course serializer to courses app

--- timetable/serializers.py
+++
@@ -1,9 +0,0 @@
-from rest_framework import serializers
-
-from timetable.models import Course
-
-
-class CourseSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Course
-        fields = 'code name school description num_credits'.split()
