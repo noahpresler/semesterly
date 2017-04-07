32d9f862c371cc937118a745219797d063c22f75
felixzhuologist <felix.czhu@gmail.com>
2017-04-06 22:20:44 -0400

    Change advanced search method to GET

--- searches/tests.py
+++ searches/tests.py
@@ -6,6 +6,7 @@ from test_utils.test_cases import UrlTestCase
 
 class BasicSearchTest(TestCase):
     school = 'uoft'
+    search_endpoint = 'search'
     request_headers = {
         'HTTP_HOST': '{}.sem.ly:8000'.format(school)
     }
@@ -17,14 +18,30 @@ class BasicSearchTest(TestCase):
         Offering.objects.create(section=section, day='M', time_start='8:00', time_end='10:00')
 
     def test_course_exists(self):
-        response =  self.client.get('/api/search/Winter/1995/sea/', **self.request_headers)
+        response =  self.client.get('/api/{}/Winter/1995/sea/'.format(self.search_endpoint), **self.request_headers)
         self.assertEqual(response.status_code, 200)
         self.assertNotEqual(len(response.json()),  0)
 
-    def test_course_does_not_exit(self):
-        response = self.client.get('/api/search/Fall/2016/sea/', **self.request_headers)
+    def test_no_course_exists(self):
+        response = self.client.get('/api/{}/Fall/2016/sea/'.format(self.search_endpoint), **self.request_headers)
         self.assertEqual(response.status_code, 200)
         self.assertEqual(len(response.json()), 0)
+
+
+class AdvancedSearchTest(BasicSearchTest):
+    search_endpoint = 'advanced_search'
+
+    def setUp(self):
+        super(AdvancedSearchTest, self).setUp()
+
+    def test_filter_times(self):
+        pass
+
+    def test_filter_levels(self):
+        pass
+
+    def test_pagination(self):
+        pass
 
 
 class UrlsTest(UrlTestCase):
