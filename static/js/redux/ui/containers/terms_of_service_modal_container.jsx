0648d899d5dfab71a13bad4d809ed31945642b19
Eric Calder <ecalder6@jhu.edu>
2017-04-07 23:15:08 -0400

    TOS: initial setup.

---
+++ static/js/redux/ui/containers/terms_of_service_modal_container.jsx
@@ -0,0 +1,25 @@
+import {connect} from "react-redux";
+import {TermsOfServiceModal} from "../terms_of_service_modal.jsx";
+import * as ActionTypes from "../../constants/actionTypes.jsx";
+
+const mapStateToProps = (state) => {
+    return {
+        isVisible: state.TermsOfServiceModal.isVisible,
+        userInfo: state.userInfo.data,
+    }
+}
+
+const mapDispatchToProps = (dispatch) => {
+    return {
+        toggleTermsOfServiceModal: () => {
+            dispatch({type: ActionTypes.TOGGLE_TOS_MODAL})
+        },
+    }
+}
+
+const TermsOfServiceModalContainer = connect(
+    mapStateToProps,
+    mapDispatchToProps
+)(TermsOfServiceModal);
+
+export default TermsOfServiceModalContainer;
