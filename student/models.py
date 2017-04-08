2dff3360f15e3cd7ddb2c613c9f62466d3f0916d
Eric Calder <ecalder6@jhu.edu>
2017-04-07 23:15:08 -0400

    TOS: initial setup.

--- student/models.py
+++ student/models.py
@@ -32,6 +32,7 @@ class Student(models.Model):
                                           blank=True)
     time_created = models.DateTimeField(auto_now_add=True)
     school = models.CharField(max_length=100, null=True)
+    time_accepted_tos = models.DateTimeField(null=True)
 
 
 class Reaction(models.Model):
