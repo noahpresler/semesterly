1623775fc1ce3356705f0987a87d2384d0f3c602
Noah Presler <noah@presler.me>
2017-04-10 18:15:10 -0400

    finish refactoring containers

--- static/js/redux/ui/containers/pagination_container.jsx
+++ static/js/redux/ui/containers/pagination_container.jsx
@@ -1,7 +1,5 @@
 import {connect} from "react-redux";
 import {Pagination} from "../pagination.jsx";
-import {autoSave} from "../../actions/user_actions.jsx";
-import * as ActionTypes from "../../constants/actionTypes.jsx";
 import {setActiveTimetable} from "../../actions/timetable_actions.jsx";
 
 const mapStateToProps = (state) => {
