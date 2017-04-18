e268bc07206644947ad5ad3af39fcf6959319531
Noah Presler <noah@presler.me>
2017-04-17 20:39:11 -0400

    all containers linting

--- static/js/redux/ui/containers/textbook_modal_container.jsx
+++ static/js/redux/ui/containers/textbook_modal_container.jsx
@@ -1,22 +1,22 @@
-import {connect} from "react-redux";
-import {TextbookModal} from "../textbook_modal";
-import {toggleTextbookModal} from "../../actions/modal_actions";
+import { connect } from 'react-redux';
+import { TextbookModal } from '../textbook_modal';
+import { toggleTextbookModal } from '../../actions/modal_actions';
 
 const mapStateToProps = (state) => {
-    let activeTimetable = state.timetables.items[state.timetables.active];
-    return {
-        isVisible: state.textbookModal.isVisible,
-        liveTimetableCourses: activeTimetable.courses.filter(c => !c.fake),
-        isLoading: state.timetables.isFetching
-    }
-}
+  const activeTimetable = state.timetables.items[state.timetables.active];
+  return {
+    isVisible: state.textbookModal.isVisible,
+    liveTimetableCourses: activeTimetable.courses.filter(c => !c.fake),
+    isLoading: state.timetables.isFetching,
+  };
+};
 
 
 const TextbookModalContainer = connect(
     mapStateToProps,
-    {
-        toggleTextbookModal
-    }
+  {
+    toggleTextbookModal,
+  },
 )(TextbookModal);
 
 export default TextbookModalContainer;
