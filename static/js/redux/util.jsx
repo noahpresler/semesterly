044eea6ad8854e2bfc4b79099f629b1960f2aacf
Noah Presler <noah@presler.me>
2017-04-19 16:26:36 -0400

    util fix

--- static/js/redux/util.jsx
+++ static/js/redux/util.jsx
@@ -1,4 +1,5 @@
-import {getLogFinalExamViewEndpoint} from "./constants/endpoints";
+import { getLogFinalExamViewEndpoint } from './constants/endpoints';
+
 export const randomString = (length = 30, chars = '!?()*&^%$#@![]0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ') => {
   let result = '';
   for (let i = length; i > 0; --i) result += chars[Math.floor(Math.random() * chars.length)];
