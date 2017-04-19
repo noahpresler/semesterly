3bc18c2ee82bd25ac3048a92e3af7bd83f9b4e0f
felixzhuologist <felix.czhu@gmail.com>
2017-04-19 09:06:33 -0400

    Add url tests for new student endpoints

--- student/tests.py
+++ student/tests.py
@@ -22,3 +22,16 @@ class UrlsTest(UrlTestCase):
         self.assertUrlResolvesToView('/user/get_most_classmates_count/', 'student.views.get_most_classmate_count')
         self.assertUrlResolvesToView('/user/find_friends/', 'student.views.find_friends')
         self.assertUrlResolvesToView('/user/add_to_gcal/', 'student.views.add_tt_to_gcal')
+
+        # api
+        self.assertUrlResolvesToView('/user/settings/', 'student.views.UserView')
+        self.assertUrlResolvesToView('/user/timetables/', 'student.views.UserTimetableView')
+        self.assertUrlResolvesToView('/user/timetables/Fall/2016/', 'student.views.UserTimetableView',
+                                     kwargs={'sem_name': 'Fall', 'year': '2016'})
+        self.assertUrlResolvesToView('/user/timetables/Fall/2016/mytt/', 'student.views.UserTimetableView',
+                                     kwargs={'sem_name': 'Fall', 'year': '2016', 'tt_name': 'mytt'})
+        # social
+        self.assertUrlResolvesToView('/user/classmates/Fall/2016?courseids=1&courseids=2',
+                                     'student.views.ClassmateView',
+                                     kwargs={'sem_name': 'Fall', 'year': '2016'})
+        self.assertUrlResolvesToView('/user/gcal/', 'student.views.GCalView')
