9c4711c443b0f05df3bf577255cf42f9f568ee7a
felixzhuologist <felix.czhu@gmail.com>
2017-04-19 09:06:06 -0400

    Add new student endpoints

--- student/urls.py
+++ student/urls.py
@@ -25,18 +25,15 @@ urlpatterns = patterns('',
                        url(r'^user/add_to_gcal/*$', student.views.add_tt_to_gcal),
 
                        # api:
-                       # url(r'^unsubscribe/(?P<id>[\w.@+-]+)/(?P<token>[\w.:\-_=]+)/$', student.views.unsubscribe), # patch?
-                       # url(r'^user/save_settings/$', student.views.save_settings), # patch
-                       # url(r'^me/*', timetable.views.profile),
-                       # # single view:
-                       # url(r'^user/save_timetable/$', student.views.save_timetable), # post
-                       # url(r'^user/duplicate_timetable/$', student.views.duplicate_timetable), # post
-                       # url(r'^user/delete_timetable/$', student.views.delete_timetable), # delete
-                       # url(r'^user/get_saved_timetables/(?P<school>.+)/(?P<sem_name>.+)/(?P<year>[0-9]{4})',
-                       #     student.views.get_student_tts_wrapper), # get
+                       url(r'^user/settings/$', student.views.UserView.as_view()),  # patch
+                       url(r'^user/timetables/$', student.views.UserTimetableView.as_view()), # post (create, and duplicate)
+                       url(r'^user/timetables/(?P<sem_name>.+)/(?P<year>[0-9]{4})/$',
+                           student.views.UserTimetableView.as_view()),  # get all
+                       url(r'^user/timetables/(?P<sem_name>.+)/(?P<year>[0-9]{4})/(?P<tt_name>.+)/$',
+                           student.views.UserTimetableView.as_view()), # delete
+
                        # # social
-                       # url(r'^user/get_classmates/$', student.views.get_classmates), # get?
-                       # url(r'^user/get_most_classmates_count/$', student.views.get_most_classmate_count), # get?
-                       # url(r'^user/find_friends/*$', student.views.find_friends), # get?
-                       # url(r'^user/add_to_gcal/*$', student.views.add_tt_to_gcal), # post
+                       url(r'^user/classmates/(?P<sem_name>.+)/(?P<year>[0-9]{4})',
+                           student.views.ClassmateView.as_view()),
+                       url(r'^user/gcal/$', student.views.GCalView.as_view()), # post
                        )
