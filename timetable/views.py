28a38861a6e7950ab78e02126d14afe8298be878
felixzhuologist <felix.czhu@gmail.com>
2017-04-20 12:51:35 -0400

    Add class based views, tests, urls for timetable

--- timetable/views.py
+++ timetable/views.py
@@ -2,6 +2,9 @@ import itertools
 import logging
 
 from django.db.models import Count
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework import status
 from hashids import Hashids
 
 from analytics.views import *
@@ -374,11 +377,11 @@ class TimetableGenerator:
   def get_day_to_usage(self):
     """Initialize day_to_usage dictionary, which has custom events blocked out."""
     day_to_usage = {
-      'M': [set() for i in range(14 * 60 / school_to_granularity[SCHOOL])],
-      'T': [set() for i in range(14 * 60 / school_to_granularity[SCHOOL])],
-      'W': [set() for i in range(14 * 60 / school_to_granularity[SCHOOL])],
-      'R': [set() for i in range(14 * 60 / school_to_granularity[SCHOOL])],
-      'F': [set() for i in range(14 * 60 / school_to_granularity[SCHOOL])]
+      'M': [set() for _ in range(14 * 60 / school_to_granularity[SCHOOL])],
+      'T': [set() for _ in range(14 * 60 / school_to_granularity[SCHOOL])],
+      'W': [set() for _ in range(14 * 60 / school_to_granularity[SCHOOL])],
+      'R': [set() for _ in range(14 * 60 / school_to_granularity[SCHOOL])],
+      'F': [set() for _ in range(14 * 60 / school_to_granularity[SCHOOL])]
     }
 
     for event in self.custom_events:
@@ -587,4 +590,100 @@ def log_final_exam_view(request):
     school=request.subdomain
   ).save()
   return HttpResponse(json.dumps({}), content_type="application/json")
-  
+
+
+class TimetableView(APIView):
+
+  def post(self, request):
+    """Generate best timetables given the user's selected courses"""
+    global SCHOOL
+    SCHOOL = request.subdomain
+
+    try:
+      params = request.data
+    except ValueError:  # someone is trying to manually send requests
+      return HttpResponse(json.dumps({'timetables': [], 'new_c_to_s': {}}),
+                          content_type='application/json')
+    else:
+      try:
+        params['semester'] = Semester.objects.get_or_create(**params['semester'])[0]
+      except TypeError:
+        params['semester'] = Semester.objects.get(name="Fall", year="2016") if params[
+                                                                                 'semester'] == "F" else Semester.objects.get(
+          name="Spring", year="2017")
+
+    sid = params['sid']
+    course_ids = params['courseSections'].keys()
+    courses = [Course.objects.get(id=cid) for cid in course_ids]
+    locked_sections = params['courseSections']
+
+    save_analytics_timetable(courses, params['semester'], SCHOOL, get_student(request))
+
+    for updated_course in params.get('updated_courses', []):
+      cid = str(updated_course['course_id'])
+      locked_sections[cid] = locked_sections.get(cid, {})
+      if cid not in course_ids:
+        courses.append(Course.objects.get(id=int(cid)))
+
+      for locked_section in filter(bool, updated_course['section_codes']):
+        update_locked_sections(locked_sections, cid, locked_section)
+
+    # temp optional course implementation
+    opt_course_ids = params.get('optionCourses', [])
+    max_optional = params.get('numOptionCourses', len(opt_course_ids))
+    optional_courses = [Course.objects.get(id=cid) for cid in opt_course_ids]
+    optional_course_subsets = [subset for k in range(max_optional, -1, -1) \
+                               for subset in itertools.combinations(optional_courses, k)]
+
+    custom_events = params.get('customSlots', [])
+    generator = TimetableGenerator(params['semester'],
+                                   params['school'],
+                                   locked_sections,
+                                   custom_events,
+                                   params['preferences'],
+                                   opt_course_ids)
+    result = [timetable for opt_courses in optional_course_subsets \
+              for timetable in generator.courses_to_timetables(courses + list(opt_courses))]
+
+    # updated roster object
+    response = {'timetables': result, 'new_c_to_s': locked_sections}
+    return Response(response, status=status.HTTP_200_OK)
+
+
+class TimetableLinkView(APIView):
+
+  def get(self, request, slug):
+    student = get_student(request)
+    try:
+      timetable_id = hashids.decrypt(slug)[0]
+      shared_timetable_obj = SharedTimetable.objects.get(school=request.subdomain, id=timetable_id)
+      shared_timetable = convert_tt_to_dict(shared_timetable_obj, include_last_updated=False)
+      # get default semester info
+      return view_timetable(request, year=shared_timetable_obj.semester.year,
+                            sem_name=shared_timetable_obj.semester.name, shared_timetable=shared_timetable)
+    except Exception as e:
+      raise Http404
+
+  def post(self, request):
+    school = request.subdomain
+    courses = request.data['timetable']['courses']
+    has_conflict = request.data['timetable'].get('has_conflict', False)
+    semester, _ = Semester.objects.get_or_create(**request.data['semester'])
+    student = get_student(request)
+    shared_timetable = SharedTimetable.objects.create(
+      student=student, school=school, semester=semester,
+      has_conflict=has_conflict)
+    shared_timetable.save()
+
+    for course in courses:
+      course_obj = Course.objects.get(id=course['id'])
+      shared_timetable.courses.add(course_obj)
+      enrolled_sections = course['enrolled_sections']
+      for section in enrolled_sections:
+        section_obj = course_obj.section_set.get(meeting_section=section,
+                                                 semester=semester)
+        shared_timetable.sections.add(section_obj)
+    shared_timetable.save()
+
+    response = {'slug': hashids.encrypt(shared_timetable.id)}
+    return Response(response, status=status.HTTP_200_OK)
