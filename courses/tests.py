4704d69d863d8d55aa78b07bd325616577bef0d7
felixzhuologist <felix.czhu@gmail.com>
2017-04-20 14:01:38 -0400

    Frontend calls new course endopoints

--- courses/tests.py
+++ courses/tests.py
@@ -25,7 +25,7 @@ class CourseDetail(APITestCase):
         Offering.objects.create(section=section, day='M', time_start='8:00', time_end='10:00')
 
     def test_course_exists(self):
-        response =  self.client.get('/api/courses/{}/{}/id/{}'.format(self.sem_name, self.year, self.cid),
+        response =  self.client.get('/courses/{}/{}/id/{}'.format(self.sem_name, self.year, self.cid),
                                     **self.request_headers)
         self.assertEqual(response.status_code, status.HTTP_200_OK)
 
@@ -34,17 +34,13 @@ class CourseDetail(APITestCase):
         self.assertEqual(course_info['code'], self.code)
 
     def test_no_course_exists(self):
-        response = self.client.get('/api/courses/{}/{}/id/{}'.format(self.sem_name, self.year, self.cid + 1),
+        response = self.client.get('/courses/{}/{}/id/{}'.format(self.sem_name, self.year, self.cid + 1),
                                    **self.request_headers)
         self.assertEqual(response.status_code, status.HTTP_404_NOT_FOUND)
 
 
 class SchoolListTest(APITestCase):
     school = 'uoft'
-    search_endpoint = 'search'
-    request_headers = {
-        'HTTP_HOST': '{}.sem.ly:8000'.format(school)
-    }
 
     def setUp(self):
         self.areas = 'area'
@@ -56,7 +52,7 @@ class SchoolListTest(APITestCase):
         Updates.objects.create(school=self.school, update_field='Course', last_updated=self.time)
 
     def test_school_exists(self):
-        response =  self.client.get('/api/school_info/', **self.request_headers)
+        response =  self.client.get('/school/{}/'.format(self.school))
         self.assertEqual(response.status_code, status.HTTP_200_OK)
 
         school_info = response.data
@@ -66,7 +62,7 @@ class SchoolListTest(APITestCase):
         self.assertIsNotNone(school_info['last_updated'])
 
     def test_school_does_not_exist(self):
-        response = self.client.get('/api/school_info/', HTTP_HOST='jhu.sem.ly:8000')
+        response = self.client.get('/school/{}/'.format('notuoft'))
         self.assertEqual(response.status_code, status.HTTP_200_OK)
 
         school_info = response.data
@@ -80,13 +76,9 @@ class UrlsTest(UrlTestCase):
     """ Test courses/urls.py """
 
     def test_urls_call_correct_views(self):
-        self.assertUrlResolvesToView('/courses/uoft/code/cs1234', 'courses.views.get_course_id')
-        self.assertUrlResolvesToView('/courses/uoft/Fall/2020/id/38510', 'courses.views.get_course')
-        self.assertUrlResolvesToView('/course_classmates/jhu/Spring/2018/id/9932', 'courses.views.get_classmates_in_course')
         self.assertUrlResolvesToView('/c/somecode0350!', 'courses.views.course_page')
         self.assertUrlResolvesToView('/course/music101/Summer/2021', 'timetable.views.view_timetable')
         self.assertUrlResolvesToView('/courses', 'courses.views.all_courses')
-        self.assertUrlResolvesToView('/school_info/semuni', 'courses.views.school_info')
 
-        self.assertUrlResolvesToView('/api/courses/Fall/2019/id/82', 'courses.views.CourseDetail')
-        self.assertUrlResolvesToView('/api/school_info/', 'courses.views.SchoolList')
+        self.assertUrlResolvesToView('/courses/Fall/2019/id/82', 'courses.views.CourseDetail')
+        self.assertUrlResolvesToView('/school/uoft/', 'courses.views.SchoolList', kwargs={'school': 'uoft'})
