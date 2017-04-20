067699df6963bfaa9befcf559cb7ed96812a870c
felixzhuologist <felix.czhu@gmail.com>
2017-04-20 10:31:18 -0400

    Add new urls, tests, views for exams

--- exams/views.py
+++ exams/views.py
@@ -2,6 +2,9 @@ import json
 
 from django.http import Http404, HttpResponse
 from django.views.decorators.csrf import csrf_exempt
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework import status
 
 from timetable.jhu_final_exam_scheduler import JHUFinalExamScheduler
 from timetable.utils import validate_subdomain
@@ -20,3 +23,10 @@ def view_final_exams(request):
 def final_exam_scheduler(request):
     final_exam_schedule = JHUFinalExamScheduler().make_schedule(json.loads(request.body))
     return HttpResponse(json.dumps(final_exam_schedule), content_type="application/json")
+
+
+class ExamView(APIView):
+
+    def get(self, request):
+        final_exam_schedule = JHUFinalExamScheduler().make_schedule(request.data)
+        return Response(final_exam_schedule, status=status.HTTP_200_OK)
