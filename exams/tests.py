ea253ec33e0d81c0f4fa8d979a66de7147589e39
felixzhuologist <felix.czhu@gmail.com>
2017-04-20 10:31:18 -0400

    Add new urls, tests, views for exams

--- exams/tests.py
+++ exams/tests.py
@@ -7,3 +7,5 @@ class UrlsTest(UrlTestCase):
     def test_urls_call_correct_views(self):
         self.assertUrlResolvesToView('/get_final_exams/', 'exams.views.final_exam_scheduler')
         self.assertUrlResolvesToView('/final_exams/', 'exams.views.view_final_exams')
+
+        self.assertUrlResolvesToView('/final-exams/', 'exams.views.ExamView')
