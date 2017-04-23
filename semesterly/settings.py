52179fa68134fc625a96ca1dad1bb6559452d528
Michael N Miller <mnm364@gmail.com>
2017-04-23 02:09:30 -0400

    Celery integration - cleanup :bathtub: :shower:
    Pep8'ing, decerease verbosity, file structure modifications

--- semesterly/settings.py
+++ semesterly/settings.py
@@ -8,10 +8,12 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/1.6/ref/settings/
 """
 
+import djcelery
+
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 import os
 BASE_DIR = os.path.dirname(os.path.dirname(__file__))
-PROJECT_DIRECTORY = os.getcwd() 
+PROJECT_DIRECTORY = os.getcwd()
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/1.6/howto/deployment/checklist/
@@ -340,7 +342,6 @@ if not DEBUG:
 
 
 # Begin Celery stuff.
-import djcelery
 djcelery.setup_loader()
 
 BROKER_URL = 'redis://localhost:6379/0'
@@ -352,36 +353,14 @@ CELERYBEAT_SCHEDULER = 'djcelery.schedulers.DatabaseScheduler'
 CELERY_TIMEZONE = 'America/New_York'
 
 # App instance to use
-CELERY_APP="semesterly"
+CELERY_APP = "semesterly"
 
 # Where to chdir at start.
-CELERYBEAT_CHDIR=BASE_DIR
-CELERYD_CHDIR=BASE_DIR
+CELERYBEAT_CHDIR = BASE_DIR
+CELERYD_CHDIR = BASE_DIR
 
 # # Can set up cron like scheduling here.
 # from celery.schedules import crontab
 # CELERYBEAT_SCHEDULE = {}
-
-# Absolute or relative path to the 'celery' command:
-#CELERY_BIN="/virtualenvs/def/bin/celery"
-
-# comment out this line if you don't use an app
-# or fully qualified:
-#CELERY_APP="proj.tasks:app"
-
-# Where to chdir at start.
-# CELERYBEAT_CHDIR=BASE_DIR
-# CELERYD_CHDIR=BASE_DIR
-
-# Extra arguments to celerybeat
-# CELERYBEAT_OPTS="--schedule=/var/run/celery/celerybeat-schedule"
-
-# CELERY_BROKER_URL = 'amqp://guest:guest@localhost//'
-
-# #: Only add pickle to this list if your broker is secured
-# #: from unwanted access (see userguide/security.html)
-# CELERY_ACCEPT_CONTENT = ['json']
-# CELERY_RESULT_BACKEND = 'db+sqlite:///results.sqlite'
-# CELERY_TASK_SERIALIZER = 'json'
 
 # End Celery stuff.
