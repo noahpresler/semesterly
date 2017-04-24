5fa262e22ab5f20cd0162aabcef13889a023b8ab
Noah Presler <noah@presler.me>
2017-04-24 16:59:10 -0400

    backend created for sharing final exams

--- exams/views.py
+++ exams/views.py
@@ -20,3 +20,23 @@ def view_final_exams(request):
 def final_exam_scheduler(request):
     final_exam_schedule = JHUFinalExamScheduler().make_schedule(json.loads(request.body))
     return HttpResponse(json.dumps(final_exam_schedule), content_type="application/json")
+
+@csrf_exempt
+def share_final_exam_schedule(request):
+    exam_json = json.loads(request.body)
+    share = FinalExamShare.objects.create(
+        school=request.subdomain,
+        exam_json=exam_json,
+    )
+    share.save()
+    response = {'link': hashids.encrypt(shared_timetable.id)}
+    return HttpResponse(json.dumps(response), content_type='application/json')
+    
+@validate_subdomain
+def view_final_exam_share(request, ref):
+    try:
+        exam_json = FinalExamShare.objects.get(id=hashids.decrypt(ref)[0]).exam_json
+        final_exam_schedule = JHUFinalExamScheduler().make_schedule(json.loads(exam_json))    
+        return view_timetable(request, final_exam_share=final_exam_schedule)
+    except Exception:
+        raise Http404
