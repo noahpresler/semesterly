f9d808e466a1f5d45081eba830101f42e1a92555
felixzhuologist <felix.czhu@gmail.com>
2017-04-25 12:06:00 -0400

    Use new endpoints for ClassmateView

--- static/js/redux/constants/endpoints.jsx
+++ static/js/redux/constants/endpoints.jsx
@@ -13,10 +13,10 @@ export const getLoadSavedTimetablesEndpoint = semester => `/user/timetables/${se
 export const getSaveTimetableEndpoint = () => '/user/timetables/';
 export const getDeleteTimetableEndpoint = (semester, name) => `/user/timetables/${semester.name}/${semester.year}/${name}/`;
 export const getSaveSettingsEndpoint = () => '/user/settings/';
-export const getClassmatesEndpoint = () => '/user/get_classmates/';
+export const getClassmatesEndpoint = (semester, courses) => `/user/classmates/${semester.name}/${semester.year}/${$.param({ course_ids: courses })}`;
 export const getClassmatesInCourseEndpoint = courseId => `/course_classmates/${getSchool()}/${getSemester()}/id/${courseId}/`;
-export const getMostClassmatesCountEndpoint = () => '/user/get_most_classmates_count/';
-export const getFriendsEndpoint = () => '/user/find_friends/';
+export const getMostClassmatesCountEndpoint = (semester, courses) => `/user/classmates/${semester.name}/${semester.year}/${$.param({ course_ids: courses, count: true })}`;
+export const getFriendsEndpoint = semester => `/user/classmates/${semester.name}/${semester.year}/`;
 export const getSchoolInfoEndpoint = () => `/school/${getSchool()}/`;
 export const getReactToCourseEndpoint = () => '/react/';
 export const getRequestShareTimetableLinkEndpoint = () => '/timetables/links/';
