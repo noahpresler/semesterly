126db0afbfb83984287b8454ee748c5d5b0d28b3
felixzhuologist <felix.czhu@gmail.com>
2017-04-25 12:18:16 -0400

    cleanup old views and urls

--- student/urls.py
+++ student/urls.py
@@ -14,27 +14,13 @@ urlpatterns = patterns('',
                        url(r'^user/settings/$', student.views.UserView.as_view()),
 
                        # timetable management
-                       url(r'^user/save_timetable/$', student.views.save_timetable),
-                       url(r'^user/duplicate_timetable/$', student.views.duplicate_timetable),
-                       url(r'^user/delete_timetable/$', student.views.delete_timetable),
-                       url(r'^user/get_saved_timetables/(?P<school>.+)/(?P<sem_name>.+)/(?P<year>[0-9]{4})',
-                           student.views.get_student_tts_wrapper),
-                       # social
-                       url(r'^user/get_classmates/$', student.views.get_classmates),
-                       url(r'^user/get_most_classmates_count/$', student.views.get_most_classmate_count),
-                       url(r'^user/find_friends/*$', student.views.find_friends),
-                       url(r'^user/add_to_gcal/*$', student.views.add_tt_to_gcal),
-
-                       # api:
                        url(r'^user/timetables/$', student.views.UserTimetableView.as_view()),
-                       # post (create, and duplicate)
                        url(r'^user/timetables/(?P<sem_name>.+?)/(?P<year>[0-9]{4})/$',
-                           student.views.UserTimetableView.as_view()),  # get all
+                           student.views.UserTimetableView.as_view()),
                        url(r'^user/timetables/(?P<sem_name>.+)/(?P<year>[0-9]{4})/(?P<tt_name>.+)/$',
-                           student.views.UserTimetableView.as_view()),  # delete
-
-                       # # social
+                           student.views.UserTimetableView.as_view()),
+                       # social
                        url(r'^user/classmates/(?P<sem_name>.+)/(?P<year>[0-9]{4})',
                            student.views.ClassmateView.as_view()),
-                       url(r'^user/gcal/$', student.views.GCalView.as_view()),  # post
+                       url(r'^user/gcal/$', student.views.GCalView.as_view()),
                        )
