6e7b43fce7a049829c3237ab144983fbf82ecc8b
felixzhuologist <felix.czhu@gmail.com>
2017-04-29 11:05:16 -0400

    Rewrite react endpoint as class based view

--- student/urls.py
+++ student/urls.py
@@ -6,8 +6,6 @@ import student.views
 admin.autodiscover()
 
 urlpatterns = patterns('',
-                       url(r'^react/*', student.views.react_to_course),
-
                        # profile management
                        url(r'^user/logout/$', 'django.contrib.auth.views.logout', {'next_page': '/'}),
                        url(r'^unsubscribe/(?P<id>[\w.@+-]+)/(?P<token>[\w.:\-_=]+)/$', student.views.unsubscribe),
@@ -22,5 +20,6 @@ urlpatterns = patterns('',
                        # social
                        url(r'^user/classmates/(?P<sem_name>.+)/(?P<year>[0-9]{4})',
                            student.views.ClassmateView.as_view()),
-                       url(r'^user/gcal/$', student.views.GCalView.as_view()),
+                       url(r'^user/gcal/?$', student.views.GCalView.as_view()),
+                       url(r'^user/reactions/?$', student.views.ReactionView.as_view())
                        )
