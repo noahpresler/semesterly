8f7d1e8e43ad06c8f35307bf138b0353f0026ddf
felixzhuologist <felix.czhu@gmail.com>
2017-04-29 12:32:48 -0400

    Add pylint settings file

--- authpipe/views.py
+++ authpipe/views.py
@@ -14,16 +14,16 @@ class RegistrationTokenView(ValidateSubdomainMixin, APIView):
         token = request.data
         school = request.subdomain
         student = get_student(request)
-        rt, rt_was_created = RegistrationToken.objects.update_or_create(auth=token['auth'],
-                                                                        p256dh=token['p256dh'],
-                                                                        endpoint=token['endpoint'])
+        token, _ = RegistrationToken.objects.update_or_create(auth=token['auth'],
+                                                              p256dh=token['p256dh'],
+                                                              endpoint=token['endpoint'])
         if student:
-            rt.student = student
-            rt.save()
+            token.student = student
+            token.save()
             student.school = school
             student.save()
 
-        return Response(model_to_dict(rt), status=status.HTTP_201_CREATED)
+        return Response(model_to_dict(token), status=status.HTTP_201_CREATED)
 
     def delete(self, request, endpoint):
         to_delete = RegistrationToken.objects.filter(endpoint=endpoint)
