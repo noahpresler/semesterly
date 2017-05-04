86600027751a64f37bbed89bff04459b9771eb9d
felixzhuologist <felix.czhu@gmail.com>
2017-05-04 05:44:17 -0400

    Update url tests for feature flows

--- exams/tests.py
+++ exams/tests.py
@@ -5,6 +5,7 @@ class UrlsTest(UrlTestCase):
     """ Test exams/urls.py """
 
     def test_urls_call_correct_views(self):
-        self.assertUrlResolvesToView('/final_exams/', 'exams.views.view_final_exams')
+        self.assertUrlResolvesToView('/final_exams/',
+          'timetable.utils.FeatureFlowView')
 
         self.assertUrlResolvesToView('/exams/', 'exams.views.ExamView')
