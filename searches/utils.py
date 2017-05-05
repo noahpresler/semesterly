0422c6427ad26c0f2b5d51c9f876cc94c498d5cc
Alex Ahn <alexahn917@gmail.com>
2017-05-04 21:12:03 -0400

    implemented relevance scoring

--- searches/utils.py
+++ searches/utils.py
@@ -26,12 +26,17 @@ def vectorize():
     TFIDF_TF = TfidfTransformer(use_idf=False).fit(descp_counts)
     course_vectors = TFIDF_TF.transform(course_counts)
 
-    # calculate norms
-    course_vectors_norms = []
-    for i in range(course_vectors.shape[0]):
-        course_vectors_norms.append(linalg.norm(course_vectors[i,:]))
-    
-    return CV, course_vectors, course_vectors_norms
+    # calculate norms (if not using TFIDF)
+    #course_vectors_norms = []
+    #for i in range(course_vectors.shape[0]):
+    #    course_vectors_norms.append(linalg.norm(course_vectors[i,:]))
+
+    i = 0
+    for course in Course.objects.all():
+        #picklify(course, course_vectors[i])
+        i+=1
+
+    return CV
 
 def vocabularize(corpus):
     return set([word for doc in corpus for word in tokenizer(doc)])
@@ -47,4 +52,41 @@ def wordify(CV, course_vector):
 def picklify(course_object, course_vector):
     course_object.vector = course_vector
     course_object.save()
-    print(course_object.vector)
+
+def vectorize_query(query):
+    if not query:
+        return None
+    CV = vectorize()
+    query_vector = CV.transform([query])
+    return query_vector
+
+def compute_qry2course_relevancy(query, course):
+    if not query:
+        return None
+    query_vector = vectorize_query(query.lower())
+    return compute_cosine_sim(query_vector, course.vect)
+
+def compute_all_qry2course_relevancy(query):
+    if not query:
+        return None
+    query_tokens = query.lower().split(' ')
+    query_vector = vectorize_query(query.lower())
+    scores = []
+    for course in Course.objects.all():
+        scores.append((course.name, compute_cosine_sim(query_vector, course.vector) + match_title(query_tokens, course.name)))
+    scores.sort(key=lambda tup:-tup[1])
+    return scores[:10]
+
+def compute_cosine_sim(sparse_vec1, sparse_vec2):
+    cross_product = np.sum(sparse_vec1.multiply(sparse_vec2))
+    # cross_product = np.sum(sparse_vec1.multiply(sparse_vec2)) / math.sqrt(vec1_norm * vec2_norm)
+    return cross_product
+
+def match_title(query_tokens, course_name):
+    count = 0
+    for q in query_tokens:
+        if q in course_name.lower():
+            count+=1
+    if count == len(query_tokens):
+        return 1
+    return 0
