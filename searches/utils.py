d9625c4cc8e13d39fac97b4e51e468e8586dabb8
Alex Ahn <alexahn917@gmail.com>
2017-05-06 18:42:36 -0400

    stemming/acronym/weighting implemented

--- searches/utils.py
+++ searches/utils.py
@@ -8,8 +8,10 @@ from sklearn.feature_extraction.text import CountVectorizer
 from sklearn.feature_extraction.text import TfidfTransformer
 from picklefield.fields import PickledObjectField
 from django.db import models
+from django.db.models import Q
 from timetable.models import Semester, Course
 from nltk.stem.porter import *
+from collections import defaultdict
 
 TITLE_WEIGHT = 3
 stemmer = PorterStemmer()
@@ -21,53 +23,73 @@ def vectorize():
         raw_word_counts.append(stem_course(course.name, course.description, TITLE_WEIGHT))
 
     # vectorize course objects
-    CV = CountVectorizer(ngram_range=(1,2), stop_words='english')
-    processed_word_counts = CV.fit_transform(raw_word_counts)
+    count_vectorizer = CountVectorizer(ngram_range=(1,2), stop_words='english')
+    processed_word_counts = count_vectorizer.fit_transform(raw_word_counts)
     TFIDF_TF = TfidfTransformer(use_idf=False).fit(processed_word_counts)
     course_vectors = TFIDF_TF.transform(processed_word_counts)
 
     # save course vector
     i = 0
     for course in Course.objects.all():
-        #picklify(course, course_vectors[i])
+        picklify(course, course_vectors[i])
         i+=1
 
     # store CountVectorizer in the memory
-    return CV
+    with open('count_vectorizer.pickle', 'wb') as handle:
+        pickle.dump(count_vectorizer, handle, protocol=pickle.HIGHEST_PROTOCOL)
+
+def load_count_vectorizer():
+    with open('count_vectorizer.pickle', 'r') as handle:
+        return pickle.load(handle)
 
 def stem_course(name, description, W):
     stemmed_doc = ""
     if name:
         name_doc = name.encode('ascii', 'ignore')
-        stemmed_name_doc = ' '.join([stemmer.stem(w.lower()) for w in name_doc.split(' ')])
+        stemmed_name_doc = stem_doc(name_doc)
         for i in range(W):
             stemmed_doc += " " + stemmed_name_doc
         stemmed_doc += " "
     if description:
         desc_doc = description.encode('ascii', 'ignore')
-        stemmed_desc_doc = ' '.join([stemmer.stem(w.lower()) for w in desc_doc.split(' ')])
+        stemmed_desc_doc = stem_doc(desc_doc)
         stemmed_doc += stemmed_desc_doc
     return stemmed_doc
 
+def get_acronym(name):
+    name = name.replace("and", "").replace("&", "").lower()
+    return ''.join([i[:1] for i in name.split(' ')])
 
-def wordify(CV, course_vector):
-    print(CV.inverse_transform(course_vector))
+def stem_doc(doc):
+    return ' '.join([stemmer.stem(w.lower()) for w in doc.split(' ')])
+
+def wordify(course_vector):
+    count_vectorizer = load_count_vectorizer()
+    print(count_vectorizer.inverse_transform(course_vector))
 
 def picklify(course_object, course_vector):
     course_object.vector = course_vector
     course_object.save()
 
 def vectorize_query(query):
-    CV = vectorize()
-    query_vector = CV.transform([query])
+    start_time = time.time()
+    count_vectorizer = load_count_vectorizer()
+    elapsed_time = time.time() - start_time
+    print("Time to take CourseVectorizer object: %f (seconds)\n" %elapsed_time)
+    stemmed_qry = stem_doc(query)
+    query_vector = count_vectorizer.transform([stemmed_qry])
     return query_vector
 
 def compute_cosine_sim(sparse_vec1, sparse_vec2):
     cross_product = np.sum(sparse_vec1.multiply(sparse_vec2))
-    # cross_product = np.sum(sparse_vec1.multiply(sparse_vec2)) / math.sqrt(vec1_norm * vec2_norm)
     return cross_product
 
-def match_title(query_tokens, course_name):
+def match_title(query, course_name):
+    query_tokens = query.lower().split(' ')
+    # acronym
+    if len(query_tokens) is 1 and get_acronym(course_name) == query:
+        return 1
+    # matching title
     count = 0
     for q in query_tokens:
         if q in course_name.lower():
@@ -76,30 +98,36 @@ def match_title(query_tokens, course_name):
         return 1
     return 0
 
+def get_course(code):
+    for course in Course.objects.all():
+        if course.code == code:
+            return course
+    return None
+
 def compute_qry2course_relevancy(query, course):
     query_vector = vectorize_query(query.lower())
-    return compute_cosine_sim(query_vector, course.vect)
+    return compute_cosine_sim(query_vector, course.vector)
 
 def compute_all_qry2course_relevancy(query):
     return get_relevant_courses(query, Course.objects.all())
 
 def get_relevant_courses(query, course_filtered):
     start_time = time.time()
-
-    query_tokens = query.lower().split(' ')
     query_vector = vectorize_query(query.lower())
-    scores = []
+    course_score = defaultdict(int)
     for course in course_filtered:
-        score = compute_cosine_sim(query_vector, course.vector)# + match_title(query_tokens, course.name)
-        if score > 0.10:
-            scores.append((course, score))
+        score = compute_cosine_sim(query_vector, course.vector) + match_title(query, course.name)
+        if score > 0.0 and score > course_score[course.name]:
+            course_score[course.name] = score
+    #print([(k, v) for k,v in course_score.items()])
+    scores = [(k, v) for k,v in course_score.items()]
     scores.sort(key=lambda tup:-tup[1])
-    results = []
-    for i in range(5):
-        try:
-            results.append(scores.pop(0)[0])
-        except:
-            continue
     elapsed_time = time.time() - start_time
-    print("serach done in %f (seconds)" %elapsed_time)
-    return results
+    if len(scores) < 10:
+        for i in range(len(scores)):
+            print(str(scores[i][0]) + ": " + str(scores[i][1]))
+    else:
+        for i in range(10):
+            print(str(scores[i][0]) + ": " + str(scores[i][1]))
+    
+    print("\nSearched in %f (seconds)" %elapsed_time)
