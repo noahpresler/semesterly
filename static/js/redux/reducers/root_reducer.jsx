bb9f046779bfbbcdd3a086fd12b5e15930fdb5d1
Max Yeo <mzwyeo@gmail.com>
2017-05-08 19:27:36 -0400

    tos banner starting to style

--- static/js/redux/reducers/root_reducer.jsx
+++ static/js/redux/reducers/root_reducer.jsx
@@ -23,7 +23,7 @@ import notificationToken from './notification_token_reducer';
 import integrationModal from './integration_modal_reducer';
 import saveCalendarModal from './save_calendar_modal_reducer';
 import termsOfServiceModal from './terms_of_service_modal_reducer';
-import termsOfServiceBannerModal from './terms_of_service_banner_modal_reducer';
+import termsOfServiceBanner from './terms_of_service_banner_reducer';
 import userAcquisitionModal from './user_acquisition_modal_reducer';
 import textbookModal from './textbook_modal_reducer';
 import finalExamsModal from './final_exams_modal_reducer';
@@ -54,7 +54,7 @@ const rootReducer = combineReducers({
   saveCalendarModal,
   userAcquisitionModal,
   termsOfServiceModal,
-  termsOfServiceBannerModal,
+  termsOfServiceBanner,
   textbookModal,
   finalExamsModal,
 });
