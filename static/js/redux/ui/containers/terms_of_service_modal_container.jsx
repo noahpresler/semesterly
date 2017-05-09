3946f1795bc4f53f7f0d13a2bb15f0269c25ef42
Eric Calder <ecalder6@jhu.edu>
2017-05-08 21:27:31 -0400

    TOS: misc bug fix.

--- static/js/redux/ui/containers/terms_of_service_modal_container.jsx
+++ static/js/redux/ui/containers/terms_of_service_modal_container.jsx
@@ -1,5 +1,5 @@
 import { connect } from 'react-redux';
-import { TermsOfServiceModal } from '../terms_of_service_modal';
+import TermsOfServiceModal from '../terms_of_service_modal';
 import { triggerTermsOfServiceModal } from '../../constants/actionTypes';
 
 const mapStateToProps = state => ({
