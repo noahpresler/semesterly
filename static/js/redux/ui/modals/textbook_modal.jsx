5ef46b97039afdf46aa8ad59af391d1f72179bf3
Noah Presler <noah@presler.me>
2017-05-15 19:30:40 -0400

    create modals directories

--- static/js/redux/ui/modals/textbook_modal.jsx
+++ static/js/redux/ui/modals/textbook_modal.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import Modal from 'boron/WaveModal';
-import Textbook from './textbook';
-import * as PropTypes from '../constants/propTypes';
+import Textbook from '../textbook';
+import * as PropTypes from '../../constants/propTypes';
 
 class TextbookModal extends React.Component {
   componentDidMount() {
