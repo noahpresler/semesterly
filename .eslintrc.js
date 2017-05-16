daf3b6604f8bba58987751a9da56e092b3c4d045
Noah Presler <noah@presler.me>
2017-05-16 09:03:17 -0400

    snapshot test of course modal

--- .eslintrc.js
+++ .eslintrc.js
@@ -9,11 +9,12 @@ module.exports = {
 		"_": true,
 		"initData": true, // homepage context data passed from backend
 		"reactAlertEvents": true,
-		"twemoji": true,
 	},
 	rules: {
 		"no-plusplus": 0,
-    "jsx-a11y/no-static-element-interactions": 0
+    "jsx-a11y/no-static-element-interactions": 0,
+		"jsx-a11y/no-noninteractive-element-interactions": 0,
+		"react/jsx-max-props-per-line": 0
 	},
 	extends: "airbnb",
 };
