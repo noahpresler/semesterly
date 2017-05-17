a8859b8dd5cacbb8c1b46f0c59c848ccb66564c0
felixzhuologist <felix.czhu@gmail.com>
2017-05-17 15:52:06 -0400

    PEP8 reformatting

--- searches/views.py
+++ searches/views.py
@@ -3,49 +3,31 @@ import operator
 from braces.views import CsrfExemptMixin
 from django.core.paginator import Paginator, EmptyPage
 from django.db.models import Q
-from django.views.decorators.csrf import csrf_exempt
-from rest_framework.views import APIView
-from rest_framework.response import Response
 from rest_framework import status
+from rest_framework.response import Response
+from rest_framework.views import APIView
 
 from analytics.views import save_analytics_course_search
+from courses.serializers import get_detailed_course_json, get_basic_course_json
+from searches.utils import get_course_matches
 from student.models import Student
 from student.utils import get_student
-from timetable.models import Semester, Course
+from timetable.models import Semester
 from timetable.utils import ValidateSubdomainMixin
-from courses.serializers import get_detailed_course_json, get_basic_course_json
-
-
-def get_course_matches(school, query, semester):
-    if query == "":
-        return Course.objects.filter(school=school)
-
-    query_tokens = query.split()
-    course_name_contains_query = reduce(
-        operator.and_, map(course_name_contains_token, query_tokens))
-    return Course.objects.filter(
-        Q(school=school) &
-        course_name_contains_query &
-        Q(section__semester=semester)
-    ).distinct()
-
-
-def course_name_contains_token(token):
-    return (Q(code__icontains=token) |
-            Q(name__icontains=token.replace("&", "and")) |
-            Q(name__icontains=token.replace("and", "&")))
 
 
 class CourseSearchList(CsrfExemptMixin, ValidateSubdomainMixin, APIView):
-
     def get(self, request, query, sem_name, year):
+        """ Return basic search results. """
         sem = Semester.objects.get_or_create(name=sem_name, year=year)[0]
         course_match_objs = get_course_matches(request.subdomain, query, sem)[:4]
-        save_analytics_course_search(query[:200], course_match_objs[:2], sem, request.subdomain, get_student(request))
+        save_analytics_course_search(query[:200], course_match_objs[:2], sem, request.subdomain,
+                                     get_student(request))
         course_matches = [get_basic_course_json(course, sem) for course in course_match_objs]
         return Response(course_matches, status=status.HTTP_200_OK)
 
     def post(self, request, query, sem_name, year):
+        """ Return advanced search results. """
         school = request.subdomain
         page = int(request.query_params.get('page', 1))
         sem, _ = Semester.objects.get_or_create(name=sem_name, year=year)
@@ -57,17 +39,20 @@ class CourseSearchList(CsrfExemptMixin, ValidateSubdomainMixin, APIView):
         if request.data.get('areas'):
             course_match_objs = course_match_objs.filter(areas__in=request.data.get('areas'))
         if request.data.get('departments'):
-            course_match_objs = course_match_objs.filter(department__in=request.data.get('departments'))
+            course_match_objs = course_match_objs.filter(
+                department__in=request.data.get('departments'))
         if request.data.get('levels'):
             course_match_objs = course_match_objs.filter(level__in=request.data.get('levels'))
         if request.data.get('times'):
-            day_map = {"Monday": "M", "Tuesday": "T", "Wednesday": "W", "Thursday": "R", "Friday": "F"}
+            day_map = {"Monday": "M", "Tuesday": "T", "Wednesday": "W", "Thursday": "R",
+                       "Friday": "F"}
             course_match_objs = course_match_objs.filter(
-                reduce(operator.or_, (Q(section__offering__time_start__gte="{0:0=2d}:00".format(min_max['min']),
-                                        section__offering__time_end__lte="{0:0=2d}:00".format(min_max['max']),
-                                        section__offering__day=day_map[min_max['day']],
-                                        section__semester=sem,
-                                        section__section_type="L") for min_max in request.data.get('times'))
+                reduce(operator.or_,
+                       (Q(section__offering__time_start__gte="{0:0=2d}:00".format(min_max['min']),
+                          section__offering__time_end__lte="{0:0=2d}:00".format(min_max['max']),
+                          section__offering__day=day_map[min_max['day']],
+                          section__semester=sem,
+                          section__section_type="L") for min_max in request.data.get('times'))
                        )
             )
         try:
@@ -76,12 +61,14 @@ class CourseSearchList(CsrfExemptMixin, ValidateSubdomainMixin, APIView):
         except EmptyPage:
             return Response([])
 
-        save_analytics_course_search(query[:200], course_match_objs[:2], sem, school, get_student(request),
+        save_analytics_course_search(query[:200], course_match_objs[:2], sem, school,
+                                     get_student(request),
                                      advanced=True)
         student = None
         logged = request.user.is_authenticated()
         if logged and Student.objects.filter(user=request.user).exists():
             student = Student.objects.get(user=request.user)
-        json_data = [get_detailed_course_json(request.subdomain, course, sem, student) for course in course_match_objs]
+        json_data = [get_detailed_course_json(request.subdomain, course, sem, student) for course in
+                     course_match_objs]
 
         return Response(json_data, status=status.HTTP_200_OK)
