4f8bc3a1267c9158848969decf504b7d67c472ec
felixzhuologist <felix.czhu@gmail.com>
2017-05-28 22:05:27 -0400

    PEP8 and code cleanup

--- agreement/urls.py
+++ agreement/urls.py
@@ -2,9 +2,12 @@ from django.conf.urls import patterns, url
 from django.contrib import admin
 from django.views.generic import TemplateView
 
+
 admin.autodiscover()
 
 urlpatterns = patterns('',
-                       url(r'termsofservice/*$', TemplateView.as_view(template_name="termsofservice.html")),
-                       url(r'privacypolicy/*$', TemplateView.as_view(template_name="privacypolicy.html")),
+                       url(r'termsofservice/*$',
+                           TemplateView.as_view(template_name="termsofservice.html")),
+                       url(r'privacypolicy/*$',
+                           TemplateView.as_view(template_name="privacypolicy.html")),
                        )
