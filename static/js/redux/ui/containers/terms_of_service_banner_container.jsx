e659bc19c3eced75608a2213f9802c105eac3fe7
Noah Presler <noah@presler.me>
2017-05-28 22:01:52 -0400

    test working

--- static/js/redux/ui/containers/terms_of_service_banner_container.jsx
+++ static/js/redux/ui/containers/terms_of_service_banner_container.jsx
@@ -1,10 +1,12 @@
 import { connect } from 'react-redux';
 import TermsOfServiceBanner from '../terms_of_service_banner';
 import { triggerTermsOfServiceBanner, dismissTermsOfServiceBanner } from '../../actions/modal_actions';
+import { acceptTOS } from '../../actions/user_actions';
 
 const mapStateToProps = state => ({
   isVisible: state.termsOfServiceBanner.isVisible,
   userInfo: state.userInfo.data,
+  acceptTOS,
 });
 
 const TermsOfServiceBannerContainer = connect(
