e8042d325fcba376d669102abdfabd37645cf81e
Noah Presler <noah@presler.me>
2017-05-29 15:34:31 -0400

    refactor store

--- static/js/redux/__tests__/modals/terms_of_service_modal_test.jsx
+++ static/js/redux/__tests__/modals/terms_of_service_modal_test.jsx
@@ -1,13 +1,32 @@
 import React from 'react';
+import thunk from 'redux-thunk';
+import Provider from 'react-redux/src/components/Provider';
+import configureMockStore from 'redux-mock-store';
 import renderer from 'react-test-renderer';
 import TermsOfServiceModal from '../../ui/terms_of_service_modal';
 import { unacceptedFixture } from '../../__fixtures__/terms_of_service_modal.fixture';
+import TermsOfServiceModalContainer from '../../ui/containers/terms_of_service_modal_container';
 
-it('TOS renders as expected if time_accepted is blank', () => {
+const middlewares = [thunk];
+const mockStore = configureMockStore(middlewares);
+
+it('TOS Modal renders as expected if visible', () => {
   const tree = renderer.create(
     <TermsOfServiceModal {...unacceptedFixture} />,
   ).toJSON();
   delete tree.children[0].children[0].props.style.animationName;
   delete tree.children[1].props.style.animationName;
+  expect(tree).toMatchSnapshot();
+});
+
+it('TOS Modal Container renders if isVisible', () => {
+  const store = mockStore({
+    termsOfService: {
+      isVisible: true,
+    },
+  });
+  const tree = renderer.create(
+    <Provider store={store}><TermsOfServiceModalContainer /></Provider>,
+  ).toJSON();
   expect(tree).toMatchSnapshot();
 });
