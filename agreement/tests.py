d1ccf4ec414fc72e53538c891c3651f2355c13af
felixzhuologist <felix.czhu@gmail.com>
2017-05-28 23:36:11 -0400

    Remove unused import

--- agreement/tests.py
+++ agreement/tests.py
@@ -1,21 +1,8 @@
-from datetime import datetime, timedelta
+from django.test import TestCase
 
-import pytz
-from rest_framework.test import APITestCase, APIRequestFactory
-from rest_framework import status
 
-from django.contrib.auth.models import User, AnonymousUser
-from student.models import Student
-from agreement.models import Agreement
+class UrlsTest(TestCase):
 
-class AgreementTest(APITestCase):
-    def setUp(self):
-        self.user = User.objects.create_user(username='jacob', password='top_secret')
-        self.anon = AnonymousUser
-        self.student = Student.objects.create(user=self.user)
-        self.agreement_time = pytz.utc.localize(datetime.utcnow())
-        self.agreement = Agreement.objects.create(last_updated=self.agreement_time)
-        self.factory = APIRequestFactory()
-    
-    def test_agreement_modal(self):
-        pass
+    def test_urls_call_correct_templates(self):
+        self.assertTemplateUsed(self.client.get('/termsofservice'), 'termsofservice.html')
+        self.assertTemplateUsed(self.client.get('/privacypolicy'), 'privacypolicy.html')
