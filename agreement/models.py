769fa64943ccae9619ef0ce1f975b0436ece2002
felixzhuologist <felix.czhu@gmail.com>
2017-05-28 22:05:27 -0400

    PEP8 and code cleanup

--- agreement/models.py
+++ agreement/models.py
@@ -2,7 +2,7 @@ from __future__ import unicode_literals
 
 from django.db import models
 
+
 class Agreement(models.Model):
-    """ Database object representing when terms of service is last updated.
-    """
+    """ Database object representing when terms of service is last updated. """
     last_updated = models.DateTimeField()
