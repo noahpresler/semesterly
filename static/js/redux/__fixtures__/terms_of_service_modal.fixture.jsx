e9fa50aba74200627746ea80fa9bdcad8267d6f6
Noah Presler <noah@presler.me>
2017-05-28 22:01:52 -0400

    test working

---
+++ static/js/redux/__fixtures__/terms_of_service_modal.fixture.jsx
@@ -0,0 +1,38 @@
+/* eslint-disable no-useless-escape */
+
+export const unacceptedFixture = {
+  isVisible: true,
+  acceptTOS: () => null,
+  userInfo: {
+    GoogleLoggedIn: false,
+    major: 'Accounting',
+    emails_enabled: true,
+    GoogleSignedUp: false,
+    school: 'jhu',
+    fbook_uid: '10206009352974277',
+    gender: 'male',
+    timetables: [
+
+    ],
+    FacebookSignedUp: true,
+    social_offerings: false,
+    class_year: 2017,
+    social_all: false,
+    userFirstName: 'Noah',
+    integrations: [
+
+    ],
+    isLoggedIn: true,
+    userLastName: 'Presler',
+    LoginHash: 'KR',
+    time_accepted_tos: null,
+    social_courses: true,
+    LoginToken: '1dF7iR:soPf19ymwY4E9Bns0mxCbnA__C8',
+    img_url: 'https:\/\/scontent.xx.fbcdn.net\/v\/t1.0-1\/p50x50\/18193968_10208' +
+    '799143997309_4182518417368859549_n.jpg?oh=c2805b229bc9b4a785c7c78d8520eaf3&oe=59AECF0A',
+  },
+};
+
+export const acceptedFixture = {
+
+};
