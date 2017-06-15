366e19a632bea1288beb919a76f2b9c1f0da417a
felixzhuologist <felix.czhu@gmail.com>
2017-06-14 23:57:07 -0700

    Remove sid usage

--- static/js/redux/util.jsx
+++ static/js/redux/util.jsx
@@ -1,10 +1,5 @@
 import { getLogFinalExamViewEndpoint } from './constants/endpoints';
 
-export const randomString = (length = 30, chars = '!?()*&^%$#@![]0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ') => {
-  let result = '';
-  for (let i = length; i > 0; --i) result += chars[Math.floor(Math.random() * chars.length)];
-  return result;
-};
 export const browserSupportsLocalStorage = () => {
   try {
     localStorage.setItem('test', 'test');
