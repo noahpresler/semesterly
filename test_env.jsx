684dc98879d7672c8d8d62cf492c0712847202f2
felixzhuologist <felix.czhu@gmail.com>
2017-06-14 21:47:28 -0700

    Add maybeSetSemester tests

--- test_env.jsx
+++ test_env.jsx
@@ -1,6 +1,9 @@
 /* eslint-disable */
 import $ from 'jquery';
+import _ from 'underscore';
+import 'babel-polyfill'; // e.g. Object.values()
 
 window.$ = global.$ = global.jQuery = $;
+global._ = _;
 
 /* eslint-enable */
