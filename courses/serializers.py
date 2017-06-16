ab84ba041ce7596fd7be60977d58d9a13cfd0745
felixzhuologist <felix.czhu@gmail.com>
2017-06-15 20:32:18 -0700

    Move course serialization code into CourseSerializer class

--- courses/serializers.py
+++ courses/serializers.py
@@ -1,47 +1,119 @@
 import itertools
+import re
+from operator import attrgetter
 
 from django.forms import model_to_dict
+from django.db import models
 from rest_framework import serializers
 
-from timetable.models import Course
+from timetable.models import Course, Evaluation, CourseIntegration, Integration
+
+
+class EvaluationSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Evaluation
+        fields = '__all__'
 
 
 class CourseSerializer(serializers.ModelSerializer):
+    eval_info = serializers.SerializerMethodField()
+    integrations = serializers.SerializerMethodField()
+    related_courses = serializers.SerializerMethodField()
+    reactions = serializers.SerializerMethodField()
+    textbooks = serializers.SerializerMethodField()
+    regexed_courses = serializers.SerializerMethodField()
+    popularity_percent = serializers.SerializerMethodField()
+    sections = serializers.SerializerMethodField()
+
+    def get_eval_info(self, course):
+        """
+        Flag all eval instances s.t. there exists repeated term+year values.
+        Return:
+          List of modified evaluation dictionaries (added flag 'unique_term_year')
+        """
+        evals = map(model_to_dict, Evaluation.objects.filter(course=course).order_by('year'))
+        years = Evaluation.objects.filter(course=course).values('year').annotate(models.Count('id')) \
+            .filter(id__count__gt=1).values_list('year')
+        years = {e[0] for e in years}
+        for course_eval in evals:
+            course_eval['unique_term_year'] = not course_eval['year'] in years
+        return evals
+
+    def get_integrations(self, course):
+        ids = CourseIntegration.objects.filter(course__id=course.id).values_list("integration",
+                                                                                 flat=True)
+        return list(Integration.objects.filter(id__in=ids).values_list("name", flat=True))
+
+    def get_related_courses(self, course):
+        info = []
+        related = course.related_courses.filter(
+            section__semester=self.context['semester']).distinct()[:5]
+        for course in related:
+            info.append(model_to_dict(course, exclude=['related_courses', 'unstopped_description']))
+        return info
+
+    def get_reactions(self, course):
+        return course.get_reactions(self.context.get('student'))
+
+    def get_textbooks(self, course):
+        return list(
+            set(tb for section in course.section_set.filter(semester=self.context['semester'])
+                for tb in section.get_textbooks()))
+
+    def get_regexed_courses(self, course):
+        """
+        Given course data, search for all occurrences of a course code in the course description and
+        prereq info and return a map from course code to course name for each course code.
+        """
+        school_to_course_regex = {
+            'jhu': r'([A-Z]{2}\.\d{3}\.\d{3})',
+            'uoft': r'([A-Z]{3}[A-Z0-9]\d{2}[HY]\d)',
+            'vandy': r'([A-Z-&]{2,7}\s\d{4}[W]?)',
+            'gw': r'([A-Z]{2,5}\s\d{4}[W]?)',
+            'umich': r'([A-Z]{2,8}\s\d{3})',
+            'chapman': r'([A-Z]{2,4}\s\d{3})',
+            'salisbury': r'([A-Z]{3,4} \d{2,3})',
+        }
+        course_code_to_name = {}
+        if self.context['school'] in school_to_course_regex:
+            course_code_matches = re.findall(school_to_course_regex[self.context['school']],
+                                             course.description + course.prerequisites)
+            # TODO: get all course objects in one db access
+            for course_code in course_code_matches:
+                try:
+                    course = Course.objects.get(school=self.context['school'],
+                                                code__icontains=course_code)
+                    course_code_to_name[course_code] = course.name
+                except (Course.DoesNotExist, Course.MultipleObjectsReturned):
+                    pass
+        return course_code_to_name
+
+    def get_popularity_percent(self, course):
+        """ Return percentage of course capacity that is filled by registered students. """
+        tts_with_course = course.personaltimetable_set.filter(semester=self.context['semester'])
+        num_students_in_course = tts_with_course.values('student').distinct().count()
+        sections = course.section_set.filter(semester=self.context['semester'])
+        course_capacity = sum(sections.values_list('size', flat=True)) if sections else 0
+        return num_students_in_course / float(course_capacity) if course_capacity else 0
+
+    def get_sections(self, course):
+        section_type_to_sections = {}
+        course_section_list = sorted(course.section_set.filter(semester=self.context['semester']),
+                                     key=attrgetter('section_type'))
+
+        for section_type, sections in itertools.groupby(course_section_list,
+                                                        attrgetter('section_type')):
+            section_type_to_sections[section_type] = {
+                section.meeting_section: [get_section_offerings(section) for section in sections]
+            }
+        return section_type_to_sections
+
     class Meta:
         model = Course
-        fields = ('code', 'name', 'id', 'description', 'department', 'num_credits', 'areas', 'campus')
-
-
-def get_detailed_course_json(school, course, sem, student=None):
-    json_data = get_basic_course_json(course, sem, ['prerequisites', 'exclusions', 'areas'])
-    json_data['eval_info'] = course.eval_add_unique_term_year_flag()
-    json_data['related_courses'] = course.get_related_course_info(sem, limit=5)
-    json_data['reactions'] = course.get_reactions(student)
-    json_data['textbooks'] = course.get_textbooks(sem)
-    json_data['integrations'] = list(course.get_course_integrations())
-    json_data['regexed_courses'] = course.get_regexed_courses(school)
-    json_data['popularity_percent'] = course.get_percentage_enrolled(sem)
-    return json_data
-
-
-def get_basic_course_json(course, sem, extra_model_fields=None):
-    extra_model_fields = extra_model_fields or []
-    basic_fields = 'code name id description department num_credits areas campus'.split()
-    course_json = model_to_dict(course, basic_fields + extra_model_fields)
-    course_json['evals'] = course.get_eval_info()
-    course_json['integrations'] = list(course.get_course_integrations())
-    course_json['sections'] = {}
-
-    course_section_list = sorted(course.section_set.filter(semester=sem),
-                                 key=lambda section: section.section_type)
-
-    # TODO: flatten dictionary with one key
-    for section_type, sections in itertools.groupby(course_section_list, lambda s: s.section_type):
-        course_json['sections'][section_type] = {
-            section.meeting_section: get_section_offerings(section) for section in sections
-        }
-
-    return course_json
+        fields = ('code', 'name', 'id', 'description', 'department', 'num_credits', 'areas',
+                  'campus', 'eval_info', 'integrations', 'related_courses', 'reactions', 'textbooks',
+                  'regexed_courses', 'popularity_percent', 'sections', 'prerequisites',
+                  'exclusions', 'areas')
 
 
 def get_section_offerings(section):
@@ -61,6 +133,7 @@ def augment_course_dict(course_dict, sections):
                     for _, section, course_offerings in sections
                     for co in course_offerings]
     course_dict['enrolled_sections'] = [section.meeting_section for _, section, _ in sections]
-    course_dict['textbooks'] = {section.meeting_section: section.get_textbooks() for _, section, _ in sections}
+    course_dict['textbooks'] = {section.meeting_section: section.get_textbooks() for _, section, _
+                                in sections}
     course_dict['slots'] = slot_objects
     return course_dict
