d721cdbc2194f115c9c33b2c37b22f16c65cfc05
felixzhuologist <felix.czhu@gmail.com>
2017-06-16 18:56:57 -0700

    Move sectionTypeToSections code to frontend

--- static/js/redux/__tests__/selectors.test.jsx
+++ static/js/redux/__tests__/selectors.test.jsx
@@ -1,10 +1,27 @@
 import * as selectors from '../reducers/root_reducer';
+import { getSectionTypeToSections } from '../reducers/search_results_reducer';
 
 describe('timetable selectors', () => {
   describe('active TT selector', () => {
     it('gets correct timetable', () => {
       const state = { timetables: { items: [0, 1, 2, 3, 4], active: 2 } };
       expect(selectors.getActiveTT(state)).toEqual(2);
+    });
+  });
+});
+
+describe('course selectors', () => {
+  describe('section type to sections selector', () => {
+    it('returns correct mapping', () => {
+      const xOne = { section_type: 'x', meeting_section: 'A' };
+      const xTwo = { section_type: 'x', meeting_section: 'B' };
+      const yOne = { section_type: 'y', meeting_section: 'C' };
+
+      const state = { sections: [xOne, xTwo, yOne] };
+      expect(getSectionTypeToSections(state)).toEqual({
+        x: [xOne, xTwo],
+        y: [yOne],
+      });
     });
   });
 });
