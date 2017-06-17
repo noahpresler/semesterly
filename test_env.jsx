12eac9f0dbb6a7caa284b3dfff39086e1dd88af3
felixzhuologist <felix.czhu@gmail.com>
2017-06-17 13:28:18 -0700

    Migrate from underscore to lodash
    Reasons for the change:
    1. Lodash is a superset of underscore and is better maintained. lodash.merge is
    used for the entity reducer but is not available in underscore.
    2. Lodash is also now installed as an npm module instead of included in
    html which removes the need to use the _ global

--- test_env.jsx
+++ test_env.jsx
@@ -1,9 +1,7 @@
 /* eslint-disable */
 import $ from 'jquery';
-import _ from 'underscore';
 import 'babel-polyfill'; // e.g. Object.values()
 
 window.$ = global.$ = global.jQuery = $;
-global._ = _;
 
 /* eslint-enable */
