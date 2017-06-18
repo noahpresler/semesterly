4b4fd140a4fdb3a248a86950b1066f14741ac88b
felixzhuologist <felix.czhu@gmail.com>
2017-06-17 23:11:29 -0700

    Use normalized course data for searchResults

--- static/js/redux/__tests__/schema.test.js
+++ static/js/redux/__tests__/schema.test.js
@@ -2,7 +2,15 @@ import { normalize } from 'normalizr';
 import * as schemas from '../schema';
 
 describe('course schema', () => {
-  const unnormalized = [{
+  const normalized = {
+      result: ['C1'],
+      entities: {
+        courses: { C1: { id: 1, code: 'C1', sections: ['C1-S1'] } },
+        sections: { 'C1-S1': { meeting_section: 'S1', offering_set: [1] } },
+        offerings: { 1: { id: 1, day: 'M' } },
+      },
+    };
+  const denormalized = [{
     id: 1, // course
     code: 'C1',
     sections: [{ // sections
@@ -13,15 +21,12 @@ describe('course schema', () => {
       }],
     }],
   }];
-  it('gets normalized correctly', () => {
-    const normalized = normalize(unnormalized, [schemas.courseSchema]);
-    expect(normalized).toEqual({
-      result: ['C1'],
-      entities: {
-        courses: { C1: { id: 1, code: 'C1', sections: ['C1-S1'] } },
-        sections: { 'C1-S1': { meeting_section: 'S1', offering_set: [1] } },
-        offerings: { 1: { id: 1, day: 'M' } },
-      },
-    });
+  it('normalizes course array (e.g. search results) correctly', () => {
+    const result = normalize(denormalized, [schemas.courseSchema]);
+    expect(result).toEqual(normalized);
+  });
+  it('normalizes singel course correctly', () => {
+    const result = normalize(denormalized[0], schemas.courseSchema);
+    expect(result).toEqual({ ...normalized, result: 'C1' });
   });
 });
