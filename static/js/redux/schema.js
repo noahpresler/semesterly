e21c021970e21bf3178b2d181b35050e8c59aad9
felixzhuologist <felix.czhu@gmail.com>
2017-06-19 14:54:38 -0700

    Get timetable response normalization working

--- static/js/redux/schema.js
+++ static/js/redux/schema.js
@@ -1,4 +1,5 @@
 import { schema } from 'normalizr';
+import { strPropertyCmp } from './util';
 
 export const offeringSchema = new schema.Entity('offering_set');
 
@@ -8,4 +9,13 @@ export const sectionSchema = new schema.Entity('sections', { offering_set: [offe
 
 export const courseSchema = new schema.Entity('courses', { sections: [sectionSchema] }, {
   idAttribute: value => value.code,
+});
+
+export const serializeTimetable = timetable =>
+    timetable.courses.sort(strPropertyCmp('id')).map(course => {
+      return `${course.id}:${course.sections.map(section => section.id).join(',')}`;
+    }).join(';');
+
+export const timetableSchema = new schema.Entity('timetables', { courses: [courseSchema] }, {
+  idAttribute: value => serializeTimetable(value),
 });
