149ba9fc2e4054fadf594bf7035fb45f2d2f55ea
felixzhuologist <felix.czhu@gmail.com>
2017-06-19 14:54:38 -0700

    Get timetable response normalization working

--- static/js/redux/__tests__/schema.test.js
+++ static/js/redux/__tests__/schema.test.js
@@ -2,19 +2,22 @@ import { normalize } from 'normalizr';
 import { getDenormCourseById } from '../reducers/entities_reducer';
 import * as schemas from '../schema';
 
+const course = {
+  result: ['C1'],
+  entities: {
+    courses: { C1: { id: 1, code: 'C1', sections: ['C1-S1'] } },
+    sections: { 'C1-S1': { id: 2, meeting_section: 'S1', offering_set: [1] } },
+    offering_set: { 1: { id: 1, day: 'M' } },
+  },
+};
+
 describe('course schema', () => {
-  const normalized = {
-      result: ['C1'],
-      entities: {
-        courses: { C1: { id: 1, code: 'C1', sections: ['C1-S1'] } },
-        sections: { 'C1-S1': { meeting_section: 'S1', offering_set: [1] } },
-        offering_set: { 1: { id: 1, day: 'M' } },
-      },
-    };
+  const normalized = course;
   const denormalized = [{
     id: 1, // course
     code: 'C1',
     sections: [{ // sections
+      id: 2,
       meeting_section: 'S1',
       offering_set: [{ // offerings
         id: 1,
@@ -32,5 +35,27 @@ describe('course schema', () => {
   });
   it('denormalizes single course correctly', () => {
     expect(getDenormCourseById(normalized.entities, 'C1')).toEqual(denormalized[0]);
+  });
+  it('normalizes timetable correctly', () => {
+    const tt = { courses: denormalized, name: 'my_tt' };
+    const result = normalize(tt, schemas.timetableSchema);
+    const ttId = schemas.serializeTimetable(tt);
+    expect(result).toEqual({
+      result: ttId,
+      entities: {
+        ...normalized.entities,
+        timetables: { [ttId]: { name: 'my_tt', courses: ['C1'] } },
+      },
+    });
+  });
+});
+
+describe('timetable serializer', () => {
+  it('serializes timetable with multiple sections and courses', () => {
+    const courses = [
+      { id: 'C1', sections: [ { id: 'S1' }, { id: 'S2' }, { id: 'S3' } ] },
+      { id: 'C2', sections: [ { id: 'S4' } ] },
+    ];
+    expect(schemas.serializeTimetable({ courses })).toEqual('C1:S1,S2,S3;C2:S4')
   })
 });
