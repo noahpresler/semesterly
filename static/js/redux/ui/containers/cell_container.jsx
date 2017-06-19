0ab265050e5a29e53010930d7f81fd364c6aefa6
felixzhuologist <felix.czhu@gmail.com>
2017-06-19 13:05:24 -0700

    Use getActiveTT selector

--- static/js/redux/ui/containers/cell_container.jsx
+++ static/js/redux/ui/containers/cell_container.jsx
@@ -1,14 +1,13 @@
 import { connect } from 'react-redux';
 import Cell from '../calendar_cell';
+import { getActiveTT } from '../../reducers/root_reducer';
 import { addCustomSlot, updateCustomSlot } from '../../actions/timetable_actions';
 import { getMaxEndHour } from '../../util';
 
 const mapStateToProps = (state) => {
-  const timetables = state.timetables.items;
-  const active = state.timetables.active;
-  const hasTimetables = timetables[active].courses.length > 0;
+  const hasTimetables = getActiveTT(state).courses.length > 0;
   return {
-    endHour: getMaxEndHour(timetables[active], hasTimetables),
+    endHour: getMaxEndHour(getActiveTT(state), hasTimetables),
   };
 };
 
