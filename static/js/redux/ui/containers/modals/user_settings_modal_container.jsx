b14e5a9f4948d450c5ded07f84360cac46e97c19
Noah Presler <noah@presler.me>
2017-06-19 23:26:05 -0400

    fix user signup flow

--- static/js/redux/ui/containers/modals/user_settings_modal_container.jsx
+++ static/js/redux/ui/containers/modals/user_settings_modal_container.jsx
@@ -1,7 +1,14 @@
 import { connect } from 'react-redux';
 import UserSettingsModal from '../../modals/user_settings_modal';
-import { saveSettings, setARegistrationToken, unRegisterAToken } from '../../../actions/user_actions';
-import { changeUserInfo, overrideSettingsShow } from '../../../actions/modal_actions';
+import {
+  acceptTOS, saveSettings, setARegistrationToken,
+  unRegisterAToken,
+} from '../../../actions/user_actions';
+import {
+  changeUserInfo, overrideSettingsShow, setUserSettingsModalHidden,
+  setUserSettingsModalVisible,
+} from '../../../actions/modal_actions';
+import { userPreferencesIncomplete } from '../../../reducers/root_reducer';
 
 const mapStateToProps = state => ({
   userInfo: state.userInfo.data,
@@ -9,14 +16,19 @@ const mapStateToProps = state => ({
   hideOverrided: state.userInfo.overrideHide,
   tokenRegistered: state.notificationToken.hasToken,
   highlightNotifs: state.ui.highlightNotifs,
+  userPreferencesIncomplete: userPreferencesIncomplete(state),
+  signingUp: !state.userInfo.overrideShow && userPreferencesIncomplete(state),
 });
 
 const UserSettingsModalContainer = connect(
     mapStateToProps,
   {
     saveSettings,
+    acceptTOS,
     closeUserSettings: () => overrideSettingsShow(false),
     changeUserInfo,
+    setVisible: setUserSettingsModalVisible,
+    setHidden: setUserSettingsModalHidden,
     subscribeToNotifications: setARegistrationToken,
     unsubscribeToNotifications: unRegisterAToken,
   },
