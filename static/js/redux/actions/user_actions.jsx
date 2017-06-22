208f39b0467171f40dadcc5c62af01f2b9cb3f40
felixzhuologist <felix.czhu@gmail.com>
2017-06-21 19:00:34 -0700

    Add getActiveTimetableCourses and use where possible

--- static/js/redux/actions/user_actions.jsx
+++ static/js/redux/actions/user_actions.jsx
@@ -15,7 +15,7 @@ import {
     getSetRegistrationTokenEndpoint,
     acceptTOSEndpoint,
 } from '../constants/endpoints';
-import { getActiveTT, getCurrentSemester } from '../reducers/root_reducer';
+import { getActiveTT, getActiveTimetableCourses, getCurrentSemester } from '../reducers/root_reducer';
 import { fetchCourseClassmates } from './modal_actions';
 import { getNumberedName, loadTimetable, nullifyTimetable } from './timetable_actions';
 import { MAX_TIMETABLE_NAME_LENGTH } from '../constants/constants';
@@ -108,7 +108,7 @@ export const fetchClassmates = courses => (dispatch, getState) => {
     return;
   }
   setTimeout(() => {
-    dispatch(fetchMostClassmatesCount(getActiveTT(state).courses.map(c => c.id)));
+    dispatch(fetchMostClassmatesCount(getActiveTimetableCourses(state).map(c => c.id)));
   }, 500);
   dispatch(requestClassmates());
   fetch(getClassmatesEndpoint(getCurrentSemester(state), courses), {
@@ -335,9 +335,8 @@ export const saveSettings = callback => (dispatch, getState) => {
   })
     .then((response) => {
       const state = getState();
-      const activeTT = getActiveTT(state);
       if (state.userInfo.data.social_courses) {
-        dispatch(fetchClassmates(activeTT.courses.map(c => c.id)));
+        dispatch(fetchClassmates(getActiveTimetableCourses(state).map(c => c.id)));
         if (state.courseInfo.id) {
           dispatch(fetchCourseClassmates(state.courseInfo.id));
         }
@@ -400,7 +399,7 @@ export const fetchFriends = () => (dispatch, getState) => {
 export const autoSave = (delay = 2000) => (dispatch, getState) => {
   const state = getState();
   clearTimeout(autoSaveTimer);
-  const numTimetables = getActiveTT(state).courses.length;
+  const numTimetables = getActiveTimetableCourses(state).length;
   const numEvents = state.customSlots.length;
   autoSaveTimer = setTimeout(() => {
     if (state.userInfo.data.isLoggedIn && numTimetables + numEvents > 0) {
