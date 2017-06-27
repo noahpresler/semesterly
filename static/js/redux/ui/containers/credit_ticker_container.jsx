f1b2e7f52a08748dab2d42c056b719a6c5cbf5bf
felixzhuologist <felix.czhu@gmail.com>
2017-06-26 21:30:27 -0700

    Clean up code

--- static/js/redux/ui/containers/credit_ticker_container.jsx
+++ static/js/redux/ui/containers/credit_ticker_container.jsx
@@ -1,21 +1,10 @@
 import { connect } from 'react-redux';
-import { getActiveTT } from '../../reducers/root_reducer';
+import { getActiveTimetableCourses } from '../../reducers/root_reducer';
 import CreditTicker from '../credit_ticker';
 
-const mapStateToProps = (state) => {
-  const liveTimetableCourses = getActiveTT(state).courses.filter(c => !c.fake);
-  const school = state.school.school;
-  let numCredits = 0;
-  if (school === 'uoft') {
-    numCredits = 0.5 * liveTimetableCourses.length;
-  } else {
-    numCredits = liveTimetableCourses.length > 0 ? liveTimetableCourses
-      .reduce((prev, c) => c.num_credits + prev, 0) : 0;
-  }
-  return {
-    numCredits,
-  };
-};
+const mapStateToProps = state => ({
+  numCredits: getActiveTimetableCourses(state).reduce((prev, c) => c.num_credits + prev, 0),
+});
 
 const CreditTickerContainer = connect(
     mapStateToProps,
