601d990d563de6107cb35196ef1369a2fd1c257e
mnm364 <mnm364@gmail.com>
2017-06-28 19:46:58 -0400

    umich parser pep8 compliance

--- scripts/umich/umich_courses.py
+++ scripts/umich/umich_courses.py
@@ -15,50 +15,49 @@
 # @author   Michael N. Miller
 # @date     2/13/17
 
-from __future__ import print_function, division, absolute_import # NOTE: slowly move toward Python3
+from __future__ import absolute_import, division, print_function
 
 from scripts.peoplesoft.courses import UPeoplesoftParser
 
+
 class UmichParser(UPeoplesoftParser):
 
-	def __init__(self, **kwargs):
-		school = 'umich'
-		url = 'https://csprod.dsc.umich.edu/psc/csprodpa/EMPLOYEE/HRMS/c/COMMUNITY_ACCESS.M_SR_SC_CLS_SRCH.GBL'
-		super(UmichParser,self).__init__(school, url,
-			term_base_url='https://csprod.dsc.umich.edu/services/schedofclasses', **kwargs)
+    def __init__(self, **kwargs):
+        school = 'umich'
+        url = 'https://csprod.dsc.umich.edu/psc/csprodpa/EMPLOYEE/HRMS/c/' \
+              'COMMUNITY_ACCESS.M_SR_SC_CLS_SRCH.GBL'
+        base_url = 'https://csprod.dsc.umich.edu/services/schedofclasses'
+        super(UmichParser, self).__init__(school, url,
+                                          term_base_url=base_url,
+                                          **kwargs)
 
-	def start(self,
-		years=None,
-		terms=None,
-		departments=None,
-		textbooks=True,
-		verbosity=3,
-		**kwargs):
+    def start(self,
+              years=None,
+              terms=None,
+              departments=None,
+              textbooks=True,
+              verbosity=3,
+              **kwargs):
 
-		# Hardcoded years-term codes.
-		years_and_terms = {
-			'2016': {
-				'Summer': '2100',
-				'Fall': '2110',
-			},
-			'2017': {
-				'Winter': '2120',
-				'Spring': '2130',
-				'Spring/Summer': '2140',
-				'Summer': '2150',
-				'Fall': '2160',
-			}
-		}
+        # Hardcoded years-term codes.
+        years_and_terms = {
+            '2016': {
+                'Summer': '2100',
+                'Fall': '2110',
+            },
+            '2017': {
+                'Winter': '2120',
+                'Spring': '2130',
+                'Spring/Summer': '2140',
+                'Summer': '2150',
+                'Fall': '2160',
+            }
+        }
 
-		self.parse(
-			years_and_terms=years_and_terms,
-			cmd_years=years,
-			cmd_terms=terms,
-			cmd_departments=departments,
-			cmd_textbooks=textbooks,
-			verbosity=verbosity)
-
-def main():
-	raise NotImplementedError('run parser with manage.py')
-if __name__ == "__main__":
-	main()
+        self.parse(
+            years_and_terms=years_and_terms,
+            cmd_years=years,
+            cmd_terms=terms,
+            cmd_departments=departments,
+            cmd_textbooks=textbooks,
+            verbosity=verbosity)
