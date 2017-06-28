949c2379a0d8b9d39aa522ba50a7db510b1559c5
mnm364 <mnm364@gmail.com>
2017-06-28 19:46:58 -0400

    umich parser pep8 compliance

--- scripts/umich/umich_textbooks.py
+++ scripts/umich/umich_textbooks.py
@@ -12,10 +12,11 @@
 
 from scripts.textbooks.bn_textbook_parser import BNParser
 
+
 class UmichTextbookParser(BNParser):
-    def __init__(self, term="Fall", year=2017,**kwargs):
+    def __init__(self, term="Fall", year=2017, **kwargs):
         super(UmichTextbookParser, self).__init__(
-            "28052", # storeid
+            "28052",  # storeid
             "umichigan.bncollege.com",
             "umich",
             "-",
