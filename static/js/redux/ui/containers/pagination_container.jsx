bd3a271a9d20f63cf9543d477e8958e066ef695c
felixzhuologist <felix.czhu@gmail.com>
2017-06-28 21:22:23 -0700

    Replace all direct references to timetables.items outside of reducer

--- static/js/redux/ui/containers/pagination_container.jsx
+++ static/js/redux/ui/containers/pagination_container.jsx
@@ -1,9 +1,10 @@
 import { connect } from 'react-redux';
 import Pagination from '../pagination';
+import { getActiveTimetableCourses } from '../../reducers/root_reducer';
 import { setActiveTimetable } from '../../actions/timetable_actions';
 
 const mapStateToProps = state => ({
-  count: state.timetables.items.length,
+  count: getActiveTimetableCourses(state).length,
   active: state.timetables.active,
 });
 
