e299240582597d7a6f6e105f6e8e1bf445c7b8bb
felixzhuologist <felix.czhu@gmail.com>
2017-07-06 19:00:43 -0700

    Readd semester tests

--- static/js/redux/__tests__/search_actions.test.jsx
+++ static/js/redux/__tests__/search_actions.test.jsx
@@ -1,70 +1,67 @@
-// import { applyMiddleware, createStore } from 'redux';
-// import thunkMiddleware from 'redux-thunk';
-// import nock from 'nock';
-// import rootReducer from '../reducers/root_reducer';
-// import { maybeSetSemester } from '../actions/search_actions';
-// import {
-//   loggedIn,
-//   emptyTimetable,
-//   withTimetables,
-//   sampleSemesters,
-// } from '../__fixtures__/state';
+import { applyMiddleware, createStore } from 'redux';
+import thunkMiddleware from 'redux-thunk';
+import nock from 'nock';
+import rootReducer from '../reducers/root_reducer';
+import { maybeSetSemester } from '../actions/search_actions';
+import {
+  loggedIn,
+  timetable,
+  withTimetables,
+  sampleSemesters,
+  entities,
+} from '../__fixtures__/state';
 
 
 describe('maybeSetSemester', () => {
-  // TODO: add back after finished normalizing timetable state
-  // const backend = nock('https://localhost')
-  //   .defaultReplyHeaders({ 'Content-Type': 'application/json' });
-  //
-  // it('switches for logged in user', () => {
-  //   const store = createStore(
-  //     rootReducer,
-  //     { userInfo: loggedIn, timetables: withTimetables, semester: sampleSemesters },
-  //     applyMiddleware(thunkMiddleware),
-  //   );
-  //
-  //   const newSemester = 1;
-  //   const { name, year } = sampleSemesters.all[newSemester];
-  //
-  //   backend
-  //     .post('/user/timetables/', () => true)
-  //     .reply(201, {
-  //       saved_timetable: emptyTimetable,
-  //       timetables: [emptyTimetable],
-  //     });
-  //
-  //   backend
-  //     .get(`/user/timetables/${name}/${year}/`)
-  //     .reply(200, []);
-  //   //
-  //   return store.dispatch(maybeSetSemester(1)).then(() => {
-  //     expect(store.getState().semester.current).toEqual(newSemester);
-  //   });
-  // });
-  //
-  // it('switches for anonymous user without timetables', () => {
-  //   const store = createStore(
-  //     rootReducer,
-  //     { semester: sampleSemesters },
-  //     applyMiddleware(thunkMiddleware),
-  //   );
-  //   store.dispatch(maybeSetSemester(1));
-  //   expect(store.getState().semester.current).toEqual(1);
-  // });
-  //
-  // it('does not switch for anonymous user with timetables', () => {
-  //   const store = createStore(
-  //     rootReducer,
-  //     { semester: sampleSemesters, timetables: withTimetables },
-  //     applyMiddleware(thunkMiddleware),
-  //   );
-  //   store.dispatch(maybeSetSemester(1));
-  //   const newState = store.getState();
-  //   expect(newState.alerts.alertChangeSemester).toEqual(true);
-  //   expect(newState.alerts.desiredSemester).toEqual(1);
-  //   expect(newState.semester.current).toEqual(0);
-  // });
-  it('temporary dumb test', () => {
-    expect(true).toEqual(true);
+  const backend = nock('https://localhost')
+    .defaultReplyHeaders({ 'Content-Type': 'application/json' });
+
+  it('switches for logged in user', () => {
+    const store = createStore(
+      rootReducer,
+      { userInfo: loggedIn, timetables: withTimetables, semester: sampleSemesters, entities },
+      applyMiddleware(thunkMiddleware),
+    );
+
+    const newSemester = 1;
+    const { name, year } = sampleSemesters.all[newSemester];
+
+    backend
+      .post('/user/timetables/', () => true)
+      .reply(201, {
+        saved_timetable: timetable,
+        timetables: [timetable],
+      });
+
+    backend
+      .get(`/user/timetables/${name}/${year}/`)
+      .reply(200, { timetables: [], courses: [] });
+
+    return store.dispatch(maybeSetSemester(1)).then(() => {
+      expect(store.getState().semester.current).toEqual(newSemester);
+    });
+  });
+
+  it('switches for anonymous user without timetables', () => {
+    const store = createStore(
+      rootReducer,
+      { semester: sampleSemesters },
+      applyMiddleware(thunkMiddleware),
+    );
+    store.dispatch(maybeSetSemester(1));
+    expect(store.getState().semester.current).toEqual(1);
+  });
+
+  it('does not switch for anonymous user with timetables', () => {
+    const store = createStore(
+      rootReducer,
+      { semester: sampleSemesters, timetables: withTimetables, entities },
+      applyMiddleware(thunkMiddleware),
+    );
+    store.dispatch(maybeSetSemester(1));
+    const newState = store.getState();
+    expect(newState.alerts.alertChangeSemester).toEqual(true);
+    expect(newState.alerts.desiredSemester).toEqual(1);
+    expect(newState.semester.current).toEqual(0);
   });
 });
