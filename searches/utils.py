bd5f4e5625181b23f34726354f2c67a31a49531a
Alex Ahn <alexahn917@gmail.com>
2017-07-08 01:48:04 +0900

    removed non-using part of the code for now

--- searches/utils.py
+++ searches/utils.py
@@ -1,47 +1,42 @@
 from __future__ import division
 import pickle
 import numpy as np
-import math
 import operator
 import os
 from sklearn.feature_extraction.text import CountVectorizer
 from sklearn.feature_extraction.text import TfidfTransformer
-from sklearn import mixture
-from sklearn.decomposition import LatentDirichletAllocation
-import matplotlib.pyplot as plt
 from django.db.models import Q
-from timetable.models import Semester, Course
+from timetable.models import Course
 from nltk.stem.porter import *
 
 
-# Vectorizer class creates a dictionary over courses and build course vectorizer pickle object
+# Vectorizer class creates a dictionary over courses and build course vectorizer pickle object.
 class Vectorizer():
     def __init__(self):
         self.TITLE_WEIGHT = 3
         self.stemmer = PorterStemmer()
 
     def vectorize(self):
-        # get names (titles) and descriptions for creating vocabulary
+        # get names (titles) and descriptions for creating vocabulary.
         raw_word_counts = []
         for course in Course.objects.all():
             raw_word_counts.append(self.get_stem_course(course.name,
                                                         course.description,
                                                         course.areas,
                                                         self.TITLE_WEIGHT))
-        # vectorize course objects
-        count_vectorizer = CountVectorizer(ngram_range=(1, 2),
-                                           stop_words='english')
+        # vectorize course objects.
+        count_vectorizer = CountVectorizer(ngram_range=(1, 2), stop_words='english')
         processed_word_counts = count_vectorizer.fit_transform(raw_word_counts)
         tfidf_tf = TfidfTransformer(use_idf=True).fit(processed_word_counts)
         course_vectors = tfidf_tf.transform(processed_word_counts)
 
-        # save course vector
+        # save course vector to model.
         i = 0
         for course in Course.objects.all():
             self.picklify(course, course_vectors[i])
             i += 1
 
-        # store CountVectorizer in the memory
+        # export CountVectorizer.pickle.
         with open('count_vectorizer.pickle', 'wb') as handle:
             pickle.dump(count_vectorizer, handle,
                         protocol=pickle.HIGHEST_PROTOCOL)
@@ -84,7 +79,6 @@ class Searcher():
         if os.path.exists('count_vectorizer.pickle'):
             with open('count_vectorizer.pickle', 'r') as handle:
                 return pickle.load(handle)
-        print("Count Vectorizer is not yet initialized.")
         return None
 
     def vectorize_query(self, query):
@@ -174,6 +168,7 @@ class Searcher():
             score = self.get_cosine_sim(query_vector, course.vector) + self.match_title(query, course.name)
             scores.append((course, score))
         scores.sort(key=lambda tup: -tup[1])
+
         # duplicate handling
         course_titles = set()
         course_object = []
@@ -195,160 +190,3 @@ class Searcher():
 
     def wordify(self, course_vector):
         print(self.count_vectorizer.inverse_transform(course_vector))
-
-
-# Model performance evaluator
-class Evaluator():
-    def __init__(self):
-        self._ = None
-        self.searcher = Searcher()
-
-    def evaluate(self, query, relevant_courses):
-        retrieved_courses = [i.code for i in
-                             self.searcher.get_all_relevant_courses(query)]
-        num_relevant_courses = len(relevant_courses)
-        num_retrieve_courses = len(retrieved_courses)
-        prec_25, prec_50, prec_75 = 0.0, 0.0, 0.0
-        prec_25_idx = int(num_relevant_courses * (1.0 / 4))
-        prec_50_idx = int(num_relevant_courses * (2.0 / 4))
-        prec_75_idx = int(num_relevant_courses * (3.0 / 4))
-        precision_array = []
-
-        num_retrieved = 0
-        i = 1
-        while num_retrieved < num_relevant_courses:
-            course_code = retrieved_courses[i - 1]
-            if course_code in relevant_courses:
-                num_retrieved += 1.0
-                print(query + ": " + str(num_retrieved))
-            precision_array.append(float(num_retrieved / i))
-            i += 1
-        prec_25 = precision_array[prec_25_idx]
-        prec_50 = precision_array[prec_50_idx]
-        prec_75 = precision_array[prec_75_idx]
-        prec_100 = precision_array[-1]
-        prec_mean_1 = (prec_25 + prec_50 + prec_75) / 3.0
-        by_10 = np.arange(0.0, num_relevant_courses,
-                          (num_relevant_courses + 0.0) / 10.0)
-        by_10 = [int(idx) for idx in by_10]
-        prec_mean_2 = 0.0
-        for i in by_10:
-            prec_mean_2 += precision_array[i]
-        prec_mean_2 = prec_mean_2 / 10
-
-        rank_sum = 0.0
-        rank_log_sum = 0.0
-
-        k_i = 1
-        for i in range(len(retrieved_courses)):
-            if retrieved_courses[i] in relevant_courses:
-                rank_sum += (i + 1) - k_i
-                rank_log_sum += math.log(i + 1) - math.log(k_i)
-                k_i += 1
-
-        recall_norm = 1.0 - (rank_sum / (num_relevant_courses *
-                                         (num_retrieve_courses - num_relevant_courses)))
-
-        prec_norm = 1.0 - (rank_log_sum / (num_retrieve_courses *
-                                           math.log(num_retrieve_courses) -
-                                           (num_retrieve_courses - num_relevant_courses) *
-                                           math.log(num_retrieve_courses - num_relevant_courses) -
-                                           num_relevant_courses *
-                                           math.log(num_relevant_courses)))
-
-        return (prec_25, prec_50, prec_75, prec_100, prec_mean_1, prec_mean_2,
-                prec_norm, recall_norm)
-
-    def evaluate_full(self):
-        total_scores = [0.0] * 8
-        queries = []
-        relevant_courses = []
-        with open("searches/query_test.txt") as f:
-            for line in f.read().split('\n'):
-                query, codes = line.split('->')
-                queries.append(query)
-                relevant_courses.append(codes.split(', '))
-
-        for i in range(len(queries)):
-            result = self.evaluate(queries[i], relevant_courses[i])
-            show = "   %2d  %.2f   %.2f   %.2f   %.2f    %.4f    %.4f    %.3f   %.3f   \n" \
-                   % (i, result[0], result[1], result[2], result[3], result[4], result[5], result[6], result[7])
-            for i in range(8):
-                total_scores[i] += result[i]
-
-        for i in range(8):
-            total_scores[i] /= len(queries)
-
-        averaged_results = \
-            "   ----------------------------------------------------------------\n" \
-            "                          Averaged Results                         \n" \
-            "   ----------------------------------------------------------------\n" \
-            "   **  P.25   P.50   P.75   P1.00   P_mean1   P_mean2.  P_norm  R_norm\n" \
-            "   ==  ====   ====   ====   =====   =======   ========  ======  ======\n" \
-            "   %2d  %.2f   %.2f   %.2f   %.2f    %.4f    %.4f    %.3f   %.3f   \n" \
-            % (len(queries), total_scores[0], total_scores[1], total_scores[2], total_scores[3], total_scores[4],
-               total_scores[5], total_scores[6], total_scores[7])
-        print(averaged_results)
-
-
-# Experiment
-class Experiment():
-    def __init__(self):
-        self._ = None
-
-    def run_lda(self):
-        X = []
-        for course in Course.objects.all():
-            X.append(course.name + " " + course.description)
-        n_features = 1000
-        n_topics = 50
-        n_top_words = 10
-        n_samples = len(X)
-        X_trans, topics, topic_components = self.fit_lda(X, n_features,
-                                                         n_topics, n_top_words,
-                                                         n_samples)
-        self.gmm_clustering(X_trans)
-
-    def fit_lda(self, X, n_features, n_topics, n_top_words, n_samples):
-        print("Fitting LDA models with tf features, n_samples=%d and n_features=%d..." % (n_samples, n_features))
-        tf_vectorizer = CountVectorizer(max_df=0.95, min_df=2,
-                                        max_features=n_features,
-                                        ngram_range=(1, 2),
-                                        stop_words='english')
-        tf = tf_vectorizer.fit_transform(X)
-        lda = LatentDirichletAllocation(n_topics=n_topics, max_iter=5,
-                                        learning_method='online',
-                                        learning_offset=50.,
-                                        random_state=0)
-        X = lda.fit_transform(tf)
-        print("\nTopics in LDA model:")
-        tf_feature_names = tf_vectorizer.get_feature_names()
-        topics = self.print_top_words(lda, tf_feature_names, n_top_words)
-        return X, topics, lda.components_
-
-    def print_top_words(self, model, feature_names, n_top_words):
-        topics = []
-        for topic_idx, topic in enumerate(model.components_):
-            print("Course Topic #%d:" % topic_idx)
-            new_topic = " ".join([feature_names[i]
-                                  for i in topic.argsort()
-                                  [:-n_top_words - 1:-1]])
-            print(new_topic)
-            topics.append(new_topic)
-        print()
-        return topics
-
-    def gmm_clustering(self, X):
-        scores = []
-        klist = []
-        for k in range(1, 30):
-            gmm = mixture.GMM(n_components=k, covariance_type='full')
-            gmm.fit(X)
-            klist.append(k)
-            scores.append(np.mean(gmm.score(X)))
-        plt.figure()
-        plt.plot(klist, scores, 'o-')
-        plt.title("GMM clustering log-likelihood graph")
-        plt.ylabel("Log Likelihood")
-        plt.xlabel("K (number of clusters)")
-        plt.show()
