6f899fb4dde075cb94a12baeb1be31dcd6aa4ae1
felixzhuologist <felix.czhu@gmail.com>
2017-07-09 09:41:20 -0700

    Discard out of order search result responses

--- static/js/redux/reducers/search_results_reducer.jsx
+++ static/js/redux/reducers/search_results_reducer.jsx
@@ -1,9 +1,17 @@
 import * as ActionTypes from '../constants/actionTypes';
 
-const searchResults = (state = { isFetching: false, items: [] }, action) => {
+const defaultState = {
+  isFetching: false,
+  items: [],
+  // assign each search request a sequence number to avoid overwriting more recent results
+  seqNumber: 0,
+};
+
+const searchResults = (state = defaultState, action) => {
   switch (action.type) {
     case ActionTypes.RECEIVE_SEARCH_RESULTS:
       return {
+        ...state,
         isFetching: false,
         items: action.response.result,
       };
@@ -11,6 +19,7 @@ const searchResults = (state = { isFetching: false, items: [] }, action) => {
       return {
         isFetching: true,
         items: state.items,
+        seqNumber: state.seqNumber + 1,
       };
     default:
       return state;
