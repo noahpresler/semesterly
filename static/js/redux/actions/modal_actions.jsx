88744967250698e3c2a3ce76ba15cce2469375ce
felixzhuologist <felix.czhu@gmail.com>
2017-07-08 22:25:45 -0700

    Rerun linter on missed files

--- static/js/redux/actions/modal_actions.jsx
+++ static/js/redux/actions/modal_actions.jsx
@@ -20,10 +20,6 @@ export const setCourseClassmates = json => ({
   data: json,
 });
 
-export const requestCourseInfo = id => ({
-  type: ActionTypes.REQUEST_COURSE_INFO,
-});
-
 export const setCourseId = id => ({
   type: ActionTypes.SET_COURSE_ID,
   id,
@@ -41,7 +37,7 @@ export const fetchCourseClassmates = courseId => (dispatch, getState) => {
 };
 
 export const fetchCourseInfo = courseId => (dispatch, getState) => {
-  dispatch(requestCourseInfo(courseId));
+  dispatch({ type: ActionTypes.REQUEST_COURSE_INFO });
   fetch(getCourseInfoEndpoint(courseId, getSemester(getState())), {
     credentials: 'include',
   })
