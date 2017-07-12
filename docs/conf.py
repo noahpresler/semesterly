b437c0322797c563a8292454450838871b0b3a95
Noah Presler <noah@presler.me>
2017-07-11 21:37:28 -0400

    add google style support docstrings

--- docs/conf.py
+++ docs/conf.py
@@ -40,7 +40,8 @@ extensions = ['sphinx.ext.autodoc',
     'sphinx.ext.todo',
     'sphinx.ext.coverage',
     'sphinx.ext.mathjax',
-    'sphinx.ext.viewcode']
+    'sphinx.ext.viewcode',
+    'sphinx.ext.napoleon']
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
