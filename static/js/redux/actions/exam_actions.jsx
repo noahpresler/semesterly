6910eab07113f1cf4a6d7e51513735877e386db3
felixzhuologist <felix.czhu@gmail.com>
2017-07-13 08:42:41 -0700

    Add function to revert timetable to old shape

--- static/js/redux/actions/exam_actions.jsx
+++ static/js/redux/actions/exam_actions.jsx
@@ -1,8 +1,30 @@
 import fetch from 'isomorphic-fetch';
 import Cookie from 'js-cookie';
+import groupby from 'lodash/groupby';
+import flatMap from 'lodash/flatMap';
 import * as ActionTypes from '../constants/actionTypes';
 import { getActiveTimetable } from '../reducers/root_reducer';
 import { getRequestShareExamLinkEndpoint, getFinalExamSchedulerEndpoint } from '../constants/endpoints';
+
+// this is a function introduced in #934 to a revert a timetable into the timetable shape from
+// before #934 (ie { courses: [... slots: []] }, where slot here refers to an offering merged
+// with its section data. Since exams are no longer actively supported/updated after #934,
+// this function exists as the minimum change necessary to keep exam functionality alive
+export const revert = function revertTimetableFormat(denormTimetable) {
+  const groupedByCourse = groupby(denormTimetable.slots, (slot) => slot.course.id);
+  const courses = Object.keys(groupedByCourse).map((courseId) => {
+    const slots = groupedByCourse[courseId];
+    return {
+      ...slots[0].course,
+      slots: flatMap(slots, slot => slot.offerings.map(offering => ({
+        ...slot.section,
+        ...offering,
+      })))
+    }
+  });
+
+  return { courses };
+};
 
 export const fetchFinalExamSchedule = () => (dispatch, getState) => {
   const state = getState();
