5b384a1bfb293347bc3109414c7beb1dffbad24e
Noah Presler <noah@presler.me>
2017-07-12 22:12:59 -0400

    Begin documenting courses and complete timetable

--- courses/utils.py
+++ courses/utils.py
@@ -1,2 +1,5 @@
 def sections_are_filled(sections):
+    """
+    Checks if all sections are filled beyond their maximum enrollment.
+    """
     return all(section.enrolment >= section.size for section in sections)
