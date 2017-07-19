0df28a6d15844c842bfddf033afbf63822277090
felixzhuologist <felix.czhu@gmail.com>
2017-07-18 19:54:42 -0700

    Finish merge

--- exams/jhu_final_exam_scheduler.py
+++ exams/jhu_final_exam_scheduler.py
@@ -16,31 +16,32 @@ from final_exam_scheduler import FinalExamScheduler, Rule
 
 
 class JHUFinalExamScheduler(FinalExamScheduler):
-	"""
-	Database Object that has a list of JHU's rules for the current semester.
-	Should be updated every semester. Initialize each Rule with select fields
-	depening on what determines whether the Rule is valid for. See Final Exam Scheduler
-	for more information
-	"""
-	def __init__(self):
-		# FALL 2016
-		# rule1 = Rule(['M'],'8:00','12/20 2-5',start_only = True)
-		# rule2 = Rule(['M'],'9:00','12/21 2-5',start_only = True)
-		# rule3 = Rule(['M'],'10:00','12/16 2-5',start_only = True)
-		# rule4 = Rule(['M'], '11:00','12/14 9-12', start_only = True)
-		# rule5 = Rule(['M'], '12:00','12/17 2-5',start_only = True)
-		# rule6 = Rule(['M'], '13:30','12/19 9-12', start_only = True)
-		# rule7 = Rule(['M'], '15:00','12/15 9-12', start_only = True)
-		# rule8 = Rule(['M'], '16:30','12/14 2-5',start_only = True)
-		# rule9 = Rule(['M'], '18:00','12/19 6-9',start_only = True)
-		# rule10 = Rule(['W'], '18:00','12/14 6-9',start_only = True)
-		# rule11 = Rule(['T'], '9:00','12/16 9-12', start_only = True)
-		# rule12 = Rule(['T'], '10:30','12/20 9-12', start_only = True)
-		# rule13 = Rule(['T'], '12:00','12/15 2-5',start_only = True)
-		# rule14 = Rule(['T'], '13:30','12/22 9-12', start_only = True)
-		# rule15 = Rule(['T'], '15:00','12/21 9-12', start_only = True)
-		# rule16 = Rule(['T'], '16:30','12/19 2-5',start_only = True)
-		# rule17 = Rule(['T'], '18:00','12/20 6-9',start_only = True)
+    """
+    Database Object that has a list of JHU's rules for the current semester.
+    Should be updated every semester. Initialize each Rule with select fields
+    depening on what determines whether the Rule is valid for. See Final Exam Scheduler
+    for more information
+    """
+
+    def __init__(self):
+        # FALL 2016
+        # rule1 = Rule(['M'],'8:00','12/20 2-5',start_only = True)
+        # rule2 = Rule(['M'],'9:00','12/21 2-5',start_only = True)
+        # rule3 = Rule(['M'],'10:00','12/16 2-5',start_only = True)
+        # rule4 = Rule(['M'], '11:00','12/14 9-12', start_only = True)
+        # rule5 = Rule(['M'], '12:00','12/17 2-5',start_only = True)
+        # rule6 = Rule(['M'], '13:30','12/19 9-12', start_only = True)
+        # rule7 = Rule(['M'], '15:00','12/15 9-12', start_only = True)
+        # rule8 = Rule(['M'], '16:30','12/14 2-5',start_only = True)
+        # rule9 = Rule(['M'], '18:00','12/19 6-9',start_only = True)
+        # rule10 = Rule(['W'], '18:00','12/14 6-9',start_only = True)
+        # rule11 = Rule(['T'], '9:00','12/16 9-12', start_only = True)
+        # rule12 = Rule(['T'], '10:30','12/20 9-12', start_only = True)
+        # rule13 = Rule(['T'], '12:00','12/15 2-5',start_only = True)
+        # rule14 = Rule(['T'], '13:30','12/22 9-12', start_only = True)
+        # rule15 = Rule(['T'], '15:00','12/21 9-12', start_only = True)
+        # rule16 = Rule(['T'], '16:30','12/19 2-5',start_only = True)
+        # rule17 = Rule(['T'], '18:00','12/20 6-9',start_only = True)
 
         # SPRING 2017
         self.list_of_rules = [
