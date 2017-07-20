cb952bfdd5a4b4b11fb923718efd10ca9754a9bb
mnm364 <mnm364@gmail.com>
2017-07-20 12:37:14 -0400

    moved and documented external_utils to utils

--- scripts/parser_library/utils.py
+++ scripts/parser_library/utils.py
@@ -1,6 +1,8 @@
 """Filler."""
 
+import collections
 import re
+import simplejson as json
 
 UNICODE_WHITESPACE = re.compile(r'(?:\u00a0)|(?:\xc2)|(?:\xa0)', re.IGNORECASE)
 
@@ -12,7 +14,7 @@ def clean(dirt):
         - remove `None` elements
         - `None` on empty list
 
-    `dict`::
+    :obj:`dict`::
         - filter out None valued key, value pairs
         - `None` on empty dict
 
@@ -63,3 +65,90 @@ def make_list(x):
     if not isinstance(x, list):
         x = [x]
     return x
+
+
+class DotDict(dict):
+    """Dot notation access for dictionary.
+
+    TODO - add example(s) here
+    """
+
+    __getattr__ = dict.get
+    __setattr__ = dict.__setitem__
+    __delattr__ = dict.__delitem__
+
+    def __init__(self, dct):
+        """Create instance.
+
+        Args:
+            dct (dict): Dictionary to create DotDict with.
+        """
+        for key, value in dct.items():
+            if hasattr(value, 'keys'):
+                value = DotDict(value)
+            self[key] = value
+
+
+def pretty_json_string(obj):
+    """Prettify object as JSON.
+
+    Args:
+        obj (dict): Serializable object to JSONify.
+
+    Returns:
+        str: Prettified JSON.
+    """
+    return '{}'.format(json.dumps(obj,
+                                  sort_keys=True,
+                                  indent=2,
+                                  separators=(',', ': ')))
+
+
+def safe_cast(val, to_type, default=None):
+    """Attempt to cast to specified type or return default.
+
+    Args:
+        val: Value to cast.
+        to_type: Type to cast to.
+        default (None, optional): Description
+
+    Returns:
+        to_type: Description
+    """
+    try:
+        return to_type(val)
+    except (ValueError, TypeError):
+        return default
+
+
+def update(d, u):
+    """Recursive update to dictionary w/o overwriting upper levels."""
+    for k, v in u.iteritems():
+        if isinstance(v, collections.Mapping):
+            r = update(d.get(k, {}), v)
+            d[k] = r
+        else:
+            d[k] = u[k]
+    return d
+
+
+def iterrify(x):
+    """Create iterable object if not already.
+
+    Will wrap `str` types in extra iterable eventhough `str` is iterable.
+
+    Examples:
+        >>> for i in iterrify(1):
+        >>>     print(i)
+        1
+        >>> for i in iterrify([1]):
+        >>>     print(i)
+        1
+        >>> for i in iterrify('hello'):
+        >>>     print(i)
+        'hello'
+    """
+    if isinstance(x, collections.Iterable) and not isinstance(x, basestring):
+        return x
+    else:
+        return (x,)
