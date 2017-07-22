9885f9ac209c0254bd461a141e1a5141c8f34e2c
Alex Ahn <alexahn917@gmail.com>
2017-07-23 02:58:16 +0900

    modify license description, formatting and searches.rst description

--- timetable/models.py
+++ timetable/models.py
@@ -1,25 +1,21 @@
-"""
-Copyright (C) 2017 Semester.ly Technologies, LLC
+# Copyright (C) 2017 Semester.ly Technologies, LLC
+#
+# Semester.ly is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# Semester.ly is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
 
-Semester.ly is free software: you can redistribute it and/or modify
-it under the terms of the GNU General Public License as published by
-the Free Software Foundation, either version 3 of the License, or
-(at your option) any later version.
-
-Semester.ly is distributed in the hope that it will be useful,
-but WITHOUT ANY WARRANTY; without even the implied warranty of
-MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-GNU General Public License for more details.
-"""
-
-import re
 import os
 
 os.environ['DJANGO_SETTINGS_MODULE'] = 'semesterly.settings'
 
 from django.forms.models import model_to_dict
 from django.db import models
-from django.db.models import Count
 from picklefield.fields import PickledObjectField
 
 
