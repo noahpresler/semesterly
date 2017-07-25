4dfa4ed90034fbf4843a5cd07f6870c38f5c93f0
mnm364 <mnm364@gmail.com>
2017-07-25 14:24:08 -0400

    partial refactor of digestor + bugfix in digest diff

--- .eslintrc.js
+++ .eslintrc.js
@@ -12,22 +12,22 @@ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 GNU General Public License for more details.
 **/
 
-module.exports = {
-	"env": {
-		"browser": true,
-		"node": true,
-		"jasmine": true
-	},
-	"globals": {
-	  "$": true,
-		"initData": true, // homepage context data passed from backend
-		"reactAlertEvents": true,
-	},
-	rules: {
-		"no-plusplus": 0,
-    "jsx-a11y/no-static-element-interactions": 0,
-		"jsx-a11y/no-noninteractive-element-interactions": 0,
-		"react/jsx-max-props-per-line": 0
-	},
-	extends: "airbnb",
-};
+// module.exports = {
+// 	"env": {
+// 		"browser": true,
+// 		"node": true,
+// 		"jasmine": true
+// 	},
+// 	"globals": {
+// 	  "$": true,
+// 		"initData": true, // homepage context data passed from backend
+// 		"reactAlertEvents": true,
+// 	},
+// 	rules: {
+// 		"no-plusplus": 0,
+//     "jsx-a11y/no-static-element-interactions": 0,
+// 		"jsx-a11y/no-noninteractive-element-interactions": 0,
+// 		"react/jsx-max-props-per-line": 0
+// 	},
+// 	extends: "airbnb",
+// };
