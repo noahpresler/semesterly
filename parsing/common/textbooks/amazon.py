6668d2b39b2e73349f9523bf1be1de8bba862dc0
mnm364 <mnm364@gmail.com>
2017-08-19 16:42:54 -0400

    upgraded amazon api

--- parsing/common/textbooks/amazon.py
+++ parsing/common/textbooks/amazon.py
@@ -10,39 +10,28 @@
 # MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 # GNU General Public License for more details.
 
-# @what     Amazon API function definition
-# @org      Semeseter.ly
-# @author   Michael N. Miller
-# @date     2/22/17
+from amazon.api import AmazonAPI
+from amazon.api import AsinNotFound
+AMAZON_ACCESS_KEY = 'AKIAJGUOXN3COOYBPTHQ'
+AMAZON_SECRET_KEY = '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'
+AMAZON_ASSOC_TAG = 'semesterly-20'
+amazon = AmazonAPI(AMAZON_ACCESS_KEY, AMAZON_SECRET_KEY, AMAZON_ASSOC_TAG)
 
- # NOTE: slowly move toward Python3
-from django.utils.encoding import smart_str
-
-from amazonproduct import API
-api = API(locale='us')
-
-def eval_field(response, field):
-    try:
-        return smart_str(eval(field))
-    except:
-        return '' # TODO - change to 'Cannot be found'
 
 def amazon_textbook_fields(isbn):
-    response = None
     try:
-        if len(isbn) == 9:
-            response = api.item_lookup(isbn, IdType='ISBN', SearchIndex='Book', ResponseGroup='Large')
-        elif len(isbn) == 13:
-            response = api.item_lookup(isbn, IdType='EAN', SearchIndex='All', ResponseGroup='Large')
-    except Exception as e:
-        pass
+        product = amazon.lookup(ItemId=isbn,
+                                IdType='ISBN',
+                                SearchIndex='Books')
+    except AsinNotFound:
+        return
 
-    if response is None:
-        return None
+    if isinstance(product, list):
+        product = product[0]
 
     return {
-        'detail_url': eval_field(response, "response.Items.Item.DetailPageURL"),
-        'image_url' : eval_field(response, "response.Items.Item.MediumImage.URL"),
-        'author':     eval_field(response, "response.Items.Item.ItemAttributes.Author"),
-        'title':      eval_field(response, "response.Items.Item.ItemAttributes.Title")
+        'detail_url': product.detail_page_url,
+        'image_url': product.medium_image_url,
+        'author': product.author,
+        'title': product.title,
     }
