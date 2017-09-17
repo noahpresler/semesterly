d73a666ca3b3e0bebbee43feec457e74d873fbd0
mnm364 <mnm364@gmail.com>
2017-09-17 14:31:52 -0400

    python3: bypassed vectorizer and removed vectorizer description search

--- searches/apps.py
+++ searches/apps.py
@@ -22,11 +22,12 @@ class SearchesConfig(AppConfig):
 
     def ready(self):
         """ Constructs Searcher object to be used if it can be built using course.vector field """
-        from searches.utils import Searcher
-        if not self.searcher:
-            try:
-                self.searcher = Searcher()
-            except Exception as e:
-                self.searcher = None
-                print(("Unable to create Searcher object: setting searcher object to None " +
-                      "and using baseline_search instead. \nError:%s" % str(e)))
+        return
+        # from searches.utils import Searcher
+        # if not self.searcher:
+        #     try:
+        #         self.searcher = Searcher()
+        #     except Exception as e:
+        #         self.searcher = None
+        #         print(("Unable to create Searcher object: setting searcher object to None " +
+        #               "and using baseline_search instead. \nError:%s" % str(e)))
