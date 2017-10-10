70e2a4acc186b206cce0ff6803682692bf7ec8a7
Ryan Hughes <ryanhughes624@gmail.com>
2017-10-10 19:02:54 -0400

    Fixed linting erors

--- parsing/schools/neu/main.js
+++ parsing/schools/neu/main.js
@@ -1,20 +1,19 @@
-require("babel-core/register");
-require("babel-polyfill");
-var searchneu = require('searchneu')
-var path = require('path')
-var fs = require('fs')
+require('babel-core/register');
+require('babel-polyfill');
+const searchneu = require('searchneu');
+const path = require('path');
+const fs = require('fs');
 
+searchneu.default.main(true).then((retVal) => {
+  const semesterlyString = JSON.stringify(retVal, null, 4);
+  fs.writeFile(path.join(__dirname, 'data', 'courses.json'), semesterlyString, (err) => {
+    if (err) {
+      window.console.log(err);
+      return;
+    }
 
-searchneu.default.main(true).then(function(retVal) {
-    let semesterlyString = JSON.stringify(retVal, null, 4)
-    fs.writeFile(path.join(__dirname, 'data', 'courses.json'), semesterlyString, function (err) {
-	    if (err) {
-	    	console.log(err)
-	    	return;
-	    }
-
-	    console.log('saved semesterly data')
-    })
-}).catch(function (err) {
-	console.log(err)
-})
+    window.console.log('saved semesterly data');
+  });
+}).catch((err) => {
+  window.console.log(err);
+});
