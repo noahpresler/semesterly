133fa0c3d038c2497ad908ef8384726343ed8018
Kristin Yim <k.yim33@gmail.com>
2018-02-01 17:01:25 -0500

    updated my dev cred

--- semesterly/dev_credentials.py
+++ semesterly/dev_credentials.py
@@ -35,7 +35,7 @@ SECRETS = {
     'QUEENS_PASS': None,
     'GW_PASS': None,
     'GW_SECURITY_ANSWER': None,
-    'JHU_API_KEY': None,
+    'JHU_API_KEY': '00VaZ62fe3TFNtzi29klWwMBIU9nQxtg',
     'VANDY_USER': None,
     'VANDY_PASS': None,
 
