1903a563c6d66e4e417b33114f6a63dd498362b2
Kristin Yim <k.yim33@gmail.com>
2018-02-10 13:37:21 -0500

    sends dummy data to sis

--- static/js/redux/ui/calendar.jsx
+++ static/js/redux/ui/calendar.jsx
@@ -142,26 +142,20 @@ class Calendar extends React.Component {
   render() {
     const saveIcon = this.props.saving ? <i className="fa fa-spin fa-circle-o-notch" /> :
     <i className="fa fa-floppy-o" />;
-    const data = '{"action": "AddToCart",\n' +
-      '\n' +
-      '    "data": {\n' +
-      '\n' +
-      '        "year": "2018",\n' +
-      '\n' +
-      '        "term": "Spring",\n' +
-      '\n' +
-      '        "sections": [\n' +
-      '\n' +
-      '            { "course": "EN.601.226", "section": "01"},\n' +
-      '\n' +
-      '            { "course": "EN.601.310", "section": "01"}\n' +
-      '\n' +
-      '        ]\n' +
-      '\n' +
-      '    }}'
-    const jsonData = JSON.stringify(data);
+    const jsonRequest = {
+      action: 'AddToCart',
+      data: {
+        year: '2018',
+        term: 'Spring',
+        sections: [
+          { course: 'EN.601.226', section: '01' },
+          { course: 'EN.601.310', section: '01' },
+        ],
+      },
+    };
+    const jsonData = JSON.stringify(jsonRequest);
     const toilet = 'http://ptsv2.com/t/l70bk-1518071398/post';
-    const sis = 'https://sisdevelopment.sis.jhu.edu/sem/sswf/go';
+    const sis = 'https://sisdevelopment.sis.jhu.edu/sem/sswf/go/';
     const addSISButton = (
       <div>
         <form id="form1" action={sis} method="post" encType="application/x-www-form-urlencoded">
