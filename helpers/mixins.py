b171a900a70ee05517b81129b6cb35ee3427024c
Kristin Yim <k.yim33@gmail.com>
2018-09-16 23:08:01 -0400

    redirect queens.semester.ly to queens error page

--- helpers/mixins.py
+++ helpers/mixins.py
@@ -35,7 +35,9 @@ class ValidateSubdomainMixin(object):
     """
 
     def dispatch(self, request, *args, **kwargs):
-        if request.subdomain not in ACTIVE_SCHOOLS:
+        if request.subdomain == "queens":
+            return render(request, 'queens_error.html')
+        elif request.subdomain not in ACTIVE_SCHOOLS:
             return render(request, 'index.html')
         return super(ValidateSubdomainMixin, self).dispatch(request, *args, **kwargs)
 
