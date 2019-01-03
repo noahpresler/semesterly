94ce532760a2f9f26da2c8ec4ddfdbaf1fd411a8
Ali Soylu <alisoylu@jhu.edu>
2019-01-03 18:45:21 -0500

    restore terms, back to hardcoded for speed for now

--- parsing/schools/jhu/courses.py
+++ parsing/schools/jhu/courses.py
@@ -206,7 +206,7 @@ class Parser(BaseParser):
         self.verbosity = verbosity
 
         # Default to hardcoded current year.
-        years = {'2019', '2018'}
+        years = {'2019', '2018', '2017', '2016', '2015'}
         terms = {'Spring', 'Fall', 'Summer', 'Intersession'}
 
         years_and_terms = dict_filter_by_dict(
