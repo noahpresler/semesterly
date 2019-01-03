09b231c1c54a50fd21c1dcfcf76f20f71d9bf47c
Ali Soylu <alisoylu@jhu.edu>
2019-01-03 18:26:24 -0500

    Force terms to parse

--- build/run_parser.sh
+++ build/run_parser.sh
@@ -2,6 +2,11 @@
 
 echo starting;
 cd /code
-/usr/bin/python manage.py ingest jhu;
+# TODO: No params does not honor active-only semesters, this is hardcoded for now
+/usr/bin/python manage.py ingest jhu --term Spring --years 2019;
 /usr/bin/python manage.py digest jhu;
+
+/usr/bin/python manage.py ingest jhu --term Fall --years 2018;
+/usr/bin/python manage.py digest jhu;
+
 echo done;
