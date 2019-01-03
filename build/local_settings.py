822906065935b8cd9e277159e299d8593b52c6e9
Ali Soylu <alisoylu@jhu.edu>
2019-01-03 18:09:30 -0500

    Use db name from env in docker image

--- build/local_settings.py
+++ build/local_settings.py
@@ -9,7 +9,7 @@ TEMPLATE_DEBUG = DEBUG
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'postgres',  # os.path.join(BASE_DIR, 'db.postgresql')
+        'NAME': os.environ['DB_NAME'],
         'USER': os.environ['DB_USER'],
         'PASSWORD': os.environ['DB_PASSWORD'],
         'HOST': os.environ['DB_HOST'],
