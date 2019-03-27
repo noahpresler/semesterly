f23b7dd5e2c98e7f06608e597b88f6015f45bc52
Kristin Yim <k.yim33@gmail.com>
2019-03-26 23:41:23 -0400

    Revert "idk tbh"
    This reverts commit 22f736bb295f1b7afad9f9502a11112844b3b0e4.

--- static/js/redux/ui/search_bar.jsx
+++ static/js/redux/ui/search_bar.jsx
@@ -143,14 +143,14 @@ class SearchBar extends React.Component {
     const currSem = ($(window).width() < 767) ?
             SearchBar.getAbbreviatedSemesterName(this.props.semester) :
             SearchBar.getSemesterName(this.props.semester);
-    // const transformSearchAppearance = this.props.searchResults.length > 0 && this.state.focused;
+    const transformSearchAppearance = this.props.searchResults.length > 0 && this.state.focused;
     return (
-      <div className={classNames('search-bar no-print', { results: this.props.searchResults.length > 0 && this.state.focused })} >
-        <div className="search-bar__wrapper">
+      <div className="search-bar no-print">
+        <div className={"search-bar__wrapper " + (transformSearchAppearance ? '__after' : '__before')} >
 
           <ClickOutHandler onClickOut={this.onClickOut}>
             <div
-              className={classNames('search-bar__semester', { results: this.props.searchResults.length > 0 && this.state.focused })}
+              className={"search-bar__semester "+(transformSearchAppearance ? 'white-background' : 'grey-background')}
               onMouseDown={this.toggleDropdown}
             >
               <span
@@ -170,7 +170,8 @@ class SearchBar extends React.Component {
             <input
               ref={(c) => { this.input = c; }}
               placeholder={`Searching ${currSem}`}
-              className={classNames(this.props.isFetching ? 'results-loading-gif' : '')}
+              className={classNames(this.props.isFetching ? 'results-loading-gif' : '', { search_drop: this.state.focused && results.length !== 0 })
+              +(transformSearchAppearance ? '__after white-background' : '__before grey-background')}
               onInput={this.fetchSearchResults}
               onFocus={() => this.setState({ focused: true, showDropdown: false })}
               onBlur={() => this.setState({ focused: false })}
