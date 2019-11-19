430ab458b1c698a9b5288b2874015735dfe57ec4
miaboloix <MbMv2017>
2019-11-19 15:48:45 -0500

    created first view

--- pilot/views.py
+++ pilot/views.py
@@ -1,9 +1,19 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 
 # Create your views here.
 from django.http import HttpResponse
+from student.utils import get_student
 
 
 def index(request):
-	return render(request, 'pilot/welcome.html/')
-
+	if request.method == 'POST':
+		context = {
+			'title': 'Welcome | PILOT Registration'
+		}
+		return redirect('pilot:home', context=context)
+	else:
+		student = get_student(request)
+		context = {
+			'student': student
+		}
+		return render(request, 'pilot/welcome.html/', context=context)
