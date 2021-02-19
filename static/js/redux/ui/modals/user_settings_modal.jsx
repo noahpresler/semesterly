737cacee55cd3a954fa053cedf69909140c961d2
Sebastian Cabrejos <Boccca2014@gmail.com>
2021-02-19 12:20:07 -0500

    modified graduating class year options

--- static/js/redux/ui/modals/user_settings_modal.jsx
+++ static/js/redux/ui/modals/user_settings_modal.jsx
@@ -303,13 +303,13 @@ class UserSettingsModal extends React.Component {
                 name="form-field-name"
                 value={this.props.userInfo.class_year}
                 options={[
-                                    { value: 2017, label: 2017 },
-                                    { value: 2018, label: 2018 },
-                                    { value: 2019, label: 2019 },
-                                    { value: 2020, label: 2020 },
                                     { value: 2021, label: 2021 },
                                     { value: 2022, label: 2022 },
                                     { value: 2023, label: 2023 },
+                                    { value: 2024, label: 2024 },
+                                    { value: 2025, label: 2025 },
+                                    { value: 2026, label: 2026 },
+                                    { value: 2027, label: 2027 },
                 ]}
                 searchable
                 onChange={this.changeClassYear}
