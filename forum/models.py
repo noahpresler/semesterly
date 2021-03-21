11cd35068385860f98c1e77b9bf03cffdecdb27a
Rishibrata Biswas <rbis1001@gmail.com>
2021-03-21 17:49:24 -0400

    Added models to forum app
    Added Comment model and Transcript model to forum app

--- forum/models.py
+++ forum/models.py
@@ -3,4 +3,13 @@ from __future__ import unicode_literals
 
 from django.db import models
 
-# Create your models here.
+class Comment(models.Model):
+    author = models.ForeignKey('student.Student')
+    content = models.TextField()
+    timestamp = models.DateTimeField(auto_now_add=True)
+    transcript_id = models.ForeignKey('forum.Transcript')
+
+class Transcript(models.Model):
+    owner = models.ForeignKey('student.Student')
+    advisors = models.ManyToManyField('student.Student')
+    semester_id = models.ForeignKey('timetable.Semester')
