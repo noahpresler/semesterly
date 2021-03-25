85ef09bd07a484a2523636531e3de04fc256f033
Rishibrata Biswas <rbis1001@gmail.com>
2021-03-25 14:00:16 -0400

    Changed Syntax in forum models
    No longer using single quote syntax, and now adhering to standard
    presented in Django documentation. For edxample, 'student.Student'
    is replaced with student.models.Student

--- forum/models.py
+++ forum/models.py
@@ -2,14 +2,16 @@
 from __future__ import unicode_literals
 
 from django.db import models
-
-class Comment(models.Model):
-    author = models.ForeignKey('student.Student')
-    content = models.TextField()
-    timestamp = models.DateTimeField(auto_now_add=True)
-    transcript_id = models.ForeignKey('forum.Transcript')
+import student.models as student_models
+import timetable.models as timetable_models
 
 class Transcript(models.Model):
-    owner = models.ForeignKey('student.Student', related_name='owner')
-    advisors = models.ManyToManyField('student.Student', related_name='advisors')
-    semester_id = models.ForeignKey('timetable.Semester')
+    owner = models.ForeignKey(student_models.Student, related_name='owner')
+    advisors = models.ManyToManyField(student_models.Student, related_name='advisors')
+    semester_id = models.ForeignKey(timetable_models.Semester)
+
+class Comment(models.Model):
+    author = models.ForeignKey(student_models.Student)
+    content = models.TextField()
+    timestamp = models.DateTimeField(auto_now_add=True)
+    transcript_id = models.ForeignKey(Transcript)
