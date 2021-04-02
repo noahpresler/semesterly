404b06c539e74eb6e77af5f2bc35100d541edbf2
Sebastian Cabrejos <Boccca2014@gmail.com>
2021-04-01 20:37:59 -0400

    notes for creating models

--- advising/models.py
+++ advising/models.py
@@ -4,3 +4,10 @@ from __future__ import unicode_literals
 from django.db import models
 
 # Create your models here.
+# Sebastian Notes to self -> 
+# map section object (timetable) in object 
+# course object to map to offering
+# courseobjects.filter(id)
+# function to create many-to-many property (enrollments from list of sections)
+# two tables for each majors and minors
+
