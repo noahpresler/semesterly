7dbe1b0cfe6248ba4c3d5f4d64c04e8b3628bec0
miaboloix <MbMv2017>
2021-04-03 16:17:40 -0400

    Commented out GCal export functionality

--- static/js/redux/ui/modals/save_calendar_modal.jsx
+++ static/js/redux/ui/modals/save_calendar_modal.jsx
@@ -58,11 +58,11 @@ class SaveCalendarModal extends React.Component {
       + 'google_icon_2x-745c2280e5004d4c90e3ca4e60e3f677.png'}
     />);
     UploadIcon = this.props.isUploading ? <div className="loader" /> : UploadIcon;
-    UploadIcon = this.props.hasUploaded ? <i className="done fa fa-check" /> : UploadIcon;
+    //UploadIcon = this.props.hasUploaded ? <i className="done fa fa-check" /> : UploadIcon;
 
     let DownloadText = 'Add to Google Calendar';
     DownloadText = this.props.isUploading ? 'One Moment...' : DownloadText;
-    DownloadText = this.props.hasUploaded ? 'Added to Your Google Calendar' : DownloadText;
+    //DownloadText = this.props.hasUploaded ? 'Added to Your Google Calendar' : DownloadText;
 
     return (
       <Modal
@@ -76,33 +76,33 @@ class SaveCalendarModal extends React.Component {
       >
         {modalHeader}
         <div className="save-calendar-modal__container">
-          <button
-            className="btn abnb-btn" onClick={() => {
-              if (!this.props.userInfo.isLoggedIn || !this.props.userInfo.GoogleSignedUp ||
-                !this.props.userInfo.GoogleLoggedIn) {
-                const link = document.createElement('a');
-                link.href = `/login/google-oauth2/?next=${location.protocol}//${location.host}
-                /callback/google_calendar&student_token=${this.props.userInfo.LoginToken}
-                &login_hash=${this.props.userInfo.LoginHash}`;
-                document.body.appendChild(link);
-                link.click();
-              } else {
-                this.props.addTTtoGCal();
-              }
-            }}
-          >
-            <span className="img-icon">
-              { UploadIcon }
-            </span>
-            <span>{ DownloadText }</span>
-          </button>
-          <p className="method-details">Add to your Google Calendar in just one click, no
-                        downloads. No
-                        importing.</p>
-          <div className="or-separator">
-            <span className="h6 or-separator--text">or</span>
-            <hr />
-          </div>
+          {/*<button*/}
+          {/*  className="btn abnb-btn" onClick={() => {*/}
+          {/*    if (!this.props.userInfo.isLoggedIn || !this.props.userInfo.GoogleSignedUp ||*/}
+          {/*      !this.props.userInfo.GoogleLoggedIn) {*/}
+          {/*      const link = document.createElement('a');*/}
+          {/*      link.href = `/login/google-oauth2/?next=${location.protocol}//${location.host}*/}
+          {/*      /callback/google_calendar&student_token=${this.props.userInfo.LoginToken}*/}
+          {/*      &login_hash=${this.props.userInfo.LoginHash}`;*/}
+          {/*      document.body.appendChild(link);*/}
+          {/*      link.click();*/}
+          {/*    } else {*/}
+          {/*      this.props.addTTtoGCal();*/}
+          {/*    }*/}
+          {/*  }}*/}
+          {/*>*/}
+          {/*  <span className="img-icon">*/}
+          {/*    { UploadIcon }*/}
+          {/*  </span>*/}
+          {/*  <span>{ DownloadText }</span>*/}
+          {/*</button>*/}
+          {/*<p className="method-details">Add to your Google Calendar in just one click, no*/}
+          {/*              downloads. No*/}
+          {/*              importing.</p>*/}
+          {/*<div className="or-separator">*/}
+          {/*  <span className="h6 or-separator--text">or</span>*/}
+          {/*  <hr />*/}
+          {/*</div>*/}
           <button
             className="btn abnb-btn secondary" onClick={() => {
               this.props.createICalFromTimetable();
@@ -124,7 +124,7 @@ class SaveCalendarModal extends React.Component {
 
 SaveCalendarModal.propTypes = {
   createICalFromTimetable: PropTypes.func.isRequired,
-  addTTtoGCal: PropTypes.func.isRequired,
+  //addTTtoGCal: PropTypes.func.isRequired,
   toggleSaveCalendarModal: PropTypes.func.isRequired,
   isDownloading: PropTypes.bool.isRequired,
   hasDownloaded: PropTypes.bool.isRequired,
