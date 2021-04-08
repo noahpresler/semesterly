054a18522be65797ef32ec9ac6bfc8994d7e6c51
Sebastian Cabrejos <Boccca2014@gmail.com>
2021-04-07 22:24:11 -0400

    removed view, uJWT Token is known

--- advising/views.py
+++ advising/views.py
@@ -10,39 +10,9 @@ import semesterly.views
 # currently unused. TODO: add get_feature_flow()
 
 # feature_name = 'ADVISING'
-import jwt
-import json
-from rest_framework import views
-from rest_framework.response import Response
+# import jwt
+# import json
+# from rest_framework import views
+# from rest_framework.response import Response
 from models import User
 
-class Login(views.APIView):
-    def post(self, request, *args, **kwargs):
-        if not request.data:
-            return Response({'Error': "Please provide username/password"}, status="400")
-
-        username = request.data['username']
-        password = request.data['password']
-        try:
-            user = User.objects.get(username=username, password=password)
-        except User.DoesNotExist:
-            return Response({'Error': "Invalid username/password"}, status="400")
-        if user:
-
-            payload = {
-                'id': user.id,
-                'email': user.email,
-            }
-            jwt_token = {'token': jwt.encode(payload, "SECRET_KEY")}
-
-           return HttpResponse(
-              json.dumps(jwt_token),
-              status=200,
-              content_type="application/json"
-            )
-        else:
-            return Response(
-              json.dumps({'Error': "Invalid credentials"}),
-              status=400,
-              content_type="application/json"
-            )
