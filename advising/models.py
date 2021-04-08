0a161620a3e97776aa9d1a208fb062f75623c3f1
Sebastian Cabrejos <Boccca2014@gmail.com>
2021-04-08 00:23:05 -0400

    adding fields for advising models

--- advising/models.py
+++ advising/models.py
@@ -1,7 +1,24 @@
-# -*- coding: utf-8 -*-
-from __future__ import unicode_literals
+# Copyright (C) 2017 Semester.ly Technologies, LLC
+#
+# Semester.ly is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# Semester.ly is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+
+""" Models pertaining to Students. """
 
 from django.db import models
+import timetable.models as timetable_models
+import jwt
+import json
+# from rest_framework import views
+# from rest_framework.response import Response
+from models import User
 
 # Create your models here.
 # Sebastian Notes to self ->
@@ -10,87 +27,89 @@ from django.db import models
 # courseobjects.filter(id)
 # function to create many-to-many property (enrollments from list of sections)
 # two tables for each majors and minors
-import student.models as student_models
-import timetable.models as timetable_models
-import jwt
-import json
-from rest_framework import views
-from rest_framework.response import Response
-from models import User
+
 
 class Student(models.Model):
+    first_name = models.CharField(max_length=255, default='', null=True)
+    last_name = models.CharField(max_length=255, default='', null=True)
+    jhed_id = models.CharField(max_length=255, null=True, default='')
+    primary_major = models.CharField(
+        max_length=255, null=True, default='Undecided')
+    email_address = models.CharField(
+        max_length=255, null=True, default='Undecided')
+    majors = models.ManyToOneField(Major)
+    minors = models.ManyToOneField(Minor)
 
 
 class Advisor(models.Model):
-    advisors = models.ManyToManyField(student_models.Student)
+    students = models.ManyToManyField(models.Student)
 
 
 class Major(model.Model):
-    majors = models.ManyToOneField(student_models.Student)
+    majors_name = models.CharField(max_length=255, default='', null=True)
 
 
 class Minor(model.Model):
-    minors = models.ManyToOneField(student_models.Student)
-
+    minors_name = models.CharField(max_length=255, default='', null=True)
 
 class Section(model.Model):
-    # must check through section id in timetable
-    # sections = ManyToManyField()
+    # match to appropriate offering name
+    sections = ManyToManyField(timetable_models.Course.code)
+    section_id = ManyToManyField(timetable_models.Section.course_section_id)
 
+# This may be unnecessary, attempting to understand PyJWT implementation
+# class TokenAuthentication(BaseAuthentication):
+#     model = None
 
-class TokenAuthentication(BaseAuthentication):
+#     def get_model(self):
+#         return User
 
-    model = None
+#     def authenticate(self, request):
+#         auth = get_authorization_header(request).split()
+#         if not auth or auth[0].lower() != b'token':
+#             return None
 
-    def get_model(self):
-        return User
+#         if len(auth) == 1:
+#             msg = 'Invalid token header. No credentials provided.'
+#             raise exceptions.AuthenticationFailed(msg)
+#         elif len(auth) > 2:
+#             msg = 'Invalid token header'
+#             raise exceptions.AuthenticationFailed(msg)
 
-    def authenticate(self, request):
-        auth = get_authorization_header(request).split()
-        if not auth or auth[0].lower() != b'token':
-            return None
+#         try:
+#             token = auth[1]
+#             if token == "null":
+#                 msg = 'Null token not allowed'
+#                 raise exceptions.AuthenticationFailed(msg)
+#         except UnicodeError:
+#             msg = 'Invalid token header. Token string should not contain invalid characters.'
+#             raise exceptions.AuthenticationFailed(msg)
 
-        if len(auth) == 1:
-            msg = 'Invalid token header. No credentials provided.'
-            raise exceptions.AuthenticationFailed(msg)
-        elif len(auth) > 2:
-            msg = 'Invalid token header'
-            raise exceptions.AuthenticationFailed(msg)
+#         return self.authenticate_credentials(token)
 
-        try:
-            token = auth[1]
-            if token == "null":
-                msg = 'Null token not allowed'
-                raise exceptions.AuthenticationFailed(msg)
-        except UnicodeError:
-            msg = 'Invalid token header. Token string should not contain invalid characters.'
-            raise exceptions.AuthenticationFailed(msg)
+#     def authenticate_credentials(self, token):
+#         model = self.get_model()
+#         payload = jwt.decode(token, "SECRET_KEY")
+#         email = payload['email']
+#         userid = payload['id']
+#         msg = {'Error': "Token mismatch", 'status': "401"}
+#         try:
 
-        return self.authenticate_credentials(token)
+#             user = User.objects.get(
+#                 email=email,
+#                 id=userid,
+#                 is_active=True
+#             )
 
-    def authenticate_credentials(self, token):
-        model = self.get_model()
-        payload = jwt.decode(token, "SECRET_KEY")
-        email = payload['email']
-        userid = payload['id']
-        msg = {'Error': "Token mismatch", 'status': "401"}
-        try:
+#             if not user.token['token'] == token:
+#                 raise exceptions.AuthenticationFailed(msg)
 
-            user = User.objects.get(
-                email=email,
-                id=userid,
-                is_active=True
-            )
+#         except jwt.ExpiredSignature or jwt.DecodeError or jwt.InvalidTokenError:
+#             return HttpResponse({'Error': "Token is invalid"}, status="403")
+#         except User.DoesNotExist:
+#             return HttpResponse({'Error': "Internal server error"}, status="500")
 
-            if not user.token['token'] == token:
-                raise exceptions.AuthenticationFailed(msg)
+#         return (user, token)
 
-        except jwt.ExpiredSignature or jwt.DecodeError or jwt.InvalidTokenError:
-            return HttpResponse({'Error': "Token is invalid"}, status="403")
-        except User.DoesNotExist:
-            return HttpResponse({'Error': "Internal server error"}, status="500")
-
-        return (user, token)
-
-    def authenticate_header(self, request):
-        return 'Token'
+#     def authenticate_header(self, request):
+#         return 'Token'
