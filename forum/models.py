8aa53e4b20bf0fd6b028b0a2c3d5725f866a1726
James S. Wang <JJamesWWang@gmail.com>
2021-04-18 00:56:13 -0400

    Add comments for models in advising and forum

--- forum/models.py
+++ forum/models.py
@@ -8,6 +8,18 @@ from advising.models import Advisor
 
 
 class Transcript(models.Model):
+    """
+    Represents a conversation between an Advisor and a Student for a single
+    semester. Students have one Transcript per semester.
+
+    Attributes:
+        owner (:obj:`Student`): The student who owns this transcript
+        advisors (:obj:`ManyToManyField` of :obj:`Student`): The list of
+            authenticated advisors the student has invited
+        pending_advisors (:obj:`ManyToManyField` of :obj:`Advisor`): The list
+            unauthenticated advisors the student has invited
+        semester (:obj:`Semester`): The semester this transcript is for
+    """
     owner = models.ForeignKey(student_models.Student,
                               related_name='owned_transcripts')
     advisors = models.ManyToManyField(
@@ -21,6 +33,15 @@ class Transcript(models.Model):
 
 
 class Comment(models.Model):
+    """
+    Represents a single message sent inside of a Transcript.
+
+    Attributes:
+        author (:obj:`Student`): The user who sent this comment
+        content (:obj:`TextField`): The actual message
+        timestamp (:obj:`DateTimeField`): The time the comment was sent
+        transcript (:obj:`Transcript`): The transcript this comment was sent in
+    """
     author = models.ForeignKey(student_models.Student)
     content = models.TextField()
     timestamp = models.DateTimeField(auto_now_add=True)
