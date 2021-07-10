3aed944762a7ba1d854a0f59f592d50ad694ec45
Sebastian Cabrejos <14218992+Boccca2014@users.noreply.github.com>
2021-07-10 19:08:05 -0400

    removed outdated theme

--- docs/conf.py
+++ docs/conf.py
@@ -181,7 +181,7 @@ texinfo_documents = [
 # Example configuration for intersphinx: refer to the Python standard library.
 intersphinx_mapping = {'https://docs.python.org/': None}
 
-html_theme = "sphinx_rtd_theme"
+# html_theme = "sphinx_rtd_theme"
 
 html_static_path = ['_static']
 
