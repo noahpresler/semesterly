e6fd580644ad588909adb938c09c86f9e6f851cc
Jiaqi (Jacky) Wang <jacky200218@gmail.com>
2021-10-20 22:35:04 -0400

    refactor term_of_service_modal and update snapshot

--- static/js/redux/__tests__/modals/terms_of_service_modal_test.jsx
+++ static/js/redux/__tests__/modals/terms_of_service_modal_test.jsx
@@ -11,58 +11,63 @@ but WITHOUT ANY WARRANTY; without even the implied warranty of
 MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 GNU General Public License for more details.
 */
-
-import React from 'react';
-import thunk from 'redux-thunk';
-import Provider from 'react-redux/src/components/Provider';
-import configureMockStore from 'redux-mock-store';
-import renderer from 'react-test-renderer';
-import { tosModalFixture, userInfoFixture } from '../../__fixtures__/terms_of_service_modal.fixture';
-import TermsOfServiceModalContainer from '../../ui/containers/terms_of_service_modal_container';
-import { handleAgreement } from '../../actions/user_actions';
-import * as ActionTypes from '../../constants/actionTypes';
+import React from "react";
+import { render } from "@testing-library/react";
+import thunk from "redux-thunk";
+import Provider from "react-redux/src/components/Provider";
+import configureMockStore from "redux-mock-store";
+import {
+  tosModalFixture,
+  userInfoFixture,
+} from "../../__fixtures__/terms_of_service_modal.fixture";
+import { handleAgreement } from "../../actions/user_actions";
+import * as ActionTypes from "../../constants/actionTypes";
+import TermsOfServiceModalContainer from "../../ui/containers/terms_of_service_modal_container";
 
 const middlewares = [thunk];
 const mockStore = configureMockStore(middlewares);
 
-describe('TOS Modal', () => {
-  it('shows when isVisible is true', () => {
+describe("TOS Modal", () => {
+  it("shows when isVisible is true", () => {
     const store = mockStore({
       termsOfServiceModal: tosModalFixture,
       userInfo: userInfoFixture,
     });
-    const tree = renderer.create(
-      <Provider store={store}><TermsOfServiceModalContainer /></Provider>,
-    ).toJSON();
-    delete tree.children[0].children[0].props.style.animationName;
-    delete tree.children[1].props.style.animationName;
-    expect(tree).toMatchSnapshot();
+
+    const { container } = render(
+      <Provider store={store}>
+        <TermsOfServiceModalContainer />
+      </Provider>
+    );
+    expect(container).toMatchSnapshot();
   });
 
-  it('shows welcome message for new users', () => {
+  it("shows welcome message for new users", () => {
     const newUser = userInfoFixture;
     newUser.data.timeAcceptedTos = null;
     const store = mockStore({
       termsOfServiceModal: tosModalFixture,
       userInfo: userInfoFixture,
     });
-    const tree = renderer.create(
-      <Provider store={store}><TermsOfServiceModalContainer /></Provider>,
-    ).toJSON();
-    delete tree.children[0].children[0].props.style.animationName;
-    delete tree.children[1].props.style.animationName;
-    expect(tree).toMatchSnapshot();
+    const { container, debug } = render(
+      <Provider store={store}>
+        <TermsOfServiceModalContainer />
+      </Provider>
+    );
+    expect(container).toMatchSnapshot();
   });
 
-  it('is hidden when isVisible is false', () => {
+  it("is hidden when isVisible is false", () => {
     const store = mockStore({
       termsOfServiceModal: { ...tosModalFixture, isVisible: false },
       userInfo: userInfoFixture,
     });
-    const tree = renderer.create(
-      <Provider store={store}><TermsOfServiceModalContainer /></Provider>,
-    ).toJSON();
-    expect(tree).toMatchSnapshot();
+    const { container } = render(
+      <Provider store={store}>
+        <TermsOfServiceModalContainer />
+      </Provider>
+    );
+    expect(container).toMatchSnapshot();
   });
 });
 
