91668cdc07712a45419901c3defbba35f30db7c9
Jiaqi (Jacky) Wang <jacky200218@gmail.com>
2021-11-08 18:05:50 -0500

    use native reducers to perform test as outlined in react-redux doc

---
+++ static/js/redux/test-utils.jsx
@@ -0,0 +1,21 @@
+import React from 'react';
+import { render as rtlRender } from '@testing-library/react';
+import { configureStore } from '@reduxjs/toolkit';
+import { Provider } from 'react-redux';
+import rootReducer from './reducers/root_reducer';
+
+function renderWithRedux(
+  ui,
+  {
+    preloadedState,
+    store = configureStore({ reducer: rootReducer, preloadedState }),
+    ...renderOptions
+  } = {}
+) {
+  function Wrapper({ children }) {
+    return <Provider store={store}>{children}</Provider>;
+  }
+  return rtlRender(ui, { wrapper: Wrapper, ...renderOptions });
+}
+
+export { renderWithRedux };
