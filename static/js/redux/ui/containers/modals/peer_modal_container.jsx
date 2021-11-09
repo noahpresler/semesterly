7235adfa9a2feae7b06bcf949b123507d5663704
Jiaqi (Jacky) Wang <jacky200218@gmail.com>
2021-11-08 22:43:13 -0500

    update import

--- static/js/redux/ui/containers/modals/peer_modal_container.jsx
+++ static/js/redux/ui/containers/modals/peer_modal_container.jsx
@@ -14,7 +14,8 @@ GNU General Public License for more details.
 
 import { connect } from 'react-redux';
 import { getActiveTimetableDenormCourses } from '../../../reducers/root_reducer';
-import { changeUserInfo, fetchFriends, saveSettings } from '../../../actions/user_actions';
+import { fetchFriends, saveSettings } from '../../../actions/user_actions';
+import { changeUserInfo } from '../../../actions'
 import PeerModal from '../../modals/peer_modal';
 import { openSignUpModal, togglePeerModal } from '../../../actions/modal_actions';
 
