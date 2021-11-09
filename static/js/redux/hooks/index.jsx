4939500de7e6ec158d599626ced2a093c5ad7c76
Jiaqi (Jacky) Wang <jacky200218@gmail.com>
2021-11-08 22:59:57 -0500

    rename hooks file

--- static/js/redux/hooks/index.jsx
+++ static/js/redux/hooks/index.jsx
@@ -1,6 +1,7 @@
 import { useDispatch } from 'react-redux';
 import { bindActionCreators } from 'redux';
 import * as actionCreators from '../actions';
+/* eslint-disable import/prefer-default-export, react/prop-types */
 
 // custom hook used to combine all actionCreators to an object
 export const useActions = () => {
