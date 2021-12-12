681817f7385398fc840a4b5530860d0f73e110d0
Jiaqi (Jacky) Wang <jacky200218@gmail.com>
2021-12-12 00:21:21 -0500

    create initAllState action

--- static/js/redux/actions/index.jsx
+++ static/js/redux/actions/index.jsx
@@ -1,3 +1,5 @@
+import { createAction } from "@reduxjs/toolkit";
+
 export * from './calendar_actions';
 export * from './exam_actions';
 export * from './modal_actions';
@@ -5,3 +7,7 @@ export * from './school_actions';
 export * from './search_actions';
 export * from './timetable_actions';
 export * from './user_actions';
+
+
+// the INIT_STATE action typed version
+export const initAllState = createAction('global/init')
