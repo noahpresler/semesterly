58564930fba3ba65cfb8a130267828b4cc21e2bf
Jiaqi (Jacky) Wang <jacky200218@gmail.com>
2021-12-21 18:18:24 -0800

    create file to store types

---
+++ static/js/redux/constants/commonTypes.ts
@@ -0,0 +1,12 @@
+/**
+ * This file stores typescript types used by both react and redux
+ */
+
+/**
+ * Course reactions
+ */
+export interface Reaction {
+    title: String;
+    count: Number;
+    reacted: boolean;
+}
