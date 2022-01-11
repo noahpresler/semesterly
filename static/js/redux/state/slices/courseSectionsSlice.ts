1be59a66081f65f973a28033e63ecec409c103e8
James S. Wang <JJamesWWang@gmail.com>
2022-01-11 11:09:45 -0500

    Course Sections: Add slice typescript equivalent

---
+++ static/js/redux/state/slices/courseSectionsSlice.ts
@@ -0,0 +1,36 @@
+/*
+Copyright (C) 2017 Semester.ly Technologies, LLC
+
+Semester.ly is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+Semester.ly is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+*/
+
+import { createSlice } from "@reduxjs/toolkit";
+
+interface CourseSectionsSliceState {
+  objects: any;
+}
+
+const initialState: CourseSectionsSliceState = {
+  objects: {},
+};
+
+const courseSectionsSlice = createSlice({
+  name: "courseSections",
+  initialState: initialState,
+  reducers: {
+    receiveCourseSections: (state, action) => {
+      state.objects = action.payload;
+    },
+  },
+});
+
+export const courseSectionsActions = courseSectionsSlice.actions;
+export default courseSectionsSlice.reducer;
