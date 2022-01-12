711d6d9c0806682ba8718507d1a22cc09f165f64
James S. Wang <JJamesWWang@gmail.com>
2022-01-12 16:20:56 +0000

    Isomorphic fetch: Add eslint ignore

--- __mocks__/isomorphic-fetch.js
+++ __mocks__/isomorphic-fetch.js
@@ -1,5 +1,6 @@
 // Monkey patch fetch function to use absolute urls during testing (required by nock)
 
+// eslint-disable-next-line no-undef
 const fetch = jest.requireActual("isomorphic-fetch");
 
 module.exports = (url, config) => fetch(`https://localhost${url}`, config);
