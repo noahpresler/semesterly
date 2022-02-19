888d37ace4408e38cfc8eb1745ebd7c7bca93bc8
alexcjwei <alexcj.wei@gmail.com>
2022-02-18 22:09:19 -0500

    tests: update reducer test to use semesterActions

--- static/js/redux/__tests__/semester_reducer.test.jsx
+++ static/js/redux/__tests__/semester_reducer.test.jsx
@@ -1,10 +1,9 @@
-import semester from "../state/slices/semesterSlice";
+import semester, { semesterActions } from "../state/slices/semesterSlice";
 
 describe("Semester reducer", () => {
   it("updates semester correctly", () => {
     const before = { current: 0, all: [] };
     const after = { current: 1, all: [] };
-    const action = { type: "global/updateSemester", payload: 1 };
-    expect(semester(before, action)).toEqual(after);
+    expect(semester(before, semesterActions.updateSemester(1))).toEqual(after);
   });
 });
