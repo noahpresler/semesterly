b59ef6c300b7520eba673afead240dc5d9f2eda2
James S. Wang <JJamesWWang@gmail.com>
2022-03-04 20:30:46 +0000

    News update: Register to admin site

--- notifications/admin.py
+++ notifications/admin.py
@@ -1,1 +1,7 @@
 from django.contrib import admin
+from .models import NewsUpdate
+
+
+@admin.register(NewsUpdate)
+class NewsUpdateAdmin(admin.ModelAdmin):
+    list_display = ("title", "body", "date")
