95f9754554d2a02b66578da798bfc4c0754164b4
James S. Wang <JJamesWWang@gmail.com>
2022-03-04 20:29:31 +0000

    News update: Create initial model and migration

--- notifications/models.py
+++ notifications/models.py
@@ -1,1 +1,9 @@
 from django.db import models
+
+
+class NewsUpdate(models.Model):
+    """Represents a news update that is displayed on the home page."""
+
+    title = models.CharField(max_length=255)
+    body = models.TextField()
+    date = models.DateTimeField(auto_now_add=True)
