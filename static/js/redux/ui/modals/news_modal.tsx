7f0bcc7bab41e658860504c36ab4a86d91c6245d
Brian Rui <Brian.Rui01@gmail.com>
2022-03-09 04:56:55 +0000

    News modal: fetch news from API and only display when appropriate

--- static/js/redux/ui/modals/news_modal.tsx
+++ static/js/redux/ui/modals/news_modal.tsx
@@ -2,36 +2,58 @@ import React, { useState, useEffect, useRef } from "react";
 
 // @ts-ignore
 import { WaveModal } from "boron-15";
-import { useAppDispatch, useAppSelector } from "../../hooks";
+import { getNewsEndpoint } from "../../constants/endpoints";
+import parse from "html-react-parser";
 
 const NewsModal = () => {
   const modal = useRef<WaveModal>();
 
-  const [isModalVisible] = useState(false);
+  const [isModalVisible, setIsModalVisible] = useState(false);
   const [newsTitle, setNewsTitle] = useState("");
   const [newsBody, setNewsBody] = useState("");
-  
-  useEffect(() => {
-    // TODO: check to see if there are new announcements
 
-    // TODO: check to see if the user has already seen the news
+  // Get the time that the user last viewed a news post
+  const lastViewedTime = new Date(localStorage.getItem("lastViewedNewsDate"));
+
+  useEffect(() => {
+    const fetchData = async () => {
+      const response = await fetch(getNewsEndpoint());
+      const data = await response.json();
+
+      // Only display modal if the news was posted after the last viewed time
+      if (data.date && new Date(data.date) > lastViewedTime) {
+        setIsModalVisible(true);
+        // Set to current date and time
+        localStorage.setItem("lastViewedNewsDate", new Date(Date.now()).toISOString());
+      }
+
+      setNewsTitle(data.title);
+      setNewsBody(data.body);
+    };
+
+    fetchData();
 
     if (isModalVisible) {
       modal.current.show();
     }
   }, [isModalVisible]);
 
+  const modalStyle = {
+    width: "100%",
+  };
+
   const modalHeader = (
     <div className="modal-content">
       <div className="modal-header">
-        <h1>News</h1>
+        <h1>{newsTitle}</h1>
       </div>
     </div>
   );
 
-  return(
-    <WaveModal ref={modal}>
+  return (
+    <WaveModal ref={modal} className="signup-modal max-modal" modalStyle={modalStyle}>
       {modalHeader}
+      {parse(newsBody)}
     </WaveModal>
   );
 };
