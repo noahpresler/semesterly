32c818c17232a2aacf83d26bb66786f3b5e0a752
Sabrina Li <project.xibrin@gmail.com>
2022-04-08 12:50:56 -0400

    Import change: changed import for timetable name input to tsx file

--- static/js/redux/ui/SideBar.tsx
+++ static/js/redux/ui/SideBar.tsx
@@ -17,7 +17,7 @@ import classNames from "classnames";
 // @ts-ignore no available type
 import ClickOutHandler from "react-onclickout";
 import MasterSlot from "./MasterSlot";
-import TimetableNameInput from "./timetable_name_input";
+import TimetableNameInput from "./TimetableNameInput";
 import CreditTicker from "./credit_ticker";
 import { alertsActions } from "../state/slices";
 import { getNextAvailableColour } from "../util";
