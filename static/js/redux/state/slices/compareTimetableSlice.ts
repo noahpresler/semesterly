498da878b544af19a2fe9c6aa3ce452f0beea393
James S. Wang <JJamesWWang@gmail.com>
2022-04-12 17:20:29 +0000

    Compare Timetable Slice: Add state for active and compared timetable

--- static/js/redux/state/slices/compareTimetableSlice.ts
+++ static/js/redux/state/slices/compareTimetableSlice.ts
@@ -1,19 +1,32 @@
-import { createSlice } from "@reduxjs/toolkit";
+import { createSlice, PayloadAction } from "@reduxjs/toolkit";
+import { Timetable } from "../../constants/commonTypes";
 
 interface CompareTimetableSliceState {
   showCompareTimetableSideBar: boolean;
+  activeTimetable: Timetable | null;
+  comparedTimetable: Timetable | null;
 }
 
 const initialState: CompareTimetableSliceState = {
   showCompareTimetableSideBar: false,
+  activeTimetable: null,
+  comparedTimetable: null,
 };
 
 const compareTimetableSlice = createSlice({
   name: "compareTimetable",
   initialState,
   reducers: {
-    toggleCompareTimetableSideBar: (state) => {
+    toggleCompareTimetableSideBar: (
+      state,
+      action: PayloadAction<{
+        activeTimetable: Timetable;
+        comparedTimetable: Timetable;
+      }>
+    ) => {
       state.showCompareTimetableSideBar = !state.showCompareTimetableSideBar;
+      state.activeTimetable = action.payload.activeTimetable;
+      state.comparedTimetable = action.payload.comparedTimetable;
     },
   },
 });
