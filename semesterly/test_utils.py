faef53ff248de3fa4a1b8872d74356b394e7ca12
James S. Wang <JJamesWWang@gmail.com>
2022-04-18 17:46:52 +0000

    Selenium test: Fix switch_to syntax change

--- semesterly/test_utils.py
+++ semesterly/test_utils.py
@@ -400,11 +400,11 @@ class SeleniumTestCase(StaticLiveServerTestCase):
         if not str(url).startswith("http"):
             url = "%s%s" % ("http://", url)
         self.driver.execute_script("window.open()")
-        self.driver.switch_to_window(self.driver.window_handles[1])
+        self.driver.switch_to.window(self.driver.window_handles[1])
         self.driver.get(url)
         validate()
         self.driver.close()
-        self.driver.switch_to_window(self.driver.window_handles[0])
+        self.driver.switch_to.window(self.driver.window_handles[0])
 
     def follow_share_link_from_modal(self):
         modal = self.find((By.CLASS_NAME, "course-modal"))
