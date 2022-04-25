c0d690e28f2cc8c7caba3fbcc0d3a0d4cf00dee2
Sabrina Li <project.xibrin@gmail.com>
2022-04-25 12:42:06 -0400

    Courses serializer testing: added test suite for course serializer, two possible bugs

--- courses/tests.py
+++ courses/tests.py
@@ -10,33 +10,39 @@
 # MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 # GNU General Public License for more details.
 
+from os import set_inheritable
 from django.test import TestCase
 from rest_framework import status
 from rest_framework.test import APITestCase
 
-from timetable.models import Semester, Course, Section, Offering
+from timetable.models import Semester, Course, Section, Offering, Evaluation, Integration, CourseIntegration, Timetable
 from parsing.models import DataUpdate
 from helpers.test.test_cases import UrlTestCase
-from .serializers import CourseSerializer
+from .serializers import CourseSerializer, SectionSerializer
+from student.models import Student, Reaction
+from django.contrib.auth.models import User
+from student.models import PersonalTimetable
 
 
 class Serializers(TestCase):
-    def test_course_serialization(self):
+
+    def setUp(self):
         self.sem_name = "Winter"
         self.year = "1995"
         self.cid = 1
         self.name = "Intro"
         self.code = "SEM101"
         self.school = "uoft"
-        sem = Semester.objects.create(name=self.sem_name, year=self.year)
-        course = Course.objects.create(
+        self.sem = Semester.objects.create(name=self.sem_name, year=self.year)
+        self.course = Course.objects.create(
             id=self.cid, school=self.school, code=self.code, name=self.name
         )
-        section = Section.objects.create(
-            course=course, semester=sem, meeting_section="L1"
+        
+        self.section = Section.objects.create(
+            course=self.course, semester=self.sem, meeting_section="L1"
         )
         Offering.objects.create(
-            section=section,
+            section=self.section,
             day="M",
             date_start="08-29-1995",
             date_end="12-10-1995",
@@ -45,17 +51,307 @@ class Serializers(TestCase):
             is_short_course=False,
         )
 
+    def test_course_serialization_basic_attributes(self):
         serialized = CourseSerializer(
-            course,
+            self.course,
             context={
-                "semester": sem,
+                "semester": self.sem,
                 "school": self.school,
-                "sections": [section],
             },
         )
 
         self.assertTrue(serialized.data["code"] == self.code)
+        self.assertTrue(serialized.data["name"] == self.name)
+        self.assertTrue(serialized.data["id"] == self.cid)
 
+    def valid_evals(self, evals):
+        for course_eval in evals:
+            if course_eval["year"] == self.year:
+                if course_eval["unique_term_year"]:
+                    return False
+            else:
+                if not course_eval["unique_term_year"]:
+                    return False
+        return True
+
+    def test_course_serialization_get_evals(self):
+        Evaluation.objects.create(course=self.course, score=5.0, summary="This class was great", professor="prof",course_code=self.code,year=self.year)
+        Evaluation.objects.create(course=self.course, score=1.0, summary="This class was terrible", professor="prof",course_code=self.code,year=self.year)
+        Evaluation.objects.create(course=self.course, score=3.0, summary="This class was meh", professor="prof",course_code=self.code,year=str(int(self.year)+1))
+        Evaluation.objects.create(course=self.course, score=3.0, summary="Loved it", professor="prof",course_code=self.code,year=str(int(self.year)+2))
+        serialized = CourseSerializer(
+            self.course,
+            context={
+                "semester": self.sem,
+                "school": self.school,
+            },
+        )
+        self.assertTrue(self.valid_evals(serialized.data["evals"]))
+
+    def test_course_serialization_get_integrations(self):
+        testSem = Semester.objects.create(name="Spring", year="2022")
+        integration = Integration.objects.create(name="testIntegration")
+        courseInt = CourseIntegration.objects.create(course=self.course, integration=integration, json="randomJson")
+        courseInt.semester.add(testSem)
+        courseInt.semester.add(self.sem)
+        serialized = CourseSerializer(
+            self.course,
+            context={
+                "semester": self.sem,
+                "school": self.school,
+            },
+        )
+        integration_data = serialized.data["integrations"]
+        self.assertTrue(integration_data[0] == "testIntegration")
+
+    def test_course_serialization_get_related_courses(self):
+        testSem = Semester.objects.create(name="Spring", year="2022")
+        relatedCourseOne = Course.objects.create(
+            id=2, school=self.school, code="SEM102", name="STAD1"
+        )
+        courseOneSec = Section.objects.create(
+            course=relatedCourseOne, meeting_section="01", size=10, enrolment=3, waitlist=0, waitlist_size=10, instructors="instructor", semester=self.sem, was_full=True, course_section_id=1 
+        )
+        relatedCourseTwo = Course.objects.create(
+            id=3, school=self.school, code="SEM103", name="STAD2"
+        )
+        courseTwoSec = Section.objects.create(
+            course=relatedCourseTwo, meeting_section="01", size=10, enrolment=3, waitlist=0, waitlist_size=10, instructors="instructor", semester=self.sem, was_full=True, course_section_id=1 
+        )
+        relatedCourseThree = Course.objects.create(
+            id=4, school=self.school, code="SEM104", name="STAD3"
+        )
+        courseThreeSec = Section.objects.create(
+            course=relatedCourseThree, meeting_section="01", size=10, enrolment=3, waitlist=0, waitlist_size=10, instructors="instructor", semester=self.sem, was_full=True, course_section_id=1 
+        )
+        relatedCourseFour = Course.objects.create(
+            id=5, school=self.school, code="SEM105", name="STAD4"
+        )
+        courseFourSec = Section.objects.create(
+            course=relatedCourseFour, meeting_section="01", size=10, enrolment=3, waitlist=0, waitlist_size=10, instructors="instructor", semester=self.sem, was_full=True, course_section_id=1 
+        )
+        relatedCourseFive = Course.objects.create(
+            id=6, school=self.school, code="SEM106", name="STAD5"
+        )
+        courseFiveSec = Section.objects.create(
+            course=relatedCourseFive, meeting_section="01", size=10, enrolment=3, waitlist=0, waitlist_size=10, instructors="instructor", semester=self.sem, was_full=True, course_section_id=1 
+        )
+        
+        self.course.related_courses.add(relatedCourseOne)
+        self.course.related_courses.add(relatedCourseTwo)
+        self.course.related_courses.add(relatedCourseThree)
+        self.course.related_courses.add(relatedCourseFour)
+        self.course.related_courses.add(relatedCourseFive)
+
+        serialized = CourseSerializer(
+            self.course,
+            context={
+                "semester": self.sem,
+                "school": self.school,
+            },
+        )
+        data = serialized.data["related_courses"]
+        self.assertTrue(data[0]["code"] == "SEM102")
+        self.assertTrue(data[1]["code"] == "SEM103")
+        self.assertTrue(data[2]["code"] == "SEM104")
+        self.assertTrue(data[3]["code"] == "SEM105")
+        self.assertTrue(data[4]["code"] == "SEM106")
+        
+    def test_get_reactions_no_student(self):
+        serialized = CourseSerializer(
+            self.course,
+            context={
+                "semester": self.sem,
+                "school": self.school,
+            },
+        )
+        data = serialized.data["reactions"]
+        self.assertTrue(not data)
+
+    def test_get_reactions_with_student(self):
+        testSem = Semester.objects.create(name="Spring", year="2022")
+       
+        user1 = User.objects.create_user(username='user', email='student@jhu.edu', password='password')
+        student = Student.objects.create(
+            preferred_name="nickname", class_year=2022, user=user1, img_url="no_url", major="English"
+        )
+        react = Reaction.objects.create(
+            student=student, title="FIRE"
+        )
+        react.course.add(self.course)
+        serialized = CourseSerializer(
+            self.course,
+            context={
+                "semester": self.sem,
+                "school": self.school,
+                "student": student
+            },
+        )
+        data = serialized.data["reactions"]
+        self.assertTrue(data)
+
+    def test_get_reactions_with_multiple_reviews_and_multiple_students(self):
+        testSem = Semester.objects.create(name="Spring", year="2022")
+        courseOne = Course.objects.create(
+            id=2, school=self.school, code="SEM102", name="STAD1"
+        )
+        
+        user1 = User.objects.create_user(username='user', email='student@jhu.edu', password='password')
+        user2 = User.objects.create_user(username='person', email='staff@jhu.edu', password='password')
+        student1 = Student.objects.create(
+            preferred_name="nickname", class_year=2022, user=user1, img_url="no_url", major="English"
+        )
+        student2 = Student.objects.create(
+            preferred_name="nicknames", class_year=2022, user=user2, img_url="no_url", major="undecided"
+        )
+        reaction1 = Reaction.objects.create(
+            student=student1, title="FIRE"
+        )
+        reaction2 = Reaction.objects.create(
+            student=student2, title="CRAP"
+        )
+        reaction1.course.add(self.course)
+        reaction2.course.add(self.course)
+        serialized = CourseSerializer(
+            self.course,
+            context={
+                "semester": self.sem,
+                "school": self.school,
+                "student": student1
+            },
+        )
+        data = serialized.data["reactions"]
+        self.assertTrue(data[0]["title"] == "CRAP")
+        self.assertTrue(data[1]["title"] == "FIRE")
+
+    def test_get_popularity_percent_with_sections(self):
+        # testSem = Semester.objects.create(name="Spring", year="2022")
+        # relatedCourseOne = Course.objects.create(
+        #     id=2, school=self.school, code="SEM102", name="STAD1"
+        # )
+        # courseOneSec1 = Section.objects.create(
+        #     course=relatedCourseOne, meeting_section="01", size=10, enrolment=3, waitlist=0, waitlist_size=10, instructors="instructor", semester=self.sem, was_full=False, course_section_id=1 
+        # )
+        # courseOneSec2 = Section.objects.create(
+        #     course=relatedCourseOne, meeting_section="02", size=10, enrolment=3, waitlist=0, waitlist_size=10, instructors="instructor", semester=self.sem, was_full=False, course_section_id=1 
+        # )
+        # user1 = User.objects.create_user(username='user', email='student@jhu.edu', password='password')
+        # user2 = User.objects.create_user(username='person', email='staff@jhu.edu', password='password')
+        # student1 = Student.objects.create(
+        #     preferred_name="nickname", class_year=2022, user=user1, img_url="no_url", major="English"
+        # )
+        # student2 = Student.objects.create(
+        #     preferred_name="nicknames", class_year=2022, user=user2, img_url="no_url", major="undecided"
+        # )
+        # personalTimetable1 = PersonalTimetable.objects.create(name="timetable1", student=student1, semester=testSem, school=self.school)
+        # personalTimetable1.courses.add(relatedCourseOne)
+        # personalTimetable1.sections.add(courseOneSec1)
+
+        # personalTimetable2 = PersonalTimetable.objects.create(name="timetable2", student=student2, semester=testSem, school=self.school)
+        # personalTimetable2.courses.add(relatedCourseOne)
+        # personalTimetable2.sections.add(courseOneSec1)
+        serialized = CourseSerializer(
+            self.course,
+            context={
+                "semester": self.sem,
+                "school": self.school,
+            },
+        )
+        data = serialized.data["popularity_percent"]
+        self.assertTrue(data == -0.0) # TODO: why is it negative?
+        
+    def test_get_popularity_percent_without_sections(self):
+        testSem = Semester.objects.create(name="Spring", year="2022")
+        courseOne = Course.objects.create(
+            id=2, school=self.school, code="SEM102", name="STAD1"
+        )
+        serialized = CourseSerializer(
+            courseOne,
+            context={
+                "semester": testSem,
+                "school": self.school,
+            },
+        )
+        data = serialized.data["popularity_percent"]
+        self.assertTrue(data == 0.0)
+
+    def test_get_is_waitlist_only_one_sec_full(self):
+        testSem = Semester.objects.create(name="Spring", year="2022")
+        courseOne = Course.objects.create(
+            id=2, school=self.school, code="SEM102", name="STAD1"
+        )
+        courseOneSec1 = Section.objects.create(
+            course=courseOne, meeting_section="01", size=10, enrolment=10, waitlist=0, waitlist_size=10, instructors="instructor", semester=self.sem, was_full=False, course_section_id=1 
+        )
+        courseOneSec2 = Section.objects.create(
+            course=courseOne, meeting_section="02", size=10, enrolment=0, waitlist=0, waitlist_size=10, instructors="instructor", semester=self.sem, was_full=False, course_section_id=1 
+        )
+        serialized = CourseSerializer(
+            courseOne,
+            context={
+                "semester": testSem,
+                "school": self.school,
+            },
+        )
+        data = serialized.data["is_waitlist_only"]
+        self.assertFalse(data)
+
+    def test_get_is_waitlist_both_sec_full(self):
+        testSem = Semester.objects.create(name="Spring", year="2022")
+        courseOne = Course.objects.create(
+            id=2, school=self.school, code="SEM102", name="STAD1"
+        )
+        courseOneSec1 = Section.objects.create(
+            course=courseOne, meeting_section="01", size=10, enrolment=10, waitlist=1, waitlist_size=10, instructors="instructor", semester=self.sem, was_full=False, course_section_id=1 
+        )
+        courseOneSec2 = Section.objects.create(
+            course=courseOne, meeting_section="02", size=10, enrolment=10, waitlist=1, waitlist_size=10, instructors="instructor", semester=self.sem, was_full=False, course_section_id=2 
+        )
+
+        print("sec one full: " + str(courseOneSec1.is_full()))
+        print("sec two full: " + str(courseOneSec2.is_full()))
+        serialized = CourseSerializer(
+            courseOne,
+            context={
+                "semester": testSem,
+                "school": self.school,
+            },
+        )
+        data = serialized.data["is_waitlist_only"]
+        self.assertTrue(data)
+
+    def test_get_is_waitlist_no_sec(self):
+        testSem = Semester.objects.create(name="Spring", year="2022")
+        courseOne = Course.objects.create(
+            id=2, school=self.school, code="SEM102", name="STAD1"
+        )
+        serialized = CourseSerializer(
+            courseOne,
+            context={
+                "semester": testSem,
+                "school": self.school,
+            },
+        )
+        data = serialized.data["is_waitlist_only"]
+        self.assertTrue(data)
+        # TODO: what is expected behavior here?
+
+    def test_get_sections(self):
+        serialized_course = CourseSerializer(
+            self.course,
+            context={
+                "semester": self.sem,
+                "school": self.school,
+            },
+        )
+
+        serialized_sec = SectionSerializer(
+            self.section
+        )
+
+        self.assertEqual(serialized_course.data["sections"][0], serialized_sec.data)
+
+        
 
 class CourseDetail(APITestCase):
     school = "uoft"
