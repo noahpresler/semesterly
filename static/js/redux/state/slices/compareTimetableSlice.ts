67f5fb2d8140be8a6445b65025b5843132b508cc
Jiaqi (Jacky) Wang <jacky200218@gmail.com>
2022-05-29 17:24:29 -0700

    compare-timetables: generate gradient in slice

--- static/js/redux/state/slices/compareTimetableSlice.ts
+++ static/js/redux/state/slices/compareTimetableSlice.ts
@@ -1,16 +1,27 @@
+import { SlotColorData } from "./../../constants/commonTypes";
+import { RootState } from "..";
 import { createSlice, PayloadAction } from "@reduxjs/toolkit";
 import { Timetable } from "../../constants/commonTypes";
+import { buildGradient } from "../../util";
 
 interface CompareTimetableSliceState {
   isComparing: boolean;
   activeTimetable: Timetable | null;
   comparedTimetable: Timetable | null;
+  gradient: {
+    active: SlotColorData[];
+    compared: SlotColorData[];
+  };
 }
 
 const initialState: CompareTimetableSliceState = {
   isComparing: false,
   activeTimetable: null,
   comparedTimetable: null,
+  gradient: {
+    active: [],
+    compared: [],
+  },
 };
 
 const compareTimetableSlice = createSlice({
@@ -27,15 +38,24 @@ const compareTimetableSlice = createSlice({
       state.isComparing = true;
       state.activeTimetable = action.payload.activeTimetable;
       state.comparedTimetable = action.payload.comparedTimetable;
+      const activeGradient = buildGradient(
+        "#33bfff",
+        "#2c387e",
+        action.payload.activeTimetable.slots.length
+      );
+      const comparedGradient = buildGradient(
+        "#33bfff",
+        "#2c387e",
+        action.payload.comparedTimetable.slots.length
+      );
+      state.gradient.active = activeGradient;
+      state.gradient.compared = comparedGradient;
     },
-    stopComparingTimetables: (state) => {
-      state.isComparing = false;
-      state.activeTimetable = null;
-      state.comparedTimetable = null;
-    },
+    stopComparingTimetables: (state) => initialState,
   },
 });
 
+export const selectGradient = (state: RootState) => state.compareTimetable.gradient;
 export const { startComparingTimetables, stopComparingTimetables } =
   compareTimetableSlice.actions;
 export default compareTimetableSlice.reducer;
