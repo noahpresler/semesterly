97feebd1f2eae1b6810786e8b2d69777e1c0e705
Jiaqi (Jacky) Wang <jacky200218@gmail.com>
2022-11-07 23:37:52 -0500

    darkmode: darkmode x2 color

--- static/js/redux/constants/colors.ts
+++ static/js/redux/constants/colors.ts
@@ -75,12 +75,12 @@ export const darkSlotColor: SlotColorData[] = [
   { background: "#4c7fd8", highlight: "#6598f1", border: "#375994", font: "#222" },
 
   // x2
-  { background: "#FD7473", highlight: "#e36867", border: "#b15150", font: "#222" },
-  { background: "#5CCCF2", highlight: "#52b7d9", border: "#408ea9", font: "#222" },
-  { background: "#36DEBB", highlight: "#30c7a8", border: "#259b82", font: "#222" },
-  { background: "#FFD462", highlight: "#e5be58", border: "#b29444", font: "#222" },
-  { background: "#C585DE", highlight: "#b177c7", border: "#895d9b", font: "#222" },
-  { background: "#53e997", highlight: "#4ad187", border: "#3aa369", font: "#222" },
+  { background: "#F16E66", highlight: "#d8635b", border: "#f48b85", font: "#222" },
+  { background: "#5598DF", highlight: "#4c88c8", border: "#77ade5", font: "#222" },
+  { background: "#57C3B9", highlight: "#4eafa6", border: "#79cfc7", font: "#222" },
+  { background: "#F2B25C", highlight: "#d9a052", border: "#f5c17d", font: "#222" },
+  { background: "#7C72DC", highlight: "#6f66c6", border: "#968ee3", font: "#222" },
+  { background: "#59BEA0", highlight: "#50ab90", border: "#7acbb3", font: "#222" },
   { background: "#D4DBC8", highlight: "#B5BFA3", border: "#97A086", font: "#222" },
   { background: "#E7F76D", highlight: "#C9E20A", border: "#AFC11F", font: "#222" },
   { background: "#A3F5F2", highlight: "#7CD2CF", border: "#53ABA8", font: "#222" },
