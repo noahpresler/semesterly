ea491b32837178151576e97e2892f38602fe2ec3
James S. Wang <jjameswwang@gmail.com>
2023-02-03 12:53:13 -0500

    Migrations: Add migration for removing facebook alert and notifications, fix test, set timezone to America/New_York

--- analytics/tests.py
+++ analytics/tests.py
@@ -23,10 +23,4 @@ class UrlsTest(UrlTestCase):
         self.assertUrlResolvesToView(
             "/robots.txt", "analytics.views.view_analytics_dashboard"
         )
-        self.assertUrlResolvesToView(
-            "/user/log_fb_alert_click/", "analytics.views.log_facebook_alert_click"
-        )
-        self.assertUrlResolvesToView(
-            "/user/log_fb_alert_view/", "analytics.views.log_facebook_alert_view"
-        )
         self.assertUrlResolvesToView("/user/log_ical/", "student.views.log_ical_export")
