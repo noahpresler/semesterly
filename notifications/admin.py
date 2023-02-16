3b707a50b347e6d8b75da5b68ef03316bc7a1abe
James S. Wang <jjameswwang@gmail.com>
2023-02-16 17:22:05 -0500

    Notifications: Add some minor docs on what the admin page is for

--- notifications/admin.py
+++ notifications/admin.py
@@ -4,4 +4,6 @@ from .models import NewsUpdate
 
 @admin.register(NewsUpdate)
 class NewsUpdateAdmin(admin.ModelAdmin):
+    """Enables admins to create, view, and edit news updates at /admin"""
+
     list_display = ("title", "body", "date")
