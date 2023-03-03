02319d5f6694176469b22500b410ed60f8ef2808
James S. Wang <jjameswwang@gmail.com>
2023-03-03 16:43:08 -0500

    Format script: Add status

--- format.sh
+++ format.sh
@@ -1,3 +1,7 @@
+echo "Running black..."
 black .
+echo "Running Prettier..."
 npx prettier "**/*.{js,jsx,ts,tsx}" --write
+echo "Running ESLint..."
 npx eslint . --ext .js,.jsx,.ts,.tsx --fix
+echo "Done!"
