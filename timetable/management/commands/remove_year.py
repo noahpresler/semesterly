f08b8db999583c1c1d46e74d2b1645f7fa872d74
Jiaqi (Jacky) Wang <jacky200218@gmail.com>
2023-03-30 17:51:45 -0400

    remove year: run lint

--- timetable/management/commands/remove_year.py
+++ timetable/management/commands/remove_year.py
@@ -9,17 +9,17 @@ from django.core.management.base import BaseCommand
 from django.db.models import Q
 from timetable.models import Semester
 
+
 class Command(BaseCommand):
-    help = 'Delete semester objects for a given year, which deletes all courses, sections, and timetables associated with them.'
+    help = "Delete semester objects for a given year, which deletes all courses, sections, and timetables associated with them."
 
     def add_arguments(self, parser):
-        parser.add_argument('year', type=str)
+        parser.add_argument("year", type=str)
 
     def handle(self, *args, **options):
-        year = options['year']
-        print('Deleting courses, sections, and timetables for year: ', year)
+        year = options["year"]
+        print("Deleting courses, sections, and timetables for year: ", year)
         self.delete_year(year)
-      
+
     def delete_year(self, year):
         Semester.objects.filter(Q(year=year)).delete()
-
