239f1f16b57fdddf7f47af83d0dca816ec8087dc
spencerckhuang <shuan126@jh.edu>
2023-09-24 12:10:26 -0400

    drag search: trying to fix lint by removing spaces inbetween self.descriptions

--- semesterly/tests.py
+++ semesterly/tests.py
@@ -155,18 +155,15 @@ class EndToEndTest(SeleniumTestCase):
             self.search_course("AS.110.795", 1)
             self.add_course(0, n_slots=12, n_master_slots=4)
             self.assert_ptt_const_across_refresh()
-
         with self.description("Change personal timetable name"):
             self.change_ptt_name("Testing Timetable")
             self.assert_ptt_const_across_refresh()
-
         with self.description("Remove courses"):
             self.remove_course(3)
             self.remove_course(2)
             self.remove_course(1)
             self.remove_course(0, from_slot=True)
             self.assert_ptt_const_across_refresh()
-
         with self.description("Add and remove from course modal"):
             self.search_course("AS.110.105", 1)
             self.open_course_modal_from_search(0)
@@ -179,12 +176,10 @@ class EndToEndTest(SeleniumTestCase):
             self.add_course_from_course_modal(n_slots=11, n_master_slots=3)
             self.open_course_modal_from_slot(2)
             self.remove_course_from_course_modal(n_slots_expected=8)
-
         with self.description("Share timetable"):
             self.share_timetable([course1, course2])
             self.assert_ptt_const_across_refresh()
         testing_ptt = self.ptt_to_tuple()
-
         with self.description("Create new personal timetable, validate on reload"):
             self.create_ptt("End To End Testing!")
             self.search_course("AS.110.105", 1)
@@ -193,16 +188,13 @@ class EndToEndTest(SeleniumTestCase):
             self.add_course(0, n_slots=7, n_master_slots=2)
             e2e_ptt = self.ptt_to_tuple()
             self.assert_ptt_const_across_refresh()
-
         with self.description("Switch to original ptt and validate"):
             self.switch_to_ptt("Testing Timetable")
             self.assert_ptt_equals(testing_ptt)
-
         with self.description("Compare timetables"):
             self.compare_timetable("End To End Testing!")
             self.assert_slot_presence(11, 3)
             self.exit_compare_timetable()
-
         with self.description("switch semester, create personal timetable, switch back"):
             self.change_term("Spring 2023")
             self.create_ptt("Hope ders no bugs!", finish_saving=False)
@@ -211,11 +203,9 @@ class EndToEndTest(SeleniumTestCase):
             self.add_course(0, n_slots=4, n_master_slots=1)
             self.change_to_current_term()
             self.assert_ptt_equals(e2e_ptt)
-
         with self.description("Delete a timetable"):
             self.delete_timetable("End To End Testing!")
             self.assert_timetable_not_found("End To End Testing!")
-
         with self.description("Add and edit custom events"):
             self.create_custom_event(4, 16, 20, False)
             self.assert_custom_event_exists(
