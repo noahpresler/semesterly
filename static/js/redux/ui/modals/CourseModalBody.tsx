f8616ed420b33e82622fa1bfbc64982507ce8ed9
spencerckhuang <shuan126@jh.edu>
2023-12-03 14:03:29 -0500

    prereqs: changed course codes to class names and increased first column size

--- static/js/redux/ui/modals/CourseModalBody.tsx
+++ static/js/redux/ui/modals/CourseModalBody.tsx
@@ -463,7 +463,7 @@ const CourseModalBody = (props: CourseModalBodyProps) => {
   return (
     <div className="modal-body">
       <div className="cf">
-        <div className="col-3-16">
+        <div className="col-4-16">
           <div className="credits">
             <h3>{numCredits}</h3>
             <h4>{creditsSuffix}</h4>
@@ -507,7 +507,7 @@ const CourseModalBody = (props: CourseModalBodyProps) => {
             <EvaluationList evalInfo={evalInfo} />
           </div>
         </div>
-        <div id="modal-section-lists" className="col-5-16 cf">
+        <div id="modal-section-lists" className="col-4-16 cf">
           {!isComparingTimetables && sectionGrid}
           {similarCourses}
         </div>
